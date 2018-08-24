046e66130ee28e78477ac2bd903ba09ade2b8855
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-08-23 23:33:34 -0400

    Properly aligned copyright in footer

--- app/GoodToKnow/ViewsIncludes/bottom.php
+++ app/GoodToKnow/ViewsIncludes/bottom.php
@@ -8,7 +8,7 @@
 ?>
 </div><!-- End maincontent -->
 <div id="footerbar">
-    <p style="align-self: center">Copyright 2018 - Sameh Ramzy Labib</p>
+    <p align="center">Copyright 2018 - Sameh Ramzy Labib</p>
 </div>
 <script src="/js/script.js"></script>
 </body>
