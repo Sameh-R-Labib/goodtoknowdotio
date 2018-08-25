4a68b118a84a5cbb0311456469420d7252f62849
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-08-24 22:31:18 -0400

    Changed system message

--- app/GoodToKnow/ViewsIncludes/top.php
+++ app/GoodToKnow/ViewsIncludes/top.php
@@ -43,8 +43,7 @@
 <!-- scriptoutput -->
 <div id="scriptoutput">
     <div id="sendtoauthor"><p><a href="#">🖌 Text Author</a></p></div>
-    <div id="scriptmessage"><p>This is an example of what a session message would look like (especially if
-            it was long enough to take up more than one line inside this div section. Hopefully it won't look weird.</p>
+    <div id="scriptmessage"><p>😏 System Message: [none]</p>
     </div>
 </div>
 <!-- maincontent -->
