44ce1e788ac2866f410705226348935ac117f108
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-08-24 21:44:30 -0400

    Added lorem ipsum to body

--- app/GoodToKnow/Views/homepage.php
+++ app/GoodToKnow/Views/homepage.php
@@ -8,3 +8,29 @@
 ?>
 <?php require SESSIONMESSAGE; ?>
 <p>Hi there!</p>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus condimentum ipsum quis massa pretium, ut dapibus
+    tellus fermentum. Vivamus id elementum orci. Pellentesque habitant morbi tristique senectus et netus et malesuada
+    fames ac turpis egestas. Cras volutpat sagittis odio consectetur fringilla. Phasellus in nulla ipsum. Nunc mauris
+    nisi, ornare a fermentum vitae, maximus at justo. Nulla tincidunt magna id erat luctus lacinia. Aliquam nec iaculis
+    mi. Sed at dolor at libero sollicitudin congue in eu diam. Vestibulum vulputate enim in magna aliquam, sed
+    vulputate massa tincidunt. Quisque turpis magna, malesuada vitae lacus vel, tempor consectetur augue. Suspendisse
+    ut iaculis dui, eu interdum eros. Cras faucibus eros at nulla malesuada condimentum. Nullam ultricies, nisi ac
+    tincidunt dapibus, velit odio porttitor mi, eu elementum libero mauris vel risus.</p>
+
+<p>Integer efficitur efficitur diam sed sagittis. Mauris tincidunt eleifend ligula, nec ornare ipsum. In hac habitasse
+    platea dictumst. Pellentesque in augue velit. Ut non libero id libero sollicitudin sagittis a id nibh. Praesent
+    euismod sem semper, tempor libero id, vehicula sapien. Vestibulum non velit blandit, scelerisque erat semper, semper
+    ligula. Praesent libero lacus, gravida id tristique at, tempor sit amet nibh. Nam massa sapien, varius ut venenatis
+    sed, viverra vitae neque. Sed euismod velit in pharetra porta. Aliquam iaculis finibus nulla eget tincidunt. Nam
+    porta, lorem vitae placerat eleifend, justo eros blandit massa, et finibus sem erat eu ipsum. Suspendisse hendrerit
+    iaculis orci, id auctor risus eleifend id.</p>
+
+<p>Etiam id libero dolor. Phasellus vel fringilla nibh, ut egestas quam. Vivamus ac gravida velit, at faucibus lectus.
+    Nunc a nulla quis velit euismod aliquet eu id urna. Sed placerat malesuada nisl ac blandit. Duis volutpat tristique
+    est et aliquam. In non nisl venenatis, porta ligula ac, egestas velit. Pellentesque dui quam, auctor nec nisl ut,
+    dictum ullamcorper neque. Proin ultricies viverra euismod. Aliquam volutpat, justo luctus placerat ultricies, odio
+    nisi vehicula felis, aliquet varius enim lacus sed ante. Morbi molestie nibh nec nisi porttitor fringilla. Integer
+    vel justo ut metus elementum gravida sit amet id elit. Quisque cursus ac purus sed hendrerit. Nullam lectus nisi,
+    sollicitudin a laoreet vitae, bibendum vitae lorem. In et fermentum urna, at semper erat. Nam eget euismod lacus,
+    quis malesuada massa.</p>
