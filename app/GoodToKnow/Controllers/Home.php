8ad86157c313df88ed980c7dc1d52a63083af592
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-08-26 09:07:50 -0400

    Moved some code out of the controller method

--- app/GoodToKnow/Controllers/Home.php
+++ app/GoodToKnow/Controllers/Home.php
@@ -13,35 +13,12 @@ class Home
 {
     public function page()
     {
-        session_start();
-
-        /**
-         * Get data from the session
-         */
-        $sessionMessage = (isset($_SESSION['message'])) ? $_SESSION['message'] : '';
-        $user_id = (isset($_SESSION['user_id'])) ? $_SESSION['user_id'] : 0;
-        $role = (isset($_SESSION['role'])) ? $_SESSION['role'] : '';
-        // The name of community which the user wants to see.
-        $community_name = (isset($_SESSION['community_name'])) ? $_SESSION['community_name'] : '';
-        // The names of communities the user belongs to.
-        $community_name_array = (isset($_SESSION['community_name_array'])) ? $_SESSION['community_name_array'] : [];
-        $community_id = (isset($_SESSION['community_id'])) ? $_SESSION['community_id'] : 0;
-        $topic_id = (isset($_SESSION['topic_id'])) ? $_SESSION['topic_id'] : 0;
-        $page_id = (isset($_SESSION['page_id'])) ? $_SESSION['page_id'] : 0;
-
-        /**
-         * Deduce information from session data
-         */
-        $is_logged_in = (!empty($user_id)) ? true : false;
-        $is_bubba = ($role === 'bubba') ? true : false;
-
-        /**
-         * Show the page
-         */
         $html_title = 'GoodToKnow.io';
 
         require TOP;
+
         require VIEWS . DIRSEP . 'homepage.php';
+
         require BOTTOM;
     }
 }
--- web/routehandler.php
+++ web/routehandler.php
@@ -46,6 +46,37 @@ require $path4;
 //}
 
 /**
+ * Before we call the controller method
+ * let us gather some knowledge from
+ * the session and put it into variables
+ * which have friendly names.
+ */
+session_start();
+
+$sessionMessage = (isset($_SESSION['message'])) ? $_SESSION['message'] : '';
+
+$user_id = (isset($_SESSION['user_id'])) ? $_SESSION['user_id'] : 0;
+
+$role = (isset($_SESSION['role'])) ? $_SESSION['role'] : '';
+
+// The name of community which the user wants to see.
+$community_name = (isset($_SESSION['community_name'])) ? $_SESSION['community_name'] : '';
+
+// The names of communities the user belongs to.
+$community_name_array = (isset($_SESSION['community_name_array'])) ? $_SESSION['community_name_array'] : [];
+
+$community_id = (isset($_SESSION['community_id'])) ? $_SESSION['community_id'] : 0;
+
+$topic_id = (isset($_SESSION['topic_id'])) ? $_SESSION['topic_id'] : 0;
+
+$page_id = (isset($_SESSION['page_id'])) ? $_SESSION['page_id'] : 0;
+
+$is_logged_in = (!empty($user_id)) ? true : false;
+
+$is_admin = ($role === 'admin') ? true : false;
+
+
+/**
  * Section Description:
  * HERE WE DETERMINE WHICH CONTROLLER
  *                   WHICH METHOD
