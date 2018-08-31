be80dd6290a7351305af91566accb93021c0edf8
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-08-31 17:26:33 -0400

    Put in static radio button values

--- app/GoodToKnow/Views/adminpasscodegenerationform.php
+++ app/GoodToKnow/Views/adminpasscodegenerationform.php
@@ -24,49 +24,49 @@
 </head>
 <body>
 <div class="form-wrapper">
-    <h2>Jesus & Burritos</h2>
-    <p>Do you think Jesus could microwave a burrito, so hot, the he, himself, could not eat it?</p>
-    <form action="">
+    <h2>Generate Pass-Code</h2>
+    <p>A pass-code gets used to create a new user account or to add a community membership to an existing account.</p>
+    <form action="/ax1/AdminPassCodeGenFormProcessor/script">
         <label for="choice-1">
-            <input type="radio" id="choice-1" name="choice" value=""/>
+            <input type="radio" id="choice-1" name="choice" value="2"/>
             <div>
-                No. The Lord can do anything.
-                <span>Are you sure?</span>
+                GoodStack
+                <span>Documentation for technologies used by GoodToKnow.io</span>
             </div>
         </label>
 
         <label for="choice-2">
-            <input type="radio" id="choice-2" name="choice" value=""/>
+            <input type="radio" id="choice-2" name="choice" value="4"/>
             <div>
-                Jesus didn't have microwaves.
-                <span>Good point.</span>
+                C++
+                <span>UMBC CMSC related C++ notes</span>
             </div>
         </label>
 
         <label for="choice-3">
-            <input type="radio" id="choice-3" name="choice" value=""/>
+            <input type="radio" id="choice-3" name="choice" value="5"/>
             <div>
-                He had to wait at least 3 minutes.
-                <span>Prove it.</span>
+                C
+                <span>UMBC CMSC related C programming notes</span>
             </div>
         </label>
 
         <label for="choice-4">
-            <input type="radio" id="choice-4" name="choice" value=""/>
+            <input type="radio" id="choice-4" name="choice" value="6"/>
             <div>
-                Depends on what kind of drink he has.
-                <span>No it doesn't.</span>
+                GoodFamily
+                <span>Knowledge shared within the family of Sameh Ramzy Labib</span>
             </div>
         </label>
 
         <label for="choice-5">
-            <input type="radio" id="choice-5" name="choice" value=""/>
+            <input type="radio" id="choice-5" name="choice" value="7"/>
             <div>
-                Yes. Why was he so special?
-                <span>Because he was a Carpenter.</span>
+                School Bus Drivers
+                <span>Howard and Anne Arundel school bus driver community</span>
             </div>
         </label>
-        <button type="submit">Submit</button>
+        <button type="submit" name="submit" value="Submit">Submit</button>
     </form>
 </div> <!-- .form-wrapper -->
 
