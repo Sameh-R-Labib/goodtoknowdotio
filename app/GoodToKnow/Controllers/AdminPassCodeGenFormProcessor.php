ee16ae632fbc3207f9ea6f1e399542093f4d0a9f
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-09-03 17:47:23 -0400

    Improved the debug code

--- app/GoodToKnow/Controllers/AdminPassCodeGenFormProcessor.php
+++ app/GoodToKnow/Controllers/AdminPassCodeGenFormProcessor.php
@@ -34,7 +34,9 @@ class AdminPassCodeGenFormProcessor
          */
         echo "\n\n<p>Begin debug code output.</p>\n\n";
         echo "\n\n<p>Var_dump of \$_POST array: </p>\n\n";
+        echo "\n\n<pre>";
         var_dump($_POST);
+        echo "</pre>\n\n";
         die("\n\n<p>End of debug code output.</p>\n\n");
     }
 }
