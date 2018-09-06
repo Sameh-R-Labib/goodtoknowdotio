fb4eb506d56d4d659b8770a808d3ae49ef7992b0
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-09-06 18:31:19 -0400

    Added user to community class

---
+++ app/GoodToKnow/Models/UserToCommunity.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samehlabib
+ * Date: 9/6/18
+ * Time: 6:25 PM
+ */
+
+namespace GoodToKnow\Models;
+
+
+class UserToCommunity extends GoodObject
+{
+    /**
+     * @var string
+     */
+    protected static $table_name = "user_to_community";
+
+    /**
+     * @var array
+     */
+    protected static $fields = ['id', 'user_id', 'community_id'];
+
+    /**
+     * @var int
+     */
+    public $id;
+
+    /**
+     * @var int
+     */
+    public $user_id;
+
+    /**
+     * @var int
+     */
+    public $community_id;
+}
