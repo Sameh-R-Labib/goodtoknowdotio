82f4de59e7633e2ba3ed8de3c0b35c5d8a2a6599
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-09-06 17:59:38 -0400

    Added field information of User class

--- app/GoodToKnow/Models/User.php
+++ app/GoodToKnow/Models/User.php
@@ -11,5 +11,64 @@ namespace GoodToKnow\Models;
 
 class User extends GoodObject
 {
+    /**
+     * @var string
+     */
+    protected static $table_name = "users";
 
+    /**
+     * @var array
+     */
+    protected static $fields = ['id', 'username', 'password', 'id_of_default_community', 'title', 'role',
+        'race', 'is_suspended', 'date', 'comment'];
+
+    /**
+     * @var int
+     */
+    public $id;
+
+    /**
+     * @var string
+     */
+    public $username;
+
+    /**
+     * @var string
+     */
+    public $password;
+
+    /**
+     * @var int
+     */
+    public $id_of_default_community;
+
+    /**
+     * @var string
+     */
+    public $title;
+
+    /**
+     * @var string
+     */
+    public $role;
+
+    /**
+     * @var string
+     */
+    public $race;
+
+    /**
+     * @var int
+     */
+    public $is_suspended;
+
+    /**
+     * @var string
+     */
+    public $date;
+
+    /**
+     * @var string
+     */
+    public $comment;
 }
