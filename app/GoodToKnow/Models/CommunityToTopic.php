8ecde2f1687b9b59c5c7b08622f7a53d1f7343d1
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-09-15 20:55:26 -0400

    Completed get_topics_array_for_a_community

--- app/GoodToKnow/Models/CommunityToTopic.php
+++ app/GoodToKnow/Models/CommunityToTopic.php
@@ -37,13 +37,12 @@ class CommunityToTopic extends GoodObject
     public $topic_id;
 
 
-    public static function get_topics_array_for_a_community(int $community_id)
+    public static function get_topics_array_for_a_community(\mysqli $db, string &$error, $community_id)
     {
         /**
-         * Code a static method which:
          * Gets an associative array where the key is the topic_id
-         * and the value is the topic name. And this is an array
-         * for all the topics which belong to a particular community.
+         * and the value is the topic name for all the topics which
+         * belong to a particular community.
          *
          * I'm using this static method in SetHomePageCommunityTopicPost.
          * But, it will be useful in other places. That is why I'm making it
@@ -52,17 +51,33 @@ class CommunityToTopic extends GoodObject
 
         /**
          * First we need all the CommunityToTopic objects for $community_id
+         * In other words we need an array of CommunityToTopic objects for $community_id
          */
+        $sql = 'SELECT * FROM community_to_topic WHERE `community_id`=' . $community_id;
+        $community_to_topic_array = CommunityToTopic::find_by_sql($db, $sessionMessage, $sql);
+
+        if (!$community_to_topic_array) {
+            $sessionMessage .= " CommunityToTopic get_topics_array_for_a_community() this community
+             has no topics. Otherwise something else went wrong. ";
+            return false;
+        }
 
         /**
-         * Second we need to get all the Topic objects corresponding
-         * to the topic ids found in the CommunityToTopic objects we
-         * found in our first step.
+         * Build the array I'm looking for (the one for return.)
          */
+        $topics_for_this_community = [];
+        foreach ($community_to_topic_array as $value) {
+            // Talking about the right side of the assignment statement
+            // First we're getting a Community object
+            $topics_for_this_community[$value->topic_id] = Topic::find_by_id($db, $sessionMessage, $value->topic_id);
+            if (!$topics_for_this_community[$value->topic_id]) {
+                $sessionMessage .= " CommunityToTopic get_topics_array_for_a_community() says err_no 70737. ";
+                return false;
+            }
+            // Then we're getting the community_name from that object
+            $topics_for_this_community[$value->topic_id] = $topics_for_this_community[$value->topic_id]->topic_name;
+        }
 
-        /**
-         * Third we will create and return the array we desire
-         * as it is described in the introduction to this function.
-         */
+        return $topics_for_this_community;
     }
 }
