1c669af4cb676c7569ed0742569d72665b0b46cc
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-09-18 22:13:19 -0400

    Removed all debug code

--- app/GoodToKnow/Controllers/Home.php
+++ app/GoodToKnow/Controllers/Home.php
@@ -27,33 +27,6 @@ class Home
         global $saved_str02;
         global $type_of_resource_being_requested;  // result of running SetHomePageCommunityTopicPost
 
-
-        /**
-         * Debug
-         */
-        echo "\n<p>Begin debug</p>\n";
-        echo "<br><p>Var_dump \$sessionMessage: </p>\n<pre>";
-        var_dump($sessionMessage);
-        echo "</pre>\n";
-        echo "<br><p>Var_dump \$community_id: </p>\n<pre>";
-        var_dump($community_id);
-        echo "</pre>\n";
-        echo "<br><p>Var_dump \$topic_id: </p>\n<pre>";
-        var_dump($topic_id);
-        echo "</pre>\n";
-        echo "<br><p>Var_dump \$post_id: </p>\n<pre>";
-        var_dump($post_id);
-        echo "</pre>\n";
-        echo "<br><p>Var_dump \$type_of_resource_being_requested: </p>\n<pre>";
-        var_dump($type_of_resource_being_requested);
-        echo "</pre>\n";
-        echo "<br><p>Var_dump \$communities_for_this_user: </p>\n<pre>";
-        var_dump($communities_for_this_user);
-        echo "</pre>\n";
-        die("<br><p>End debug</p>\n");
-
-
-
         if (!$is_logged_in) {
             $_SESSION['message'] = $sessionMessage;
             redirect_to("/ax1/InfiniteLoopPrevent/page");
