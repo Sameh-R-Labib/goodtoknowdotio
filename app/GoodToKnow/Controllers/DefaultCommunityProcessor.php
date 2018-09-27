3bf8db09a7f53a3f9653e67cca8f94e9546dd320
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-09-27 14:25:13 -0400

    Debug 2 DefaultCommunityProcessor

--- app/GoodToKnow/Controllers/DefaultCommunityProcessor.php
+++ app/GoodToKnow/Controllers/DefaultCommunityProcessor.php
@@ -16,29 +16,41 @@ class DefaultCommunityProcessor
         global $is_logged_in;
         global $sessionMessage;
         global $is_admin;
+        global $special_community_array;
 
         if (!$is_logged_in OR $is_admin) {
             $_SESSION['message'] = $sessionMessage;
             redirect_to("/ax1/Home/page");
         }
 
+        if (empty($_POST['choice'])) {
+            $_SESSION['message'] .= " Expected submission of choice not found. ";
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * Make sure the submitted choice is valid for this user.
+         */
+        $is_found = false;
+        if (array_key_exists($_POST['choice'], $special_community_array)) $is_found = true;
+        if (!$is_found) {
+            $_SESSION['message'] .= " Choice is not valid. ";
+            redirect_to("/ax1/Home/page");
+        }
 
         /**
          * Debug Code
          */
         echo "\n<p>Begin debug</p>\n";
-        echo "<br><p>Var_dump \$_POST['choice']: </p>\n<pre>";
-        var_dump($_POST['choice']);
+        echo "<br><p>Var_dump \$is_found: </p>\n<pre>";
+        var_dump($is_found);
         echo "</pre>\n";
-        echo "<br><p>Print_r \$_POST: </p>\n<pre>";
-        print_r($_POST);
+        echo "<br><p>Print_r \$_POST['choice']: </p>\n<pre>";
+        print_r($_POST['choice']);
+        echo "</pre>\n";
+        echo "<br><p>Print_r \$special_community_array: </p>\n<pre>";
+        print_r($special_community_array);
         echo "</pre>\n";
         die("<br><p>End debug</p>\n");
-
-
-        if (empty($_POST['choice'])) {
-            $_SESSION['message'] .= " Aborted! Expected submission of choice not found. ";
-            redirect_to("/ax1/Home/page");
-        }
     }
 }
