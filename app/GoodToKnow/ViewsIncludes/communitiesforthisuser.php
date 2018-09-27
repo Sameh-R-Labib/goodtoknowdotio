222068fe7527d800433ef0deef01cee054af3aaa
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-09-27 16:14:33 -0400

    Removed symbol between communities

--- app/GoodToKnow/ViewsIncludes/communitiesforthisuser.php
+++ app/GoodToKnow/ViewsIncludes/communitiesforthisuser.php
@@ -1,3 +1,3 @@
 <?php foreach ($special_community_array as $key => $value): ?>
-    <a href="/ax1/SetHomePageCommunityTopicPost/page/<?php echo $key; ?>/0/0"><?php echo $value; ?></a> ↬
+    <a href="/ax1/SetHomePageCommunityTopicPost/page/<?php echo $key; ?>/0/0"><?php echo $value; ?></a>&nbsp;&nbsp;
 <?php endforeach; ?>
