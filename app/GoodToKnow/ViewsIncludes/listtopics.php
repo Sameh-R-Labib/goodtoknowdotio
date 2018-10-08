b8c5ee56c2b6a7bd82e2f20cc63280c4204b0401
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-10-08 17:17:33 -0400

    Debug

--- app/GoodToKnow/ViewsIncludes/listtopics.php
+++ app/GoodToKnow/ViewsIncludes/listtopics.php
@@ -1,4 +1,9 @@
-<?php foreach ($special_topic_array as $key => $value): ?>
+<?php
+
+var_dump($special_topic_array);
+die('That is special topic array');
+
+foreach ($special_topic_array as $key => $value): ?>
     <p>
         <a href="/ax1/SetHomePageCommunityTopicPost/page/<?php echo $community_id; ?>/<?php echo $key; ?>/0"><?php echo $value; ?></a>
     </p>
