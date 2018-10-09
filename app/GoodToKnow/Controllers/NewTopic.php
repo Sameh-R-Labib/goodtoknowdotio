2d178cc7ca5c2f3a1e176140fb7341e55aa963cf
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-10-09 14:16:40 -0400

    NewTopic page completed

--- app/GoodToKnow/Controllers/NewTopic.php
+++ app/GoodToKnow/Controllers/NewTopic.php
@@ -11,5 +11,40 @@ namespace GoodToKnow\Controllers;
 
 class NewTopic
 {
+    public function page()
+    {
+        /**
+         * We need to determine whether or not
+         * the community has any topics.
+         * If it has no topics then we assign
+         * the sequence number for the new topic
+         * a value of 500000 and redirect to
+         * where we ask for the name of the topic.
+         * If the community has one or more topics
+         * then we redirect to where we as for the
+         * insertion point.
+         */
 
+        global $special_topic_array;
+        global $is_logged_in;
+        global $sessionMessage;
+
+        if (!$is_logged_in) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        if (sizeof($special_topic_array) > 0) {
+            $is_empty = false;
+        } else {
+            $is_empty = true;
+        }
+
+        if ($is_empty) {
+            $_SESSION['$saved_int01'] = 500000;
+            redirect_to("/ax1/NewTopicName/page");
+        } else {
+            redirect_to("/ax1/NewTopicInsertPoint/page");
+        }
+    }
 }
