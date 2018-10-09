045c2b99fdc03c2d22179a9b39d9c334ca0449b5
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-10-09 14:25:56 -0400

    NewTopicInsertPoint

---
+++ app/GoodToKnow/Controllers/NewTopicInsertPoint.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samehlabib
+ * Date: 10/9/18
+ * Time: 2:24 PM
+ */
+
+namespace GoodToKnow\Controllers;
+
+
+class NewTopicInsertPoint
+{
+    public function page()
+    {
+
+    }
+}
