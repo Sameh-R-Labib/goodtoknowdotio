4118a340f8ba84b21f4186bd6c9627bb935279be
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-10-12 15:21:59 -0400

    newtopicname

---
+++ app/GoodToKnow/Controllers/NewTopicName.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samehlabib
+ * Date: 10/12/18
+ * Time: 3:16 PM
+ */
+
+namespace GoodToKnow\Controllers;
+
+
+class NewTopicName
+{
+    public function page()
+    {
+        /**
+         * The goal is to present a form
+         * for entering the title of
+         * the new topic.
+         */
+        global $is_logged_in;
+        global $sessionMessage;
+
+        if (!$is_logged_in) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $html_title = "What's its name?";
+
+        require VIEWS . DIRSEP . 'newtopicname.php';
+    }
+}
---
+++ app/GoodToKnow/Views/newtopicname.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samehlabib
+ * Date: 10/12/18
+ * Time: 3:21 PM
+ */
