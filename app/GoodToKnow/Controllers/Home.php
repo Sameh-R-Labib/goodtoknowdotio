e27667985dbbbb25f2164c84e27957e52844dd71
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-10-15 17:07:09 -0400

    Refreshed communities

--- app/GoodToKnow/Controllers/Home.php
+++ app/GoodToKnow/Controllers/Home.php
@@ -9,6 +9,10 @@
 namespace GoodToKnow\Controllers;
 
 
+use GoodToKnow\Models\UserToCommunity;
+use GoodToKnow\Models\Community;
+
+
 class Home
 {
     public function page()
@@ -23,6 +27,9 @@ class Home
         global $special_post_array;         // array of posts for current topic
         global $post_content;               // string containing the html for current post
         global $type_of_resource_requested; // result of running SetHomePageCommunityTopicPost
+        global $last_refresh_communities;
+        global $last_refresh_topics;
+        global $last_refresh_posts;
         global $sessionMessage;
         global $is_logged_in;
         global $is_admin;
@@ -34,6 +41,42 @@ class Home
         if (!$is_logged_in) {
             $_SESSION['message'] = $sessionMessage;
             redirect_to("/ax1/LoginForm/page");
+        }
+
+        /**
+         * If the special_community_array has not been
+         * refreshed for a period of time longer than
+         * 3 hours then refresh it.
+         */
+        $time_since_refresh = time() - $last_refresh_communities;  // seconds
+        if ($time_since_refresh > 10800) {
+            $db = db_connect($sessionMessage);
+
+            if (!empty($sessionMessage)) {
+                $_SESSION['message'] = $sessionMessage;
+                redirect_to("/ax1/Home/page");
+            }
+            $sql = 'SELECT * FROM user_to_community WHERE `user_id`=' . $user_id;
+            $user_to_community_array = UserToCommunity::find_by_sql($db, $sessionMessage, $sql);
+            if (!$user_to_community_array) {
+                $sessionMessage .= " Home page() says unexpected no user_to_community_array. ";
+                $_SESSION['message'] = $sessionMessage;
+                redirect_to("/ax1/Home/page");
+            }
+            $special_community_array = [];
+            foreach ($user_to_community_array as $value) {
+                // Talking about the right side of the assignment statement
+                // First we're getting a Community object
+                $special_community_array[$value->community_id] = Community::find_by_id($db, $sessionMessage, $value->community_id);
+                if (!$special_community_array[$value->community_id]) {
+                    $sessionMessage .= " Home page() says err_no 30848. ";
+                    $_SESSION['message'] = $sessionMessage;
+                    redirect_to("/ax1/Home/page");
+                }
+                // Then we're getting the community_name from that object
+                $special_community_array[$value->community_id] = $special_community_array[$value->community_id]->community_name;
+            }
+            $_SESSION['special_community_array'] = $special_community_array;
         }
 
         $html_title = 'GoodToKnow.io';
