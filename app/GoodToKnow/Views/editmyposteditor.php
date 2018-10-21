abf584e0124ae41cc315295546b6369b2f3786da
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-10-21 18:34:15 -0400

    EditMyPostEditor view

--- app/GoodToKnow/Views/editmyposteditor.php
+++ app/GoodToKnow/Views/editmyposteditor.php
@@ -1,1 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <title><?php /** @noinspection PhpUndefinedVariableInspection */
+        echo $html_title; ?></title>
+</head>
+<body>
+<form action="/ax1/ChangePasswordProcessor/page" method="post">
+    <h1>Edit</h1>
+    <?php require SESSIONMESSAGE; ?>
+    <section>
+        <label for="textarea">Markdown</label>
+        <textarea id="textarea" name="markdown" rows="28"
+                  cols="80"><?php /** @noinspection PhpUndefinedVariableInspection */
+            echo $markdown; ?></textarea>
+    </section>
+    <section>
+        <p>
+            <button type="submit" name="submit" value="Submit">Submit</button>
+        </p>
+    </section>
+</form>
+</body>
+</html>
