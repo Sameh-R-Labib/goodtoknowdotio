d4fc497456def72be77040239a7bcc8ddce7db61
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-10-22 16:51:51 -0400

    31 x 78

--- app/GoodToKnow/Views/editmyposteditor.php
+++ app/GoodToKnow/Views/editmyposteditor.php
@@ -21,8 +21,8 @@
     <section>
         <p>
             <label for="textarea"></label>
-            <textarea id="textarea" name="markdown" rows="38"
-                      cols="73"><?php /** @noinspection PhpUndefinedVariableInspection */
+            <textarea id="textarea" name="markdown" rows="31"
+                      cols="78"><?php /** @noinspection PhpUndefinedVariableInspection */
                 echo $markdown; ?></textarea>
         </p>
     </section>
