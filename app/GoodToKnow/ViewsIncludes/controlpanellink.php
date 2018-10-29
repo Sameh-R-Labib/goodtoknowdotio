2d5111ba155ac63e73077a06480283ce661afc32
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-10-29 18:13:18 -0400

    Got rid of grey height

--- app/GoodToKnow/ViewsIncludes/controlpanellink.php
+++ app/GoodToKnow/ViewsIncludes/controlpanellink.php
@@ -1,11 +1,11 @@
 <?php if ($is_admin): ?>
     <div class="tooltip"><a href="/ax1/AdminHome/page"><img src="/cpicon.png" alt="Admin Panel" height="86"
-                                                            width="99"></a>
+                                                            width="108"></a>
         <span class="tooltiptext tooltip-top">Admin Control Panel</span>
     </div>
 <?php else: ?>
     <div class="tooltip"><a href="/ax1/ControlPanel/page"><img src="/cpicon.png" alt="Admin Panel" height="86"
-                                                               width="99"></a>
+                                                               width="108"></a>
         <span class="tooltiptext tooltip-top">Control Panel</span>
     </div>
 <?php endif; ?>
