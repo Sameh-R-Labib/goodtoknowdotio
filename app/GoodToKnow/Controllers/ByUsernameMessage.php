d6e94082c40736a9dbe4382bf761db548b17b880
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-11-08 14:14:19 -0500

    ByUsernameMessage class

---
+++ app/GoodToKnow/Controllers/ByUsernameMessage.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samehlabib
+ * Date: 11/8/18
+ * Time: 2:00 PM
+ */
+
+namespace GoodToKnow\Controllers;
+
+
+class ByUsernameMessage
+{
+    public function page()
+    {
+        /**
+         * The goal of this series of routes
+         * is to make it possible to message
+         * a user if the only thing you know
+         * about this user is their username.
+         */
+
+        /**
+         * Present a form for entering a username.
+         */
+
+        /**
+         * Display the editor interface.
+         */
+        $html_title = 'Username Message a User';
+
+        require VIEWS . DIRSEP . 'byusernamemessage.php';
+    }
+}
---
+++ app/GoodToKnow/Views/byusernamemessage.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samehlabib
+ * Date: 11/8/18
+ * Time: 2:13 PM
+ */
