ceb4c7f725d4094b3f43ed0de83c481e51974f61
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-11-08 14:39:07 -0500

    ByUsernameMessageProcessor class

---
+++ app/GoodToKnow/Controllers/ByUsernameMessageProcessor.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samehlabib
+ * Date: 11/8/18
+ * Time: 2:38 PM
+ */
+
+namespace GoodToKnow\Controllers;
+
+
+class ByUsernameMessageProcessor
+{
+    public function page()
+    {
+
+    }
+}
