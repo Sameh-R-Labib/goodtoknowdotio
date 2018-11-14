3ffec354a7ecba51637381670e31e9b71ebdb343
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-11-14 01:35:09 -0500

    BroadcastMsg started

---
+++ app/GoodToKnow/Controllers/BroadcastMsg.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samehlabib
+ * Date: 11/14/18
+ * Time: 1:33 AM
+ */
+
+namespace GoodToKnow\Controllers;
+
+
+class BroadcastMsg
+{
+    public function page()
+    {
+
+    }
+}
--- app/GoodToKnow/ViewsIncludes/sendmessagelink.php
+++ app/GoodToKnow/ViewsIncludes/sendmessagelink.php
@@ -1,5 +1,5 @@
 <?php if ($is_admin): ?>
-    <p><a href="#">👲 user</a></p>
+    <p><a href="/ax1/BroadcastMsg/page">👲 user</a></p>
 <?php else: ?>
     <p><a href="/ax1/WriteToAdmin/page">♠👔♠ admin</a></p>
 <?php endif; ?>
