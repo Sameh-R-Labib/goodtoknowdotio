1e3835772af69b9d1dadce8c9a34668d1c0d4758
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-11-16 15:06:15 -0500

    BroadcastMsgProcessor started

---
+++ app/GoodToKnow/Controllers/BroadcastMsgProcessor.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samehlabib
+ * Date: 11/16/18
+ * Time: 3:05 PM
+ */
+
+namespace GoodToKnow\Controllers;
+
+
+class BroadcastMsgProcessor
+{
+    public function page()
+    {
+
+    }
+}
