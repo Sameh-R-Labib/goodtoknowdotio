c02f26612a8b8a848ff27e51af025332a2a0ec99
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-11-16 14:56:02 -0500

    BroadcastMsg page completed

--- app/GoodToKnow/Controllers/BroadcastMsg.php
+++ app/GoodToKnow/Controllers/BroadcastMsg.php
@@ -13,6 +13,33 @@ class BroadcastMsg
 {
     public function page()
     {
+        global $is_logged_in;
+        global $sessionMessage;
+        global $is_admin;
+        global $user_username;
 
+        if (!$is_logged_in || !$is_admin || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * Display the editor interface.
+         */
+        $html_title = 'Broadcast a Message';
+
+        $pre_populate = <<<ROI
+Dear User,
+
+[Broadcast to all]
+
+Sincerely,
+
+Admin {$user_username}
+
+
+ROI;
+
+        require VIEWS . DIRSEP . 'broadcastmsg.php';
     }
 }
