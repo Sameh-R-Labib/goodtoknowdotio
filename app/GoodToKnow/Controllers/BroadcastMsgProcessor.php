81bb612b1449d2720e7f048f822bbd80893c6e1a
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-11-16 15:22:39 -0500

    BroadcastMsgProcessor pre-boilerplate

--- app/GoodToKnow/Controllers/BroadcastMsgProcessor.php
+++ app/GoodToKnow/Controllers/BroadcastMsgProcessor.php
@@ -13,6 +13,21 @@ class BroadcastMsgProcessor
 {
     public function page()
     {
+        /**
+         * This function takes the submitted broadcastmsg.php
+         * form and saves the message and saves records in the
+         * message_to_user table where each record represents
+         * a user who will receive the message. In fact all
+         * users will receive this message.
+         */
 
+        global $is_logged_in;
+        global $sessionMessage;
+        global $is_admin;
+
+        if (!$is_logged_in || !$is_admin || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
     }
 }
