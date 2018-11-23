91ef6f28c890bf380409bdb529e8935e10b10107
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-11-23 18:03:18 -0500

    Made room for users

--- app/GoodToKnow/Views/adminhome.php
+++ app/GoodToKnow/Views/adminhome.php
@@ -19,7 +19,7 @@
 <div id="adminhometop">
     <a href="https://goodtoknow.io/ax1"><img src="/good1.jpg" alt="GoodToKnow.io" height="70" width="302"
                                              style="float: left"></a>
-    <div id="sendmessage"><p><a href="#">👲 user</a></p></div>
+    <div id="sendmessage"><?php require SENDMESSAGELINK; ?></div>
     <div id="inboxlink"><p><a href="#">🎫 inbox</a></p></div>
     <div id="logindiv"><p><a href="/ax1/Logout/page">👋 log out</a></p></div>
 </div>
