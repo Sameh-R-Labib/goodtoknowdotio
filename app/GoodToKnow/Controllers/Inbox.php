6016922109341da9de5d20369da8f8d554742c5a
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-11-30 18:31:19 -0500

    Inbox class started

---
+++ app/GoodToKnow/Controllers/Inbox.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samehlabib
+ * Date: 11/30/18
+ * Time: 6:28 PM
+ */
+
+namespace GoodToKnow\Controllers;
+
+
+class Inbox
+{
+    public function page()
+    {
+
+    }
+}
--- app/GoodToKnow/Views/home.php
+++ app/GoodToKnow/Views/home.php
@@ -20,7 +20,7 @@
     <a href="https://goodtoknow.io/ax1"><img src="/good1.jpg" alt="GoodToKnow.io" height="70" width="302"
                                              style="float: left"></a>
     <div id="sendmessage"><?php require SENDMESSAGELINK; ?></div>
-    <div id="inboxlink"><p><a href="#">🎫 inbox</a></p></div>
+    <div id="inboxlink"><p><a href="/ax1/Inbox/page">🎫 inbox</a></p></div>
     <div id="logindiv"><p><a href="/ax1/Logout/page">👋 log out</a></p></div>
 </div>
 <!-- communities -->
