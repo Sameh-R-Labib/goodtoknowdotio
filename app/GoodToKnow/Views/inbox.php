17aef1c9356676339f134610d516d9fdc9f17fe9
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-12-06 18:02:37 -0500

    Made inbox section called message a user by U/N

--- app/GoodToKnow/Views/inbox.php
+++ app/GoodToKnow/Views/inbox.php
@@ -19,7 +19,7 @@
 <div id="topbar">
     <a href="https://goodtoknow.io/ax1"><img src="/good1.jpg" alt="GoodToKnow.io" height="70" width="302"
                                              style="float: left"></a>
-    <div id="sendmessage"><?php require SENDMESSAGELINK; ?></div>
+    <div id="sendmessage"><p><a href="/ax1/ByUsernameMessage/page">📧 👲 by U/N</a></p></div>
     <div id="inboxlink"><p><a href="/ax1/Inbox/page">🎫 inbox</a></p></div>
     <div id="logindiv"><p><a href="/ax1/Logout/page">👋 log out</a></p></div>
 </div>
