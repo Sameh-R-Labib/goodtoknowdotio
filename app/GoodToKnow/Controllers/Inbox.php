a2870bb8831335dd48990b3eeb4c71be65a375a1
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-12-06 17:20:36 -0500

    Put in the main code for Inbox page

--- app/GoodToKnow/Controllers/Inbox.php
+++ app/GoodToKnow/Controllers/Inbox.php
@@ -13,6 +13,16 @@ class Inbox
 {
     public function page()
     {
+        global $sessionMessage;
+        global $is_logged_in;
 
+        if (!$is_logged_in) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/LoginForm/page");
+        }
+
+        $html_title = 'Inbox';
+
+        require VIEWS . DIRSEP . 'inbox.php';
     }
 }
