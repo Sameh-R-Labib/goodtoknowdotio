077627d464e7a104392af789b28e871299b8072a
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-12-06 00:10:47 -0500

    Added captions

--- app/GoodToKnow/Views/controlpanel.php
+++ app/GoodToKnow/Views/controlpanel.php
@@ -46,20 +46,59 @@
         <li><a href="/ax1/EditMyPost/page">Edit Post</a></li>
         <li><a href="/ax1/ByUsernameMessage/page">Username Message a User</a></li>
     </ul>
-    <a href="http://www.fsf.org/associate/support_freedom/join_fsf?referrer=2442">
-        <img class="photo" alt="Support freedom" src="//static.fsf.org/fsforg/img/normal-image.png"
-             title="Help protect your freedom, join the Free Software Foundation"/></a>
-    <img class="photo" alt="Richard M. Stallman and Julian Assange" src="/0_eT5LwH4rOihgpThm.jpeg">
-    <img class="photo" alt="Aaron Swartz" src="/Aaron-Swartz.jpg">
-    <img class="photo" alt="Ross Ulbricht" src="/Ross-Ulbricht2.jpg">
-    <img class="photo" alt="Edward Snowden" src="/edward_snowden.jpg">
-    <img class="photo" alt="Andreas Antonopoulos" src="/andreas_antonopoulos.jpg">
-    <img class="photo" alt="Richard D. Wolf" src="/richard_d_wolff.jpg">
-    <img class="photo" alt="Stacey Abrams" src="/stacey_abrams.jpg" height="360" width="641">
-    <img class="photo" alt="Abby Martin" src="/AbbyMartinVenezuela.jpg">
-    <img class="photo" alt="Bernie Sanders" src="/burnie_sanders.jpg">
-    <img class="photo" alt="Chris Hedges" src="/chris_hedges.jpg">
-    <img class="photo" alt="Film: The Panama Deception" src="/usattack.png">
+    <figure>
+        <a href="http://www.fsf.org/associate/support_freedom/join_fsf?referrer=2442">
+            <img class="photo" alt="Support freedom" src="//static.fsf.org/fsforg/img/normal-image.png"
+                 title="Help protect your freedom, join the Free Software Foundation"/></a>
+        <figcaption>I joined the FSF because I use the GNU Affero version 3 license</figcaption>
+    </figure>
+
+    <figure>
+        <img class="photo" alt="Richard M. Stallman and Julian Assange" src="/0_eT5LwH4rOihgpThm.jpeg">
+        <figcaption>Richard M. Stallman and Julian Assange holding up a picture of Edward Snowden</figcaption>
+    </figure>
+    <figure>
+        <img class="photo" alt="Aaron Swartz" src="/Aaron-Swartz.jpg">
+        <figcaption>Aaron Swartz (who was crushed by the system)</figcaption>
+    </figure>
+    <figure>
+        <img class="photo" alt="Ross Ulbricht" src="/Ross-Ulbricht2.jpg">
+        <figcaption>Ross Ulbricht (whose mistrial gave him a double life sentence plus forty years without the
+            possibility of parole)
+        </figcaption>
+    </figure>
+    <figure>
+        <img class="photo" alt="Edward Snowden" src="/edward_snowden.jpg">
+        <figcaption>Edward Snowden (living in exile)</figcaption>
+    </figure>
+    <figure>
+        <img class="photo" alt="Andreas Antonopoulos" src="/andreas_antonopoulos.jpg">
+        <figcaption>Andreas Antonopoulos (Bitcoin evangelist)</figcaption>
+    </figure>
+    <figure>
+        <img class="photo" alt="Richard D. Wolf" src="/richard_d_wolff.jpg">
+        <figcaption>Richard D. Wolf professor of economics</figcaption>
+    </figure>
+    <figure>
+        <img class="photo" alt="Stacey Abrams" src="/stacey_abrams.jpg" height="360" width="641">
+        <figcaption>Stacey Abrams (should have won the election)</figcaption>
+    </figure>
+    <figure>
+        <img class="photo" alt="Abby Martin" src="/AbbyMartinVenezuela.jpg">
+        <figcaption>Abby Martin (reporter on south american affairs)</figcaption>
+    </figure>
+    <figure>
+        <img class="photo" alt="Bernie Sanders" src="/burnie_sanders.jpg">
+        <figcaption>Bernie Sanders (fighting overspending on military)</figcaption>
+    </figure>
+    <figure>
+        <img class="photo" alt="Chris Hedges" src="/chris_hedges.jpg">
+        <figcaption>Chris Hedges (Author who speaks against corruption in government)</figcaption>
+    </figure>
+    <figure>
+        <img class="photo" alt="Film: The Panama Deception" src="/usattack.png">
+        <figcaption>Film: The Panama Deception</figcaption>
+    </figure>
 </div>
 <!-- footerbar -->
 <div id="footerbar">
