d96b162286337a41d26db2a1c35501a8524d3974
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-12-06 17:31:03 -0500

    Copied most of view for inbox from home

--- app/GoodToKnow/Views/inbox.php
+++ app/GoodToKnow/Views/inbox.php
@@ -1,1 +1,58 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <title><?php /** @noinspection PhpUndefinedVariableInspection */
+        echo $html_title; ?></title>
+</head>
+<body>
+<!-- topbar -->
+<div id="topbar">
+    <a href="https://goodtoknow.io/ax1"><img src="/good1.jpg" alt="GoodToKnow.io" height="70" width="302"
+                                             style="float: left"></a>
+    <div id="sendmessage"><?php require SENDMESSAGELINK; ?></div>
+    <div id="inboxlink"><p><a href="/ax1/Inbox/page">🎫 inbox</a></p></div>
+    <div id="logindiv"><p><a href="/ax1/Logout/page">👋 log out</a></p></div>
+</div>
+<!-- communities -->
+<div id="communities">
+    <p>👪:&nbsp;&nbsp;<?php require COMMUNITIESFORTHISUSER; ?></p>
+</div>
+<!-- breadcrumbs -->
+<div id="breadcrumbs">
+    <p>⛺:&nbsp;&nbsp;
+        <a href="/ax1/SetHomePageCommunityTopicPost/page/<?php echo $community_id; ?>/0/0"><?php echo $community_name; ?></a>
+        <?php require CURRENTTOPIC; ?><?php require CURRENTPOST; ?></p>
+</div>
+<!-- scriptoutput -->
+<div id="scriptoutput">
+    <div id="leftsodiv">
+        <div id="sendtoauthor"><?php require MESSAGETHEAUTHOR; ?></p></div>
+        <div id="admindiv">
+            <?php require CONTROLPANELLINK; ?>
+        </div>
+    </div>
+    <div id="scriptmessage">
+        <?php require SESSIONMESSAGE; ?>
+    </div>
+</div>
+<!-- maincontent -->
+<div id="maincontent">
+
+</div><!-- End maincontent -->
+<!-- footerbar -->
+<div id="footerbar">
+    <p align="center" style="font-size: 1em;">Copyright 2018 - Sameh Ramzy Labib</p>
+</div>
+<script src="/js/script.js"></script>
+</body>
+</html>
