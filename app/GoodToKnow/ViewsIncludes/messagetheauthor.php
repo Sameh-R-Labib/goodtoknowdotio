2bf17ea859494e06b83813cea9b19958f81e68db
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-12-06 23:23:17 -0500

    Fixed logic of condition

--- app/GoodToKnow/ViewsIncludes/messagetheauthor.php
+++ app/GoodToKnow/ViewsIncludes/messagetheauthor.php
@@ -1,4 +1,4 @@
-<?php if ($show_poof === true || $type_of_resource_requested === 'post'): ?>
+<?php if ($show_poof != true && $type_of_resource_requested === 'post'): ?>
     <p><a href="/ax1/MessageTheAuthor/page"><?php echo $author_username; ?></a></p>
 <?php else: ?>
     <p>💨</p>
