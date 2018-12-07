a8b6356d10a254cd378350668c0bd2a67c85d7a5
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-12-06 21:28:55 -0500

    Shortened field entry

--- app/GoodToKnow/Views/inbox.php
+++ app/GoodToKnow/Views/inbox.php
@@ -20,7 +20,7 @@
     <a href="https://goodtoknow.io/ax1"><img src="/good1.jpg" alt="GoodToKnow.io" height="70" width="302"
                                              style="float: left"></a>
     <div id="sendmessage"><?php require SENDMESSAGELINK; ?></div>
-    <div id="inboxlink"><p><a href="/ax1/ByUsernameMessage/page">📧 👲 by U/N</a></p></div>
+    <div id="inboxlink"><p><a href="/ax1/ByUsernameMessage/page">U/N 📧 👲</a></p></div>
     <div id="logindiv"><p><a href="/ax1/Logout/page">👋 log out</a></p></div>
 </div>
 <!-- communities -->
