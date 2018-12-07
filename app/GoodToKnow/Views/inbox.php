f097d40b601c203359f0a07527e6c6a1a0425278
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-12-06 21:25:13 -0500

    Fixed my screw up

--- app/GoodToKnow/Views/inbox.php
+++ app/GoodToKnow/Views/inbox.php
@@ -19,8 +19,8 @@
 <div id="topbar">
     <a href="https://goodtoknow.io/ax1"><img src="/good1.jpg" alt="GoodToKnow.io" height="70" width="302"
                                              style="float: left"></a>
-    <div id="sendmessage"><p><a href="/ax1/ByUsernameMessage/page">📧 👲 by U/N</a></p></div>
-    <div id="inboxlink"><p><a href="/ax1/Inbox/page">🎫 inbox</a></p></div>
+    <div id="sendmessage"><?php require SENDMESSAGELINK; ?></div>
+    <div id="inboxlink"><p><a href="/ax1/ByUsernameMessage/page">📧 👲 by U/N</a></p></div>
     <div id="logindiv"><p><a href="/ax1/Logout/page">👋 log out</a></p></div>
 </div>
 <!-- communities -->
