3dd7c7ae8820200940492562aa8f69a61b43da8b
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-12-07 01:27:51 -0500

    Added link to inbox in other views

--- app/GoodToKnow/Views/controlpanel.php
+++ app/GoodToKnow/Views/controlpanel.php
@@ -20,7 +20,7 @@
     <a href="https://goodtoknow.io/ax1"><img src="/good1.jpg" alt="GoodToKnow.io" height="70" width="302"
                                              style="float: left"></a>
     <div id="sendmessage"><p><a href="#">♠👔♠ admin</a></p></div>
-    <div id="inboxlink"><p><a href="#">🎫 inbox</a></p></div>
+    <div id="inboxlink"><p><a href="/ax1/Inbox/page">🎫 inbox</a></p></div>
     <div id="logindiv"><p><a href="/ax1/Logout/page">👋 log out</a></p></div>
 </div>
 <!-- communities -->
