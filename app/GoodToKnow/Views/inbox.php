12d5283669757f6a92f728e2db81a62a1a739dd9
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-12-20 01:51:29 -0500

    Used bold headings

--- app/GoodToKnow/Views/inbox.php
+++ app/GoodToKnow/Views/inbox.php
@@ -50,8 +50,8 @@
     <?php if (!empty($inbox_messages_array)): ?>
         <?php foreach ($inbox_messages_array as $message): ?>
             <hr>
-            <p>Time: <?php echo $message->created; ?></p>
-            <p>Sender: <?php echo $message->user_id; ?></p>
+            <p><b>Time: </b><?php echo $message->created; ?></p>
+            <p><b>Sender: </b><?php echo $message->user_id; ?></p>
             <?php echo $message->content; ?>
             <hr>
         <?php endforeach; ?>
