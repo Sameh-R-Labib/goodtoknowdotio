f3206708a5c66f2a94b290d5f1dce46a2ea5cd2c
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-12-22 23:06:14 -0500

    Removed whitespace

--- app/GoodToKnow/ViewsIncludes/breadcrumbs.php
+++ app/GoodToKnow/ViewsIncludes/breadcrumbs.php
@@ -1,10 +1,6 @@
-<?php if ($page === 'Inbox') {
-    echo "<a href=\"/ax1/Inbox/page\">Inbox</a>";
-} elseif ($page === 'Admin') {
-    echo "<a href=\"/ax1/AdminHome/page\">Admin Home</a>";
-} elseif ($page === 'CP') {
-    echo "<a href=\"/ax1/ControlPanel/page\">Control Panel</a>";
-} else {
+<?php if ($page === 'Inbox') echo "<a href=\"/ax1/Inbox/page\">Inbox</a>"; elseif ($page === 'Admin') echo "<a href=\"/ax1/AdminHome/page\">Admin Home</a>";
+elseif ($page === 'CP') echo "<a href=\"/ax1/ControlPanel/page\">Control Panel</a>";
+else {
     require CURRENTCOMMUNITY;
     require CURRENTTOPIC;
     require CURRENTPOST;
