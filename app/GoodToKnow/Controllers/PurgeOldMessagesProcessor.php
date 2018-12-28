dc1ee4c5a09324220526b8def8ddcd3de4566f7e
Sameh-R-Labib <schoolbuscompany@gmail.com>
2018-12-27 23:01:16 -0500

    PurgeOldMessagesProcessor similar to AdminCreateUser

--- app/GoodToKnow/Controllers/PurgeOldMessagesProcessor.php
+++ app/GoodToKnow/Controllers/PurgeOldMessagesProcessor.php
@@ -11,5 +11,13 @@ namespace GoodToKnow\Controllers;
 
 class PurgeOldMessagesProcessor
 {
-
+    public function page()
+    {
+        /**
+         * This code will:
+         *   1) Receive submitted date.
+         *   2) Delete the messages.
+         *   3) Report success or failure.
+         */
+    }
 }
