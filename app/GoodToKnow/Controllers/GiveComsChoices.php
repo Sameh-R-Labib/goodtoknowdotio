179baeee28d079dbc8486fed03f4e892920a68fe
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-01-09 17:19:58 -0500

    GiveComsChoices Debug Pre

--- app/GoodToKnow/Controllers/GiveComsChoices.php
+++ app/GoodToKnow/Controllers/GiveComsChoices.php
@@ -13,6 +13,22 @@ class GiveComsChoices
 {
     public function page()
     {
+        global $is_logged_in;
+        global $sessionMessage;
+        global $saved_str01;
 
+        if (!$is_logged_in || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * Debug Code
+         */
+        echo "\n<p>Begin debug</p>\n";
+        echo "<br><p>Var_dump \$saved_str01: </p>\n<pre>";
+        var_dump($saved_str01);
+        echo "</pre>\n";
+        die("<br><p>End debug</p>\n");
     }
 }
