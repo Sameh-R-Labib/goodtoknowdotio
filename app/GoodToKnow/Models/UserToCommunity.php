aed8c887b18017135c069a08a607f8f289ccbba8
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-01-10 18:13:14 -0500

    coms_user_belongs_to

--- app/GoodToKnow/Models/UserToCommunity.php
+++ app/GoodToKnow/Models/UserToCommunity.php
@@ -38,6 +38,23 @@ class UserToCommunity extends GoodObject
 
     public static function coms_user_belongs_to(\mysqli $db, string &$error, int $user_id)
     {
+        /**
+         * Returns array of community objects or returns false.
+         */
 
+        /**
+         * First get the UserToCommunity objects which belong to the user.
+         */
+        $sql = 'SELECT * FROM user_to_community WHERE `user_id`=' . $user_id;
+        $user_to_community_array = self::find_by_sql($db, $sessionMessage, $sql);
+
+        if (!$user_to_community_array) {
+            $error .= " UserToCommunity::coms_user_belongs_to() found no communities for the specified user. ";
+            return false;
+        }
+
+        /**
+         * Second get the Community objects which belong to the user.
+         */
     }
 }
