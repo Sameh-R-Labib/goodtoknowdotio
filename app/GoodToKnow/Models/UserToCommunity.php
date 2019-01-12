35763c8c15bbd76abe1cdce4ddbfaec6b66a1864
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-01-12 13:24:13 -0500

    coms_user_belongs_to completed

--- app/GoodToKnow/Models/UserToCommunity.php
+++ app/GoodToKnow/Models/UserToCommunity.php
@@ -36,6 +36,12 @@ class UserToCommunity extends GoodObject
      */
     public $community_id;
 
+    /**
+     * @param \mysqli $db
+     * @param string $error
+     * @param int $user_id
+     * @return array|bool
+     */
     public static function coms_user_belongs_to(\mysqli $db, string &$error, int $user_id)
     {
         /**
@@ -56,5 +62,22 @@ class UserToCommunity extends GoodObject
         /**
          * Second get the Community objects which belong to the user.
          */
+        $array_of_coms_for_this_user = [];
+        foreach ($user_to_community_array as $user_to_community_object) {
+            // Add a community object to $array_of_coms_for_this_user.
+            // Obviously this community which we will add will be the one specified by the UserToCommunity object.
+            $community = Community::find_by_id($db, $error, $user_to_community_object->community_id);
+            if ($community === false) {
+                return false;
+            } else {
+                $array_of_coms_for_this_user[] = $community;
+            }
+        }
+        if (empty($array_of_coms_for_this_user)) {
+            $error .= " UserToCommunity::coms_user_belongs_to() says: Unexpected empty array_of_coms_for_this_user. ";
+            return false;
+        } else {
+            return $array_of_coms_for_this_user;
+        }
     }
 }
