534d609905938a6e8e6f012ef697dc5f8d47c609
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-01-12 14:11:40 -0500

    Got all the communities that exist

--- app/GoodToKnow/Controllers/GiveComsChoices.php
+++ app/GoodToKnow/Controllers/GiveComsChoices.php
@@ -9,6 +9,7 @@
 namespace GoodToKnow\Controllers;
 
 
+use GoodToKnow\Models\Community;
 use GoodToKnow\Models\User;
 use GoodToKnow\Models\UserToCommunity;
 
@@ -64,15 +65,20 @@ class GiveComsChoices
         // First get all the communities the user DOES belong to.
         $coms_user_belongs_to = UserToCommunity::coms_user_belongs_to($db, $sessionMessage, $user_id);
         if ($coms_user_belongs_to === false) {
-            $sessionMessage .= " Unexpected unable to retrieve communities for this user. ";
+            $sessionMessage .= " Error encountered trying to retrieve communities for this user. ";
             $_SESSION['message'] = $sessionMessage;
             redirect_to("/ax1/Home/page");
         }
 
         // Second get all the communities that exist in this system.
         // By "this system" I mean this instance of the app.
-        $coms_in_this_system = UserToCommunity::coms_in_this_system($db, $sessionMessage);
-        // As above redirect if unexpected error
+        $coms_in_this_system = Community::find_all($db, $sessionMessage);
+        if ($coms_in_this_system === false) {
+            $sessionMessage .= " Unable to retrieve any communities. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
 
+        // Third best on what we've gathered so far get the communities the user DOES NOT belong to.
     }
 }
