9567088a81a248917d27f0a838f19007878e4f0c
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-02-19 16:57:45 -0500

    Link to RemoveCommunitiesFromAUser

---
+++ app/GoodToKnow/Controllers/RemoveCommunitiesFromAUser.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samehlabib
+ * Date: 2019-02-19
+ * Time: 16:52
+ */
+
+namespace GoodToKnow\Controllers;
+
+
+class RemoveCommunitiesFromAUser
+{
+    public function page()
+    {
+
+    }
+}
