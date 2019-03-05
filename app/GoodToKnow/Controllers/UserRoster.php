ec778d6d68968125389fb3347679430b5aed4f87
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-03-04 22:00:35 -0500

    Put initial code in UserRoster page

--- app/GoodToKnow/Controllers/UserRoster.php
+++ app/GoodToKnow/Controllers/UserRoster.php
@@ -9,10 +9,49 @@
 namespace GoodToKnow\Controllers;
 
 
+use GoodToKnow\Models\User;
+
 class UserRoster
 {
     public function page()
     {
+        global $sessionMessage;
+        global $is_logged_in;
+        global $is_admin;
 
+        if (!$is_logged_in || !$is_admin || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $db = db_connect($sessionMessage);
+
+        if (!empty($sessionMessage) || $db === false) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $html_title = 'User Roster';
+
+        $page = 'UserRoster';
+
+        $show_poof = true;
+
+        /**
+         * Get an array of User objects corresponding
+         * to all the Users in the system.
+         */
+        $user_objects_array = User::find_all($db, $sessionMessage);
+
+        if ($user_objects_array === false || empty($user_objects_array)) {
+            $sessionMessage .= " Unable to retrieve any user objects. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * Replace the field values that can benefit the viewer upon being replaced
+         * (so that they present better.)
+         */
     }
 }
