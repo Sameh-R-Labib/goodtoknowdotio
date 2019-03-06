ef2fad1cd8524217e9b399239ffa3ecb0bff1293
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-03-06 16:08:23 -0500

    Clean up

--- app/GoodToKnow/Controllers/UserRoster.php
+++ app/GoodToKnow/Controllers/UserRoster.php
@@ -52,16 +52,12 @@ class UserRoster
         }
 
         /**
-         * $user_objects_array is square business
-         */
-
-        /**
          * Replace the field values that can benefit the viewer upon being replaced
          * (so that they present better.)
          */
 
         // We need to have an array of a different object type called ReadableUser.
-        $readable_user_array = [];
+        $readable_user_objects_array = [];
 
         // Assign $community_values_array. $community_values_array is described in class ReadableUser.
         $community_values_array = [];
@@ -75,15 +71,10 @@ class UserRoster
             $community_values_array[$community->id] = $community->community_name;
         }
 
-        // For each user object replace the id_of_default_community with the community's name.
         foreach ($user_objects_array as $user) {
-            $readable_user_array[] = new ReadableUser($user, $community_values_array);
+            $readable_user_objects_array[] = new ReadableUser($user, $community_values_array);
         }
 
-        // For each user object replace the hyphens and underscores in the role with a space and capitalize the first letter of each word.
 
-        // For each  user object replace the hyphens in the race with a space and capitalize the first letter of each word.
-
-        // For each  user object replace a 0 with No and a positive integer with a Yes in the is_suspended.
     }
 }
