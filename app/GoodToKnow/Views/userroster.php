38b29b0d9864be62d566183f2f7ce25cbe10d7cd
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-03-07 16:24:03 -0500

    View got as far as Communities

--- app/GoodToKnow/Views/userroster.php
+++ app/GoodToKnow/Views/userroster.php
@@ -1,1 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <title><?php /** @noinspection PhpUndefinedVariableInspection */
+        echo $html_title; ?></title>
+</head>
+<body>
+<!-- topbar -->
+<div id="topbar">
+    <a href="/ax1"><img src="/good1.jpg" alt="GoodToKnow.io" height="70" width="302"
+                        style="float: left"></a>
+    <div id="sendmessage"><?php require SENDMESSAGELINK; ?></div>
+    <div id="inboxlink"><p><a href="/ax1/ByUsernameMessage/page">U/N 📧 👲</a></p></div>
+    <div id="logindiv"><p><a href="/ax1/Logout/page">👋 log out</a></p></div>
+</div>
+<!-- communities -->
+<div id="communities">
+    <p>👪:&nbsp;&nbsp;<?php require COMMUNITIESFORTHISUSER; ?></p>
+</div>
