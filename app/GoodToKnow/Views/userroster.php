f887b57f7ad1492e70aeecaf4de5c378d65cb33d
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-03-09 16:02:33 -0500

    Maincontent and Footer code added

--- app/GoodToKnow/Views/userroster.php
+++ app/GoodToKnow/Views/userroster.php
@@ -43,3 +43,39 @@
         <?php require SESSIONMESSAGE; ?>
     </div>
 </div>
+<!-- maincontent -->
+<div id="maincontent">
+    <?php if (!empty($readable_user_objects_array)): ?>
+        <?php $last = count($readable_user_objects_array) - 1; ?>
+        <?php foreach ($readable_user_objects_array as $key => $user): ?>
+            <p>&nbsp;</p>
+            <p><b>U/N: </b><?php echo $user->username; ?></p>
+            <p><b>Default Community: </b><?php echo $user->readable_community_name; ?></p>
+            <p><b>Title: </b><?php echo $user->title; ?></p>
+            <p><b>Role: </b><?php echo $user->readable_role; ?></p>
+            <p><b>Race: </b><?php echo $user->readable_race; ?></p>
+            <p><b>Is-Suspended: </b><?php echo $user->readable_is_suspended; ?></p>
+            <p><b>Date: </b><?php echo $user->date; ?></p>
+            <p>&nbsp;</p>
+            <p><b>Comment: </b><?php echo $user->comment; ?></p>
+            <p>&nbsp;</p>
+            <?php if ($key != $last): ?>
+                <hr>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <p>No users found in the system.</p>
+    <?php endif; ?>
+</div><!-- End maincontent -->
+<!-- footerbar -->
+<div id="footerbar">
+    <p align="center" style="font-size: 1em;">
+        <img src="/Gnu-head-30-years-anniversary.svg" style="float:left;height: 32px;width: 32px;margin-top: -6px">
+        © 2018 - Sameh Ramzy Labib
+        <img src="/2000px-GPLv3_Logo.svg.png"
+             height="32" width="70"
+             style="float:right;;margin-top: -6px"></p>
+</div>
+<script src="/js/script.js"></script>
+</body>
+</html>
