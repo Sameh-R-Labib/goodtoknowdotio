dda584d1881e1654f3b59362f27e0c5e956f516f
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-03-11 18:03:25 -0400

    MemberMemoEditor page() code

--- app/GoodToKnow/Controllers/MemberMemoEditor.php
+++ app/GoodToKnow/Controllers/MemberMemoEditor.php
@@ -13,6 +13,22 @@ class MemberMemoEditor
 {
     public function page()
     {
+        global $is_logged_in;
+        global $is_admin;
+        global $sessionMessage;
 
+        if (!$is_logged_in || !$is_admin || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * Present a form which collects
+         * the username.
+         */
+
+        $html_title = "Member's Memo Editor";
+
+        require VIEWS . DIRSEP . 'membermemoeditor.php';
     }
 }
