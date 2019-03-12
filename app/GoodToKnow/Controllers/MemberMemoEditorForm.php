afc22110c5932321489fd6159244917028338547
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-03-11 21:38:35 -0400

    MemberMemoEditorProcessor code

---
+++ app/GoodToKnow/Controllers/MemberMemoEditorForm.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samehlabib
+ * Date: 2019-03-11
+ * Time: 21:37
+ */
+
+namespace GoodToKnow\Controllers;
+
+
+class MemberMemoEditorForm
+{
+    public function page()
+    {
+
+    }
+}
