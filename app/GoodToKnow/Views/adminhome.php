287732d57c6a9ce4be301d52db2b12eb6190a3d3
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-03-13 21:27:10 -0400

    Rearranged Admin Home

--- app/GoodToKnow/Views/adminhome.php
+++ app/GoodToKnow/Views/adminhome.php
@@ -48,11 +48,6 @@
     <h1>Admin Scripts</h1>
     <ul>
         <li><a href="/ax1/AdminPassCodeGenerationForm/page">Create Account for Someone</a></li>
-        <li><a href="/ax1/DefaultCommunity/page">Change Default Community</a></li>
-        <li><a href="/ax1/ChangePassword/page">Change Password</a></li>
-        <li><a href="/ax1/CreateNewPost/page">Create Post</a></li>
-        <li><a href="/ax1/NewTopic/page">Create Topic</a></li>
-        <li><a href="/ax1/EditMyPost/page">Edit Post</a></li>
         <li><a href="/ax1/PurgeOldMessages/page">Purge Old Messages</a></li>
         <li><a href="/ax1/ByUsernameMessage/page">Username Message a User</a></li>
         <li><a href="/ax1/GiveCommunitiesToUser/page">Give Communities to User</a></li>
@@ -60,6 +55,14 @@
         <li><a href="/ax1/UserRoster/page">User Roster</a></li>
         <li><a href="/ax1/MemberMemoEditor/page">Member's Memo Editor</a></li>
         <li><a href="/ax1/SuspendAccount/page">Suspend Account</a></li>
+    </ul>
+    <h2>Regular Member Scripts</h2>
+    <ul>
+        <li><a href="/ax1/DefaultCommunity/page">Change Default Community</a></li>
+        <li><a href="/ax1/ChangePassword/page">Change Password</a></li>
+        <li><a href="/ax1/CreateNewPost/page">Create Post</a></li>
+        <li><a href="/ax1/NewTopic/page">Create Topic</a></li>
+        <li><a href="/ax1/EditMyPost/page">Edit Post</a></li>
     </ul>
     <p>When writing a post, our goal is to concisely write in a manner which helps the reader wrap their head around the
         <em>community's</em> subject matter. Where I said "<em>community</em>" I mean as in our hierarchy: community
