33ce030aa00245ad40605fd1079404c8a660480d
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-03-26 17:55:40 -0400

    SuspendAccountProcessor completed

--- app/GoodToKnow/Controllers/SuspendAccountProcessor.php
+++ app/GoodToKnow/Controllers/SuspendAccountProcessor.php
@@ -13,6 +13,41 @@ class SuspendAccountProcessor
 {
     public function page()
     {
+        global $is_logged_in;
+        global $is_admin;
+        global $sessionMessage;
 
+        if (!$is_logged_in || !$is_admin || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * Goal:
+         *  1) Validate $_POST['username']
+         *  2) Save $_POST['username']
+         *  3) Redirect to a route
+         */
+
+        $submitted_username = (isset($_POST['username'])) ? $_POST['username'] : '';
+
+        $db = db_connect($sessionMessage);
+
+        if (!empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $is_username = GiveComsToUsrProcessor::is_username_in_our_system($db, $sessionMessage, $submitted_username);
+
+        if (!$is_username) {
+            $sessionMessage .= " The username is not valid. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $_SESSION['saved_str01'] = $submitted_username;
+
+        redirect_to("/ax1/SuspendAccountSuspend/page");
     }
 }
