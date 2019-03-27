84634cc4c5566ad1ce6bd492de21c89298c195f7
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-03-27 18:29:51 -0400

    UnsuspendAccountProcessor started

---
+++ app/GoodToKnow/Controllers/UnsuspendAccountProcessor.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samehlabib
+ * Date: 2019-03-27
+ * Time: 18:29
+ */
+
+namespace GoodToKnow\Controllers;
+
+
+class UnsuspendAccountProcessor
+{
+    public function page()
+    {
+    }
+}
