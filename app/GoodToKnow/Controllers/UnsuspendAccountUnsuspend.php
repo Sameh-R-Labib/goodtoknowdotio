bfd0582c353e1299b088626165c5f9c5bcac1113
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-03-27 18:41:53 -0400

    UnsuspendAccountUnsuspend coded

---
+++ app/GoodToKnow/Controllers/UnsuspendAccountUnsuspend.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samehlabib
+ * Date: 2019-03-27
+ * Time: 18:36
+ */
+
+namespace GoodToKnow\Controllers;
+
+
+use GoodToKnow\Models\User;
+
+
+class UnsuspendAccountUnsuspend
+{
+    public function page()
+    {
+        global $is_logged_in;
+        global $is_admin;
+        global $sessionMessage;
+        global $saved_str01; // Has user's username
+
+        if (!$is_logged_in || !$is_admin || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * Goals for this function:
+         *  1) Retrieve the User object for the member
+         *     whose is_suspended field the admin wants to edit.
+         *  2) Change the value of is_suspended to 0.
+         *  3) Save changes to User object. In other words update the User record in the database.
+         *  4) Show a message indicating we've successfully suspended the user's account.
+         */
+
+        $db = db_connect($sessionMessage);
+        if (!empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * 1) Retrieve the User object for the member
+         *     whose is_suspended field the admin wants to edit.
+         */
+        $user_object = User::find_by_username($db, $sessionMessage, $saved_str01);
+        if (!$user_object) {
+            $sessionMessage .= " Unexpected unable to retrieve target user's object. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * 2) Change the value of is_suspended to 0.
+         */
+        $user_object->is_suspended = "0";
+
+        /**
+         * 3) Save changes to User object. In other words update the User record in the database.
+         */
+        $consequence_of_save = $user_object->save($db, $sessionMessage);
+
+        if (!$consequence_of_save) {
+            $sessionMessage .= ' The save method for User returned false. ';
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        if (!empty($sessionMessage)) {
+            $sessionMessage .= " The save method for User did not return false but it did send back a message.
+             Therefore, it probably did not update {$saved_str01}'s account. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * 4) Show a message indicating we've successfully suspended the user's account.
+         */
+        $sessionMessage .= " User {$saved_str01}'s account has been <em>un</em>suspended successfully! Yay 😅 🤟 ";
+        $_SESSION['message'] = $sessionMessage;
+        redirect_to("/ax1/Home/page");
+    }
+}
