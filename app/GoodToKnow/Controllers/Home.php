18e0144643428a464617d45df457b45f555206d0
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-04-09 17:31:26 -0400

    refresh_vars_which_may_be_stale

--- app/GoodToKnow/Controllers/Home.php
+++ app/GoodToKnow/Controllers/Home.php
@@ -54,99 +54,9 @@ class Home
 
         self::logout_the_user_if_he_is_suspended($db, $sessionMessage, $user_id, $when_last_checked_suspend);
 
-        /**
-         * If the special_community_array has not been
-         * refreshed for a period of time longer than
-         * 3 hours then refresh it.
-         */
-        $time_since_refresh = time() - $last_refresh_communities;  // seconds
-        if ($time_since_refresh > 10800) {
-            if ($db == 'not connected') {
-                $db = db_connect($sessionMessage);
-                if ($db === false) {
-                    $sessionMessage .= " Failed to connect to the database. ";
-                    $_SESSION['message'] = $sessionMessage;
-                    redirect_to("/ax1/InfiniteLoopPrevent/page");
-                }
-            }
-            $special_community_array = EnfoFindCommunitiesOfUser::find_communities_of_user($db, $sessionMessage, $user_id);
-            if ($special_community_array === false) {
-                $sessionMessage .= " Failed to find the array of the user's communities. ";
-            }
-            $_SESSION['special_community_array'] = $special_community_array;
-            $_SESSION['last_refresh_communities'] = time();
-        }
-
-        /**
-         * If the type_of_resource_requested == 'community'
-         * and the special_topic_array has not been refreshed
-         * for a period longer than 12 minutes then refresh it.
-         */
-        $time_since_refresh = time() - $last_refresh_topics;
-        if ($time_since_refresh > 720 && $type_of_resource_requested == 'community') {
-            if ($db == 'not connected') {
-                $db = db_connect($sessionMessage);
-                if ($db === false) {
-                    $sessionMessage .= " Failed to connect to the database. ";
-                    $_SESSION['message'] = $sessionMessage;
-                    redirect_to("/ax1/InfiniteLoopPrevent/page");
-                }
-            }
-            $special_topic_array = CommunityToTopic::get_topics_array_for_a_community($db, $sessionMessage, $community_id);
-            if ($special_topic_array === false) $special_topic_array = [];
-            $_SESSION['special_topic_array'] = $special_topic_array;
-            $_SESSION['last_refresh_topics'] = time();
-        }
-
-        /**
-         * If the type_of_resource_requested == 'topic'
-         * and the special_post_array has not been refreshed
-         * for a period longer than 3 minutes then refresh it.
-         */
-        $time_since_refresh = time() - $last_refresh_posts;
-        if ($time_since_refresh > 180 && $type_of_resource_requested == 'topic') {
-            if ($db == 'not connected') {
-                $db = db_connect($sessionMessage);
-                if ($db === false) {
-                    $sessionMessage .= " Failed to connect to the database. ";
-                    $_SESSION['message'] = $sessionMessage;
-                    redirect_to("/ax1/InfiniteLoopPrevent/page");
-                }
-            }
-            $special_post_array = TopicToPost::special_get_posts_array_for_a_topic($db, $sessionMessage, $topic_id);
-            if ($special_post_array === false) $special_post_array = [];
-            $_SESSION['special_post_array'] = $special_post_array;
-            $_SESSION['last_refresh_posts'] = time();
-        }
-
-        /**
-         * If the type_of_resource_requested == 'post'
-         * and the post_content has not been refreshed
-         * for a period longer than 3 minutes then refresh it.
-         */
-        $time_since_refresh = time() - $last_refresh_content;
-        if ($time_since_refresh > 180 && $type_of_resource_requested == 'post') {
-            if ($db == 'not connected') {
-                $db = db_connect($sessionMessage);
-                if ($db === false) {
-                    $sessionMessage .= " Failed to connect to the database. ";
-                    $_SESSION['message'] = $sessionMessage;
-                    redirect_to("/ax1/InfiniteLoopPrevent/page");
-                }
-            }
-            $post_object = Post::find_by_id($db, $sessionMessage, $post_id);
-            if ($post_object === false) {
-                $sessionMessage .= " Home page() says: Unable to get post object from the database. ";
-            } else {
-                $post_content = file_get_contents($post_object->html_file);
-                if ($post_content === false) {
-                    $sessionMessage .= " Unable to read the post's file. ";
-                    $post_content = '';
-                }
-            }
-            $_SESSION['post_content'] = $post_content;
-            $_SESSION['last_refresh_content'] = time();
-        }
+        self::refresh_vars_which_may_be_stale($db, $sessionMessage, $special_community_array, $special_topic_array,
+            $post_content, $user_id, $community_id, $topic_id, $post_id, $type_of_resource_requested,
+            $last_refresh_communities, $last_refresh_topics, $last_refresh_posts, $last_refresh_content);
 
         if ($type_of_resource_requested === 'community') {
             if (!empty(trim($community_description))) {
@@ -175,6 +85,122 @@ class Home
         $page = "Home";
 
         require VIEWS . DIRSEP . 'home.php';
+    }
+
+    /**
+     * @param $db
+     * @param $error
+     * @param $special_community_array
+     * @param $special_topic_array
+     * @param $post_content
+     * @param $user_id
+     * @param $community_id
+     * @param $topic_id
+     * @param $post_id
+     * @param $type_of_resource_requested
+     * @param $last_refresh_communities
+     * @param $last_refresh_topics
+     * @param $last_refresh_posts
+     * @param $last_refresh_content
+     */
+    private static function refresh_vars_which_may_be_stale(&$db, &$error, &$special_community_array, &$special_topic_array,
+                                                            &$post_content, $user_id, $community_id, $topic_id, $post_id,
+                                                            $type_of_resource_requested, $last_refresh_communities,
+                                                            $last_refresh_topics, $last_refresh_posts, $last_refresh_content)
+    {
+        /**
+         * If the special_community_array has not been
+         * refreshed for a period of time longer than
+         * 3 hours then refresh it.
+         */
+        $time_since_refresh = time() - $last_refresh_communities;  // seconds
+        if ($time_since_refresh > 10800) {
+            if ($db == 'not connected') {
+                $db = db_connect($error);
+                if ($db === false) {
+                    $error .= " Failed to connect to the database. ";
+                    $_SESSION['message'] = $error;
+                    redirect_to("/ax1/InfiniteLoopPrevent/page");
+                }
+            }
+            $special_community_array = EnfoFindCommunitiesOfUser::find_communities_of_user($db, $error, $user_id);
+            if ($special_community_array === false) {
+                $error .= " Failed to find the array of the user's communities. ";
+            }
+            $_SESSION['special_community_array'] = $special_community_array;
+            $_SESSION['last_refresh_communities'] = time();
+        }
+
+        /**
+         * If the type_of_resource_requested == 'community'
+         * and the special_topic_array has not been refreshed
+         * for a period longer than 12 minutes then refresh it.
+         */
+        $time_since_refresh = time() - $last_refresh_topics;
+        if ($time_since_refresh > 720 && $type_of_resource_requested == 'community') {
+            if ($db == 'not connected') {
+                $db = db_connect($error);
+                if ($db === false) {
+                    $error .= " Failed to connect to the database. ";
+                    $_SESSION['message'] = $error;
+                    redirect_to("/ax1/InfiniteLoopPrevent/page");
+                }
+            }
+            $special_topic_array = CommunityToTopic::get_topics_array_for_a_community($db, $error, $community_id);
+            if ($special_topic_array === false) $special_topic_array = [];
+            $_SESSION['special_topic_array'] = $special_topic_array;
+            $_SESSION['last_refresh_topics'] = time();
+        }
+
+        /**
+         * If the type_of_resource_requested == 'topic'
+         * and the special_post_array has not been refreshed
+         * for a period longer than 3 minutes then refresh it.
+         */
+        $time_since_refresh = time() - $last_refresh_posts;
+        if ($time_since_refresh > 180 && $type_of_resource_requested == 'topic') {
+            if ($db == 'not connected') {
+                $db = db_connect($error);
+                if ($db === false) {
+                    $error .= " Failed to connect to the database. ";
+                    $_SESSION['message'] = $error;
+                    redirect_to("/ax1/InfiniteLoopPrevent/page");
+                }
+            }
+            $special_post_array = TopicToPost::special_get_posts_array_for_a_topic($db, $error, $topic_id);
+            if ($special_post_array === false) $special_post_array = [];
+            $_SESSION['special_post_array'] = $special_post_array;
+            $_SESSION['last_refresh_posts'] = time();
+        }
+
+        /**
+         * If the type_of_resource_requested == 'post'
+         * and the post_content has not been refreshed
+         * for a period longer than 3 minutes then refresh it.
+         */
+        $time_since_refresh = time() - $last_refresh_content;
+        if ($time_since_refresh > 180 && $type_of_resource_requested == 'post') {
+            if ($db == 'not connected') {
+                $db = db_connect($error);
+                if ($db === false) {
+                    $error .= " Failed to connect to the database. ";
+                    $_SESSION['message'] = $error;
+                    redirect_to("/ax1/InfiniteLoopPrevent/page");
+                }
+            }
+            $post_object = Post::find_by_id($db, $error, $post_id);
+            if ($post_object === false) {
+                $error .= " Home page() says: Unable to get post object from the database. ";
+            } else {
+                $post_content = file_get_contents($post_object->html_file);
+                if ($post_content === false) {
+                    $error .= " Unable to read the post's file. ";
+                    $post_content = '';
+                }
+            }
+            $_SESSION['post_content'] = $post_content;
+            $_SESSION['last_refresh_content'] = time();
+        }
     }
 
     /**
