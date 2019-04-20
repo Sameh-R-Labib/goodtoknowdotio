5ab49cbb1ec18c94b80c5a7040dc83dc9aee293d
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-04-19 22:55:09 -0400

    class NewCommunity

---
+++ app/GoodToKnow/Controllers/NewCommunity.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace GoodToKnow\Controllers;
+
+
+class NewCommunity
+{
+    public function page()
+    {
+        /**
+         * This route will generate a form
+         * where the admin can enter the
+         * information needed to generate
+         * a new community.
+         *
+         * What are the db fields for a community?
+         *  - id int(10)
+         *  - community_name varchar(200)
+         *  - community_description varchar(230)
+         */
+
+        global $is_logged_in;
+        global $is_admin;
+        global $sessionMessage;
+
+        if (!$is_logged_in || !$is_admin || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+    }
+}
