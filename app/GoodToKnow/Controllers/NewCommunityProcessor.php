5e2c4d5d8e51ac32aaa1c17528801565f982cdcf
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-04-22 18:12:20 -0400

    progress 1

--- app/GoodToKnow/Controllers/NewCommunityProcessor.php
+++ app/GoodToKnow/Controllers/NewCommunityProcessor.php
@@ -21,5 +21,16 @@ class NewCommunityProcessor
 
         $community_name = trim($community_name);
         $community_description = trim($community_description);
+
+        if (empty($community_name) || empty($community_description)) {
+            $sessionMessage .= " Either you did not fill out the input fields or the session expired. Start over. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $community_name = htmlspecialchars($community_name, ENT_NOQUOTES | ENT_HTML5);
+        $community_description = htmlspecialchars($community_description, ENT_NOQUOTES | ENT_HTML5);
+
+
     }
 }
