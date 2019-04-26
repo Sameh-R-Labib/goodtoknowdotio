35b29332c4579dc692e9b925ef727243078dc6d0
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-04-26 15:51:38 -0400

    AuthorDeletesOwnPostChoosePost class started

---
+++ app/GoodToKnow/Controllers/AuthorDeletesOwnPostChoosePost.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace GoodToKnow\Controllers;
+
+
+class AuthorDeletesOwnPostChoosePost
+{
+    public function page()
+    {
+
+    }
+}
--- app/GoodToKnow/Controllers/AuthorDeletesOwnPostProcessor.php
+++ app/GoodToKnow/Controllers/AuthorDeletesOwnPostProcessor.php
@@ -8,6 +8,40 @@ class AuthorDeletesOwnPostProcessor
 {
     public function page()
     {
+        global $special_topic_array;
+        global $is_logged_in;
+        global $sessionMessage;
 
+        if (!$is_logged_in || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * We should have a post variable called
+         * choice whose value is the topic id
+         * the user intends to create a post in
+         */
+
+        /**
+         * I can't assume this post variables exist so I do the following.
+         */
+        $chosen_topic_id = (isset($_POST['choice'])) ? (int)$_POST['choice'] : 0;
+
+        /**
+         * Make sure $chosen_topic_id is among the ids of $special_topic_array
+         */
+        if (!array_key_exists($chosen_topic_id, $special_topic_array)) {
+            $sessionMessage .= " Unexpected error: topic id not found in topic array. ";
+            $_SESSION['message'] .= $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * Save it in the session
+         */
+        $_SESSION['saved_int01'] = $chosen_topic_id;
+
+        redirect_to("/ax1/AuthorDeletesOwnPostChoosePost/page");
     }
 }
