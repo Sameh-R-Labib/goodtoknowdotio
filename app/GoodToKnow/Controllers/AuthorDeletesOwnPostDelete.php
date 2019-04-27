3723225c6eaa5e18da64ce9900a631508ad8a4fd
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-04-26 20:19:17 -0400

    AuthorDeletesOwnPostDelete intro comment

--- app/GoodToKnow/Controllers/AuthorDeletesOwnPostDelete.php
+++ app/GoodToKnow/Controllers/AuthorDeletesOwnPostDelete.php
@@ -8,6 +8,21 @@ class AuthorDeletesOwnPostDelete
 {
     public function page()
     {
+        /**
+         * This route will simply determine
+         * which post the user chose to delete,
+         * store its info in the session, and
+         * present a form asking the user if he
+         * is sure he wants to delete the post.
+         */
 
+        global $is_logged_in;
+        global $sessionMessage;
+        global $user_id;
+
+        if (!$is_logged_in || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
     }
 }
