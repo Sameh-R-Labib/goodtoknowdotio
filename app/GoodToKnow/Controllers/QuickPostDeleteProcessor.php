d1fb852f1c7fc7aca6dcb0b6a11b004b889e5f0b
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-04-29 18:00:43 -0400

    QuickPostDeleteProcessor going on break save

--- app/GoodToKnow/Controllers/QuickPostDeleteProcessor.php
+++ app/GoodToKnow/Controllers/QuickPostDeleteProcessor.php
@@ -8,6 +8,16 @@ class QuickPostDeleteProcessor
 {
     public function page()
     {
+        global $special_topic_array;
+        global $is_logged_in;
+        global $sessionMessage;
+        global $is_admin;
+
+        if (!$is_logged_in || !$is_admin || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
 
     }
 }
