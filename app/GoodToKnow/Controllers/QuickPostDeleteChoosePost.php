c2ba6d8e630a8f2a14617c58e45ba5de59b520d2
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-04-29 21:41:05 -0400

    QuickPostDeleteChoosePost saving to take a break

--- app/GoodToKnow/Controllers/QuickPostDeleteChoosePost.php
+++ app/GoodToKnow/Controllers/QuickPostDeleteChoosePost.php
@@ -8,6 +8,40 @@ class QuickPostDeleteChoosePost
 {
     public function page()
     {
+        /**
+         * The goal is to present a form
+         * with radio buttons for admin
+         * to choose the post to delete.
+         * We are ONLY presenting posts
+         * found in the topic which was
+         * already selected.
+         * If we can't find any posts then
+         * we'll store a session message
+         * and redirect back home.
+         *
+         * For each post we will show the
+         * complete name of the post along
+         * with the username of its author.
+         */
+
+        global $is_logged_in;
+        global $sessionMessage;
+        global $is_admin;
+        global $saved_int01;        // id of topic
+
+        if (!$is_logged_in || !$is_admin || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $db = db_connect($sessionMessage);
+
+        if (!empty($sessionMessage) || $db === false) {
+            $sessionMessage .= ' Database connection failed. ';
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
 
     }
 }
