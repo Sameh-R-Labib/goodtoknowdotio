80bb9c9b5b004fb2f2e4f7fd91b0da5ca7c91d9f
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-05-03 21:26:01 -0400

    Debug code before coding the view

--- app/GoodToKnow/Views/quickpostdeletechoosepost.php
+++ app/GoodToKnow/Views/quickpostdeletechoosepost.php
@@ -1,1 +1,15 @@
 <?php
+/**
+ * Debug Code
+ */
+echo "\n<p>Begin debug</p>\n";
+echo "<br><p>Var_dump \$sessionMessage: </p>\n<pre>";
+var_dump($sessionMessage);
+echo "</pre>\n";
+echo "<br><p>Var_dump \$array_of_post_objects: </p>\n<pre>";
+var_dump($array_of_post_objects);
+echo "</pre>\n";
+echo "<br><p>Var_dump \$array_of_author_usernames: </p>\n<pre>";
+var_dump($array_of_author_usernames);
+echo "</pre>\n";
+die("<br><p>End debug</p>\n");
