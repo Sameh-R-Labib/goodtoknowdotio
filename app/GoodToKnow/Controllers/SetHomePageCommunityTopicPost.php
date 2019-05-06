73a2e3f1b87323606c5d0e51e02f4bdf1aac5e9f
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-05-06 17:06:14 -0400

    Abbreviations

--- app/GoodToKnow/Controllers/SetHomePageCommunityTopicPost.php
+++ app/GoodToKnow/Controllers/SetHomePageCommunityTopicPost.php
@@ -114,8 +114,8 @@ class SetHomePageCommunityTopicPost
             $_SESSION['post_name'] = $post_object->title;
             $epoch_time = (int)$post_object->created;
             $publish_date = date("m/d/Y", $epoch_time);
-            $_SESSION['post_full_name'] = '"' . $post_object->title . ' | ' . $post_object->extensionfortitle . '" [Published ' .
-                $publish_date . ' New York timezone]';
+            $_SESSION['post_full_name'] = '"' . $post_object->title . ' | ' . $post_object->extensionfortitle . '" [Pub. ' .
+                $publish_date . ' NY time]';
             // Then do the rest.
             $_SESSION['special_post_array'] = $special_post_array;
             $_SESSION['last_refresh_posts'] = time();
