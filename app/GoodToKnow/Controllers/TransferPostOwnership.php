43ae8ce83cde2cf053e5ece81758ecb034855631
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-05-07 16:42:29 -0400

    TransferPostOwnership saving because going out

--- app/GoodToKnow/Controllers/TransferPostOwnership.php
+++ app/GoodToKnow/Controllers/TransferPostOwnership.php
@@ -8,6 +8,25 @@ class TransferPostOwnership
 {
     public function page()
     {
+        /**
+         * This is the first route for transferring ownership of a post.
+         *
+         * It's goal is to be the first in a bunch of routes to determine which post is to be deleted.
+         *
+         * It will present radio buttons for choosing which topic the post is in.
+         * It will also describe what is being done.
+         *
+         * As usual the topics presented are the topics in the current community.
+         */
 
+        global $is_logged_in;
+        global $sessionMessage;
+        global $community_id;
+        global $is_admin;
+
+        if (!$is_logged_in || !$is_admin || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
     }
 }
