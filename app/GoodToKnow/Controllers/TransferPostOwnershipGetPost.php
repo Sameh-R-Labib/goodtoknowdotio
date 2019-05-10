0128b319157ad4718900253d047ecafe004cb745
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-05-10 19:02:04 -0400

    TransferPostOwnershipGetPost fixed missing page()

--- app/GoodToKnow/Controllers/TransferPostOwnershipGetPost.php
+++ app/GoodToKnow/Controllers/TransferPostOwnershipGetPost.php
@@ -6,19 +6,39 @@ namespace GoodToKnow\Controllers;
 
 class TransferPostOwnershipGetPost
 {
-    /**
-     * This route will (1) determine
-     * which post the admin chose to do a transfer of ownership to,
-     * (2) stores the post's info in the session, and
-     * (3) presents a form asking the user if he
-     * is sure this is the post he wants to transfer the ownership of.
-     *
-     * For step (3):
-     * Based on the submitted post id the script will
-     * derive and present:
-     *  - Community name
-     *  - Topic name
-     *  - Post title | extensionfortitle
-     *  - Author username
-     */
+    public function page()
+    {
+        /**
+         * This route will (1) determine
+         * which post the admin chose to do a transfer of ownership to,
+         * (2) stores the post's info in the session, and
+         * (3) presents a form asking the user if he
+         * is sure this is the post he wants to transfer the ownership of.
+         *
+         * For step (3):
+         * Based on the submitted post id the script will
+         * derive and present:
+         *  - Community name
+         *  - Topic name
+         *  - Post title | extensionfortitle
+         *  - Author username
+         */
+
+        global $is_logged_in;
+        global $sessionMessage;
+        global $is_admin;
+
+        if (!$is_logged_in || !$is_admin || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $db = db_connect($sessionMessage);
+
+        if (!empty($sessionMessage) || $db === false) {
+            $sessionMessage .= ' Database connection failed. ';
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+    }
 }
