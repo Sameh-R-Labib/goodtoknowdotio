a003e4182f9e7e8074dc73cb5cb8b5d5955c32be
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-05-11 23:32:51 -0400

    transferpostownershipgetusername stub created

--- app/GoodToKnow/Controllers/TransferPostOwnershipGetUsername.php
+++ app/GoodToKnow/Controllers/TransferPostOwnershipGetUsername.php
@@ -18,5 +18,34 @@ class TransferPostOwnershipGetUsername
          * will reset the session variables used so far
          * and redirect to home page.
          */
+
+        global $is_logged_in;
+        global $sessionMessage;
+        global $is_admin;
+
+        if (!$is_logged_in || !$is_admin || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $choice = (isset($_POST['choice'])) ? $_POST['choice'] : "";
+
+        if ($choice != "yes" && $choice != "no") {
+            $sessionMessage .= " You didn't enter a choice. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        if ($choice == "no") {
+            $_SESSION['saved_int01'] = 0;
+            $_SESSION['saved_int02'] = 0;
+            $sessionMessage .= " I'm acknowledging that you changed your mind about transferring ownership of the post. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $html_title = 'What is the username of the person whom you want to assign the post to?';
+
+        require VIEWS . DIRSEP . 'transferpostownershipgetusername.php';
     }
 }
