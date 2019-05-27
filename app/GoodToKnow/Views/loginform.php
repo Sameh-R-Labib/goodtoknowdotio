32db0b0478912a3153260594c466013334c384ce
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-05-26 20:31:24 -0400

    Fixed loginform

--- app/GoodToKnow/Views/loginform.php
+++ app/GoodToKnow/Views/loginform.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <link rel="stylesheet" href="/css/normalize.css">
     <link rel="stylesheet" href="/css/loginform.css">
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
