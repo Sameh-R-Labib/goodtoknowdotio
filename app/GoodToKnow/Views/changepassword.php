ea94ce7253d74968dba6a0153bff33bdaaf0a391
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-05-26 20:10:58 -0400

    Let's see

--- app/GoodToKnow/Views/changepassword.php
+++ app/GoodToKnow/Views/changepassword.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="/css/payment-form.css">
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/editor.css">
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
