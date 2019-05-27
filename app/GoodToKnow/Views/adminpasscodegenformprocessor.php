ba38211cb22b81e0f6c4da152d2dc17218fd068e
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-05-26 21:29:43 -0400

    Temporarily save adminpasscodegenformprocessor

--- app/GoodToKnow/Views/adminpasscodegenformprocessor.php
+++ app/GoodToKnow/Views/adminpasscodegenformprocessor.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="/css/payment-form.css">
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/editor.css">
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
@@ -18,96 +19,70 @@
 <form action="/ax1/AdminCreateUser/page" method="post">
     <h1>User</h1>
     <?php require SESSIONMESSAGE; ?>
-    <p>Required = <strong><abbr title="required">*</abbr></strong>.</p>
+    <h2>Log-in Credentials</h2>
+    <p>The rules for the username: Must consist of two words separated by an underscore.
+        The first word must start with an upper case letter.
+        That first letter is the only uppercase letter.
+        The first word must be 4 to 9 characters in length.
+        The second word is numeric two digits long.</p>
     <section>
-        <h2>Log-in Credentials</h2>
-        <p>The rules for the username: Must consist of two words separated by an underscore.
-            The first word must start with an upper case letter.
-            That first letter is the only uppercase letter.
-            The first word must be 4 to 9 characters in length.
-            The second word is numeric two digits long.</p>
         <p>
-            <label for="name">
-                <span>Username: </span>
-                <strong><abbr title="required">*</abbr></strong>
-                <em>like Buddy_52</em>
-            </label>
-            <input type="text" id="name" name="username">
+            <label for="name">Username (<em>like Buddy_52</em>:) </label>
+            <input id="name" name="username" type="text" value="" required minlength="6" spellcheck="false">
         </p>
         <p>
-            <label for="first_try">
-                <span>Password 1st try: </span>
-                <strong><abbr title="required">*</abbr></strong>
-            </label>
-            <input type="password" id="first_try" name="first_try">
+            <label for="first_try">Password 1st try: </label>
+            <input id="first_try" name="first_try" type="password" value="" required minlength="6" spellcheck="false">
         </p>
         <p>
-            <label for="pwd">
-                <span>Password 2nd try: </span>
-                <strong><abbr title="required">*</abbr></strong>
-            </label>
-            <input type="password" id="pwd" name="password">
-        </p>
-        <p>The rules for the password: 10 to 18 characters, 2 digits, 2 or more non-alpha, 2 or more capitals.</p>
-    </section>
-    <section>
-        <h2>Person's Characteristics</h2>
-        <p align="center"><strong><abbr title="required">*</abbr></strong></p>
-        <fieldset>
-            <legend>Title</legend>
-            <ul>
-                <li>
-                    <label for="title_1">
-                        <input type="radio" id="title_1" name="title" value="Mr.">
-                        Mr.
-                    </label>
-                </li>
-                <li>
-                    <label for="title_2">
-                        <input type="radio" id="title_2" name="title" value="Mrs.">
-                        Mrs.
-                    </label>
-                </li>
-            </ul>
-        </fieldset>
-        <p>
-            <label for="card">
-                <span>Race: <strong><abbr title="required">*</abbr></strong></span>
-            </label>
-            <select id="card" name="race">
-                <option value="caucasian-american">Caucasian American</option>
-                <option value="caucasian-european">Caucasian European</option>
-                <option value="caucasian-african">Caucasian African</option>
-                <option value="black-european">Black European</option>
-                <option value="black-american">Black American</option>
-                <option value="black-african">Black African</option>
-                <option value="asian">Asian</option>
-                <option value="south-american">South American</option>
-                <option value="greek">Greek</option>
-                <option value="middle-eastern-christian">Middle Eastern Christian</option>
-                <option value="middle-eastern-moslem">Middle Eastern Moslem</option>
-                <option value="native-american">Native American</option>
-            </select>
-        </p>
-        <p>
-            <label for="box1">
-                <span>Description: </span>
-                <strong><abbr title="required">*</abbr></strong>
-            </label>
-            <textarea name="comment" id="box1" rows="10" cols="80" maxlength="800" wrap="soft">How'd we meet?</textarea>
-        </p>
-        <p>
-            <label for="date">
-                <span>Today's date:</span>
-                <strong><abbr title="required">*</abbr></strong>
-                <em>USA mm/dd/yyyy</em>
-            </label>
-            <input type="text" id="date" name="date">
+            <label for="pwd">Password 2nd try: </label>
+            <input id="pwd" name="password" type="password" value="" required minlength="6" spellcheck="false">
         </p>
     </section>
+    <p>The rules for the password: 10 to 18 characters, 2 digits, 2 or more non-alpha, 2 or more capitals.</p>
+    <h2>Person's Characteristics</h2>
+    <section>
+        <h3>Title</h3>
+        <label for="title_1">
+            <input type="radio" id="title_1" name="title" value="Mr.">
+            Mr.<br>
+        </label>
+        <label for="title_2">
+            <input type="radio" id="title_2" name="title" value="Mrs.">
+            Mrs.
+        </label>
+    </section>
+    <p>
+        <label for="card">
+            <span>Race: <strong><abbr title="required">*</abbr></strong></span>
+        </label>
+        <select id="card" name="race">
+            <option value="caucasian-american">Caucasian American</option>
+            <option value="caucasian-european">Caucasian European</option>
+            <option value="caucasian-african">Caucasian African</option>
+            <option value="black-european">Black European</option>
+            <option value="black-american">Black American</option>
+            <option value="black-african">Black African</option>
+            <option value="asian">Asian</option>
+            <option value="south-american">South American</option>
+            <option value="greek">Greek</option>
+            <option value="middle-eastern-christian">Middle Eastern Christian</option>
+            <option value="middle-eastern-moslem">Middle Eastern Moslem</option>
+            <option value="native-american">Native American</option>
+        </select>
+    </p>
+    <p>
+        <label for="box1">Description <strong><abbr title="required">*</abbr></strong>: </label>
+        <textarea id="box1" name="comment" rows="10" cols="71" wrap="soft" maxlength="800"
+                  placeholder="How'd we meet?"></textarea>
+    </p>
+    <p>
+        <label for="date">Today's date (<em>USA mm/dd/yyyy</em>:) </label>
+        <input id="date" name="date" type="text" value="" required minlength="10" spellcheck="false">
+    </p>
     <section>
         <p>
-            <button type="submit" name="submit" value="Submit">Create Account</button>
+            <button type="submit" name="submit" value="Submit">Submit</button>
         </p>
     </section>
 </form>
