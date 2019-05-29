b3fb2c5a4d69cbb13ad27223c854cbf9022b5868
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-05-28 22:24:48 -0400

    Added an Abort button on adminpasscodegenerationform

--- app/GoodToKnow/Views/adminpasscodegenerationform.php
+++ app/GoodToKnow/Views/adminpasscodegenerationform.php
@@ -31,6 +31,7 @@
     </section>
     <section>
         <p>
+            <button type="submit" name="abort" value="Abort" class="abort">Abort</button>
             <button type="submit" name="submit" value="Submit">Submit</button>
         </p>
     </section>
