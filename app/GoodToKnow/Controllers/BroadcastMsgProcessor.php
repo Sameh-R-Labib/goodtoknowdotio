de0586bb69343166268ac3d0db2caa6086ce7ede
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-05-29 19:32:06 -0400

    Code which handles abort in BroadcastMsgProcessor

--- app/GoodToKnow/Controllers/BroadcastMsgProcessor.php
+++ app/GoodToKnow/Controllers/BroadcastMsgProcessor.php
@@ -36,6 +36,12 @@ class BroadcastMsgProcessor
             redirect_to("/ax1/Home/page");
         }
 
+        if (isset($_POST['abort']) AND $_POST['abort'] === "Abort") {
+            $sessionMessage .= " You have aborted the task you were working on! The session variables were reset. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
         /**
          * Verify that a string representing
          * the message was submitted.
