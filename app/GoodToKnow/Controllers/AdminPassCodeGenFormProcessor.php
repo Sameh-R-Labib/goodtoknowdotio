d6a1c42228faf98f489eee330f52e4b4c7545bd0
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-05-29 16:26:57 -0400

    Code which handles abort in AdminPassCodeGenFormProcessor

--- app/GoodToKnow/Controllers/AdminPassCodeGenFormProcessor.php
+++ app/GoodToKnow/Controllers/AdminPassCodeGenFormProcessor.php
@@ -25,6 +25,12 @@ class AdminPassCodeGenFormProcessor
             redirect_to("/ax1/Home/page");
         }
 
+        if (isset($_POST['abort']) AND $_POST['abort'] === "Abort") {
+            $sessionMessage .= " You have aborted the task you were working on! The session variables were reset. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
         /**
          * Do something with the submitted post data $_POST['choice']
          */
