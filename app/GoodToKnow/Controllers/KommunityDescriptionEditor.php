f0a520ef766b39328cf7634c686e3ec9761b4d81
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-05-31 15:36:01 -0400

    KommunityDescriptionEditorProcessor started

--- app/GoodToKnow/Controllers/KommunityDescriptionEditor.php
+++ app/GoodToKnow/Controllers/KommunityDescriptionEditor.php
@@ -8,6 +8,22 @@ class KommunityDescriptionEditor
 {
     public function page()
     {
+        global $is_logged_in;
+        global $is_admin;
+        global $sessionMessage;
 
+        if (!$is_logged_in || !$is_admin || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * Present a form which collects
+         * the community's name.
+         */
+
+        $html_title = "Community's Description Editor";
+
+        require VIEWS . DIRSEP . 'kommunitydescriptioneditor.php';
     }
 }
---
+++ app/GoodToKnow/Controllers/KommunityDescriptionEditorProcessor.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace GoodToKnow\Controllers;
+
+
+class KommunityDescriptionEditorProcessor
+{
+    public function page()
+    {
+
+    }
+}
