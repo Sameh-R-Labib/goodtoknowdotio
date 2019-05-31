e5d8c21835a8c42d0f3734f6833072738355cd14
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-05-31 16:01:08 -0400

    KommunityDescriptionEditorProcessor finished

--- app/GoodToKnow/Controllers/KommunityDescriptionEditorProcessor.php
+++ app/GoodToKnow/Controllers/KommunityDescriptionEditorProcessor.php
@@ -4,10 +4,60 @@
 namespace GoodToKnow\Controllers;
 
 
+use GoodToKnow\Models\Community;
+
+
 class KommunityDescriptionEditorProcessor
 {
     public function page()
     {
+        global $is_logged_in;
+        global $is_admin;
+        global $sessionMessage;
 
+        if (!$is_logged_in || !$is_admin || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        if (isset($_POST['abort']) AND $_POST['abort'] === "Abort") {
+            $sessionMessage .= " You have aborted the task you were working on! The session variables were reset. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * Goal:
+         *  1) Validate $_POST['community']
+         *  2) Save $_POST['community'] in the session.
+         *  3) Redirect to a route.
+         */
+
+        $submitted_community_name = (isset($_POST['username'])) ? $_POST['username'] : '';
+
+        if (trim($submitted_community_name) === "") {
+            $sessionMessage .= " Aborted because you have not submitted a community name. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $db = db_connect($sessionMessage);
+
+        if (!empty($sessionMessage) || $db === false) {
+            $sessionMessage .= ' Database connection failed. ';
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $community = Community::find_by_community_name($db, $sessionMessage, $submitted_community_name);
+        if (!$community) {
+            $sessionMessage .= " Unable to retrieve community object (possibly because the name you gave was invalid.) ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $_SESSION['saved_str01'] = $submitted_community_name;
+
+        redirect_to("/ax1/KommunityDescriptionEditorForm/page");
     }
 }
