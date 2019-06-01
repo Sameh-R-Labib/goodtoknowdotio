01eb64910f6d80c28ded6254679f6a20e3b8348d
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-06-01 15:39:29 -0400

    TopicDescriptionEditorProcessor completed

--- app/GoodToKnow/Controllers/TopicDescriptionEditorProcessor.php
+++ app/GoodToKnow/Controllers/TopicDescriptionEditorProcessor.php
@@ -22,5 +22,48 @@ class TopicDescriptionEditorProcessor
          *  3) Save the topic name in the session (we know what that is from the $special_topic_array.)
          *  4) Redirect to a function which will bring up the editor for the description.
          */
+        global $is_logged_in;
+        global $is_admin;
+        global $sessionMessage;
+        global $special_topic_array;
+
+        if (!$is_logged_in || !$is_admin || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        if (isset($_POST['abort']) AND $_POST['abort'] === "Abort") {
+            $sessionMessage .= " You have aborted the task you were working on! The session variables were reset. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * 1) Validate the submission.
+         */
+        $chosen_topic_id = (isset($_POST['choice'])) ? (int)$_POST['choice'] : 0;
+
+        // Make sure $chosen_topic_id is among the ids of $special_topic_array
+        if (!array_key_exists($chosen_topic_id, $special_topic_array)) {
+            $sessionMessage .= " I've encountered an unexpected error which is that the topic id was not found in topic
+             array. ";
+            $_SESSION['message'] .= $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * 2) Save the topic id in the session.
+         */
+        $_SESSION['saved_int01'] = $chosen_topic_id;
+
+        /**
+         * 3) Save the topic name in the session (we know what that is from the $special_topic_array.)
+         */
+        $_SESSION['saved_str01'] = $special_topic_array[$chosen_topic_id];
+
+        /**
+         * 4) Redirect to a function which will bring up the editor for the description.
+         */
+        redirect_to("/ax1/TopicDescriptionEditorForm/page");
     }
 }
