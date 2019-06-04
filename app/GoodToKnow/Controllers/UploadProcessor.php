ff07be7b3ac6e397268e517bbded0686081e074a
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-06-03 23:16:47 -0400

    I did the basics for processing the upload

---
+++ app/GoodToKnow/Controllers/UploadProcessor.php
@@ -0,0 +1,122 @@
+<?php
+
+
+namespace GoodToKnow\Controllers;
+
+
+class UploadProcessor
+{
+    public function page()
+    {
+        /**
+         * REPEATING:
+         * The goal of this series of routes
+         * is to make it possible to upload
+         * an image file (jpg, jpeg, png, gif)
+         * to the IMAGE folder on the server
+         * and to save its URL to the session
+         * variable called url_of_most_recent_upload.
+         * It shall do all this while making sure
+         * the upload contains no malicious code.
+         */
+
+        global $is_logged_in;
+        global $sessionMessage;
+
+        if (!$is_logged_in || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        if (isset($_POST['abort']) AND $_POST['abort'] === "Abort") {
+            $sessionMessage .= " You have aborted the task you were working on! The session variables were reset. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        if (!isset($_POST['submit'])) {
+            $sessionMessage .= " An invalid form submission occured. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * Start
+         */
+        $target_dir = IMAGE . "/";
+
+        /**
+         * Store in a var the file name (w/ its path)
+         * where the uploaded content will permanently
+         * live.
+         *
+         * NOTE: The entire old file name (w/ extension) is used for the new file name.
+         */
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+
+        /**
+         * Get a string value for the file type based
+         * on the file name's extension.
+         */
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+        /**
+         * Run the getimagesize function on the temporarily uploaded file.
+         * Doing so it will either return false if that file is not an image file
+         * or it will return an array which contains some useful information about
+         * the file including its mime type.
+         *
+         * NOTE: $_FILES["fileToUpload"]["tmp_name"]
+         *       is the temporarily uploaded file/path.
+         */
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+
+        /**
+         * Store the results of getimagesize() (i.e. $image_file_mime_type).
+         * Or kick ot if upload is NOT an image.
+         */
+        if ($check !== false) {
+            $image_file_mime_type = $check["mime"];
+        } else {
+            // File is not an image.
+            $sessionMessage .= " Error 546224. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * Check file size.
+         */
+        if ($_FILES["fileToUpload"]["size"] > 5767168) {
+            $sessionMessage .= " Sorry, your file is too large. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * Allow (only) certain file formats.
+         *
+         * NOTE: Although at this point we know it's an image we do NOT know it's a web image
+         */
+        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
+            $sessionMessage .= " Sorry, only JPG, JPEG, PNG & GIF files are allowed. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * Save the file if we are able to move it to its permanent location.
+         */
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            $sessionMessage .= " The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded. ";
+        } else {
+            $sessionMessage .= " Sorry, there was an error uploading your file. ";
+        }
+
+        /**
+         * Report outcome of this process.
+         */
+        $_SESSION['message'] = $sessionMessage;
+        redirect_to("/ax1/Home/page");
+    }
+}
