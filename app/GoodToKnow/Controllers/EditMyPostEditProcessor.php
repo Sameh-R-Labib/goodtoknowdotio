3e39d7a0e054b040c06ee13084b48d264d9e5110
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-06-04 16:11:41 -0400

    Editor gives link to post

--- app/GoodToKnow/Controllers/EditMyPostEditProcessor.php
+++ app/GoodToKnow/Controllers/EditMyPostEditProcessor.php
@@ -24,6 +24,9 @@ class EditMyPostEditProcessor
         global $sessionMessage;
         global $saved_str01;                // path for markdown file
         global $saved_str02;                // path for html file
+        global $community_id;
+        global $saved_int01;                // id of edited post's Topic
+        global $saved_int02;                // id of edited post
 
         if (!$is_logged_in || !empty($sessionMessage)) {
             $_SESSION['message'] = $sessionMessage;
@@ -116,7 +119,10 @@ class EditMyPostEditProcessor
          * Declare success.
          */
         $bytes_written_text = size_as_text($bytes_written);
-        $sessionMessage .= " Written <b>{$bytes_written_text}</b> (max 37.1 KB.) ";
+        $embedded_link_to_post = '<a href="/ax1/SetHomePageCommunityTopicPost/page/' . $community_id . '/' .
+            $saved_int01 . '/' . $saved_int02 . '">here </a>';
+        $sessionMessage .= " <b>{$bytes_written_text}</b> written (max allowed 37.1 KB.) Click
+         ➡️ {$embedded_link_to_post} ⬅️ to view your edited post. ";
         $_SESSION['message'] = $sessionMessage;
         $_SESSION['saved_int01'] = 0;
         $_SESSION['saved_int02'] = 0;
