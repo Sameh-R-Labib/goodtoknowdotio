b0b20cc7d48f340579eaa7e5104c7cd448bdda8a
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-06-04 14:07:38 -0400

    Touched up

--- app/GoodToKnow/ViewsIncludes/urlofmostrecentupload.php
+++ app/GoodToKnow/ViewsIncludes/urlofmostrecentupload.php
@@ -1,16 +1,14 @@
 <?php if ($url_of_most_recent_upload): ?>
     <p>
         <small>
-            ▶️ Your Most Recent Uploaded Image URL: <?php /** @noinspection PhpUndefinedVariableInspection */
-            echo $url_of_most_recent_upload; ?> ◀️ ️✂️ Copy and 📋 Paste
+            ▶️ Your Most Recent Uploaded Image URL: [ <?php /** @noinspection PhpUndefinedVariableInspection */
+            echo $url_of_most_recent_upload; ?> ] ◀️ ️✂️ + 📋
         </small>
-
     </p>
 <?php else: ?>
     <p>
         <small>
-            ▶️ Your Most Recent Uploaded Image URL: [none during this session]
+            ▶️ Your Most Recent Uploaded Image URL: [none during this session] ◀️
         </small>
-
     </p>
 <?php endif; ?>
