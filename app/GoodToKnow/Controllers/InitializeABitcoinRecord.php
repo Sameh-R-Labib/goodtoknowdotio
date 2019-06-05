d21c0a4db6b7ae6c6b6072fefe1e8f51ae1c9489
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-06-05 16:58:21 -0400

    InitializeABitcoinRecord comments at top

--- app/GoodToKnow/Controllers/InitializeABitcoinRecord.php
+++ app/GoodToKnow/Controllers/InitializeABitcoinRecord.php
@@ -8,6 +8,21 @@ class InitializeABitcoinRecord
 {
     public function page()
     {
+        /**
+         * This feature enables any user to
+         * create a database record in the
+         * bitcoin table. The process will
+         * ask the user to ONLY supply a bitcoin
+         * address and the remaining field values
+         * will be supplied by an editor for
+         * the record.
+         */
 
+        /**
+         * This script simply presents a form
+         * for the user to supply the bitcoin
+         * address for the to be created bitcoin
+         * record.
+         */
     }
 }
