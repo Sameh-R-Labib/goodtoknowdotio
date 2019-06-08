37d02a967b655a59d3723388a7fce354b9dd8279
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-06-08 16:57:19 -0400

    EditABitcoinRecordProcessor class w/o a view yet

--- app/GoodToKnow/Controllers/EditABitcoinRecordProcessor.php
+++ app/GoodToKnow/Controllers/EditABitcoinRecordProcessor.php
@@ -4,6 +4,8 @@
 namespace GoodToKnow\Controllers;
 
 
+use GoodToKnow\Models\Bitcoin;
+
 class EditABitcoinRecordProcessor
 {
     public function page()
@@ -29,6 +31,41 @@ class EditABitcoinRecordProcessor
             redirect_to("/ax1/Home/page");
         }
 
+        /**
+         * 1) Store the submitted bitcoin record id in the session.
+         */
+        $chosen_id = (isset($_POST['choice'])) ? (int)$_POST['choice'] : 0;
+        if ($chosen_id == 0) {
+            $sessionMessage .= " You didn't choose so I've aborted the process for you. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+        $_SESSION['saved_int01'] = $chosen_id;
 
+        /**
+         * 2) Retrieve the object with that id from the database.
+         */
+        $db = db_connect($sessionMessage);
+        if (!empty($sessionMessage) || $db === false) {
+            $sessionMessage .= ' Database connection failed. ';
+            $_SESSION['message'] = $sessionMessage;
+            $_SESSION['saved_int01'] = 0;
+            redirect_to("/ax1/Home/page");
+        }
+        $bitcoin_object = Bitcoin::find_by_id($db, $sessionMessage, $chosen_id);
+        if (!$bitcoin_object) {
+            $sessionMessage .= " Unexpectedly I could not find that bitcoin record. ";
+            $_SESSION['message'] = $sessionMessage;
+            $_SESSION['saved_int01'] = 0;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * 3) Present a form which is populated with data from the object.
+         *    (except do the bitcoin address should not be changeable.)
+         */
+        $html_title = 'Edit the bitcoin record';
+
+        require VIEWS . DIRSEP . 'editabitcoinrecordprocessor.php';
     }
 }
