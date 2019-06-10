b1ce8744e44aa9922a1058b742d4da081a8204d2
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-06-10 15:46:40 -0400

    EditABitcoinRecordSubmit started

--- app/GoodToKnow/Controllers/EditABitcoinRecordSubmit.php
+++ app/GoodToKnow/Controllers/EditABitcoinRecordSubmit.php
@@ -15,5 +15,23 @@ class EditABitcoinRecordSubmit
          * 3) Modify the retrieved record by updating it with the submitted data.
          * 4) Update/save the updated record in the database.
          */
+
+        global $is_logged_in;
+        global $sessionMessage;
+
+        if (!$is_logged_in || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        if (isset($_POST['abort']) AND $_POST['abort'] === "Abort") {
+            $sessionMessage .= " You've aborted the task! Session variables reset. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         *
+         */
     }
 }
