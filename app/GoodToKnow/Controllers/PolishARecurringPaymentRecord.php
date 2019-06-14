c9f705f7a60e2c93b53ab32f65ed2efa036f80d9
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-06-14 15:11:51 -0400

    PolishARecurringPaymentRecord class and view

--- app/GoodToKnow/Controllers/PolishARecurringPaymentRecord.php
+++ app/GoodToKnow/Controllers/PolishARecurringPaymentRecord.php
@@ -4,12 +4,53 @@
 namespace GoodToKnow\Controllers;
 
 
+use GoodToKnow\Models\RecurringPayment;
+
 class PolishARecurringPaymentRecord
 {
     public function page()
     {
         /**
+         * This feature is for editing/updating
+         * a RecurringPayment record.
          *
+         * This route is for presenting a
+         * form for getting the user to tell us
+         * which RecurringPayment record he wants to edit.
+         * It will present a series of radio
+         * buttons to choose from.
          */
+
+        global $is_logged_in;
+        global $sessionMessage;
+        global $user_id;            // We need this.
+
+        if (!$is_logged_in || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $db = db_connect($sessionMessage);
+        if (!empty($sessionMessage) || $db === false) {
+            $sessionMessage .= ' Database connection failed. ';
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * Get an array of Bitcoin objects
+         * belonging to the current user.
+         */
+        $sql = 'SELECT * FROM `recurring_payment` WHERE `user_id` = "' . $db->real_escape_string($user_id) . '"';
+        $array_of_recurring_payment_objects = RecurringPayment::find_by_sql($db, $sessionMessage, $sql);
+        if (!$array_of_recurring_payment_objects || !empty($sessionMessage)) {
+            $sessionMessage .= ' 🤔 I could NOT find any recurring_payment records for you ¯\_(ツ)_/¯. ';
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $html_title = 'Which recurring_payment record?';
+
+        require VIEWS . DIRSEP . 'polisharecurringpaymentrecord.php';
     }
 }
---
+++ app/GoodToKnow/Views/polisharecurringpaymentrecord.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/editor.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <title><?php /** @noinspection PhpUndefinedVariableInspection */
+        echo $html_title; ?></title>
+</head>
+<body>
+<form action="/ax1/PolishARecurringPaymentRecordProcessor/page" method="post">
+    <h2>Which RecurringPayment Record?</h2>
+    <?php require SESSIONMESSAGE; ?>
+    <p>These are listed by RecurringPayment label.</p>
+    <section>
+        <?php /** @noinspection PhpUndefinedVariableInspection */
+        foreach ($array_of_recurring_payment_objects as $key => $rp_object): ?>
+            <label for="c<?php echo $key; ?>" class="radio">
+                <input type="radio" id="c<?php echo $key; ?>" name="choice" value="<?php echo $rp_object->id; ?>">
+                <?php echo $rp_object->label; ?><br>
+            </label>
+        <?php endforeach; ?>
+    </section>
+    <section>
+        <p>
+            <button type="submit" name="abort" value="Abort" class="abort">Abort</button>
+            <button type="submit" name="submit" value="Submit">Submit</button>
+        </p>
+    </section>
+</form>
+</body>
