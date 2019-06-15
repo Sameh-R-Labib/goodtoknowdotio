586e024df442f1a91ec51bc9149d5b4983a8cb16
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-06-15 19:23:48 -0400

    Added more h2 to CP pages

--- app/GoodToKnow/Views/adminhome.php
+++ app/GoodToKnow/Views/adminhome.php
@@ -78,21 +78,22 @@
         <li><a href="/ax1/TopicDescriptionEditor/page">Topic Description Editor</a></li>
     </ul>
     <hr>
+    <h2 class="topofpage">Instruction to users</h2>
     <p>When writing a post, let it be your goal to concisely write in a manner which helps the reader wrap their head
-        around the
-        <em>community's</em> subject matter. Where I said "<em>community</em>" I mean as in our hierarchy: community
-        &Rarr; topic
-        &Rarr; post.</p>
+        around the <em>community's</em> subject matter. Where I said "<em>community</em>" I mean as in our hierarchy:
+        community &Rarr; topic &Rarr; post.</p>
+    <hr>
+    <h2 class="topofpage">Photo Gallery</h2>
     <figure>
         <a href="http://www.fsf.org/associate/support_freedom/join_fsf?referrer=2442">
-            <img class="photo" alt="Badge" src="/1944235.png" title="Membership Badge"/>
+            <img class="photo" alt="Badge" src="/1944235.png" title="Membership Badge">
         </a>
         <figcaption>My Membership Badge</figcaption>
     </figure>
     <figure>
         <a href="http://www.fsf.org/associate/support_freedom/join_fsf?referrer=2442">
             <img class="photo" alt="Support freedom" src="//static.fsf.org/fsforg/img/normal-image.png"
-                 title="Help protect your freedom, join the Free Software Foundation"/></a>
+                 title="Help protect your freedom, join the Free Software Foundation"></a>
         <figcaption>I joined the FSF because I use the GNU Affero version 3 license</figcaption>
     </figure>
     <figure>
