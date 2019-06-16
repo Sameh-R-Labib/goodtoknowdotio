50f39b16cbc9937b8c1e4b405cc9ac5aaaf7da5f
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-06-16 18:16:26 -0400

    you'd

--- app/GoodToKnow/Views/whatisthissite.php
+++ app/GoodToKnow/Views/whatisthissite.php
@@ -49,9 +49,9 @@
         The software is meant to facilitate the parsing and sharing of practical useful information.
         This is for sharing amongst family and trusted acquaintances. Each GoodToKnow.io instance is run by at least one
         admin who is in charge of site maintenance and approval of content.</p>
-    <p>If you'ld like to become a member of this instance then contact its admin.
+    <p>If you'd like to become a member of this instance then contact its admin.
         If you stumble across a different instance you can ask its admin to join.
-        Otherwise, if you'ld like to create your own instance you can either use the source code yourself found at <a
+        Otherwise, if you'd like to create your own instance you can either use the source code yourself found at <a
                 href="https://github.com/Sameh-R-Labib/goodtoknowdotio" target="_blank">https://github.com/Sameh-R-Labib/goodtoknowdotio</a>
         or you could contact another admin and persuade them to help you set it up. You'll find the email address
         of each instance's admin on the login page/form.
