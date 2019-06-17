1721819179346566d8aebc59589befa8ca0fed0f
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-06-17 11:02:39 -0400

    Replaced admintopbar with topbar

--- app/GoodToKnow/Views/adminhome.php
+++ app/GoodToKnow/Views/adminhome.php
@@ -15,8 +15,8 @@
         echo $html_title; ?></title>
 </head>
 <body>
-<!-- adminhometop -->
-<div id="adminhometop">
+<!-- topbar -->
+<div id="topbar">
     <a href="/ax1"><img src="/good1.jpg" alt="GoodToKnow.io" height="70" width="302" style="float: left"></a>
     <div id="sendmessage"><?php require SENDMESSAGELINK; ?></div>
     <div id="inboxlink"><p><a href="/ax1/Inbox/page">🎫 inbox</a></p></div>
