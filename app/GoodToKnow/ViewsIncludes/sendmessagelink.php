cc78401c005eb890a6640480ff5eed3bcc43bba9
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-06-17 09:36:42 -0400

    Big sickle and hammer

--- app/GoodToKnow/ViewsIncludes/sendmessagelink.php
+++ app/GoodToKnow/ViewsIncludes/sendmessagelink.php
@@ -1,7 +1,7 @@
 <?php if ($is_admin): ?>
     <p><a href="/ax1/BroadcastMsg/page">🌏🌎🌍👲 users</a></p>
 <?php elseif (isset($is_guest) && $is_guest): ?>
-    <p>✊🤬😭&#x2192;🗣☭Ⓐ</p>
+    <p>✊🤬😭&#x2192;🗣<big>☭</big>Ⓐ</p>
 <?php else: ?>
     <p><a href="/ax1/WriteToAdmin/page">♠👔♠ admin</a></p>
 <?php endif; ?>
