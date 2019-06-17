f859d061b2e72c6e106d403bc607f8e3a6eec3e7
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-06-17 10:02:14 -0400

    maincontent blockquote margin-left: 132px; margin-right: 153px;

--- app/GoodToKnow/Views/whatisthissite.php
+++ app/GoodToKnow/Views/whatisthissite.php
@@ -50,8 +50,8 @@
         This is for sharing amongst family and trusted acquaintances.
         Additionally it has features which facilitate tracking things which one would normally track in one's own
         life such as recurring payments, banking transactions and recurring to-do list items.</p>
-    <blockquote class="blockquote">GoodToKnow.io instance is run by at least one
-        admin who is in charge of site maintenance and approval of content.
+    <blockquote>GoodToKnow.io instance is run by at least one admin who is in charge of site maintenance and approval of
+        content.
     </blockquote>
     <p>If you'd like to become a member of this instance then contact its admin.
         If you stumble across a different instance you can ask its admin to join.
