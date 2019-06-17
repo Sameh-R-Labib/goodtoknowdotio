bbbd3e8fcf6b89aa6aeb17e88dd576176769f183
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-06-17 09:54:21 -0400

    Added a blockquote

--- app/GoodToKnow/Views/whatisthissite.php
+++ app/GoodToKnow/Views/whatisthissite.php
@@ -47,8 +47,12 @@
     <h1>Welcome</h1>
     <p>This site is an instance of the GoodToKnow.io application. The code is GPLv3 Affero licensed FLOSS software.
         The software is meant to facilitate the parsing and sharing of practical useful information.
-        This is for sharing amongst family and trusted acquaintances. Each GoodToKnow.io instance is run by at least one
-        admin who is in charge of site maintenance and approval of content.</p>
+        This is for sharing amongst family and trusted acquaintances.
+        Additionally it has features which facilitate tracking things which one would normally track in one's own
+        life such as recurring payments, banking transactions and recurring to-do list items.</p>
+    <blockquote class="blockquote">GoodToKnow.io instance is run by at least one
+        admin who is in charge of site maintenance and approval of content.
+    </blockquote>
     <p>If you'd like to become a member of this instance then contact its admin.
         If you stumble across a different instance you can ask its admin to join.
         Otherwise, if you'd like to create your own instance you can either use the source code yourself found at <a
