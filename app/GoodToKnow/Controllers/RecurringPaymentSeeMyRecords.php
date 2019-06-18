b8f7d9695625dd3b36e6b6aaf1f88fe82dba651e
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-06-18 18:00:17 -0400

    RecurringPaymentSeeMyRecords class but no view yet

--- app/GoodToKnow/Controllers/RecurringPaymentSeeMyRecords.php
+++ app/GoodToKnow/Controllers/RecurringPaymentSeeMyRecords.php
@@ -4,12 +4,77 @@
 namespace GoodToKnow\Controllers;
 
 
+use GoodToKnow\Models\RecurringPayment;
+
 class RecurringPaymentSeeMyRecords
 {
     public function page()
     {
         /**
-         *
+         * Similar to BitcoinSeeMyRecords.
          */
+
+        global $user_id;
+        global $sessionMessage;
+        global $is_logged_in;
+        global $special_community_array;
+        global $type_of_resource_requested;
+        global $is_admin;
+
+        if (!$is_logged_in || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $db = db_connect($sessionMessage);
+        if (!empty($sessionMessage) || $db === false) {
+            $sessionMessage .= ' Database connection failed. ';
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $html_title = 'Show Me my Recurring Payments';
+
+        $page = 'RecurringPaymentSeeMyRecords';
+
+        $show_poof = true;
+
+        /**
+         * Get an array of RecurringPayment objects for the user who has id == $user_id.
+         */
+        $sql = 'SELECT * FROM `recurring_payment` WHERE `user_id` = "' . $db->real_escape_string($user_id) . '"';
+        $array_of_recurring_payment_objects = RecurringPayment::find_by_sql($db, $sessionMessage, $sql);
+        if (!$array_of_recurring_payment_objects || !empty($sessionMessage)) {
+            $sessionMessage .= ' 🤔 I could NOT find any recurring payments for you ¯\_(ツ)_/¯. ';
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * Loop through the array and replace some attributes with more readable versions of themselves.
+         */
+        foreach ($array_of_recurring_payment_objects as $object) {
+            $object->unix_time_at_last_payment = self::get_readable_time($object->unix_time_at_last_payment);
+            $object->comment = nl2br($object->comment, false);
+            // Add comma for thousands but keep the number of decimal places at 8 just in case the currency is a crypto.
+            $object->amount_paid = number_format($object->amount_paid, 8);
+        }
+
+        $sessionMessage .= ' Enjoy ʘ‿ʘ at your 🌀 💳 📽s. ';
+
+        require VIEWS . DIRSEP . 'recurringpaymentseemyrecords.php';
+    }
+
+    /**
+     * @param \mysqli $db
+     * @param string $error
+     * @param $created
+     * @return string
+     */
+    public static function get_readable_time($created)
+    {
+        $created = (int)$created;
+        $date = date('m/d/Y h:ia ', $created) . "<small>[" . date_default_timezone_get() . "]</small>";
+        return $date;
     }
 }
