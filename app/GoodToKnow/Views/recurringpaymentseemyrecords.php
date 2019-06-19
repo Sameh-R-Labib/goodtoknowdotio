6f5ce9eb829730283136ed2a069b11e7d8c45f7f
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-06-18 22:04:30 -0400

    RecurringPaymentSeeMyRecords view completed

--- app/GoodToKnow/Views/recurringpaymentseemyrecords.php
+++ app/GoodToKnow/Views/recurringpaymentseemyrecords.php
@@ -1,1 +1,75 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <title><?php /** @noinspection PhpUndefinedVariableInspection */
+        echo $html_title; ?></title>
+</head>
+<body>
+<!-- topbar -->
+<div id="topbar">
+    <a href="/ax1"><img src="/good1.jpg" alt="GoodToKnow.io" height="70" width="302" style="float: left"></a>
+    <div id="sendmessage"><?php require SENDMESSAGELINK; ?></div>
+    <div id="inboxlink"><p><a href="/ax1/ByUsernameMessage/page">U/N 📧 👲</a></p></div>
+    <div id="logindiv"><p><a href="/ax1/Logout/page">👋 log out</a></p></div>
+</div>
+<!-- communities -->
+<div id="communities">
+    <p>👪:&nbsp;&nbsp;<?php require COMMUNITIESFORTHISUSER; ?></p>
+</div>
+<!-- breadcrumbs -->
+<div id="breadcrumbs">
+    <p><a href="/ax1/Home/page">⛺</a>:&nbsp;&nbsp;<?php require BREADCRUMBS; ?></p>
+</div>
+<!-- scriptoutput -->
+<div id="scriptoutput">
+    <div id="leftsodiv">
+        <div id="sendtoauthor"><?php require MESSAGETHEAUTHOR; ?></p></div>
+        <div id="admindiv">
+            <?php require CONTROLPANELLINK; ?>
+        </div>
+    </div>
+    <div id="scriptmessage">
+        <?php require SESSIONMESSAGE; ?>
+    </div>
+</div>
+<!-- maincontent -->
+<div id="maincontent">
+    <?php if (!empty($array_of_recurring_payment_objects)): ?>
+        <?php $last = count($array_of_recurring_payment_objects) - 1; ?>
+        <?php foreach ($array_of_recurring_payment_objects as $key => $object): ?>
+            <p><b>Label: </b><?php echo $object->label; ?></p>
+            <p><b>Last payment's time: </b><?php echo $object->unix_time_at_last_payment; ?></p>
+            <p><b>Currency used: </b>$<?php echo $object->currency; ?></p>
+            <p><b>Amount paid: </b>₿<?php echo $object->amount_paid; ?></p>
+            <p>&nbsp;</p>
+            <p><?php echo $object->comment; ?></p>
+            <?php if ($key != $last): ?>
+                <hr>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <p>There are no recurring payments for you.</p>
+    <?php endif; ?>
+</div><!-- End maincontent -->
+<!-- footerbar -->
+<div id="footerbar">
+    <p align="center" style="font-size: 1em;">
+        <img src="/Gnu-head-30-years-anniversary.svg" style="float:left;height: 32px;width: 32px;margin-top: -6px">
+        © 2018 - Sameh Ramzy Labib
+        <img src="/2000px-GPLv3_Logo.svg.png"
+             height="32" width="70"
+             style="float:right;;margin-top: -6px"></p>
+</div>
+<script src="/js/script.js"></script>
+</body>
+</html>
--- app/GoodToKnow/ViewsIncludes/breadcrumbs.php
+++ app/GoodToKnow/ViewsIncludes/breadcrumbs.php
@@ -1,11 +1,14 @@
-<?php if ($page === 'Inbox') echo "<a href=\"/ax1/Inbox/page\">Inbox</a>";
+<?php
+
+if ($page === 'Inbox') echo "<a href=\"/ax1/Inbox/page\">Inbox</a>";
 elseif ($page === 'Admin') echo "<a href=\"/ax1/AdminHome/page\">Admin Home</a>";
 elseif ($page === 'CP') echo "<a href=\"/ax1/ControlPanel/page\">Control Panel</a>";
 elseif ($page === 'UserRoster') echo "<a href=\"/ax1/UserRoster/page\">User Roster</a>";
 elseif ($page === 'BitcoinSeeMyRecords') echo "<a href=\"/ax1/BitcoinSeeMyRecords/page\">My Bitcoin Records</a>";
 elseif ($page === 'About') echo "What is this site?";
+elseif ($page === 'RecurringPaymentSeeMyRecords') echo "<a href=\"/ax1/RecurringPaymentSeeMyRecords/page\">My Recurring Payments</a>";
 else {
     require CURRENTCOMMUNITY;
     require CURRENTTOPIC;
     require CURRENTPOST;
-} ?>
+}
