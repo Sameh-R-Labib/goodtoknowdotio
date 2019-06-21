7f29fabf721dc97b63edbbe06f7462787a20b827
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-06-21 17:36:42 -0400

    GenerateABankingAccountForBalances page()

--- app/GoodToKnow/Controllers/GenerateABankingAccountForBalances.php
+++ app/GoodToKnow/Controllers/GenerateABankingAccountForBalances.php
@@ -9,7 +9,32 @@ class GenerateABankingAccountForBalances
     public function page()
     {
         /**
-         *
+         * This feature enables any user to
+         * create a database record in the
+         * banking_acct_for_balances table. The process will
+         * ask the user to ONLY supply a banking_acct_for_balances
+         * acct_name and the remaining field values
+         * will be supplied by an (not included)
+         * editor for the record.
          */
+
+        /**
+         * This here script simply presents a
+         * form for the user to supply the banking_acct_for_balances
+         * acct_name for the "to be created" banking_acct_for_balances
+         * record.
+         */
+
+        global $is_logged_in;
+        global $sessionMessage;
+
+        if (!$is_logged_in || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $html_title = 'Create a New BankingAcctForBalances Record';
+
+        require VIEWS . DIRSEP . 'generateabankingaccountforbalances.php';
     }
 }
