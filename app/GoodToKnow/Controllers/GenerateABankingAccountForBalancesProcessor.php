b9a7ebba064e9b8f45610e63357aa88a37e5dded
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-06-22 14:00:04 -0400

    GenerateABankingAccountForBalancesProcessor started

---
+++ app/GoodToKnow/Controllers/GenerateABankingAccountForBalancesProcessor.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace GoodToKnow\Controllers;
+
+
+class GenerateABankingAccountForBalancesProcessor
+{
+    public function page()
+    {
+        /**
+         * Create a database record in the banking_acct_for_balances
+         * table using the banking_acct_for_balances
+         * acct_name. The remaining field values
+         * will be set to default values.
+         *
+         * $_POST['acct_name']
+         */
+    }
+}
