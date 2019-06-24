b11eb4e3e1c7404355bb5b16545c5ba46e491a8d
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-06-24 14:59:23 -0400

    PopulateABankingAccountForBalancesSubmit stub

---
+++ app/GoodToKnow/Controllers/PopulateABankingAccountForBalancesSubmit.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace GoodToKnow\Controllers;
+
+
+class PopulateABankingAccountForBalancesSubmit
+{
+    public function page()
+    {
+        /**
+         *
+         */
+    }
+}
