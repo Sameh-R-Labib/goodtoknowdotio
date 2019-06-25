766e8c857dce74c0056abf958831d4b738442f42
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-06-25 08:52:09 -0400

    ViewAllBankingAccountsForBalances link and stub

--- app/GoodToKnow/Views/controlpanel.php
+++ app/GoodToKnow/Views/controlpanel.php
@@ -56,14 +56,15 @@
         <li><a href="/ax1/AuthorDeletesOwnPost/page">Delete One of My 📄s</a></li>
         <li><a href="/ax1/InitializeABitcoinRecord/page">Create a ₿ 📽</a></li>
         <li><a href="/ax1/EditABitcoinRecord/page">Edit a ₿ 📽</a></li>
-        <li><a href="/ax1/BitcoinSeeMyRecords/page">See ₿ 📽s</a></li>
+        <li><a href="/ax1/BitcoinSeeMyRecords/page">See all ₿ 📽s</a></li>
         <li><a href="/ax1/DeleteABitcoinRecord/page">Delete a ₿ 📽</a></li>
         <li><a href="/ax1/MakeARecurringPaymentRecord/page">Create a 🌀 💳 📽</a></li>
         <li><a href="/ax1/PolishARecurringPaymentRecord/page">Edit a 🌀 💳 📽</a></li>
-        <li><a href="/ax1/RecurringPaymentSeeMyRecords/page">See 🌀 💳 📽s</a></li>
+        <li><a href="/ax1/RecurringPaymentSeeMyRecords/page">See all 🌀 💳s 📽s</a></li>
         <li><a href="/ax1/ExpungeARecurringPaymentRecord/page">Delete a 🌀 💳 📽</a></li>
         <li><a href="/ax1/GenerateABankingAccountForBalances/page">Create a 🏦 📒 for ⚖️s️</a></li>
         <li><a href="/ax1/PopulateABankingAccountForBalances/page">Edit a 🏦 📒 for ⚖️s</a></li>
+        <li><a href="/ax1/ViewAllBankingAccountsForBalances/page">See all 🏦 📒s for ⚖️s</a></li>
     </ul>
     <hr>
     <h2 class="topofpage">Instruction to users</h2>
