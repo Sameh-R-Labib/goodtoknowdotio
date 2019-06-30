f89f4847a4a663355cdf77a8ca53d00c145227ce
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-06-30 18:03:01 -0400

    BankingTransactionForBalances Model created

---
+++ app/GoodToKnow/Models/BankingTransactionForBalances.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace GoodToKnow\Models;
+
+
+class BankingTransactionForBalances extends GoodObject
+{
+    /**
+     * @var string
+     */
+    protected static $table_name = "banking_transaction_for_balances";
+
+    /**
+     * @var array
+     */
+    protected static $fields = ['id', 'user_id', 'bank_id ', 'label', 'amount', 'time'];
+
+    /**
+     * @var int
+     */
+    public $id;
+
+    /**
+     * @var int
+     */
+    public $user_id;
+
+    /**
+     * @var int
+     */
+    public $bank_id;
+
+    /**
+     * @var string
+     */
+    public $label;
+
+    /**
+     * @var float
+     */
+    public $amount;
+
+    /**
+     * @var int
+     */
+    public $time;
+
+    /**
+     * Not part of the database record.
+     *
+     * @var float
+     */
+    public $balance;
+}
