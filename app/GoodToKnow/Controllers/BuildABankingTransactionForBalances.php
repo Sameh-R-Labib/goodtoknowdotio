a2d91fbab5ab4c589e52eba6d320566b93ff2f71
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-07-03 20:52:33 -0400

    BuildABankingTransactionForBalances page() created

--- app/GoodToKnow/Controllers/BuildABankingTransactionForBalances.php
+++ app/GoodToKnow/Controllers/BuildABankingTransactionForBalances.php
@@ -9,7 +9,32 @@ class BuildABankingTransactionForBalances
     public function page()
     {
         /**
-         *
+         * This feature enables any user to
+         * create a database record in the
+         * banking_transaction_for_balances table. The process will
+         * ask the user to ONLY supply a banking_transaction_for_balances
+         * label + time . And the remaining field values
+         * will be supplied by an (not included)
+         * editor for the record.
          */
+
+        /**
+         * This here script simply presents a
+         * form for the user to supply the banking_transaction_for_balances
+         * label + time for the "to be created" banking_transaction_for_balances
+         * record.
+         */
+
+        global $is_logged_in;
+        global $sessionMessage;
+
+        if (!$is_logged_in || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $html_title = 'Create a New Banking Transaction For Balances';
+
+        require VIEWS . DIRSEP . 'buildabankingtransactionforbalances.php';
     }
 }
