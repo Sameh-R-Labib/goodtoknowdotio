910e88e2ca65959e46dadabe4a35b9a59f06ebd0
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-07-05 12:07:16 -0400

    RevampABankingTransactionForBalances page()

--- app/GoodToKnow/Controllers/RevampABankingTransactionForBalances.php
+++ app/GoodToKnow/Controllers/RevampABankingTransactionForBalances.php
@@ -9,7 +9,22 @@ class RevampABankingTransactionForBalances
     public function page()
     {
         /**
-         *
+         * This page is going to present some radio
+         * buttons for answering the question of which
+         * time range the user wants to see further
+         * choices of transactions for.
          */
+
+        global $is_logged_in;
+        global $sessionMessage;
+
+        if (!$is_logged_in || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $html_title = 'Which time range for filtering your transaction choices?';
+
+        require VIEWS . DIRSEP . 'revampabankingtransactionforbalances.php';
     }
 }
