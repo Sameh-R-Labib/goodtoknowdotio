c8e42e68f9155e550c76ce35089f70c8265e200e
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-07-05 16:30:58 -0400

    RevampABankingTransactionForBalances view

---
+++ app/GoodToKnow/Controllers/RevampABankingAccountForBalancesTimeRange.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace GoodToKnow\Controllers;
+
+
+class RevampABankingAccountForBalancesTimeRange
+{
+    public function page()
+    {
+        /**
+         *
+         */
+    }
+}
