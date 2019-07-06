a9c96e9049f69a17d236e130542452ba115ab647
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-07-06 19:18:23 -0400

    Save

--- app/GoodToKnow/Controllers/RevampABankingAccountForBalancesTimeRange.php
+++ app/GoodToKnow/Controllers/RevampABankingAccountForBalancesTimeRange.php
@@ -9,7 +9,23 @@ class RevampABankingAccountForBalancesTimeRange
     public function page()
     {
         /**
-         *
+         * 1) Validate the submitted choice of time range (A,B,C,D,E.)
+         * 2) Calculate the min and mac times of the requested rang.
+         * 3) Store the min and max in session variables.
          */
+
+        global $is_logged_in;
+        global $sessionMessage;
+
+        if (!$is_logged_in || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        if (isset($_POST['abort']) AND $_POST['abort'] === "Abort") {
+            $sessionMessage .= " You've aborted the task! Session variables reset. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
     }
 }
