89fbacaccd8bb71f374a6cd0f564363c2625468a
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-07-08 19:24:05 -0400

    RevampABankingAccountForBalancesChooseRecord page() completed

---
+++ app/GoodToKnow/Controllers/RevampABankingAccountForBalancesChooseRecord.php
@@ -0,0 +1,69 @@
+<?php
+
+
+namespace GoodToKnow\Controllers;
+
+
+use GoodToKnow\Models\BankingTransactionForBalances;
+
+class RevampABankingAccountForBalancesChooseRecord
+{
+    public function page()
+    {
+        /**
+         * This function will:
+         *  1) Retrieve the user's BankingTransactionForBalances within the time range.
+         *  2) Present them as choices.
+         *
+         * The ultimate goal is to present a BankingTransactionForBalances for editing.
+         */
+
+        global $is_logged_in;
+        global $sessionMessage;
+        global $user_id;
+        global $saved_int01;     // min time
+        global $saved_int02;     // max time
+
+        if (!$is_logged_in || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        if (isset($_POST['abort']) AND $_POST['abort'] === "Abort") {
+            $sessionMessage .= " You've aborted the task! Session variables reset. ";
+            $_SESSION['message'] = $sessionMessage;
+            $_SESSION['saved_int01'] = 0;
+            $_SESSION['saved_int02'] = 0;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $db = db_connect($sessionMessage);
+        if (!empty($sessionMessage) || $db === false) {
+            $sessionMessage .= ' Database connection failed. ';
+            $_SESSION['message'] = $sessionMessage;
+            $_SESSION['saved_int01'] = 0;
+            $_SESSION['saved_int02'] = 0;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * Get an array of BankingTransactionForBalances objects belonging to the user
+         * and falling within the prescribed time range.
+         */
+        $sql = 'SELECT * FROM `banking_transaction_for_balances` WHERE `user_id` = "' . $db->real_escape_string($user_id) . '"';
+        $sql .= ' AND `time` BETWEEN "' . $db->real_escape_string($saved_int02) . '" AND "' . $db->real_escape_string($saved_int01) . '"';
+        $sql .= ' ORDER BY `time`';
+        $array = BankingTransactionForBalances::find_by_sql($db, $sessionMessage, $sql);
+        if (!$array || !empty($sessionMessage)) {
+            $sessionMessage .= ' 🤔 I could NOT find any banking_transaction_for_balances records for you ¯\_(ツ)_/¯. ';
+            $_SESSION['message'] = $sessionMessage;
+            $_SESSION['saved_int01'] = 0;
+            $_SESSION['saved_int02'] = 0;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $html_title = 'Which banking_transaction_for_balances record?';
+
+        require VIEWS . DIRSEP . 'revampabankingaccountforbalanceschooserecord.php';
+    }
+}
