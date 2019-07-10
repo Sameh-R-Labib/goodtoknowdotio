54c7f59d7dba4eca350b8dd412120aed2e3af6ea
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-07-10 12:33:58 -0400

    RevampABankingTransactionForBalancesEdit view completed

--- app/GoodToKnow/Views/populateabankingaccountforbalancesprocessor.php
+++ app/GoodToKnow/Views/populateabankingaccountforbalancesprocessor.php
@@ -23,27 +23,24 @@
     <p>
         <label for="acct_name">Account Name (✅ emoji): </label>
         <input id="acct_name" name="acct_name" type="text"
-               value="<?php /** @noinspection PhpUndefinedVariableInspection */
-               echo $object->acct_name; ?>" required minlength="3" maxlength="30" size="30" spellcheck="false"
-               placeholder="Personal Credit Card">
+               value="<?php echo $object->acct_name; ?>" required minlength="3" maxlength="30" size="30"
+               spellcheck="false" placeholder="Personal Credit Card">
     </p>
     <p>
         <label for="start_time">Unix time at Beginning: </label>
         <input id="start_time" name="start_time" type="text"
-               value="<?php /** @noinspection PhpUndefinedVariableInspection */
-               echo $object->start_time; ?>" minlength="10" maxlength="22" size="22" placeholder="1560190617">
+               value="<?php echo $object->start_time; ?>" minlength="10" maxlength="22" size="22"
+               placeholder="1560190617">
     </p>
     <p>
         <label for="start_balance">Balance at Beginning: </label>
         <input id="start_balance" name="start_balance" type="text"
-               value="<?php /** @noinspection PhpUndefinedVariableInspection */
-               echo $object->start_balance; ?>" required minlength="1" maxlength="16" size="16">
+               value="<?php echo $object->start_balance; ?>" required minlength="1" maxlength="16" size="16">
     </p>
     <p>
         <label for="comment">Comment (🚫 html 🚫 markdown ✅ emoji ✅ line-break): </label>
         <textarea id="comment" name="comment" rows="4" cols="71" wrap="soft" maxlength="800" required
-                  placeholder="This banking account is my _ _ _ _ bank's _ _ _ _ account."><?php /** @noinspection PhpUndefinedVariableInspection */
-            echo $object->comment; ?></textarea>
+                  placeholder="This banking account is my _ _ _ _ bank's _ _ _ _ account."><?php echo $object->comment; ?></textarea>
     </p>
     <section>
         <p>
--- app/GoodToKnow/Views/revampabankingtransactionforbalancesedit.php
+++ app/GoodToKnow/Views/revampabankingtransactionforbalancesedit.php
@@ -1,1 +1,50 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/editor.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <title><?php /** @noinspection PhpUndefinedVariableInspection */
+        echo $html_title; ?></title>
+</head>
+<body>
+<form action="/ax1/RevampABankingTransactionForBalancesUpdate/page" method="post">
+    <h2><?php /** @noinspection PhpUndefinedVariableInspection */
+        echo $object->label; ?></h2>
+    <?php require SESSIONMESSAGE; ?>
+    <p>
+        <label for="label">Label (✅ emoji): </label>
+        <input id="label" name="label" type="text"
+               value="<?php echo $object->label; ?>" required minlength="3" maxlength="30" size="30" spellcheck="false"
+               placeholder="Internet Service Fee">
+    </p>
+    <p>
+        <label for="time">Time (unix time stamp): </label>
+        <input id="time" name="time" type="text"
+               value="<?php echo $object->time; ?>" minlength="10" maxlength="22" size="22" placeholder="1560190617">
+    </p>
+    <p>
+        <label for="amount">Amount: </label>
+        <input id="amount" name="amount" type="text"
+               value="<?php echo $object->amount; ?>" required minlength="1" maxlength="16" size="16">
+    </p>
+    <section>
+        <?php echo $object->bank_id; ?>
+    </section>
+    <section>
+        <p>
+            <button type="submit" name="abort" value="Abort" class="abort">Abort</button>
+            <button type="submit" name="submit" value="Submit">Submit</button>
+        </p>
+    </section>
+</form>
+</body>
+</html>
