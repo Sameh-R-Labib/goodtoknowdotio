517455bbd863f07efb35f434e11b7320376469fc
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-07-11 22:09:08 -0400

    OmitABankingTransactionForBalances view completed

---
+++ app/GoodToKnow/Controllers/OmitABankingTransactionForBalancesTimeRange.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace GoodToKnow\Controllers;
+
+
+class OmitABankingTransactionForBalancesTimeRange
+{
+    public function page()
+    {
+        /**
+         *
+         */
+    }
+}
