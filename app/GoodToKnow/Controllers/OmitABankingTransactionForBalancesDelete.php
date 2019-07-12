1674a7a8f786d73ab6dc73821359b5caccbe2c37
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-07-12 17:22:34 -0400

    OmitABankingTransactionForBalancesChooseRecord view completed

---
+++ app/GoodToKnow/Controllers/OmitABankingTransactionForBalancesDelete.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace GoodToKnow\Controllers;
+
+
+class OmitABankingTransactionForBalancesDelete
+{
+    public function page()
+    {
+        /**
+         *
+         */
+    }
+}
--- app/GoodToKnow/Views/omitabankingtransactionforbalanceschooserecord.php
+++ app/GoodToKnow/Views/omitabankingtransactionforbalanceschooserecord.php
@@ -1,1 +1,39 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/editor.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <title><?php /** @noinspection PhpUndefinedVariableInspection */
+        echo $html_title; ?></title>
+</head>
+<body>
+<form action="/ax1/OmitABankingTransactionForBalancesDelete/page" method="post">
+    <h2>Which BankingTransactionForBalances?</h2>
+    <?php require SESSIONMESSAGE; ?>
+    <section>
+        <?php /** @noinspection PhpUndefinedVariableInspection */
+        foreach ($array as $key => $object): ?>
+            <label for="c<?php echo $key; ?>" class="radio">
+                <input type="radio" id="c<?php echo $key; ?>" name="choice" value="<?php echo $object->id; ?>">
+                <?php echo $object->label; ?> [<?php echo $object->time; ?>]<br>
+            </label>
+        <?php endforeach; ?>
+    </section>
+    <section>
+        <p>
+            <button type="submit" name="abort" value="Abort" class="abort">Abort</button>
+            <button type="submit" name="submit" value="Submit">Submit</button>
+        </p>
+    </section>
+</form>
+</body>
+</html>
