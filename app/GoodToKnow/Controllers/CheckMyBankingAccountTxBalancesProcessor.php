13270f568f36cad49f6492f65490a98531f8b37e
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-07-13 16:32:06 -0400

    CheckMyBankingAccountTxBalancesProcessor completed

--- app/GoodToKnow/Controllers/CheckMyBankingAccountTxBalancesProcessor.php
+++ app/GoodToKnow/Controllers/CheckMyBankingAccountTxBalancesProcessor.php
@@ -4,12 +4,71 @@
 namespace GoodToKnow\Controllers;
 
 
+use GoodToKnow\Models\BankingAcctForBalances;
+
+
 class CheckMyBankingAccountTxBalancesProcessor
 {
     public function page()
     {
         /**
-         *
+         * 1) Store the submitted banking_acct_for_balances record id in the session.
+         * 2) Retrieve the banking_acct_for_balances object with that id from the database.
+         * 3) Store the acct_name also in the session.
+         * 4) Redirect to next piece of code.
          */
+
+        global $is_logged_in;
+        global $sessionMessage;
+
+        if (!$is_logged_in || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        if (isset($_POST['abort']) AND $_POST['abort'] === "Abort") {
+            $sessionMessage .= " You've aborted the task! Session variables reset. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * 1) Store the submitted banking_acct_for_balances record id in the session.
+         */
+        $chosen_id = (isset($_POST['choice'])) ? (int)$_POST['choice'] : 0;
+        if ($chosen_id == 0) {
+            $sessionMessage .= " You didn't choose so I've aborted the process for you. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+        $_SESSION['saved_int01'] = $chosen_id;
+
+        /**
+         * 2) Retrieve the banking_acct_for_balances object with that id from the database.
+         */
+        $db = db_connect($sessionMessage);
+        if (!empty($sessionMessage) || $db === false) {
+            $sessionMessage .= ' Database connection failed. ';
+            $_SESSION['message'] = $sessionMessage;
+            $_SESSION['saved_int01'] = 0;
+            redirect_to("/ax1/Home/page");
+        }
+        $object = BankingAcctForBalances::find_by_id($db, $sessionMessage, $chosen_id);
+        if (!$object) {
+            $sessionMessage .= " Unexpectedly I could not find that banking_acct_for_balances record. ";
+            $_SESSION['message'] = $sessionMessage;
+            $_SESSION['saved_int01'] = 0;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * 3) Store the acct_name also in the session.
+         */
+        $_SESSION['saved_str01'] = $object->acct_name;
+
+        /**
+         * 4) Redirect to next piece of code.
+         */
+        redirect_to("/ax1/CheckMyBankingAccountTxBalancesShowBalances/page");
     }
 }
---
+++ app/GoodToKnow/Controllers/CheckMyBankingAccountTxBalancesShowBalances.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace GoodToKnow\Controllers;
+
+
+class CheckMyBankingAccountTxBalancesShowBalances
+{
+    public function page()
+    {
+        /**
+         *
+         */
+    }
+}
