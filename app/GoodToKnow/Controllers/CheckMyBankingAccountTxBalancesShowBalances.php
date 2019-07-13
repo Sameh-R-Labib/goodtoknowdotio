570ec712f6b4071da1238390161a5c8ce84c1a18
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-07-13 17:42:31 -0400

    CheckMyBankingAccountTxBalancesShowBalances save 1

--- app/GoodToKnow/Controllers/CheckMyBankingAccountTxBalancesShowBalances.php
+++ app/GoodToKnow/Controllers/CheckMyBankingAccountTxBalancesShowBalances.php
@@ -4,6 +4,9 @@
 namespace GoodToKnow\Controllers;
 
 
+use GoodToKnow\Models\BankingAcctForBalances;
+
+
 class CheckMyBankingAccountTxBalancesShowBalances
 {
     public function page()
@@ -21,6 +24,56 @@ class CheckMyBankingAccountTxBalancesShowBalances
          * 4) Display our data set as a ledger. Note: Inform the user that the balances
          * will be wrong if admin has deleted transactions older than 90 days and the start
          * time for the BankingAcctForBalances is set to a time older than 90 days.
+         * Also, show the account name for BankingAcctForBalances at the top of the ledger.
+         */
+
+        global $is_logged_in;
+        global $sessionMessage;
+        global $saved_int01;    // id of BankingAcctForBalances record
+        global $saved_str01;    // acct_name of BankingAcctForBalances record
+
+        if (!$is_logged_in || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            $_SESSION['saved_int01'] = 0;
+            $_SESSION['saved_str01'] = '';
+            redirect_to("/ax1/Home/page");
+        }
+
+        if (isset($_POST['abort']) AND $_POST['abort'] === "Abort") {
+            $sessionMessage .= " You've aborted the task! Session variables reset. ";
+            $_SESSION['message'] = $sessionMessage;
+            $_SESSION['saved_int01'] = 0;
+            $_SESSION['saved_str01'] = '';
+            redirect_to("/ax1/Home/page");
+        }
+
+        $db = db_connect($sessionMessage);
+        if (!empty($sessionMessage) || $db === false) {
+            $sessionMessage .= ' Database connection failed. ';
+            $_SESSION['message'] = $sessionMessage;
+            $_SESSION['saved_int01'] = 0;
+            $_SESSION['saved_str01'] = '';
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * 1) Get (from the database) the BankingAcctForBalances object.
+         */
+        $account = BankingAcctForBalances::find_by_id($db, $sessionMessage, $saved_int01);
+        if (!$account) {
+            $sessionMessage .= " Unexpectedly I could not find that banking_acct_for_balances record. ";
+            $_SESSION['message'] = $sessionMessage;
+            $_SESSION['saved_int01'] = 0;
+            $_SESSION['saved_str01'] = '';
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * 2) Get (from the database) all the BankingTransactionForBalances which
+         * have a time stamp greater than the start time for the account. Note:
+         * it can't be equal to the start time. Also: make sure the transactions
+         * are ordered by time increasing. Obviously, these transactions must be
+         * for the user who is requesting this stuff.
          */
     }
 }
