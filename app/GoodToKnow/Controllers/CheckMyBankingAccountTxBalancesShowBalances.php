a88981b03deb8873995222e200a5df9b226b2adf
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-07-13 22:32:56 -0400

    CheckMyBankingAccountTxBalancesShowBalances save 2

--- app/GoodToKnow/Controllers/CheckMyBankingAccountTxBalancesShowBalances.php
+++ app/GoodToKnow/Controllers/CheckMyBankingAccountTxBalancesShowBalances.php
@@ -18,7 +18,8 @@ class CheckMyBankingAccountTxBalancesShowBalances
          * have a time stamp greater than the start time for the account. Note:
          * it can't be equal to the start time. Also: make sure the transactions
          * are ordered by time increasing. Obviously, these transactions must be
-         * for the user who is requesting this stuff.
+         * for the user who is requesting this stuff. Also, these transactions must
+         * be for the currently chosen BankingAcctForBalances.
          * 3) Augment our data set with a running total in each BankingTransactionForBalances
          * object. This gets assigned to each BankingTransactionForBalances object's balance field.
          * 4) Display our data set as a ledger. Note: Inform the user that the balances
@@ -29,6 +30,7 @@ class CheckMyBankingAccountTxBalancesShowBalances
 
         global $is_logged_in;
         global $sessionMessage;
+        global $user_id;
         global $saved_int01;    // id of BankingAcctForBalances record
         global $saved_str01;    // acct_name of BankingAcctForBalances record
 
@@ -73,7 +75,9 @@ class CheckMyBankingAccountTxBalancesShowBalances
          * have a time stamp greater than the start time for the account. Note:
          * it can't be equal to the start time. Also: make sure the transactions
          * are ordered by time increasing. Obviously, these transactions must be
-         * for the user who is requesting this stuff.
+         * for the user who is requesting this stuff. Also, these transactions must
+         * be for the currently chosen BankingAcctForBalances.
          */
+        $sql = 'SELECT * FROM `banking_transaction_for_balances` WHERE `user_id` = "' . $db->real_escape_string($user_id) . '"';
     }
 }
