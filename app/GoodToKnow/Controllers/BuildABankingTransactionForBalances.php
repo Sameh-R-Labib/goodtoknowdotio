e4b444c3b1be8088b8ea64b2d40b16d2cf950671
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-07-16 17:55:33 -0400

    ConceiveAPossibleTaxDeduction Intro comments

--- app/GoodToKnow/Controllers/BuildABankingTransactionForBalances.php
+++ app/GoodToKnow/Controllers/BuildABankingTransactionForBalances.php
@@ -14,8 +14,7 @@ class BuildABankingTransactionForBalances
          * banking_transaction_for_balances table. The process will
          * ask the user to ONLY supply a banking_transaction_for_balances
          * label + time . And the remaining field values
-         * will be supplied by an (not included)
-         * editor for the record.
+         * will be supplied by the editor for this type of record.
          */
 
         /**
--- app/GoodToKnow/Controllers/ConceiveAPossibleTaxDeduction.php
+++ app/GoodToKnow/Controllers/ConceiveAPossibleTaxDeduction.php
@@ -9,7 +9,22 @@ class ConceiveAPossibleTaxDeduction
     public function page()
     {
         /**
+         * This feature enables any user to
+         * create a database record in the
+         * possible_tax_deduction table.
          *
+         * The process will
+         * ask the user to ONLY supply a possible_tax_deduction
+         * label + year_paid . And the remaining field values
+         * will be supplied by the editor for this type of record.
          */
+
+        /**
+         * This here script simply presents a
+         * form for the user to supply the possible_tax_deduction
+         * label + year_paid for the "to be created" possible_tax_deduction
+         * record.
+         */
+
     }
 }
