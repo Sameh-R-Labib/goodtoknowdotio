00f8b174f843b207adbf999e05658fb92676dc1f
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-07-16 22:47:20 -0400

    standard_form_field_prep

---
+++ app/GoodToKnow/ControllerHelpers/standard_form_field_prep.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace GoodToKnow\ControllerHelpers;
+
+/**
+ * @param string $field_name
+ * @param int $min_length
+ * @param int $max_length
+ * @return string|null
+ */
+function standard_form_field_prep(string $field_name, int $min_length, int $max_length): ?string
+{
+    /**
+     * Note: This is for data which will remain as a string value in the function's calling scope.
+     *
+     * This function will take the post value for the $field_name
+     * and it will prep it and return the result to be assigned
+     * to a variable in the function's calling scope.
+     *
+     * If
+     * A) the post value is not set
+     * B) or if it fails validation
+     * then null will be returned.
+     *
+     * If the post value is an empty string and $min_length above is 0
+     * then an empty string is returned by the function.
+     *
+     * How does it prep the data?
+     * - It makes sure the string is trimmed.
+     * - It makes sure the string is not too short.
+     * - It makes sure the string is not too long.
+     * - It applies htmlspecialchars.
+     */
+
+    // If the post var is not set then return null.
+    if (!isset($_POST[$field_name])) {
+        return null;
+    }
+
+    // Initialize the string we will be returning.
+    $string_for_return = $_POST[$field_name];
+
+    // Trim.
+    $string_for_return = trim($string_for_return);
+
+    // Make sure the string is not too short.
+    if (strlen($string_for_return) < $min_length) {
+        return null;
+    }
+
+    // Make sure the string is not too long.
+    if (strlen($string_for_return) > $max_length) {
+        return null;
+    }
+
+    // Apply htmlspecialchars.
+    $string_for_return = htmlspecialchars($string_for_return);
+
+    // Return.
+    return $string_for_return;
+}
--- app/GoodToKnow/Controllers/ConceiveAPossibleTaxDeductionProcessor.php
+++ app/GoodToKnow/Controllers/ConceiveAPossibleTaxDeductionProcessor.php
@@ -4,12 +4,57 @@
 namespace GoodToKnow\Controllers;
 
 
+use function GoodToKnow\ControllerHelpers\standard_form_field_prep;
+
+
 class ConceiveAPossibleTaxDeductionProcessor
 {
     public function page()
     {
         /**
+         * Create a database record in the possible_tax_deduction
+         * table using the submitted possible_tax_deduction
+         * label and year_paid. The remaining field values will be set to default values.
          *
+         * $_POST['label'] $_POST['year_paid']
          */
+
+        global $is_logged_in;
+        global $sessionMessage;
+        global $user_id;
+
+        if (!$is_logged_in || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        if (isset($_POST['abort']) AND $_POST['abort'] === "Abort") {
+            $sessionMessage .= " You've aborted the task! Session variables reset. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        require_once CONTROLLERHELPERS . DIRSEP . 'standard_form_field_prep.php';
+
+        $label = standard_form_field_prep('label', 3, 264);
+
+        if (is_null($label)) {
+            $sessionMessage .= " Your label did not pass validation. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $label = (isset($_POST['label'])) ? $_POST['label'] : '';
+        if (empty(trim($label))) {
+            $sessionMessage .= " Either you did not fill out the input fields or the session expired. Start over. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        if (strlen($label) > 264 || strlen($label) < 3) {
+            $sessionMessage .= " Either the label is too long or too short. Start over. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
     }
 }
