836718f7c6a360b888e086325fe7514605d4105a
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-07-17 13:54:21 -0400

    AlterAPossibleTaxDeduction page()

--- app/GoodToKnow/Controllers/AlterAPossibleTaxDeduction.php
+++ app/GoodToKnow/Controllers/AlterAPossibleTaxDeduction.php
@@ -9,7 +9,22 @@ class AlterAPossibleTaxDeduction
     public function page()
     {
         /**
-         *
+         * This page is going to present a text box
+         * for entering a year_paid value to be used
+         * to narrow down the choices for which
+         * possible_tax_deduction to edit.
          */
+
+        global $is_logged_in;
+        global $sessionMessage;
+
+        if (!$is_logged_in || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $html_title = 'Which year_paid for filtering your tax deduction choices?';
+
+        require VIEWS . DIRSEP . 'alterapossibletaxdeduction.php';
     }
 }
