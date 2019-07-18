50644cb14377cee18cee513c8ee846bef1f2f5fe
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-07-18 16:00:45 -0400

    AlterAPossibleTaxDeductionUpdate page() save

--- app/GoodToKnow/Controllers/AlterAPossibleTaxDeductionUpdate.php
+++ app/GoodToKnow/Controllers/AlterAPossibleTaxDeductionUpdate.php
@@ -4,12 +4,64 @@
 namespace GoodToKnow\Controllers;
 
 
+use function GoodToKnow\ControllerHelpers\integer_form_field_prep;
+use function GoodToKnow\ControllerHelpers\standard_form_field_prep;
+
+
 class AlterAPossibleTaxDeductionUpdate
 {
     public function page()
     {
         /**
-         *
+         * This function will:
+         * 1) Validate the submitted alterapossibletaxdeductionedit.php form data.
+         *      (and apply htmlspecialchars)
+         * 2) Retrieve the existing record from the database.
+         * 3) Modify the retrieved record by updating it with the submitted data.
+         * 4) Update/save the updated record in the database.
          */
+
+        global $is_logged_in;
+        global $sessionMessage;
+        global $saved_int01;    // record id
+
+        if (!$is_logged_in || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            $_SESSION['saved_int01'] = 0;
+            $_SESSION['saved_int02'] = 0;
+            redirect_to("/ax1/Home/page");
+        }
+
+        if (isset($_POST['abort']) AND $_POST['abort'] === "Abort") {
+            $sessionMessage .= " You've aborted the task! Session variables reset. ";
+            $_SESSION['message'] = $sessionMessage;
+            $_SESSION['saved_int01'] = 0;
+            redirect_to("/ax1/Home/page");
+        }
+
+        /**
+         * This function will:
+         * 1) Validate the submitted alterapossibletaxdeductionedit.php form data.
+         *      (and apply htmlspecialchars)
+         */
+        require_once CONTROLLERHELPERS . DIRSEP . 'standard_form_field_prep.php';
+
+        $edited_label = standard_form_field_prep('label', 3, 264);
+
+        if (is_null($edited_label)) {
+            $sessionMessage .= " Your label did not pass validation. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        require_once CONTROLLERHELPERS . DIRSEP . 'integer_form_field_prep.php';
+
+        $year_paid = integer_form_field_prep('year_paid', 1992, 65535);
+
+        if (is_null($year_paid)) {
+            $sessionMessage .= " Your year_paid did not pass validation. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
     }
 }
