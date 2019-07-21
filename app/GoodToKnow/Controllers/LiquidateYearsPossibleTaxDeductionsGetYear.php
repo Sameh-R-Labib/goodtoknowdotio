2b1b8368be8bd6899874efbef34e2b559d673aca
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-07-21 17:50:46 -0400

    LiquidateYearsPossibleTaxDeductionsGetYear started

--- app/GoodToKnow/Controllers/LiquidateYearsPossibleTaxDeductionsGetYear.php
+++ app/GoodToKnow/Controllers/LiquidateYearsPossibleTaxDeductionsGetYear.php
@@ -9,7 +9,18 @@ class LiquidateYearsPossibleTaxDeductionsGetYear
     public function page()
     {
         /**
-         *
+         * 1) Validate the submitted year_paid.
+         * 2) Delete the possible_tax_deduction which have the specified year_paid.
+         * 3) Give confirmation of deletion.
          */
+
+        global $is_logged_in;
+        global $sessionMessage;
+        global $is_admin;
+
+        if (!$is_logged_in OR !$is_admin OR !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
     }
 }
