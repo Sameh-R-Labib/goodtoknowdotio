f1f12f9983a8fbef492a329235f7eb6b7463379f
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-07-21 17:30:11 -0400

    LiquidateYearsPossibleTaxDeductions page()

--- app/GoodToKnow/Controllers/LiquidateYearsPossibleTaxDeductions.php
+++ app/GoodToKnow/Controllers/LiquidateYearsPossibleTaxDeductions.php
@@ -13,5 +13,18 @@ class LiquidateYearsPossibleTaxDeductions
          *
          * Note: It's an admin script.
          */
+
+        global $is_logged_in;
+        global $sessionMessage;
+        global $is_admin;
+
+        if (!$is_logged_in OR !$is_admin OR !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $html_title = 'Which year of possible_tax_deduction(s/plural) to delete?';
+
+        require VIEWS . DIRSEP . 'liquidateyearspossibletaxdeductions.php';
     }
 }
