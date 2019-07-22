f9f394074a52521b1f7f7e99e9da0d23b35ff08f
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-07-22 14:37:04 -0400

    WipeOutAPossibleTaxDeduction page() comment

--- app/GoodToKnow/Controllers/WipeOutAPossibleTaxDeduction.php
+++ app/GoodToKnow/Controllers/WipeOutAPossibleTaxDeduction.php
@@ -9,7 +9,12 @@ class WipeOutAPossibleTaxDeduction
     public function page()
     {
         /**
+         * Ultimately, this is about deleting a PossibleTaxDeduction.
          *
+         * This page is going to present a text box
+         * for entering a year_paid value to be used
+         * to narrow down the choices for which
+         * possible_tax_deduction to delete.
          */
     }
 }
