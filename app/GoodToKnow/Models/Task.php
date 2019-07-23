be00d89708d7f23ae4ce67812f561bbb478efde6
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-07-23 16:51:38 -0400

    Task Model class file

---
+++ app/GoodToKnow/Models/Task.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace GoodToKnow\Models;
+
+
+class Task extends GoodObject
+{
+    /**
+     * @var string
+     */
+    protected static $table_name = "task";
+
+    /**
+     * @var array
+     */
+    protected static $fields = ['id', 'user_id', 'label', 'last', 'next', 'cycle_type', 'comment'];
+
+    /**
+     * @var int
+     */
+    public $id;
+
+    /**
+     * @var int
+     */
+    public $user_id;
+
+    /**
+     * @var string
+     */
+    public $label;
+
+    /**
+     * @var int
+     */
+    public $last;
+
+    /**
+     * @var int
+     */
+    public $next;
+
+    /**
+     * @var string
+     */
+    public $cycle_type;
+
+    /**
+     * @var string
+     */
+    public $comment;
+}
