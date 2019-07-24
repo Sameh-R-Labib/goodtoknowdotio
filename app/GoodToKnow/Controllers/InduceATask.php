a3e601e9f892a5d14f41e9e444a20f18d1389b64
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-07-23 22:35:16 -0400

    InduceATask page()

--- app/GoodToKnow/Controllers/InduceATask.php
+++ app/GoodToKnow/Controllers/InduceATask.php
@@ -9,7 +9,19 @@ class InduceATask
     public function page()
     {
         /**
-         *
+         * Create a task record based on a label for it.
          */
+
+        global $is_logged_in;
+        global $sessionMessage;
+
+        if (!$is_logged_in || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $html_title = 'Create a New Task';
+
+        require VIEWS . DIRSEP . 'induceatask.php';
     }
 }
