487523618c60097d76d98c69cf37e001b976fb3e
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-07-24 20:25:51 -0400

    FeatureATask page()

--- app/GoodToKnow/Controllers/FeatureATask.php
+++ app/GoodToKnow/Controllers/FeatureATask.php
@@ -4,12 +4,44 @@
 namespace GoodToKnow\Controllers;
 
 
+use GoodToKnow\Models\Task;
+
 class FeatureATask
 {
     public function page()
     {
         /**
-         *
+         * Present the Task(s/plural) as radio buttons.
          */
+
+        global $is_logged_in;
+        global $sessionMessage;
+        global $user_id;
+
+        if (!$is_logged_in || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $db = db_connect($sessionMessage);
+
+        if (!empty($sessionMessage) || $db === false) {
+            $sessionMessage .= ' Database connection failed. ';
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        // Get an array of Task objects for this user.
+        $sql = 'SELECT * FROM `task` WHERE `user_id` = ' . $db->real_escape_string($user_id);
+        $array = Task::find_by_sql($db, $sessionMessage, $sql);
+        if (!$array || !empty($sessionMessage)) {
+            $sessionMessage .= " 🤔 For you I could NOT find any tasks. ";
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $html_title = 'Which task record?';
+
+        require VIEWS . DIRSEP . 'featureatask.php';
     }
 }
