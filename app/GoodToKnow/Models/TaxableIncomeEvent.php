dc63d27c818c69a27a6fbf07f5768c425d31eb70
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-08-01 21:18:24 -0400

    TaxableIncomeEvent Model

---
+++ app/GoodToKnow/Models/TaxableIncomeEvent.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace GoodToKnow\Models;
+
+
+class TaxableIncomeEvent extends GoodObject
+{
+    /**
+     * @var string
+     */
+    protected static $table_name = "taxable_income_event";
+
+    /**
+     * @var array
+     */
+    protected static $fields = ['id', 'user_id', 'time', 'year_received', 'currency', 'amount', 'label', 'comment'];
+
+    /**
+     * @var int
+     */
+    public $id;
+
+    /**
+     * @var int
+     */
+    public $user_id;
+
+    /**
+     * @var int
+     */
+    public $time;
+
+    /**
+     * @var int
+     */
+    public $year_received;
+
+    /**
+     * @var string
+     */
+    public $currency;
+
+    /**
+     * @var float
+     */
+    public $amount;
+
+    /**
+     * @var string
+     */
+    public $label;
+
+    /**
+     * @var string
+     */
+    public $comment;
+}
