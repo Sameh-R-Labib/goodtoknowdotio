8816dee18bb334aa3cc08c8cc934a318b01042b5
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-08-02 19:56:18 -0400

    WriteOverATaxableIncomeEvent page()

--- app/GoodToKnow/Controllers/WriteOverATaxableIncomeEvent.php
+++ app/GoodToKnow/Controllers/WriteOverATaxableIncomeEvent.php
@@ -9,7 +9,22 @@ class WriteOverATaxableIncomeEvent
     function page()
     {
         /**
-         *
+         * This page is going to present a text box
+         * for entering a year_received value to be used
+         * to narrow down the choices for which
+         * taxable_income_event to edit.
          */
+
+        global $is_logged_in;
+        global $sessionMessage;
+
+        if (!$is_logged_in || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $html_title = 'Which year_received for filtering your taxable income event choices?';
+
+        require VIEWS . DIRSEP . 'writeoverataxableincomeevent.php';
     }
 }
