910d465a9ca1a99668355b77014b67de34961877
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-08-04 14:52:43 -0400

    NukeATaxableIncomeEvent page()

--- app/GoodToKnow/Controllers/NukeATaxableIncomeEvent.php
+++ app/GoodToKnow/Controllers/NukeATaxableIncomeEvent.php
@@ -9,7 +9,24 @@ class NukeATaxableIncomeEvent
     function page()
     {
         /**
+         * Ultimately, this is about deleting a TaxableIncomeEvent.
          *
+         * This page is going to present a text box
+         * for entering a year_received value to be used
+         * to narrow down the choices for which
+         * taxable_income_event to delete.
          */
+
+        global $is_logged_in;
+        global $sessionMessage;
+
+        if (!$is_logged_in || !empty($sessionMessage)) {
+            $_SESSION['message'] = $sessionMessage;
+            redirect_to("/ax1/Home/page");
+        }
+
+        $html_title = 'Which year_received for filtering your taxable income event choices?';
+
+        require VIEWS . DIRSEP . 'nukeataxableincomeevent.php';
     }
 }
