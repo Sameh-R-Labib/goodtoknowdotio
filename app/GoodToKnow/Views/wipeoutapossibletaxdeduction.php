46de160178fe0dd8802a7268cbf8e4c91941f315
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-08-04 17:25:53 -0400

    NukeATaxableIncomeEvent view

--- app/GoodToKnow/Views/wipeoutapossibletaxdeduction.php
+++ app/GoodToKnow/Views/wipeoutapossibletaxdeduction.php
@@ -17,12 +17,12 @@
 <body>
 <form action="/ax1/WipeOutAPossibleTaxDeductionYearFilter/page" method="post">
     <h1>Delete a 🤔 Tax ✍🏽🔽</h1>
-    <h2>Narrow down the data set for 🤔 Tax ✍🏽🔽</h2>
-    <p>Which year_paid does the transaction fall under?</p>
+    <h2>To narrow down the data set for 🤔 Tax ✍🏽🔽</h2>
+    <p>Which Year Paid does the transaction fall under?</p>
     <?php require SESSIONMESSAGE; ?>
     <section>
         <p>
-            <label for="year_paid">year_paid: </label>
+            <label for="year_paid">Year Paid: </label>
             <input id="year_paid" name="year_paid" type="text" value="" required minlength="4" maxlength="6"
                    size="6" placeholder="2018">
         </p>
