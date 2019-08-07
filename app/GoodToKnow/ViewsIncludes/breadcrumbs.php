b46acd482bed708204fc3be0daefa38569e725b5
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-08-07 16:59:28 -0400

    Made breadcrumbs into a switch statement

--- app/GoodToKnow/ViewsIncludes/breadcrumbs.php
+++ app/GoodToKnow/ViewsIncludes/breadcrumbs.php
@@ -1,24 +1,44 @@
 <?php
 
-if ($page === 'Inbox') echo "<a href=\"/ax1/Inbox/page\">Inbox</a>";
-elseif ($page === 'Admin') echo "<a href=\"/ax1/AdminHome/page\">Admin Home</a>";
-elseif ($page === 'CP') echo "<a href=\"/ax1/ControlPanel/page\">Control Panel</a>";
-elseif ($page === 'UserRoster') echo "<a href=\"/ax1/UserRoster/page\">User Roster</a>";
-elseif ($page === 'BitcoinSeeMyRecords') echo "<a href=\"/ax1/BitcoinSeeMyRecords/page\">My Bitcoin Records</a>";
-elseif ($page === 'About') echo "What is this site?";
-elseif ($page === 'RecurringPaymentSeeMyRecords') echo "<a href=\"/ax1/RecurringPaymentSeeMyRecords/page\">My Recurring Payments</a>";
-elseif ($page === 'ViewAllBankingAccountsForBalances')
-    echo "<a href=\"/ax1/ViewAllBankingAccountsForBalances/page\">View All Banking Accounts For Balances</a>";
-elseif ($page === 'CheckMyBankingAccountTxBalances')
-    echo "<a href=\"/ax1/CheckMyBankingAccountTxBalances/page\">Check My Banking Account Transactions For Balances</a>";
-elseif ($page === 'SeeOneYearsPossibleTaxDeductions')
-    echo "<a href=\"/ax1/SeeOneYearsPossibleTaxDeductions/page\">See One Years Possible Deductions</a>";
-elseif ($page === 'GlanceAtMyTasks')
-    echo "<a href=\"/ax1/GlanceAtMyTasks/page\">See My To-do Tasks</a>";
-elseif ($page === 'GawkAtAllTaxableIncomeEvents')
-    echo "<a href=\"/ax1/GawkAtAllTaxableIncomeEvents/page\">See One Years Taxable Income Events</a>";
-else {
-    require CURRENTCOMMUNITY;
-    require CURRENTTOPIC;
-    require CURRENTPOST;
+switch ($page) {
+    case 'Inbox':
+        echo "<a href=\"/ax1/Inbox/page\">Inbox</a>";
+        break;
+    case 'Admin':
+        echo "<a href=\"/ax1/AdminHome/page\">Admin Home</a>";
+        break;
+    case 'CP':
+        echo "<a href=\"/ax1/ControlPanel/page\">Control Panel</a>";
+        break;
+    case 'UserRoster':
+        echo "<a href=\"/ax1/UserRoster/page\">User Roster</a>";
+        break;
+    case 'BitcoinSeeMyRecords':
+        echo "<a href=\"/ax1/BitcoinSeeMyRecords/page\">My Bitcoin Records</a>";
+        break;
+    case 'About':
+        echo "What is this site?";
+        break;
+    case 'RecurringPaymentSeeMyRecords':
+        echo "<a href=\"/ax1/RecurringPaymentSeeMyRecords/page\">My Recurring Payments</a>";
+        break;
+    case 'ViewAllBankingAccountsForBalances':
+        echo "<a href=\"/ax1/ViewAllBankingAccountsForBalances/page\">View All Banking Accounts For Balances</a>";
+        break;
+    case 'CheckMyBankingAccountTxBalances':
+        echo "<a href=\"/ax1/CheckMyBankingAccountTxBalances/page\">Check My Banking Account Transactions For Balances</a>";
+        break;
+    case 'SeeOneYearsPossibleTaxDeductions':
+        echo "<a href=\"/ax1/SeeOneYearsPossibleTaxDeductions/page\">See One Years Possible Deductions</a>";
+        break;
+    case 'GlanceAtMyTasks':
+        echo "<a href=\"/ax1/GlanceAtMyTasks/page\">See My To-do Tasks</a>";
+        break;
+    case 'GawkAtAllTaxableIncomeEvents':
+        echo "<a href=\"/ax1/GawkAtAllTaxableIncomeEvents/page\">See One Years Taxable Income Events</a>";
+        break;
+    default:
+        require CURRENTCOMMUNITY;
+        require CURRENTTOPIC;
+        require CURRENTPOST;
 }
