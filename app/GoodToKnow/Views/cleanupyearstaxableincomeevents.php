f09af80acb4f39594066238fcdaf761a5ec07db3
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-08-07 22:08:32 -0400

    topforformpages for the rest of them

--- app/GoodToKnow/Views/cleanupyearstaxableincomeevents.php
+++ app/GoodToKnow/Views/cleanupyearstaxableincomeevents.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="/css/normalize.css">
-    <link rel="stylesheet" href="/css/editor.css">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
-    <title><?= $html_title ?></title>
-</head>
-<body>
+<?php require TOPFORFORMPAGES; ?>
 <form action="/ax1/CleanupYearsTaxableIncomeEventsGetYear/page" method="post">
     <h1>Admin delete a year's Taxable 💸 Event 📽s</h1>
     <p>If today's year is 2019 then do not delete 2019, 2018, 2017 or 2016 because that is what our users' are
--- app/GoodToKnow/Views/membermemoeditorform.php
+++ app/GoodToKnow/Views/membermemoeditorform.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="/css/normalize.css">
-    <link rel="stylesheet" href="/css/editor.css">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
-    <title><?= $html_title ?></title>
-</head>
-<body>
+<?php require TOPFORFORMPAGES; ?>
 <form action="/ax1/MemberMemEdFormProc/page" method="post">
     <h2><?= $saved_str01 ?> Memo</h2>
     <?php require SESSIONMESSAGE; ?>
