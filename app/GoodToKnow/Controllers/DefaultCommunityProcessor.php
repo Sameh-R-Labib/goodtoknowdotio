32c17599729c66dcf3a950906fce573aaec2d86c
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-08-15 20:56:50 -0400

    Applied prep function for DefaultCommunityProcessor

--- app/GoodToKnow/Controllers/DefaultCommunityProcessor.php
+++ app/GoodToKnow/Controllers/DefaultCommunityProcessor.php
@@ -10,6 +10,7 @@ namespace GoodToKnow\Controllers;
 
 
 use GoodToKnow\Models\User;
+use function GoodToKnow\ControllerHelpers\integer_form_field_prep;
 
 
 class DefaultCommunityProcessor
@@ -32,8 +33,13 @@ class DefaultCommunityProcessor
             redirect_to("/ax1/Home/page");
         }
 
-        if (empty($_POST['choice'])) {
-            $sessionMessage .= " Expected submission of choice not found. ";
+        require_once CONTROLLERHELPERS . DIRSEP . 'integer_form_field_prep.php';
+
+        // int(10) in mysql has max 4294967295
+        $chosen_id = integer_form_field_prep('choice', 1, 4294967295);
+
+        if (is_null($chosen_id)) {
+            $sessionMessage .= " Your choice did not pass validation. ";
             $_SESSION['message'] = $sessionMessage;
             redirect_to("/ax1/Home/page");
         }
@@ -42,7 +48,9 @@ class DefaultCommunityProcessor
          * Make sure the submitted choice is valid for this user.
          */
         $is_found = false;
-        if (array_key_exists($_POST['choice'], $special_community_array)) $is_found = true;
+
+        if (array_key_exists($chosen_id, $special_community_array)) $is_found = true;
+
         if (!$is_found) {
             $sessionMessage .= " Choice is not valid. ";
             $_SESSION['message'] = $sessionMessage;
@@ -79,6 +87,7 @@ class DefaultCommunityProcessor
 
         // User will know default community by logging out then in.
         $sessionMessage .= " Your default community has been changed to {$special_community_array[$_POST['choice']]}. ";
+        $_SESSION['message'] = $sessionMessage;
         redirect_to("/ax1/Home/page");
     }
 }
