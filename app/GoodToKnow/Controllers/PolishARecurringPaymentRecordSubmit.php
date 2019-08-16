bd666a280008812a0314cfb976db85a3ae2c4b97
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-08-16 13:54:31 -0400

    Applied prep function for PolishARecurringPaymentRecordSubmit

--- app/GoodToKnow/Controllers/PolishARecurringPaymentRecordSubmit.php
+++ app/GoodToKnow/Controllers/PolishARecurringPaymentRecordSubmit.php
@@ -5,6 +5,7 @@ namespace GoodToKnow\Controllers;
 
 
 use GoodToKnow\Models\RecurringPayment;
+use function GoodToKnow\ControllerHelpers\integer_form_field_prep;
 use function GoodToKnow\ControllerHelpers\standard_form_field_prep;
 
 
@@ -45,13 +46,36 @@ class PolishARecurringPaymentRecordSubmit
          */
         require_once CONTROLLERHELPERS . DIRSEP . 'standard_form_field_prep.php';
 
+        require_once CONTROLLERHELPERS . DIRSEP . 'integer_form_field_prep.php';
+
+        /** @var $edited_label */
+
         $edited_label = standard_form_field_prep('label', 4, 264);
+
+        /** @var  $edited_currency */
 
         $edited_currency = standard_form_field_prep('currency', 1, 15);
 
-        $edited_amount_paid = (isset($_POST['amount_paid'])) ? (float)$_POST['amount_paid'] : 0;
+        /** @var  $edited_amount_paid */
 
-        $edited_unix_time_at_last_payment = (isset($_POST['unix_time_at_last_payment'])) ? (int)$_POST['unix_time_at_last_payment'] : 1560190617;
+        $edited_amount_paid = (isset($_POST['amount_paid'])) ? (float)$_POST['amount_paid'] : 0.0;
+
+        /** @var $edited_time */
+
+        $edited_time = integer_form_field_prep('unix_time_at_last_payment', 0, PHP_INT_MAX);
+
+        if (is_null($edited_time)) {
+            $sessionMessage .= " Your time value did not pass validation. ";
+            $_SESSION['message'] = $sessionMessage;
+            $_SESSION['saved_int01'] = 0;
+            redirect_to("/ax1/Home/page");
+        }
+
+        if ($edited_time === 0) {
+            $edited_time = 1560190617;
+        }
+
+        /** @var $edited_comment */
 
         $edited_comment = standard_form_field_prep('comment', 0, 800);
 
@@ -72,8 +96,8 @@ class PolishARecurringPaymentRecordSubmit
             $_SESSION['saved_int01'] = 0;
             redirect_to("/ax1/Home/page");
         }
-        $recurring_payment_object = RecurringPayment::find_by_id($db, $sessionMessage, $saved_int01);
-        if (!$recurring_payment_object) {
+        $object = RecurringPayment::find_by_id($db, $sessionMessage, $saved_int01);
+        if (!$object) {
             $sessionMessage .= " Unexpectedly I could not find that recurring_payment record. ";
             $_SESSION['message'] = $sessionMessage;
             $_SESSION['saved_int01'] = 0;
@@ -83,16 +107,16 @@ class PolishARecurringPaymentRecordSubmit
         /**
          * 3) Modify the retrieved record by updating it with the submitted data.
          */
-        $recurring_payment_object->label = $edited_label;
-        $recurring_payment_object->currency = $edited_currency;
-        $recurring_payment_object->amount_paid = $edited_amount_paid;
-        $recurring_payment_object->unix_time_at_last_payment = $edited_unix_time_at_last_payment;
-        $recurring_payment_object->comment = $edited_comment;
+        $object->label = $edited_label;
+        $object->currency = $edited_currency;
+        $object->amount_paid = $edited_amount_paid;
+        $object->unix_time_at_last_payment = $edited_time;
+        $object->comment = $edited_comment;
 
         /**
          * 4) Update/save the updated record in the database.
          */
-        $result = $recurring_payment_object->save($db, $sessionMessage);
+        $result = $object->save($db, $sessionMessage);
         if ($result === false) {
             $sessionMessage .= " I aborted the process you were working on because I failed at saving the updated RecurringPayment object. ";
             $_SESSION['message'] = $sessionMessage;
@@ -103,7 +127,7 @@ class PolishARecurringPaymentRecordSubmit
         /**
          * 5) Report success.
          */
-        $sessionMessage .= " I've updated <b>{$recurring_payment_object->label}</b>. ";
+        $sessionMessage .= " I've updated <b>{$object->label}</b>. ";
         $_SESSION['message'] = $sessionMessage;
         $_SESSION['saved_int01'] = 0;
         redirect_to("/ax1/Home/page");
