83753d46556580c25d250122da68d2a04ee02bb2
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-08-28 12:11:07 -0400

    Defined yes_no_form_field_prep()

---
+++ app/GoodToKnow/ControllerHelpers/yes_no_form_field_prep.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace GoodToKnow\ControllerHelpers;
+
+/**
+ * @param string $field_name
+ * @return string
+ */
+function yes_no_form_field_prep(string $field_name): string
+{
+    /**
+     * This returns either the string 'yes' or 'no' depending on the content of the $_POST array element whose key is
+     * $field_name. Otherwise, it breaks out to the home page with a message if something went wrong.
+     */
+
+    require_once CONTROLLERHELPERS . DIRSEP . 'standard_form_field_prep.php';
+
+    $choice = standard_form_field_prep($field_name, 2, 3);
+
+    if (is_null($choice)) {
+        breakout(' The yes/no choice you entered did not pass validation. ');
+    }
+
+    if ($choice != "yes" && $choice != "no") {
+        breakout(' You didn\'t enter a yes/no choice. ');
+    }
+
+    return $choice;
+}
