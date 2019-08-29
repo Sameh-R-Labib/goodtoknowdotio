405e136307dbd289516a4d192830fc6f9694093a
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-08-29 16:14:48 -0400

    Created before_after_form_field_prep()

---
+++ app/GoodToKnow/ControllerHelpers/before_after_form_field_prep.php
@@ -0,0 +1,33 @@
+<?php
+
+use function GoodToKnow\ControllerHelpers\standard_form_field_prep;
+
+/**
+ * @param string $field_name
+ * @return string
+ */
+function before_after_form_field_prep(string $field_name): string
+{
+    /**
+     * This returns either the string 'before' or 'after' depending on the content of the $_POST array element whose key is
+     * $field_name. Otherwise, it breaks out to the home page with an error message if something went wrong.
+     */
+
+    require_once CONTROLLERHELPERS . DIRSEP . 'standard_form_field_prep.php';
+
+    $choice = standard_form_field_prep($field_name, 5, 6);
+
+    if (is_null($choice)) {
+
+        breakout(' The before/after choice you entered did not pass validation. ');
+
+    }
+
+    if ($choice != "before" && $choice != "after") {
+
+        breakout(' You didn\'t enter a before/after choice. ');
+
+    }
+
+    return $choice;
+}
