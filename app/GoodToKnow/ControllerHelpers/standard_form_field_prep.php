644e1c61cd91bb03f04fbb8898a20589424b5dd6
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-08-29 19:02:55 -0400

    standard_form_field_prep modified

--- app/GoodToKnow/ControllerHelpers/standard_form_field_prep.php
+++ app/GoodToKnow/ControllerHelpers/standard_form_field_prep.php
@@ -6,9 +6,9 @@ namespace GoodToKnow\ControllerHelpers;
  * @param string $field_name
  * @param int $min_length
  * @param int $max_length
- * @return string|null
+ * @return string
  */
-function standard_form_field_prep(string $field_name, int $min_length, int $max_length): ?string
+function standard_form_field_prep(string $field_name, int $min_length, int $max_length): string
 {
     /**
      * Note: This is for data which will remain as a string value in the function's calling scope.
@@ -33,30 +33,50 @@ function standard_form_field_prep(string $field_name, int $min_length, int $max_
      * - It applies htmlspecialchars.
      */
 
-    // If the post var is not set then return null.
+
+    // If the post var is not set.
+
     if (!isset($_POST[$field_name])) {
-        return null;
+
+        breakout(" A value for {$field_name} is missing. ");
+
     }
+
 
     // Initialize the string we will be returning.
+
     $string_for_return = $_POST[$field_name];
 
+
     // Trim.
+
     $string_for_return = trim($string_for_return);
 
+
     // Make sure the string is not too short.
+
     if (strlen($string_for_return) < $min_length) {
-        return null;
+
+        breakout(" The string value for {$field_name} is too short. ");
+
     }
+
 
     // Make sure the string is not too long.
+
     if (strlen($string_for_return) > $max_length) {
-        return null;
+
+        breakout(" The string value for {$field_name} is too long. ");
+
     }
 
+
     // Apply htmlspecialchars.
+
     $string_for_return = htmlspecialchars($string_for_return, ENT_NOQUOTES | ENT_HTML5);
 
+
     // Return.
+
     return $string_for_return;
 }
