d9312b0cd01264ed332bc7d7d9cebcbfdb2711cd
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-09-03 17:53:41 -0400

    Cleanup 03SEP19

--- web/routehandler.php
+++ web/routehandler.php
@@ -1,7 +1,5 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: samehlabib
  * Date: 8/18/18
  * Time: 8:22 PM
  */
--- web/test/nonascii.php
+++ web/test/nonascii.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: samehlabib
- * Date: 9/7/18
- * Time: 2:23 PM
- */
 $welcome = "This ⅔ is ÷ a string.";
 $result = mb_detect_encoding($welcome, 'ASCII', true);
 var_dump($result);
