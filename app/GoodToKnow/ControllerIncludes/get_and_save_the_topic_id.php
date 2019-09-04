b089d2173f7bfbfdd33ff11decde3653843d155e
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-09-03 21:34:42 -0400

    Created include called get_and_save_topic_id

---
+++ app/GoodToKnow/ControllerIncludes/get_and_save_the_topic_id.php
@@ -0,0 +1,23 @@
+<?php
+
+use function GoodToKnow\ControllerHelpers\integer_form_field_prep;
+
+global $special_topic_array;
+global $is_logged_in;
+global $sessionMessage;
+
+kick_out_loggedoutusers();
+
+kick_out_onabort();
+
+require_once CONTROLLERHELPERS . DIRSEP . 'integer_form_field_prep.php';
+
+$chosen_topic_id = integer_form_field_prep('choice', 1, PHP_INT_MAX);
+
+if (!array_key_exists($chosen_topic_id, $special_topic_array)) {
+
+    breakout(' Unexpected error: topic id not found in topic array. ');
+
+}
+
+$_SESSION['saved_int01'] = $chosen_topic_id;
--- app/GoodToKnow/Controllers/CreateNewPostProcessor.php
+++ app/GoodToKnow/Controllers/CreateNewPostProcessor.php
@@ -3,51 +3,12 @@
 namespace GoodToKnow\Controllers;
 
 use GoodToKnow\Models\TopicToPost;
-use function GoodToKnow\ControllerHelpers\integer_form_field_prep;
 
 class CreateNewPostProcessor
 {
     function page()
     {
-        global $special_topic_array;
-        global $is_logged_in;
-        global $sessionMessage;
-
-        kick_out_loggedoutusers();
-
-        kick_out_onabort();
-
-
-        /**
-         * We should have a post variable called choice whose value is the topic id
-         * the user intends to create a post in.
-         */
-
-        /**
-         * I can't assume this post variables exist so I do the following.
-         */
-
-        require_once CONTROLLERHELPERS . DIRSEP . 'integer_form_field_prep.php';
-
-        $chosen_topic_id = integer_form_field_prep('choice', 1, PHP_INT_MAX);
-
-
-        /**
-         * Make sure $chosen_topic_id is among the ids of $special_topic_array
-         */
-
-        if (!array_key_exists($chosen_topic_id, $special_topic_array)) {
-
-            breakout(' Unexpected error: topic id not found in topic array. ');
-
-        }
-
-
-        /**
-         * Save it in the session
-         */
-
-        $_SESSION['saved_int01'] = $chosen_topic_id;
+        require CONTROLLERINCLUDES . DIRSEP . 'get_and_save_the_topic_id.php';
 
 
         /**
