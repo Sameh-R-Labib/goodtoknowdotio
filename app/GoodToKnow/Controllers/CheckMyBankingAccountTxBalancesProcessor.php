6edf0df0f52ae72461291f63f912f784ed4dd3af
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-09-12 21:59:31 -0400

    Secured CheckMyBankingAccountTxBalancesProcessor

--- app/GoodToKnow/Controllers/CheckMyBankingAccountTxBalancesProcessor.php
+++ app/GoodToKnow/Controllers/CheckMyBankingAccountTxBalancesProcessor.php
@@ -9,14 +9,15 @@ class CheckMyBankingAccountTxBalancesProcessor
         /**
          * 1) Store the submitted banking_acct_for_balances record id in the session.
          * 2) Retrieve the banking_acct_for_balances object with that id from the database.
-         * 3) Redirect to next piece of code.
+         * 3) Make sure that object belongs to the user.
+         * 4) Redirect to next piece of code.
          */
 
         require CONTROLLERINCLUDES . DIRSEP . 'get_the_bankingaccountforbalances.php';
 
 
         /**
-         * 3) Redirect to next piece of code.
+         * 4) Redirect to next piece of code.
          */
 
         redirect_to("/ax1/CheckMyBankingAccountTxBalancesShowBalances/page");
