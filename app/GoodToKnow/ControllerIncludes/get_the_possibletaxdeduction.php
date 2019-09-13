085c13b8c140cddcd0d0913bd77606691e3ead6e
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-09-13 14:45:54 -0400

    Secured WipeOutAPossibleTaxDeductionDelete

---
+++ app/GoodToKnow/ControllerIncludes/get_the_possibletaxdeduction.php
@@ -0,0 +1,49 @@
+<?php
+
+use GoodToKnow\Models\PossibleTaxDeduction;
+use function GoodToKnow\ControllerHelpers\integer_form_field_prep;
+
+global $sessionMessage;
+
+global $user_id;
+
+kick_out_loggedoutusers();
+
+kick_out_onabort();
+
+
+/**
+ * 1) Store the submitted possible_tax_deduction id in the session.
+ */
+
+require_once CONTROLLERHELPERS . DIRSEP . 'integer_form_field_prep.php';
+
+$id = integer_form_field_prep('choice', 1, PHP_INT_MAX);
+
+$_SESSION['saved_int01'] = $id;
+
+
+/**
+ * 2) Retrieve the possible_tax_deduction object with that id from the database.
+ */
+
+$db = get_db();
+
+$object = PossibleTaxDeduction::find_by_id($db, $sessionMessage, $id);
+
+if (!$object) {
+
+    breakout(' Unexpectedly, I could not find that possible tax deduction. ');
+
+}
+
+
+/**
+ * 3) Make sure the object belongs to this user.
+ */
+
+if ($object->user_id != $user_id) {
+
+    breakout(' Error 01544111. ');
+
+}
--- app/GoodToKnow/Controllers/WipeOutAPossibleTaxDeductionDelete.php
+++ app/GoodToKnow/Controllers/WipeOutAPossibleTaxDeductionDelete.php
@@ -2,9 +2,6 @@
 
 namespace GoodToKnow\Controllers;
 
-use function GoodToKnow\ControllerHelpers\integer_form_field_prep;
-use GoodToKnow\Models\PossibleTaxDeduction;
-
 class WipeOutAPossibleTaxDeductionDelete
 {
     function page()
@@ -12,45 +9,17 @@ class WipeOutAPossibleTaxDeductionDelete
         /**
          * 1) Store the submitted possible_tax_deduction record id in the session.
          * 2) Retrieve the possible_tax_deduction object with that id from the database.
-         * 3) Present a form which is populated with data from the possible_tax_deduction object
+         * 3) Make sure the object belongs to this user.
+         * 4) Present a form which is populated with data from the possible_tax_deduction object
          *    and asks for approval for deletion to proceed.
          */
 
-        global $sessionMessage;
 
-        kick_out_loggedoutusers();
-
-        kick_out_onabort();
+        require CONTROLLERINCLUDES . DIRSEP . 'get_the_possibletaxdeduction.php';
 
 
         /**
-         * 1) Store the submitted possible_tax_deduction record id in the session.
-         */
-
-        require_once CONTROLLERHELPERS . DIRSEP . 'integer_form_field_prep.php';
-
-        $chosen_id = integer_form_field_prep('choice', 1, PHP_INT_MAX);
-
-        $_SESSION['saved_int01'] = $chosen_id;
-
-
-        /**
-         * 2) Retrieve the possible_tax_deduction object with that id from the database.
-         */
-
-        $db = get_db();
-
-        $object = PossibleTaxDeduction::find_by_id($db, $sessionMessage, $chosen_id);
-
-        if (!$object) {
-
-            breakout(' Unexpectedly I could not find that possible tax deduction. ');
-
-        }
-
-
-        /**
-         *  3) Present a form which is populated with data from the possible_tax_deduction object
+         *  4) Present a form which is populated with data from the possible_tax_deduction object
          *    and asks for approval for deletion to proceed.
          */
 
