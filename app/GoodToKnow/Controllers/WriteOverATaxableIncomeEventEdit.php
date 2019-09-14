1a9019684d4d4a37554cd8a0d82e4bf9d0656d79
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-09-13 21:19:13 -0400

    Secured WriteOverATaxableIncomeEventEdit

--- app/GoodToKnow/Controllers/WriteOverATaxableIncomeEventEdit.php
+++ app/GoodToKnow/Controllers/WriteOverATaxableIncomeEventEdit.php
@@ -12,10 +12,13 @@ class WriteOverATaxableIncomeEventEdit
         /**
          * 1) Store the submitted taxable_income_event id in the session.
          * 2) Retrieve the taxable_income_event object with that id from the database.
-         * 3) Present a form which is populated with data from the taxable_income_event object.
+         * 3) Make sure the object belongs to the user.
+         * 4) Present a form which is populated with data from the taxable_income_event object.
          */
 
         global $sessionMessage;
+
+        global $user_id;
 
         kick_out_loggedoutusers();
 
@@ -42,12 +45,25 @@ class WriteOverATaxableIncomeEventEdit
         $object = TaxableIncomeEvent::find_by_id($db, $sessionMessage, $id);
 
         if (!$object) {
+
             breakout(' Unexpectedly, I could not find that taxable income event. ');
+
         }
 
 
         /**
-         * 3) Present a form which is populated with data from the taxable_income_event object.
+         * 3) Make sure the object belongs to the user.
+         */
+
+        if ($object->user_id != $user_id) {
+
+            breakout(' Error 90703010. ');
+
+        }
+
+
+        /**
+         * 4) Present a form which is populated with data from the taxable_income_event object.
          */
 
         $html_title = 'Edit the taxable income event\'s record';
