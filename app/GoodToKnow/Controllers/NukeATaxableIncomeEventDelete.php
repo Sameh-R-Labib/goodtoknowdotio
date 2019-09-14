03db1692843a7347bf47674e31408223c0816c7d
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-09-13 21:45:58 -0400

    Secured NukeATaxableIncomeEventDelete

--- app/GoodToKnow/Controllers/NukeATaxableIncomeEventDelete.php
+++ app/GoodToKnow/Controllers/NukeATaxableIncomeEventDelete.php
@@ -2,9 +2,7 @@
 
 namespace GoodToKnow\Controllers;
 
-use GoodToKnow\Models\TaxableIncomeEvent;
 use function GoodToKnow\ControllerHelpers\get_readable_time;
-use function GoodToKnow\ControllerHelpers\integer_form_field_prep;
 use function GoodToKnow\ControllerHelpers\readable_amount_of_money;
 
 class NukeATaxableIncomeEventDelete
@@ -14,53 +12,27 @@ class NukeATaxableIncomeEventDelete
         /**
          * 1) Store the submitted taxable_income_event record id in the session.
          * 2) Retrieve the taxable_income_event object with that id from the database.
-         * 3) Present a form which is populated with data from the taxable_income_event object
+         * 3) Make sure the object belongs to the user.
+         * 4) Present a form which is populated with data from the taxable_income_event object
          *    and asks for approval for deletion to proceed.
          */
 
-        global $sessionMessage;
 
-        kick_out_loggedoutusers();
-
-        kick_out_onabort();
+        require CONTROLLERINCLUDES . DIRSEP . 'get_the_taxableincomeevent.php';
 
 
         /**
-         * 1) Store the submitted taxable_income_event record id in the session.
-         */
-
-        require_once CONTROLLERHELPERS . DIRSEP . 'integer_form_field_prep.php';
-
-        $chosen_id = integer_form_field_prep('choice', 1, PHP_INT_MAX);
-
-        $_SESSION['saved_int01'] = $chosen_id;
-
-
-        /**
-         * 2) Retrieve the taxable_income_event object with that id from the database.
-         */
-
-        $db = get_db();
-
-        $object = TaxableIncomeEvent::find_by_id($db, $sessionMessage, $chosen_id);
-
-        if (!$object) {
-            breakout(' Unexpectedly I could not find that taxable_income_event record. ');
-        }
-
-
-        /**
-         * 3) Present a form which is populated with data from the taxable_income_event object
+         * 4) Present a form which is populated with data from the taxable_income_event object
          *    and asks for approval for deletion to proceed.
          */
 
-        /**
-         * Replace attributes with more readable ones.
-         */
+        // Replace attributes with more readable ones.
 
         require_once CONTROLLERHELPERS . DIRSEP . 'get_readable_time.php';
 
         require_once CONTROLLERHELPERS . DIRSEP . 'readable_amount_of_money.php';
+
+        /** @noinspection PhpUndefinedVariableInspection */
 
         $object->time = get_readable_time($object->time);
         $object->comment = nl2br($object->comment, false);
--- app/GoodToKnow/Controllers/WriteOverATaxableIncomeEventEdit.php
+++ app/GoodToKnow/Controllers/WriteOverATaxableIncomeEventEdit.php
@@ -2,9 +2,6 @@
 
 namespace GoodToKnow\Controllers;
 
-use GoodToKnow\Models\TaxableIncomeEvent;
-use function GoodToKnow\ControllerHelpers\integer_form_field_prep;
-
 class WriteOverATaxableIncomeEventEdit
 {
     function page()
@@ -16,50 +13,8 @@ class WriteOverATaxableIncomeEventEdit
          * 4) Present a form which is populated with data from the taxable_income_event object.
          */
 
-        global $sessionMessage;
 
-        global $user_id;
-
-        kick_out_loggedoutusers();
-
-        kick_out_onabort();
-
-
-        /**
-         * 1) Store the submitted taxable_income_event id in the session.
-         */
-
-        require_once CONTROLLERHELPERS . DIRSEP . 'integer_form_field_prep.php';
-
-        $id = integer_form_field_prep('choice', 1, PHP_INT_MAX);
-
-        $_SESSION['saved_int01'] = $id;
-
-
-        /**
-         * 2) Retrieve the taxable_income_event object with that id from the database.
-         */
-
-        $db = get_db();
-
-        $object = TaxableIncomeEvent::find_by_id($db, $sessionMessage, $id);
-
-        if (!$object) {
-
-            breakout(' Unexpectedly, I could not find that taxable income event. ');
-
-        }
-
-
-        /**
-         * 3) Make sure the object belongs to the user.
-         */
-
-        if ($object->user_id != $user_id) {
-
-            breakout(' Error 90703010. ');
-
-        }
+        require CONTROLLERINCLUDES . DIRSEP . 'get_the_taxableincomeevent.php';
 
 
         /**
