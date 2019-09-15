e901b1416031d01c577f4d97135d3fbce7e6a148
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-09-14 20:40:19 -0400

    Added fields to MakeARecurringPaymentRecord's view

--- app/GoodToKnow/Views/makearecurringpaymentrecord.php
+++ app/GoodToKnow/Views/makearecurringpaymentrecord.php
@@ -1,15 +1,35 @@
 <?php require TOPFORFORMPAGES; ?>
-<form action="/ax1/MakeARecurringPaymentRecordProcessor/page" method="post">
-    <h1>Create a 🌀 💳 📽</h1>
-    <h2>Initialize the record with its recurring_payment label</h2>
-    <?php require SESSIONMESSAGE; ?>
-    <section>
-        <p>
-            <label for="label">🌀 💳 📽's label (✅ emoji): </label>
-            <input id="label" name="label" type="text" value="" required minlength="4" maxlength="264"
-                   size="67" spellcheck="false">
-        </p>
-    </section>
-    <?php require SUBMITABORT; ?>
-</form>
+    <form action="/ax1/MakeARecurringPaymentRecordProcessor/page" method="post">
+        <h1>Create a 🌀 💳 📽</h1>
+        <h2>Initialize the record with its recurring_payment label</h2>
+        <?php require SESSIONMESSAGE; ?>
+        <section>
+            <p>
+                <label for="label">Label (✅ emoji): </label>
+                <input id="label" name="label" type="text" value="" required minlength="4" maxlength="264"
+                       size="67" spellcheck="false">
+            </p>
+            <p>
+                <label for="currency">Currency (✅ emoji): </label>
+                <input id="currency" name="currency" type="text" value="" required minlength="1" maxlength="15"
+                       size="15">
+            </p>
+            <p>
+                <label for="amount_paid">Amount of currency paid: </label>
+                <input id="amount_paid" name="amount_paid" type="text" value="" required minlength="1" maxlength="16"
+                       size="16">
+            </p>
+            <p>
+                <label for="time">Unix time at last payment: </label>
+                <input id="time" name="time" type="text" value="" minlength="10" maxlength="22" size="22"
+                       placeholder="1560190617">
+            </p>
+            <p>
+                <label for="comment">Comment (🚫 markdown ✅ emoji ✅ line-break): </label>
+                <textarea id="comment" name="comment" rows="4" cols="71" wrap="soft" maxlength="800"
+                          placeholder="The frequency of this payment is _ _ _ _."></textarea>
+            </p>
+        </section>
+        <?php require SUBMITABORT; ?>
+    </form>
 <?php require BOTTOMOFPAGES; ?>
--- app/GoodToKnow/Views/polisharecurringpaymentrecordprocessor.php
+++ app/GoodToKnow/Views/polisharecurringpaymentrecordprocessor.php
@@ -1,34 +1,39 @@
 <?php require TOPFORFORMPAGES; ?>
-<form action="/ax1/PolishARecurringPaymentRecordSubmit/page" method="post">
-    <h2><?php /** @noinspection PhpUndefinedVariableInspection */
-        echo $recurring_payment_object->label; ?></h2>
-    <?php require SESSIONMESSAGE; ?>
-    <p>
-        <label for="label">Label (✅ emoji): </label>
-        <input id="label" name="label" type="text" value="<?php /** @noinspection PhpUndefinedVariableInspection */
-        echo $recurring_payment_object->label; ?>" required minlength="4" maxlength="264" size="67">
-    </p>
-    <p>
-        <label for="currency">Currency (✅ emoji): </label>
-        <input id="currency" name="currency" type="text"
-               value="<?= $recurring_payment_object->currency ?>" required minlength="1" maxlength="15" size="15">
-    </p>
-    <p>
-        <label for="amount_paid">Amount of currency paid: </label>
-        <input id="amount_paid" name="amount_paid" type="text"
-               value="<?= $recurring_payment_object->amount_paid ?>" required minlength="1" maxlength="16" size="16">
-    </p>
-    <p>
-        <label for="time">Unix time at last payment: </label>
-        <input id="time" name="time" type="text"
-               value="<?= $recurring_payment_object->time ?>" minlength="10" maxlength="22"
-               size="22" placeholder="1560190617">
-    </p>
-    <p>
-        <label for="comment">Comment (🚫 markdown ✅ emoji ✅ line-break): </label>
-        <textarea id="comment" name="comment" rows="4" cols="71" wrap="soft" maxlength="800"
-                  placeholder="The frequency of this payment is _ _ _ _."><?= $recurring_payment_object->comment ?></textarea>
-    </p>
-    <?php require SUBMITABORT; ?>
-</form>
+    <form action="/ax1/PolishARecurringPaymentRecordSubmit/page" method="post">
+        <h2><?php /** @noinspection PhpUndefinedVariableInspection */
+            echo $recurring_payment_object->label; ?></h2>
+        <?php require SESSIONMESSAGE; ?>
+        <section>
+            <p>
+                <label for="label">Label (✅ emoji): </label>
+                <input id="label" name="label" type="text"
+                       value="<?php /** @noinspection PhpUndefinedVariableInspection */
+                       echo $recurring_payment_object->label; ?>" required minlength="4" maxlength="264" size="67">
+            </p>
+            <p>
+                <label for="currency">Currency (✅ emoji): </label>
+                <input id="currency" name="currency" type="text"
+                       value="<?= $recurring_payment_object->currency ?>" required minlength="1" maxlength="15"
+                       size="15">
+            </p>
+            <p>
+                <label for="amount_paid">Amount of currency paid: </label>
+                <input id="amount_paid" name="amount_paid" type="text"
+                       value="<?= $recurring_payment_object->amount_paid ?>" required minlength="1" maxlength="16"
+                       size="16">
+            </p>
+            <p>
+                <label for="time">Unix time at last payment: </label>
+                <input id="time" name="time" type="text"
+                       value="<?= $recurring_payment_object->time ?>" minlength="10" maxlength="22"
+                       size="22" placeholder="1560190617">
+            </p>
+            <p>
+                <label for="comment">Comment (🚫 markdown ✅ emoji ✅ line-break): </label>
+                <textarea id="comment" name="comment" rows="4" cols="71" wrap="soft" maxlength="800"
+                          placeholder="The frequency of this payment is _ _ _ _."><?= $recurring_payment_object->comment ?></textarea>
+            </p>
+        </section>
+        <?php require SUBMITABORT; ?>
+    </form>
 <?php require BOTTOMOFPAGES; ?>
