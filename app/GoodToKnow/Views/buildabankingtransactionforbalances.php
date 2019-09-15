15e6056eeb4faae280294132f4e22287b290452c
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-09-15 16:49:01 -0400

    Added fields to BuildABankingTransactionForBalances's view

--- app/GoodToKnow/Views/buildabankingtransactionforbalances.php
+++ app/GoodToKnow/Views/buildabankingtransactionforbalances.php
@@ -1,23 +1,29 @@
 <?php require TOPFORFORMPAGES; ?>
-<form action="/ax1/BuildABankingTransactionForBalancesProcessor/page" method="post">
-    <h1>Create a 🏦ing 🔃 for ⚖️s</h1>
-    <h2>Initialize the banking transaction for balances record with its label and time</h2>
-    <p>
-        <small>✅ emoji for the label.</small>
-    </p>
-    <?php require SESSIONMESSAGE; ?>
-    <section>
+    <form action="/ax1/BuildABankingTransactionForBalancesProcessor/page" method="post">
+        <h1>Create a 🏦ing 🔃 for ⚖️s</h1>
         <p>
-            <label for="label">Label: </label>
-            <input id="label" name="label" type="text" value="" required minlength="3" maxlength="30"
-                   size="30" spellcheck="false" placeholder="Monthly Car Payment">
+            <small>✅ emoji for the label.</small>
         </p>
         <p>
-            <label for="time">Time (unix time stamp): </label>
-            <input id="time" name="time" type="text" value="" required minlength="10" maxlength="22"
-                   size="22" placeholder="1560190617">
+            <small>* Negative (-) amounts mean money you are spending from <b>this</b> account.</small>
         </p>
-    </section>
-    <?php require SUBMITABORT; ?>
-</form>
+        <?php require SESSIONMESSAGE; ?>
+        <section>
+            <p>
+                <label for="label">Label: </label>
+                <input id="label" name="label" type="text" value="" required minlength="3" maxlength="30"
+                       size="30" spellcheck="false" placeholder="Monthly Car Payment">
+            </p>
+            <p>
+                <label for="time">Time (unix time stamp): </label>
+                <input id="time" name="time" type="text" value="" required minlength="10" maxlength="22"
+                       size="22" placeholder="1560190617">
+            </p>
+            <p>
+                <label for="amount">Amount: </label>
+                <input id="amount" name="amount" type="text" value="" required minlength="1" maxlength="16" size="16">
+            </p>
+        </section>
+        <?php require SUBMITABORT; ?>
+    </form>
 <?php require BOTTOMOFPAGES; ?>
--- app/GoodToKnow/Views/revampabankingtransactionforbalancesedit.php
+++ app/GoodToKnow/Views/revampabankingtransactionforbalancesedit.php
@@ -1,30 +1,34 @@
 <?php require TOPFORFORMPAGES; ?>
-<form action="/ax1/RevampABankingTransactionForBalancesUpdate/page" method="post">
-    <h2><?php /** @noinspection PhpUndefinedVariableInspection */
-        echo $object->label; ?></h2>
-    <?php require SESSIONMESSAGE; ?>
-    <p>
-        <small>* Negative (-) amounts mean money you are spending from <b>this</b> account.</small>
-    </p>
-    <p>
-        <label for="label">Label (✅ emoji): </label>
-        <input id="label" name="label" type="text"
-               value="<?= $object->label ?>" required minlength="3" maxlength="30" size="30" spellcheck="false"
-               placeholder="Internet Service Fee">
-    </p>
-    <p>
-        <label for="time">Time (unix time stamp): </label>
-        <input id="time" name="time" type="text"
-               value="<?= $object->time ?>" minlength="10" maxlength="22" size="22" placeholder="1560190617">
-    </p>
-    <p>
-        <label for="amount">Amount: </label>
-        <input id="amount" name="amount" type="text"
-               value="<?= $object->amount ?>" required minlength="1" maxlength="16" size="16">
-    </p>
-    <section>
-        <?= $object->bank_id ?>
-    </section>
-    <?php require SUBMITABORT; ?>
-</form>
+    <form action="/ax1/RevampABankingTransactionForBalancesUpdate/page" method="post">
+        <h1>Edit a 🏦ing 🔃 for ⚖️s</h1>
+        <p>
+            <small>✅ emoji for the label.</small>
+        </p>
+        <p>
+            <small>* Negative (-) amounts mean money you are spending from <b>this</b> account.</small>
+        </p>
+        <?php require SESSIONMESSAGE; ?>
+        <section>
+            <p>
+                <label for="label">Label (✅ emoji): </label>
+                <input id="label" name="label" type="text"
+                       value="<?= $object->label ?>" required minlength="3" maxlength="30" size="30" spellcheck="false"
+                       placeholder="Internet Service Fee">
+            </p>
+            <p>
+                <label for="time">Time (unix time stamp): </label>
+                <input id="time" name="time" type="text"
+                       value="<?= $object->time ?>" minlength="10" maxlength="22" size="22" placeholder="1560190617">
+            </p>
+            <p>
+                <label for="amount">Amount: </label>
+                <input id="amount" name="amount" type="text"
+                       value="<?= $object->amount ?>" required minlength="1" maxlength="16" size="16">
+            </p>
+        </section>
+        <section>
+            <?= $object->bank_id ?>
+        </section>
+        <?php require SUBMITABORT; ?>
+    </form>
 <?php require BOTTOMOFPAGES; ?>
