5b85cf6a7fc145e7c6d94e2ad80ca1cec2985a61
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-09-16 00:05:59 -0400

    to persuade him/her

--- app/GoodToKnow/Views/whatisthissite.php
+++ app/GoodToKnow/Views/whatisthissite.php
@@ -19,8 +19,8 @@
             If you stumble across a different instance you can ask its admin to join.
             Otherwise, if you'd like to create your own instance you can either use the source code yourself found at <a
                     href="https://github.com/Sameh-R-Labib/goodtoknowdotio" target="_blank">https://github.com/Sameh-R-Labib/goodtoknowdotio</a>
-            or you could contact another admin and persuade them to help you set it up. You'll find the email address
-            of each instance's admin on the login page/form.
+            or you could contact an admin and try to persuade him/her to help you set it up. You'll find the email
+            address of each instance's admin on the login page/form.
         </p>
         <p>Thanks and be good to one another. &mdash; <i>Sameh R. Labib</i></p>
     </div><!-- End maincontent -->
