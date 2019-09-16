71d1b44af0417786a3cdc370c53e36106ec6eb1b
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-09-15 23:14:51 -0400

    Improved wording on what is this site

--- app/GoodToKnow/Views/whatisthissite.php
+++ app/GoodToKnow/Views/whatisthissite.php
@@ -9,9 +9,8 @@
             This is for sharing amongst family and trusted acquaintances.
             Additionally it has features which facilitate tracking things which one would normally track in one's own
             life such as recurring payments, banking transactions and recurring to-do list items.</p>
-        <blockquote>GoodToKnow.io instance is run by at least one admin who is in charge of site maintenance and
-            approval of
-            content.
+        <blockquote>GoodToKnow.io instances are run by (at least) one admin who`s in charge of site maintenance and
+            content approval.
         </blockquote>
         <p>If you'd like to become a member of this instance then contact its admin.
             If you stumble across a different instance you can ask its admin to join.
