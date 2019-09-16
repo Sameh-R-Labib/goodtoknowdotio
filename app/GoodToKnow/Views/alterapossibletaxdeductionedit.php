ceb810ad5f446be26c99de52bfb84e5cd01af9cb
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-09-15 21:05:53 -0400

    Added fields to ConceiveAPossibleTaxDeduction

--- app/GoodToKnow/Views/alterapossibletaxdeductionedit.php
+++ app/GoodToKnow/Views/alterapossibletaxdeductionedit.php
@@ -1,30 +1,33 @@
 <?php require TOPFORFORMPAGES; ?>
-<form action="/ax1/AlterAPossibleTaxDeductionUpdate/page" method="post">
-    <h1>Edit a 🏦ing 🔃 for ⚖</h1>
-    <h2><?php /** @noinspection PhpUndefinedVariableInspection */
-        echo $object->label; ?></h2>
-    <p>
-        <small>✅ emoji for the label and comment.</small>
-    </p>
-    <?php require SESSIONMESSAGE; ?>
-    <section>
+    <form action="/ax1/AlterAPossibleTaxDeductionUpdate/page" method="post">
+        <h1>Edit a 🤔 Tax ✍🏽🔽</h1>
+        <p>* Records will be deleted automatically after the fourth year. *</p>
         <p>
-            <label for="label">️Label: </label>
-            <input id="label" name="label" type="text" value="<?= $object->label ?>" required minlength="3"
-                   maxlength="264" size="61" spellcheck="false"
-                   placeholder="Monthly Linode hosting Fees for Web server of goodtoknow.io">
+            <small>✅ emoji for the label and comment.</small>
         </p>
         <p>
-            <label for="year_paid">Year You Made the Expenditure: </label>
-            <input id="year_paid" name="year_paid" type="text" value="<?= $object->year_paid ?>" required minlength="4"
-                   maxlength="6" size="6" placeholder="2018">
+            <small>year_paid is the year in which you spent the money.</small>
         </p>
-        <p>
-            <label for="comment">Comment (🚫 markdown ✅ emoji ✅ line-break): </label>
-            <textarea id="comment" name="comment" rows="4" cols="71" wrap="soft" maxlength="800"
-                      placeholder="List the actual payments here (Assuming there were multiple.)"><?= $object->comment ?></textarea>
-        </p>
-    </section>
-    <?php require SUBMITABORT; ?>
-</form>
+        <?php require SESSIONMESSAGE; ?>
+        <section>
+            <p>
+                <label for="label">️Label: </label>
+                <input id="label" name="label" type="text" value="<?= $object->label ?>" required minlength="3"
+                       maxlength="264" size="61" spellcheck="false"
+                       placeholder="Monthly Linode hosting Fees for Web server of goodtoknow.io">
+            </p>
+            <p>
+                <label for="year_paid">Year You Made the Expenditure: </label>
+                <input id="year_paid" name="year_paid" type="text" value="<?= $object->year_paid ?>" required
+                       minlength="4"
+                       maxlength="6" size="6" placeholder="2018">
+            </p>
+            <p>
+                <label for="comment">Comment (🚫 markdown ✅ emoji ✅ line-break): </label>
+                <textarea id="comment" name="comment" rows="4" cols="71" wrap="soft" maxlength="800"
+                          placeholder="List the actual payments here (Assuming there were multiple.)"><?= $object->comment ?></textarea>
+            </p>
+        </section>
+        <?php require SUBMITABORT; ?>
+    </form>
 <?php require BOTTOMOFPAGES; ?>
--- app/GoodToKnow/Views/conceiveapossibletaxdeduction.php
+++ app/GoodToKnow/Views/conceiveapossibletaxdeduction.php
@@ -1,28 +1,32 @@
 <?php require TOPFORFORMPAGES; ?>
-<form action="/ax1/ConceiveAPossibleTaxDeductionProcessor/page" method="post">
-    <h1>Create a 🤔 Tax ✍🏽🔽</h1>
-    <h2>Initialize the possible_tax_deduction record</h2>
-    <p>* Records will be deleted automatically after the fourth year. *</p>
-    <p>
-        <small>✅ emoji for the label.</small>
-    </p>
-    <p>
-        <small>year_paid is the year in which you spent the money.</small>
-    </p>
-    <?php require SESSIONMESSAGE; ?>
-    <section>
+    <form action="/ax1/ConceiveAPossibleTaxDeductionProcessor/page" method="post">
+        <h1>Create a 🤔 Tax ✍🏽🔽</h1>
+        <p>* Records will be deleted automatically after the fourth year. *</p>
         <p>
-            <label for="label">🤔 Tax ✍🏽🔽 Label: </label>
-            <input id="label" name="label" type="text" value="" required minlength="3" maxlength="264"
-                   size="61" spellcheck="false"
-                   placeholder="Monthly Linode hosting Fees for Web server of goodtoknow.io">
+            <small>✅ emoji for the label and comment.</small>
         </p>
         <p>
-            <label for="year_paid">Year You Made the Expenditure: </label>
-            <input id="year_paid" name="year_paid" type="text" value="" required minlength="4" maxlength="6"
-                   size="6" placeholder="2018">
+            <small>year_paid is the year in which you spent the money.</small>
         </p>
-    </section>
-    <?php require SUBMITABORT; ?>
-</form>
+        <?php require SESSIONMESSAGE; ?>
+        <section>
+            <p>
+                <label for="label">Label: </label>
+                <input id="label" name="label" type="text" value="" required minlength="3" maxlength="264"
+                       size="61" spellcheck="false"
+                       placeholder="Monthly Linode hosting Fees for Web server of goodtoknow.io">
+            </p>
+            <p>
+                <label for="year_paid">Year You Made the Expenditure: </label>
+                <input id="year_paid" name="year_paid" type="text" value="" required minlength="4" maxlength="6"
+                       size="6" placeholder="2018">
+            </p>
+            <p>
+                <label for="comment">Comment (🚫 markdown ✅ emoji ✅ line-break): </label>
+                <textarea id="comment" name="comment" rows="4" cols="71" wrap="soft" maxlength="800"
+                          placeholder="List the actual payments here (Assuming there were multiple.)"></textarea>
+            </p>
+        </section>
+        <?php require SUBMITABORT; ?>
+    </form>
 <?php require BOTTOMOFPAGES; ?>
