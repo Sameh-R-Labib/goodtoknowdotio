202fb16c721a912f2ff8c62d05ca67375e314574
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-09-18 11:59:08 -0400

    Improved the view for CreateNewPostProcessor

--- app/GoodToKnow/Views/createnewpost.php
+++ app/GoodToKnow/Views/createnewpost.php
@@ -1,18 +1,17 @@
 <?php require TOPFORFORMPAGES; ?>
-<form action="/ax1/CreateNewPostProcessor/page" method="post">
-    <h1>Create 📄</h1>
-    <h2>Create New Post</h2>
-    <p>These are topics in the current community.</p>
-    <p>Which topic is for the new post?</p>
-    <?php require SESSIONMESSAGE; ?>
-    <section>
-        <?php foreach ($special_topic_array as $key => $value): ?>
-            <label for="choice-<?php echo $key; ?>" class="radio">
-                <input type="radio" id="choice-<?= $key ?>" name="choice" value="<?= $key ?>">
-                <?= $value ?>
-            </label>
-        <?php endforeach; ?>
-    </section>
-    <?php require SUBMITABORT; ?>
-</form>
+    <form action="/ax1/CreateNewPostProcessor/page" method="post">
+        <h1>Create a 📄</h1>
+        <p>Which topic is for the new post?</p>
+        <?php require SESSIONMESSAGE; ?>
+        <section>
+            <?php foreach ($special_topic_array as $key => $value): ?>
+                <label for="choice-<?php echo $key; ?>" class="radio">
+                    <input type="radio" id="choice-<?= $key ?>" name="choice" value="<?= $key ?>">
+                    <?= $value ?>
+                </label>
+            <?php endforeach; ?>
+        </section>
+        <p><small>* These are topics in the current community.</small></p>
+        <?php require SUBMITABORT; ?>
+    </form>
 <?php require BOTTOMOFPAGES; ?>
