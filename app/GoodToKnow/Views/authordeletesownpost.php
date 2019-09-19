57dc4eadfc035ac14301eed01afa36b3f964abcb
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-09-18 20:38:49 -0400

    AuthorDeletesOwnPost improvements

--- app/GoodToKnow/Views/authordeletesownpost.php
+++ app/GoodToKnow/Views/authordeletesownpost.php
@@ -1,19 +1,18 @@
 <?php require TOPFORFORMPAGES; ?>
-<form action="/ax1/AuthorDeletesOwnPostProcessor/page" method="post">
-    <h1>Author Deletes Own 📄</h1>
-    <h2>Author Deletes Own Post</h2>
-    <p>Topics in current community:</p>
-    <p>In which topic?</p>
-    <?php require SESSIONMESSAGE; ?>
-    <section>
-        <?php /** @noinspection PhpUndefinedVariableInspection */
-        foreach ($special_topic_array as $key => $value): ?>
-            <label for="choice-<?php echo $key; ?>" class="radio">
-                <input type="radio" id="choice-<?= $key ?>" name="choice" value="<?= $key ?>">
-                <?= $value ?>
-            </label>
-        <?php endforeach; ?>
-    </section>
-    <?php require SUBMITABORT; ?>
-</form>
+    <form action="/ax1/AuthorDeletesOwnPostProcessor/page" method="post">
+        <h1>Author Deletes Own 📄 (Post)</h1>
+        <p>From which topic?</p>
+        <?php require SESSIONMESSAGE; ?>
+        <section>
+            <?php /** @noinspection PhpUndefinedVariableInspection */
+            foreach ($special_topic_array as $key => $value): ?>
+                <label for="choice-<?php echo $key; ?>" class="radio">
+                    <input type="radio" id="choice-<?= $key ?>" name="choice" value="<?= $key ?>">
+                    <?= $value ?>
+                </label>
+            <?php endforeach; ?>
+        </section>
+        <p><small>* These are the topics in your <b>current community</b>.</small></p>
+        <?php require SUBMITABORT; ?>
+    </form>
 <?php require BOTTOMOFPAGES; ?>
