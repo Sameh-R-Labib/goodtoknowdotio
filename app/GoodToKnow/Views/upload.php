677eec36dd7f690163cfd545cf6865183a4b0bd4
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-09-24 14:21:42 -0400

    First info tooltip

--- app/GoodToKnow/Views/upload.php
+++ app/GoodToKnow/Views/upload.php
@@ -2,6 +2,9 @@
 <form action="/ax1/UploadProcessor/page" method="post" enctype="multipart/form-data">
     <h1>Upload an 🖼️</h1>
     <h2>Choose the file from your 🖥️</h2>
+    <div class="tooltip">ℹ️
+        <span class="tooltiptext tooltip-top">Only .jpg, .jpeg, .png, .gif files can be uploaded.</span>
+    </div>
     <p>* Only .jpg, .jpeg, .png, .gif files can be uploaded.</p>
     <?php require SESSIONMESSAGE; ?>
     <section>
