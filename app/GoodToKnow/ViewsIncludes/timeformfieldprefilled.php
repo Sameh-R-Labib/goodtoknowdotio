428e52a130f9ed2b8478677850dab473c2784221
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-09-30 16:13:25 -0400

    Did the straight version of time for pre-filled

--- app/GoodToKnow/ViewsIncludes/timeformfieldprefilled.php
+++ app/GoodToKnow/ViewsIncludes/timeformfieldprefilled.php
@@ -1,28 +1,21 @@
-<p>
-    <label for="date">Date (mm/dd/yyyy): </label>
-    <input id="date" name="date" type="text" required minlength="10" maxlength="14" size="14"
-           spellcheck="false" placeholder="06/21/2009" value="<?= $time['date'] ?>">
-</p>
-<p>
-    <label for="hour">Hour (range 0-23): </label>
-    <input id="hour" name="hour" type="text" required minlength="1" maxlength="2" size="2"
-           spellcheck="false" placeholder="14" value="<?= $time['hour'] ?>">
-</p>
-<p>
-    <label for="minute">Minute (range 0-59): </label>
-    <input id="minute" name="minute" type="text" required minlength="1" maxlength="2" size="2"
-           spellcheck="false" placeholder="27" value="<?= $time['minute'] ?>">
-</p>
-<p>
-    <label for="second">Second (range 0-59): </label>
-    <input id="second" name="second" type="text" required minlength="1" maxlength="2" size="2"
-           spellcheck="false" placeholder="52" value="<?= $time['second'] ?>">
-</p>
-<p>
-    <label for="timezone">PHP Time Zone <span class="tooltip">ℹ️<span class="tooltiptext tooltip-top">See
-                            <a href="https://www.php.net/manual/en/timezones.php"
-                               target="_blank">www.php.net/manual/en/timezones.php</a></span></span>️:
-    </label>
-    <input id="timezone" name="timezone" type="text" placeholder="America/New_York" required
+<p>️
+    <label for="date"></label>
+    <input id="date" name="date" type="text" required minlength="10" maxlength="14" size="14" placeholder="mm/dd/yyyy"
+           value="<?= $time['date'] ?>">
+    <span class="tooltip">ℹ️<span class="tooltiptext tooltip-top">hr is military hour (0-23)</span></span>
+    <label for="hour"></label>
+    <input id="hour" name="hour" type="text" required minlength="1" maxlength="2" size="2" placeholder="hr"
+           value="<?= $time['hour'] ?>">
+    <label for="minute">: </label>
+    <input id="minute" name="minute" type="text" required minlength="1" maxlength="2" size="2" placeholder="mn"
+           value="<?= $time['minute'] ?>">
+    <label for="second">: </label>
+    <input id="second" name="second" type="text" required minlength="1" maxlength="2" size="2" placeholder="sd"
+           value="<?= $time['second'] ?>">
+    <label for="timezone"></label>
+    <input id="timezone" name="timezone" type="text" placeholder="PHP Timezone" value="" required
            minlength="2" maxlength="60" size="18" value="<?php echo date_default_timezone_get(); ?>">
+    <span class="tooltip">ℹ️<span class="tooltiptext tooltip-top">See
+                            <a href="https://www.php.net/manual/en/timezones.php"
+                               target="_blank">www.php.net/manual/en/timezones.php</a></span></span>
 </p>
