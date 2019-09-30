3ecbcf4fc4023c441e475d1ccbc54b06e25f395d
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-09-30 16:52:44 -0400

    Debug A

--- app/GoodToKnow/Controllers/EditABitcoinRecordProcessor.php
+++ app/GoodToKnow/Controllers/EditABitcoinRecordProcessor.php
@@ -22,6 +22,21 @@ class EditABitcoinRecordProcessor
 
 
         /**
+         * Debug Code
+         */
+        $t = date_default_timezone_get();
+
+        echo "\n<p>Begin debug</p>\n";
+        echo "<p>Var_dump \$t: </p>\n<pre>";
+        var_dump($t);
+        echo "</pre>\n";
+        echo "<p>Var_dump \$timezone: </p>\n<pre>";
+        var_dump($timezone);
+        echo "</pre>\n";
+        die("<p>End debug</p>\n");
+
+
+        /**
          * 4) Present a form which is populated with data from the object.
          *    (except do the bitcoin address should not be changeable.)
          */
