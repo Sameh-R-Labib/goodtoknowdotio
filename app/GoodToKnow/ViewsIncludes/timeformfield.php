a976d60955851136b2dbd5d1e0d40cc7fdf1a6b3
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-09-30 15:52:25 -0400

    Arranged time to appear as horizontal field in 1 of 4 places

--- app/GoodToKnow/ViewsIncludes/timeformfield.php
+++ app/GoodToKnow/ViewsIncludes/timeformfield.php
@@ -1,28 +1,16 @@
-<p>
-    <label for="date">Date (mm/dd/yyyy): </label>
-    <input id="date" name="date" type="text" required minlength="10" maxlength="14" size="14"
-           spellcheck="false" placeholder="06/21/2009">
-</p>
-<p>
-    <label for="hour">Hour (range 0-23): </label>
-    <input id="hour" name="hour" type="text" required minlength="1" maxlength="2" size="2"
-           spellcheck="false" placeholder="14">
-</p>
-<p>
-    <label for="minute">Minute (range 0-59): </label>
-    <input id="minute" name="minute" type="text" required minlength="1" maxlength="2" size="2"
-           spellcheck="false" placeholder="27">
-</p>
-<p>
-    <label for="second">Second (range 0-59): </label>
-    <input id="second" name="second" type="text" required minlength="1" maxlength="2" size="2"
-           spellcheck="false" placeholder="52">
-</p>
-<p>
-    <label for="timezone">PHP Time Zone <span class="tooltip">ℹ️<span class="tooltiptext tooltip-top">See
-                            <a href="https://www.php.net/manual/en/timezones.php"
-                               target="_blank">www.php.net/manual/en/timezones.php</a></span></span>️:
-    </label>
-    <input id="timezone" name="timezone" type="text" placeholder="America/New_York" value="" required
+<p>️Express hour as military hour (0-23)<br>
+    <label for="date"></label>
+    <input id="date" name="date" type="text" required minlength="10" maxlength="14" size="14" placeholder="mm/dd/yyyy">
+    <label for="hour"></label>
+    <input id="hour" name="hour" type="text" required minlength="1" maxlength="2" size="2" placeholder="hr">
+    <label for="minute">: </label>
+    <input id="minute" name="minute" type="text" required minlength="1" maxlength="2" size="2" placeholder="mn">
+    <label for="second">: </label>
+    <input id="second" name="second" type="text" required minlength="1" maxlength="2" size="2" placeholder="sd">
+    <label for="timezone"></label>
+    <input id="timezone" name="timezone" type="text" placeholder="PHP Timezone" value="" required
            minlength="2" maxlength="60" size="18">
+    <span class="tooltip">ℹ️<span class="tooltiptext tooltip-top">See
+                            <a href="https://www.php.net/manual/en/timezones.php"
+                               target="_blank">www.php.net/manual/en/timezones.php</a></span></span>
 </p>
