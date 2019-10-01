5ed030802a6b3f544f7eae59a69aa0764d5bc171
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-10-01 14:24:15 -0400

    Modified PolishARecurringPaymentRecordProcessor to show readable price_point

--- app/GoodToKnow/Controllers/PolishARecurringPaymentRecordProcessor.php
+++ app/GoodToKnow/Controllers/PolishARecurringPaymentRecordProcessor.php
@@ -3,6 +3,7 @@
 namespace GoodToKnow\Controllers;
 
 use function GoodToKnow\ControllerHelpers\get_date_h_m_s_from_a_timestamp;
+use function GoodToKnow\ControllerHelpers\readable_amount_no_commas;
 
 class PolishARecurringPaymentRecordProcessor
 {
@@ -24,6 +25,15 @@ class PolishARecurringPaymentRecordProcessor
         /**
          * 4) Present a form which is populated with data from the recurring_payment object.
          */
+
+
+        /**
+         * Make it so that if price_point is fiat then price_point has only two decimal places.
+         */
+
+        require CONTROLLERHELPERS . DIRSEP . 'readable_amount_no_commas.php';
+
+        $recurring_payment_object->amount_paid = readable_amount_no_commas($recurring_payment_object->currency, $recurring_payment_object->amount_paid);
 
 
         /**
