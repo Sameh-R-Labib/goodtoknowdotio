1bdd4b11ae0a71317c8cc2cf1810951f4220cdf7
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-10-01 16:27:03 -0400

    Modified PopulateABankingAccountForBalancesProcessor to show readable price_point

--- app/GoodToKnow/Controllers/PopulateABankingAccountForBalancesProcessor.php
+++ app/GoodToKnow/Controllers/PopulateABankingAccountForBalancesProcessor.php
@@ -3,6 +3,7 @@
 namespace GoodToKnow\Controllers;
 
 use function GoodToKnow\ControllerHelpers\get_date_h_m_s_from_a_timestamp;
+use function GoodToKnow\ControllerHelpers\readable_amount_no_commas;
 
 class PopulateABankingAccountForBalancesProcessor
 {
@@ -24,6 +25,15 @@ class PopulateABankingAccountForBalancesProcessor
         /**
          * 4) Present a form which is populated with data from the banking_acct_for_balances object.
          */
+
+
+        /**
+         * Make it so that if price_point is fiat then price_point has only two decimal places.
+         */
+
+        require CONTROLLERHELPERS . DIRSEP . 'readable_amount_no_commas.php';
+
+        $object->start_balance = readable_amount_no_commas($object->currency, $object->start_balance);
 
 
         /**
