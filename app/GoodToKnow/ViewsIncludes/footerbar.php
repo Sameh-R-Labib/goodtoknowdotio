c5a75a3e58895816713ca3f5799647749a2435d2
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-10-01 19:03:24 -0400

    Adjusted footer image dimensions

--- app/GoodToKnow/ViewsIncludes/footerbar.php
+++ app/GoodToKnow/ViewsIncludes/footerbar.php
@@ -1,9 +1,9 @@
 <!-- footerbar -->
 <div id="footerbar">
     <p align="center" style="font-size: 1em;">
-        <img src="/Gnu-head-30-years-anniversary.svg" style="float:left;height: 32px;width: 32px;margin-top: -0px">
+        <img src="/Gnu-head-30-years-anniversary.svg" style="float:left;height: 28px;width: 28px;margin-top: -0px">
         © 2018 - Sameh Ramzy Labib
         <img src="/2000px-GPLv3_Logo.svg.png"
-             height="32" width="70"
+             height="28" width="66"
              style="float:right;;margin-top: -0px"></p>
 </div>
