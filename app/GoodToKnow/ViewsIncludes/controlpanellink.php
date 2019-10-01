46cb76624856a64e3d3ca4b22d6a827673e2644c
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-10-01 17:28:51 -0400

    Removed the CP link tooltip

--- app/GoodToKnow/ViewsIncludes/controlpanellink.php
+++ app/GoodToKnow/ViewsIncludes/controlpanellink.php
@@ -1,15 +1,7 @@
 <?php if ($is_admin): ?>
-    <div class="tooltip"><a href="/ax1/AdminHome/page"><img src="/cpicon.png" alt="Admin Panel" height="86"
-                                                            width="108"></a>
-        <span class="tooltiptext tooltip-top">Admin Control Panel</span>
-    </div>
+    <a href="/ax1/AdminHome/page"><img src="/cpicon.png" alt="Admin Panel" height="86" width="108"></a>
 <?php elseif ($is_guest): ?>
-    <div class="tooltip"><img src="/cpicon.png" alt="Admin Panel" height="86" width="108">
-        <span class="tooltiptext tooltip-top">Control Panel</span>
-    </div>
+    <img src="/cpicon.png" alt="Admin Panel" height="86" width="108">
 <?php else: ?>
-    <div class="tooltip"><a href="/ax1/ControlPanel/page"><img src="/cpicon.png" alt="Admin Panel" height="86"
-                                                               width="108"></a>
-        <span class="tooltiptext tooltip-top">Control Panel</span>
-    </div>
+    <a href="/ax1/ControlPanel/page"><img src="/cpicon.png" alt="Admin Panel" height="86" width="108"></a>
 <?php endif; ?>
