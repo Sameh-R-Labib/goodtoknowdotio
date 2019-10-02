9b99bc62b82544d7b19a8e1047a61374667f7b52
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-10-01 23:29:36 -0400

    onclick="/ax1/ClearSessionVars/page" FIXED

--- app/GoodToKnow/ViewsIncludes/submitabort.php
+++ app/GoodToKnow/ViewsIncludes/submitabort.php
@@ -1,6 +1,6 @@
 <section>
     <p>
         <button type="submit" name="submit" value="Submit">Submit</button>
-        <button class="abort" onclick="/ax1/ClearSessionVars/page">Abort</button>
+        <button class="abort" onclick="window.location.href = '/ax1/ClearSessionVars/page';">Abort</button>
     </p>
 </section>
