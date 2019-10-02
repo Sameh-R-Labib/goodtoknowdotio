b9b00dd91df0a86aec63aeadcfc4f7c56f4523a3
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-10-01 23:41:51 -0400

    display: inline-block;

--- app/GoodToKnow/ViewsIncludes/submitabort.php
+++ app/GoodToKnow/ViewsIncludes/submitabort.php
@@ -1,6 +1,6 @@
 <section>
     <p>
         <button type="submit" name="submit" value="Submit">Submit</button>
-        <a class="abort" href="/ax1/ClearSessionVars/page" class="button">Click Here</a>
+        <a class="abort" href="/ax1/ClearSessionVars/page" class="button">Abort</a>
     </p>
 </section>
