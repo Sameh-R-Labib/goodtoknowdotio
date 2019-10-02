567aa0e19704d31828bc5412c97d602fd5f9f28c
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-10-02 14:35:56 -0400

    FatherACommoditySold page()

--- app/GoodToKnow/Controllers/FatherACommoditySold.php
+++ app/GoodToKnow/Controllers/FatherACommoditySold.php
@@ -6,6 +6,18 @@ class FatherACommoditySold
 {
     function page()
     {
+        /**
+         * This feature enables any user to create a database record in the commodities_sold table.
+         */
 
+        global $sessionMessage;
+
+        global $timezone;
+
+        kick_out_loggedoutusers();
+
+        $html_title = 'Create a Commodity Sold';
+
+        require VIEWS . DIRSEP . 'fatheracommoditysold.php';
     }
 }
--- app/GoodToKnow/Controllers/StartATaxableIncomeEvent.php
+++ app/GoodToKnow/Controllers/StartATaxableIncomeEvent.php
@@ -8,15 +8,7 @@ class StartATaxableIncomeEvent
     {
         /**
          * This feature enables any user to create a database record in the
-         * taxable_income_event table. The process will ask the user to ONLY
-         * supply a taxable_income_event label + year_received + time . And
-         * the remaining field values will be supplied by the editor for this
-         * type of record.
-         */
-
-        /**
-         * This here script simply presents a form for the user to supply the taxable_income_event
-         * label + year_received + time for the "to be created" taxable_income_event record.
+         * taxable_income_event table.
          */
 
         global $sessionMessage;
