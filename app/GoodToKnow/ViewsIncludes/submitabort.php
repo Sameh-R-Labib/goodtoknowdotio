baceb3d61775731c07c42d4d89308f1d41f13b87
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-10-01 23:38:37 -0400

    Trying it as a link

--- app/GoodToKnow/ViewsIncludes/submitabort.php
+++ app/GoodToKnow/ViewsIncludes/submitabort.php
@@ -1,6 +1,6 @@
 <section>
     <p>
         <button type="submit" name="submit" value="Submit">Submit</button>
-        <button class="abort" onclick="window.location.href = '/ax1/ClearSessionVars/page';">Abort</button>
+        <a class="abort" href="/ax1/ClearSessionVars/page" class="button">Click Here</a>
     </p>
 </section>
