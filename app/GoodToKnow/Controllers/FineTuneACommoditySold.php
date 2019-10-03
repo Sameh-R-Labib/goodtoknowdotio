5193a96ff014908011527fa1d6f709b5a2323c51
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-10-02 21:15:26 -0400

    FineTuneACommoditySold page()

--- app/GoodToKnow/Controllers/FineTuneACommoditySold.php
+++ app/GoodToKnow/Controllers/FineTuneACommoditySold.php
@@ -6,6 +6,17 @@ class FineTuneACommoditySold
 {
     function page()
     {
+        /**
+         * This page is going to present a text box for entering a tax_year value to be used
+         * to narrow down the choices for which CommoditySold to edit.
+         */
 
+        global $sessionMessage;
+
+        kick_out_loggedoutusers();
+
+        $html_title = 'Which tax year?';
+
+        require VIEWS . DIRSEP . 'finetuneacommoditysold.php';
     }
 }
