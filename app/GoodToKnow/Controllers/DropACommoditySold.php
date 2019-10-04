3d9a40f77b1adc3ee194f654537211b29791686a
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-10-04 12:03:37 -0400

    DropACommoditySold page()

--- app/GoodToKnow/Controllers/DropACommoditySold.php
+++ app/GoodToKnow/Controllers/DropACommoditySold.php
@@ -6,6 +6,19 @@ class DropACommoditySold
 {
     function page()
     {
+        /**
+         * Ultimately, this is about deleting a CommoditySold.
+         *
+         * This page is going to present a text box for entering a tax_year to be used to narrow down the choices
+         * for which CommoditySold to delete.
+         */
 
+        global $sessionMessage;
+
+        kick_out_loggedoutusers();
+
+        $html_title = 'Which tax year?';
+
+        require VIEWS . DIRSEP . 'dropacommoditysold.php';
     }
 }
--- app/GoodToKnow/Controllers/NukeATaxableIncomeEvent.php
+++ app/GoodToKnow/Controllers/NukeATaxableIncomeEvent.php
@@ -9,7 +9,7 @@ class NukeATaxableIncomeEvent
         /**
          * Ultimately, this is about deleting a TaxableIncomeEvent.
          *
-         * This page is going to present a text box for entering a year_received value to be used
+         * This page is going to present a text box for entering a year_received to be used
          * to narrow down the choices for which taxable_income_event to delete.
          */
 
