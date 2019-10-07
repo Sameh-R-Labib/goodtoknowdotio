d8ce5ce8c6eeeca9f284ad6f568b26d15af5f7b9
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-10-07 13:26:48 -0400

    Debug 1

--- app/GoodToKnow/Controllers/BlotOutSomeInboxMessagesProcessor.php
+++ app/GoodToKnow/Controllers/BlotOutSomeInboxMessagesProcessor.php
@@ -9,5 +9,15 @@ class BlotOutSomeInboxMessagesProcessor
         /**
          *
          */
+
+        global $sessionMessage;
+
+        global $user_id;
+
+        kick_out_loggedoutusers();
+
+        echo "<p>Var_dump \$_POST: </p>\n<pre>";
+        var_dump($_POST);
+        echo "</pre>\n";
     }
 }
