d20687263f94418f447688299f6674fd0f7eeb54
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-10-09 17:33:06 -0400

    CPCapitalGains buttons

--- app/GoodToKnow/Views/cpcapitalgains.php
+++ app/GoodToKnow/Views/cpcapitalgains.php
@@ -4,12 +4,11 @@
     <!-- maincontent -->
     <div id="maincontent">
         <h1>Commodity Sold 📽</h1>
-        <ul>
-            <li><a href="/ax1/FatherACommoditySold/page">Create</a></li>
-            <li><a href="/ax1/FineTuneACommoditySold/page">Edit</a></li>
-            <li><a href="/ax1/SpyCommoditiesSold/page">See One Year's</a></li>
-            <li><a href="/ax1/DropACommoditySold/page">Delete</a></li>
-        </ul>
+        <p>
+            <a class="greenbtn" href="/ax1/FatherACommoditySold/page">Create</a>
+            <a class="purplebtn" href="/ax1/FineTuneACommoditySold/page">Edit</a>
+            <a class="blackbtn" href="/ax1/SpyCommoditiesSold/page">See One Year's</a>
+            <a class="orangebtn" href="/ax1/DropACommoditySold/page">Delete</a></p>
     </div><!-- End maincontent -->
 <?php require FOOTERBAR; ?>
 <?php require BOTTOMOFPAGES; ?>
