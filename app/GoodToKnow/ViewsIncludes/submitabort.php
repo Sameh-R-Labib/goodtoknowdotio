b34a373ce938acf67af78e0d80afd62176a29cf2
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-10-08 21:38:48 -0400

    Removed class="button"

--- app/GoodToKnow/ViewsIncludes/submitabort.php
+++ app/GoodToKnow/ViewsIncludes/submitabort.php
@@ -1,6 +1,6 @@
 <section>
     <p>
         <button type="submit" name="submit" value="Submit">Submit</button>
-        <a class="abort" href="/ax1/ClearSessionVars/page" class="button">Abort</a>
+        <a class="abort" href="/ax1/ClearSessionVars/page">Abort</a>
     </p>
 </section>
