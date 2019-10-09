f63a778e07ebac94080b0a60130bbdf261778a14
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-10-09 17:16:22 -0400

    CPCrypto buttons

--- app/GoodToKnow/Views/cpcrypto.php
+++ app/GoodToKnow/Views/cpcrypto.php
@@ -4,12 +4,11 @@
     <!-- maincontent -->
     <div id="maincontent">
         <h1>Crypto</h1>
-        <ul>
-            <li><a href="/ax1/InitializeABitcoinRecord/page">Create</a></li>
-            <li><a href="/ax1/EditABitcoinRecord/page">Edit</a></li>
-            <li><a href="/ax1/BitcoinSeeMyRecords/page">See all</a></li>
-            <li><a href="/ax1/DeleteABitcoinRecord/page">Delete</a></li>
-        </ul>
+        <p>
+            <a class="blackbtn" href="/ax1/InitializeABitcoinRecord/page">Create</a>
+            <a class="orangebtn" href="/ax1/EditABitcoinRecord/page">Edit</a>
+            <a class="clearbtn" href="/ax1/BitcoinSeeMyRecords/page">See all</a>
+            <a class="orangebtn" href="/ax1/DeleteABitcoinRecord/page">Delete‍</a></p>
     </div><!-- End maincontent -->
 <?php require FOOTERBAR; ?>
 <?php require BOTTOMOFPAGES; ?>
