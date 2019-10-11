2ecfe66910cf656c40e66912dc3f1e61bfab894b
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-10-10 23:08:46 -0400

    Fingers crossed

--- app/GoodToKnow/Views/home.php
+++ app/GoodToKnow/Views/home.php
@@ -6,7 +6,7 @@
         <h1><?php include HEADINGONE; ?></h1>
         <?php if ($type_of_resource_requested === 'community') include LISTTOPICS; ?>
         <?php if ($type_of_resource_requested === 'topic') include LISTPOSTS; ?>
-        <?php if ($type_of_resource_requested === 'post' AND !empty(trim($post_content))): ?>
+        <?php if ($type_of_resource_requested === 'post' AND empty(trim($post_content))): ?>
             <?= $post_content ?>
         <?php else: ?>
             <p><em>[No post content]</em></p>
