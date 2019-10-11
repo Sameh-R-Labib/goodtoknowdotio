b0b450da27558ca80b7341f01a65cae3cc0a5680
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-10-10 22:11:16 -0400

    Messages indicating no topics and no posts

--- app/GoodToKnow/ViewsIncludes/listposts.php
+++ app/GoodToKnow/ViewsIncludes/listposts.php
@@ -1,3 +1,6 @@
+<?php if (empty($special_post_array)): ?>
+    <em>[No posts in this topic]</em>
+<?php endif; ?>
 <?php foreach ($special_post_array as $key => $value): ?>
     <p>
         <a href="/ax1/SetHomePageCommunityTopicPost/page/<?= $community_id ?>/<?= $topic_id ?>/<?= $key ?>"><?= $value ?></a>
--- app/GoodToKnow/ViewsIncludes/listtopics.php
+++ app/GoodToKnow/ViewsIncludes/listtopics.php
@@ -1,3 +1,6 @@
+<?php if (empty($special_topic_array)): ?>
+    <em>[No topics in this community]</em>
+<?php endif; ?>
 <?php foreach ($special_topic_array as $key => $value): ?>
     <p>
         <a href="/ax1/SetHomePageCommunityTopicPost/page/<?= $community_id ?>/<?= $key ?>/0"><?= $value ?></a>
