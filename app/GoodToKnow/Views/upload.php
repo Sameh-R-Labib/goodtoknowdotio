95961cf29646fc0a7de50c7bd3800b928277f6f7
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-10-10 22:44:48 -0400

    Enhanced the info about uploads

--- app/GoodToKnow/Views/upload.php
+++ app/GoodToKnow/Views/upload.php
@@ -1,19 +1,20 @@
 <?php require TOPFORFORMPAGES; ?>
-<form action="/ax1/UploadProcessor/page" method="post" enctype="multipart/form-data">
-    <h1>Upload an 🖼️</h1>
-    <h2>Choose the file from your 🖥️</h2>
-    <p class="tooltip">ℹ️
-        <span class="tooltiptext tooltip-top">Only .jpg, .jpeg, .png, .gif files can be uploaded.<br><br>
-        Please do Not upload an image if you are not able and willing to insert that image in a post immediately.
-        Basically, the upload process will yield a link which you can use within image placement markdown
-            or as a hyperlink to the image.</span>
-    </p>
-    <?php require SESSIONMESSAGE; ?>
-    <section>
-        <p>
-            <input id="fileToUpload" name="fileToUpload" type="file">
+    <form action="/ax1/UploadProcessor/page" method="post" enctype="multipart/form-data">
+        <h1>Upload an 🖼️</h1>
+        <h2>Choose the file from your 🖥️</h2>
+        <p class="tooltip">ℹ️
+            <span class="tooltiptext tooltip-top">Only .jpg, .jpeg, .png, .gif files can be uploaded.<br><br>
+        Please do Not upload an image if you are not able/willing to insert that image in your post immediately.
+        Basically, the upload process will yield a URL which you can use within image placement markdown
+            or as a hyperlink markdown to the image. Review markdown syntax first. A link to the markdown
+        documentation is in the editor.</span>
         </p>
-    </section>
-    <?php require SUBMITABORT; ?>
-</form>
+        <?php require SESSIONMESSAGE; ?>
+        <section>
+            <p>
+                <input id="fileToUpload" name="fileToUpload" type="file">
+            </p>
+        </section>
+        <?php require SUBMITABORT; ?>
+    </form>
 <?php require BOTTOMOFPAGES; ?>
