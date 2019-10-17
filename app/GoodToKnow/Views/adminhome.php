c99b8bf74d78626176020f8e147a940f45e26e01
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-10-16 21:52:42 -0400

    Improved Control Panels

--- app/GoodToKnow/Views/adminhome.php
+++ app/GoodToKnow/Views/adminhome.php
@@ -4,20 +4,20 @@
     <!-- maincontent -->
     <div id="maincontent">
         <h1>Control Panel</h1>
-        <h2 class="topofpage">👤 🧰</h2>
-        <p><a class="clearbtn" href="/ax1/CPBasics/page">Basics</a>
-            <a class="greenbtn" href="/ax1/CPTransactions/page">🏦ing Transactions</a>
-            <a class="purplebtn" href="/ax1/CPRecurringPayments/page">🌀 Payments</a>
-            <a class="blackbtn" href="/ax1/CPBankingAccounts/page">🏦ing Accounts</a>
+        <h2 class="topofpage">👤 User Tools 🧰</h2>
+        <p><a class="clearbtn" href="/ax1/CPBasics/page">Settings and Posts</a>
+            <a class="greenbtn" href="/ax1/CPTransactions/page">Bank Transactions</a>
+            <a class="purplebtn" href="/ax1/CPRecurringPayments/page">Recurring Payments</a>
+            <a class="blackbtn" href="/ax1/CPBankingAccounts/page">Bank Accounts</a>
             <a class="orangebtn" href="/ax1/CPCrypto/page">Crypto</a>
             <a class="clearbtn" href="/ax1/CPTaxDeductions/page">Tax Deductions</a>
             <a class="orangebtn" href="/ax1/CPToDoList/page">To-do List</a>
-            <a class="purplebtn" href="/ax1/CPTaxableIncome/page">Taxable Income</a>
-            <a class="blackbtn" href="/ax1/CPCapitalGains/page">Capital Gains</a></p>
-        <h2>♠👔♠ 🧰</h2>
-        <p><a class="greenbtn" href="/ax1/CPPurges/page">Purges</a>
-            <a class="clearbtn" href="/ax1/CPAccounts/page">Accounts</a>
-            <a class="orangebtn" href="/ax1/CPPostings/page">Postings</a>
+            <a class="purplebtn" href="/ax1/CPTaxableIncome/page">Possibly Taxable Income</a>
+            <a class="blackbtn" href="/ax1/CPCapitalGains/page">Commodity Sold Capital Gains</a></p>
+        <h2>♠👔♠ Admin Tools 🧰</h2>
+        <p><a class="greenbtn" href="/ax1/CPPurges/page">Purging Old Stuff</a>
+            <a class="clearbtn" href="/ax1/CPAccounts/page">Account Management</a>
+            <a class="orangebtn" href="/ax1/CPPostings/page">Post Related</a>
         </p>
         <?php require COLLAGE; ?>
     </div>
--- app/GoodToKnow/Views/controlpanel.php
+++ app/GoodToKnow/Views/controlpanel.php
@@ -4,16 +4,16 @@
     <!-- maincontent -->
     <div id="maincontent">
         <h1>Control Panel</h1>
-        <h2 class="topofpage">👤 🧰</h2>
-        <p><a class="clearbtn" href="/ax1/CPBasics/page">Basics</a>
-            <a class="greenbtn" href="/ax1/CPTransactions/page">🏦ing Transactions</a>
-            <a class="purplebtn" href="/ax1/CPRecurringPayments/page">🌀 Payments</a>
-            <a class="blackbtn" href="/ax1/CPBankingAccounts/page">🏦ing Accounts</a>
+        <h2 class="topofpage">👤 User Tools 🧰</h2>
+        <p><a class="clearbtn" href="/ax1/CPBasics/page">Settings and Posts</a>
+            <a class="greenbtn" href="/ax1/CPTransactions/page">Bank Transactions</a>
+            <a class="purplebtn" href="/ax1/CPRecurringPayments/page">Recurring Payments</a>
+            <a class="blackbtn" href="/ax1/CPBankingAccounts/page">Bank Accounts</a>
             <a class="orangebtn" href="/ax1/CPCrypto/page">Crypto</a>
             <a class="clearbtn" href="/ax1/CPTaxDeductions/page">Tax Deductions</a>
             <a class="orangebtn" href="/ax1/CPToDoList/page">To-do List</a>
-            <a class="purplebtn" href="/ax1/CPTaxableIncome/page">Taxable Income</a>
-            <a class="blackbtn" href="/ax1/CPCapitalGains/page">Capital Gains</a></p>
+            <a class="purplebtn" href="/ax1/CPTaxableIncome/page">Possibly Taxable Income</a>
+            <a class="blackbtn" href="/ax1/CPCapitalGains/page">Commodity Sold Capital Gains</a></p>
         <?php require COLLAGE; ?>
     </div>
 <?php require FOOTERBAR; ?>
