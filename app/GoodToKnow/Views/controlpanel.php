4f8217cb13515c2ff76b4076b83554971da11859
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-11-04 21:10:40 -0500

    Shrunk buttons

--- app/GoodToKnow/Views/controlpanel.php
+++ app/GoodToKnow/Views/controlpanel.php
@@ -7,8 +7,8 @@
         <h2 class="topofpage">👤 User Tools 🧰</h2>
         <p><a class="clearbtn" href="/ax1/CPBasics/page">Settings and Posts</a>
             <a class="greenbtn" href="/ax1/CPTransactions/page">Bank Transactions</a>
-            <a class="purplebtn" href="/ax1/CPRecurringPayments/page">Recurring Payments</a>
             <a class="blackbtn" href="/ax1/CPBankingAccounts/page">Bank Accounts</a>
+            <a class="purplebtn" href="/ax1/CPRecurringPayments/page">Recurring Payments</a>
             <a class="orangebtn" href="/ax1/CPCrypto/page">Crypto</a>
             <a class="clearbtn" href="/ax1/CPTaxDeductions/page">Possible Tax Deductions</a>
             <a class="orangebtn" href="/ax1/CPToDoList/page">To-do List</a>
