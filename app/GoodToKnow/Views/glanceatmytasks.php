6b7b11b60e6dc8321b32354e5adfe1469fa83863
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-12-10 13:20:36 -0500

    Compacted all tasks display

--- app/GoodToKnow/Views/glanceatmytasks.php
+++ app/GoodToKnow/Views/glanceatmytasks.php
@@ -3,19 +3,12 @@
 <?php require CBSOFREGULARPAGES; ?>
     <!-- maincontent -->
     <div id="maincontent">
-        <h1>All To-do Tasks</h1>
-        <hr>
+        <h1>All Tasks</h1>
         <?php if (!empty($array)): ?>
-            <?php $last = count($array) - 1; ?>
             <?php foreach ($array as $key => $object): ?>
                 <h2 class="topofpage"><?= $object->label ?></h2>
-                <p><b>Last: </b><?= $object->last ?></p>
                 <p><b>Next: </b><?= $object->next ?></p>
                 <p><b>Cycle Type: </b><?= $object->cycle_type ?></p>
-                <p><?= $object->comment ?></p>
-                <?php if ($key != $last): ?>
-                    <hr>
-                <?php endif; ?>
             <?php endforeach; ?>
         <?php else: ?>
             <p>There are no to-do tasks.</p>
