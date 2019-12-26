554f3ea323e51948fa4cdd2d37dad8a6ccfc0287
Sameh-R-Labib <schoolbuscompany@gmail.com>
2019-12-26 14:23:21 -0500

    More abbreviations

--- app/GoodToKnow/Views/adminhome.php
+++ app/GoodToKnow/Views/adminhome.php
@@ -5,15 +5,15 @@
     <div id="maincontent">
         <h1>Control Panel</h1>
         <h2 class="topofpage">👤 🧰</h2>
-        <p><a class="clearbtn" href="/ax1/CPBasics/page">Main Tools</a>
+        <p><a class="clearbtn" href="/ax1/CPBasics/page">M. Tools</a>
             <a class="greenbtn" href="/ax1/CPTransactions/page">A. Balance</a>
             <a class="blackbtn" href="/ax1/CPBankingAccounts/page">B. Account</a>
-            <a class="purplebtn" href="/ax1/CPRecurringPayments/page">Recurring Payments</a>
+            <a class="purplebtn" href="/ax1/CPRecurringPayments/page">R. Payment</a>
             <a class="orangebtn" href="/ax1/CPCrypto/page">Crypto</a>
-            <a class="clearbtn" href="/ax1/CPTaxDeductions/page">Write-off</a>
+            <a class="clearbtn" href="/ax1/CPTaxDeductions/page">T. Write-off</a>
             <a class="orangebtn" href="/ax1/CPToDoList/page">Task</a>
-            <a class="purplebtn" href="/ax1/CPTaxableIncome/page">Taxable Income</a>
-            <a class="blackbtn" href="/ax1/CPCapitalGains/page">Commodity Sold</a></p>
+            <a class="purplebtn" href="/ax1/CPTaxableIncome/page">T. Income</a>
+            <a class="blackbtn" href="/ax1/CPCapitalGains/page">C. Sold</a></p>
         <h2>♠👔♠ 🧰</h2>
         <p><a class="greenbtn" href="/ax1/CPPurges/page">Purging Old Stuff</a>
             <a class="clearbtn" href="/ax1/CPAccounts/page">Account Management</a>
