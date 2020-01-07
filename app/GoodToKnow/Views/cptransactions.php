1d09f669ce1830e89bd7a0ee21cc63b13a30e8ba
Sameh-R-Labib <schoolbuscompany@gmail.com>
2020-01-06 20:54:52 -0500

    Ledger

--- app/GoodToKnow/Views/cptransactions.php
+++ app/GoodToKnow/Views/cptransactions.php
@@ -6,7 +6,7 @@
         <h1>🏦ing 🔃 for ⚖️s</h1>
         <p><a class="clearbtn" href="/ax1/BuildABankingTransactionForBalances/page">Create</a>
             <a class="greenbtn" href="/ax1/RevampABankingTransactionForBalances/page">Edit</a>
-            <a class="purplebtn" href="/ax1/CheckMyBankingAccountTxBalances/page">Account Ledger</a>
+            <a class="purplebtn" href="/ax1/CheckMyBankingAccountTxBalances/page">Ledger</a>
             <a class="blackbtn" href="/ax1/OmitABankingTransactionForBalances/page">Delete</a></p>
     </div><!-- End maincontent -->
 <?php require FOOTERBAR; ?>
