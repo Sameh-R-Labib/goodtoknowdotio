1b8f61c858745539d17a5c517d3ddc75ee6cde54
Sameh-R-Labib <schoolbuscompany@gmail.com>
2020-02-04 11:39:02 -0500

    Wording for Ledger Link

--- app/GoodToKnow/Views/controlpanel.php
+++ app/GoodToKnow/Views/controlpanel.php
@@ -6,7 +6,7 @@
         <h1>Control Panel</h1>
         <h2 class="topofpage">👤 🧰</h2>
         <p><a class="clearbtn" href="/ax1/CPBasics/page">Tools</a>
-            <a class="greenbtn" href="/ax1/CPTransactions/page">Ledger</a>
+            <a class="greenbtn" href="/ax1/CPTransactions/page">BA-Ledger</a>
             <a class="blackbtn" href="/ax1/CPBankingAccounts/page">B-Account</a>
             <a class="purplebtn" href="/ax1/CPRecurringPayments/page">R-Payment</a>
             <a class="orangebtn" href="/ax1/CPCrypto/page">Crypto</a>
