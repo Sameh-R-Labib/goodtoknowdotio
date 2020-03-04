5628d5a56ea3fec93ded9716cd035550ef7ef236
Sameh-R-Labib <schoolbuscompany@gmail.com>
2020-03-04 13:48:20 -0500

    Improved words on CP buttons

--- app/GoodToKnow/Views/controlpanel.php
+++ app/GoodToKnow/Views/controlpanel.php
@@ -5,14 +5,14 @@
     <div id="maincontent">
         <h2 class="topofpage">👤 🧰</h2>
         <p><a class="clearbtn" href="/ax1/CPBasics/page">Tools</a>
-            <a class="greenbtn" href="/ax1/CPTransactions/page">BA-Ledger</a>
-            <a class="blackbtn" href="/ax1/CPBankingAccounts/page">B-Account</a>
-            <a class="purplebtn" href="/ax1/CPRecurringPayments/page">R-Payment</a>
+            <a class="greenbtn" href="/ax1/CPTransactions/page">Dollar</a>
+            <a class="blackbtn" href="/ax1/CPBankingAccounts/page">Bank Account</a>
+            <a class="purplebtn" href="/ax1/CPRecurringPayments/page">Recurring Payment</a>
             <a class="orangebtn" href="/ax1/CPCrypto/page">Crypto</a>
-            <a class="clearbtn" href="/ax1/CPTaxDeductions/page">T-Write-off</a>
+            <a class="clearbtn" href="/ax1/CPTaxDeductions/page">Tax Write-off</a>
             <a class="orangebtn" href="/ax1/CPToDoList/page">Task</a>
-            <a class="purplebtn" href="/ax1/CPTaxableIncome/page">T-Income</a>
-            <a class="blackbtn" href="/ax1/CPCapitalGains/page">C-Sold</a></p>
+            <a class="purplebtn" href="/ax1/CPTaxableIncome/page">Taxable Income</a>
+            <a class="blackbtn" href="/ax1/CPCapitalGains/page">Commodity Sold</a></p>
         <?php require COLLAGE; ?>
     </div>
 <?php require FOOTERBAR; ?>
