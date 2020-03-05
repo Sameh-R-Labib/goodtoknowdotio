1efe8a2288b8914f64c7705bffcb123baa07abce
Sameh-R-Labib <schoolbuscompany@gmail.com>
2020-03-05 10:50:40 -0500

    Improved wording again

--- app/GoodToKnow/Views/controlpanel.php
+++ app/GoodToKnow/Views/controlpanel.php
@@ -5,14 +5,14 @@
     <div id="maincontent">
         <h2 class="topofpage">👤 🧰</h2>
         <p><a class="clearbtn" href="/ax1/CPBasics/page">Tools</a>
-            <a class="greenbtn" href="/ax1/CPTransactions/page">Dollar</a>
-            <a class="blackbtn" href="/ax1/CPBankingAccounts/page">Bank Account</a>
-            <a class="purplebtn" href="/ax1/CPRecurringPayments/page">Recurring Payment</a>
+            <a class="greenbtn" href="/ax1/CPTransactions/page">Account Balances</a>
+            <a class="blackbtn" href="/ax1/CPBankingAccounts/page">Bank Accounts</a>
+            <a class="purplebtn" href="/ax1/CPRecurringPayments/page">Recurring Payments</a>
             <a class="orangebtn" href="/ax1/CPCrypto/page">Crypto</a>
-            <a class="clearbtn" href="/ax1/CPTaxDeductions/page">Tax Write-off</a>
-            <a class="orangebtn" href="/ax1/CPToDoList/page">Task</a>
+            <a class="clearbtn" href="/ax1/CPTaxDeductions/page">Tax Write-offs</a>
+            <a class="orangebtn" href="/ax1/CPToDoList/page">Tasks</a>
             <a class="purplebtn" href="/ax1/CPTaxableIncome/page">Taxable Income</a>
-            <a class="blackbtn" href="/ax1/CPCapitalGains/page">Commodity Sold</a></p>
+            <a class="blackbtn" href="/ax1/CPCapitalGains/page">Commodities Sold</a></p>
         <?php require COLLAGE; ?>
     </div>
 <?php require FOOTERBAR; ?>
