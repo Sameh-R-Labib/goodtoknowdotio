3a75a2ae3e5853683db8e0b71120123680f54eb5
Sameh-R-Labib <schoolbuscompany@gmail.com>
2020-03-07 14:56:34 -0500

    Tasks h1 changed

--- app/GoodToKnow/Views/glanceatmytasks.php
+++ app/GoodToKnow/Views/glanceatmytasks.php
@@ -3,7 +3,7 @@
 <?php require CBSOFREGULARPAGES; ?>
     <!-- maincontent -->
     <div id="maincontent">
-        <h1>⌨️ 🛠 🦺 🎙</h1>
+        <h1>Tasks</h1>
         <?php if (!empty($array)): ?>
             <?php foreach ($array as $key => $object): ?>
                 <h2 class="topofpage"><?= $object->label ?></h2>
