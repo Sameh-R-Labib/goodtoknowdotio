4b8a9f0b3b80d08bafcadfa35c25d901bb8b6140
Sameh-R-Labib <schoolbuscompany@gmail.com>
2020-03-20 14:42:07 -0400

    Moved CP links around

--- app/GoodToKnow/Views/adminhome.php
+++ app/GoodToKnow/Views/adminhome.php
@@ -7,10 +7,10 @@
         <p><a class="clearbtn" href="/ax1/CPBasics/page">Tools</a>
             <a class="greenbtn" href="/ax1/CPTransactions/page">Account Balances</a>
             <a class="blackbtn" href="/ax1/CPBankingAccounts/page">Bank Accounts</a>
-            <a class="purplebtn" href="/ax1/CPRecurringPayments/page">Recurring Payments</a>
             <a class="orangebtn" href="/ax1/CPCrypto/page">Crypto</a>
-            <a class="clearbtn" href="/ax1/CPTaxDeductions/page">Tax Write-offs</a>
             <a class="orangebtn" href="/ax1/CPToDoList/page">Tasks</a>
+            <a class="purplebtn" href="/ax1/CPRecurringPayments/page">Recurring Payments</a>
+            <a class="clearbtn" href="/ax1/CPTaxDeductions/page">Tax Write-offs</a>
             <a class="purplebtn" href="/ax1/CPTaxableIncome/page">Taxable Income</a>
             <a class="blackbtn" href="/ax1/CPCapitalGains/page">Commodities Sold</a></p>
         <h2>♠👔♠ 🧰</h2>
