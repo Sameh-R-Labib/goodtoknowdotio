6b56631045037f75d8e686810c229a299b647fa1
Sameh-R-Labib <schoolbuscompany@gmail.com>
2020-04-29 22:32:58 -0400

    Which topic

--- app/GoodToKnow/Views/editmypost.php
+++ app/GoodToKnow/Views/editmypost.php
@@ -1,11 +1,10 @@
 <?php require TOPFORFORMPAGES; ?>
-<form action="/ax1/EditMyPostProcessor/page" method="post">
-    <h1>Edit My 📄</h1>
-    <p class="tooltip">ℹ️
-        <span class="tooltiptext tooltip-top">Shown above are the topics within your current community.</span>
-    </p>
-    <p>Choose the <em>topic</em> where your post lives.</p>
-    <?php require SESSIONMESSAGE; ?>
+    <form action="/ax1/EditMyPostProcessor/page" method="post">
+        <h1>Which topic</h1>
+        <p class="tooltip">ℹ️
+            <span class="tooltiptext tooltip-top">Shown above are the topics within your current community.</span>
+        </p>
+        <?php require SESSIONMESSAGE; ?>
     <section>
         <?php foreach ($special_topic_array as $key => $value): ?>
             <label for="choice-<?= $key ?>" class="radio">
