8ffc4479263c2b4640cc1813a494365dd9292328
Sameh-R-Labib <schoolbuscompany@gmail.com>
2020-05-07 18:35:35 -0400

    Improved topic and community description editors

--- app/GoodToKnow/Views/kommunitydescriptioneditorform.php
+++ app/GoodToKnow/Views/kommunitydescriptioneditorform.php
@@ -1,6 +1,6 @@
 <?php require TOPFORFORMPAGES; ?>
     <form action="/ax1/KommunityDescriptionEditorFormProcessor/page" method="post">
-        <h1><?= $saved_str01 ?> Description</h1>
+        <h2><?= $saved_str01 ?> Description</h2>
         <p class="tooltip">ℹ️
             <span class="tooltiptext tooltip-top">🚫 markdown ✅ emoji ✅ line-break 📲️ maximum 230 bytes</span>
         </p>
@@ -8,7 +8,7 @@
         <section>
             <p>
                 <label for="textarea"></label>
-                <textarea id="textarea" spellcheck="false" name="text" rows="41"
+                <textarea id="textarea" spellcheck="false" name="text" rows="28"
                           cols="77" wrap="soft"
                           maxlength="230"><?php /** @noinspection PhpUndefinedVariableInspection */
                     echo $community_object->community_description; ?></textarea>
--- app/GoodToKnow/Views/topicdescriptioneditorform.php
+++ app/GoodToKnow/Views/topicdescriptioneditorform.php
@@ -1,17 +1,17 @@
 <?php require TOPFORFORMPAGES; ?>
-<form action="/ax1/TopicDescriptionEditorFormProcessor/page" method="post">
-    <h2>Topic Description Editor</h2>
-    <h2><?= $saved_str01 ?></h2>
-    <p class="tooltip">ℹ️
-        <span class="tooltiptext tooltip-top">🚫 markdown ✅ emoji ✅ line-break 📲️ maximum 230 bytes.</span>
-    </p>
-    <?php require SESSIONMESSAGE; ?>
-    <section>
-        <p>
-            <label for="textarea"></label>
-            <textarea id="textarea" spellcheck="false" name="text" rows="41"
-                      cols="77" wrap="soft" maxlength="230"><?php /** @noinspection PhpUndefinedVariableInspection */
-                echo $topic_object->topic_description; ?></textarea>
+    <form action="/ax1/TopicDescriptionEditorFormProcessor/page" method="post">
+        <h2><?= $saved_str01 ?> Description</h2>
+        <p class="tooltip">ℹ️
+            <span class="tooltiptext tooltip-top">🚫 markdown ✅ emoji ✅ line-break 📲️ maximum 230 bytes.</span>
+        </p>
+        <?php require SESSIONMESSAGE; ?>
+        <section>
+            <p>
+                <label for="textarea"></label>
+                <textarea id="textarea" spellcheck="false" name="text" rows="28"
+                          cols="77" wrap="soft"
+                          maxlength="230"><?php /** @noinspection PhpUndefinedVariableInspection */
+                    echo $topic_object->topic_description; ?></textarea>
         </p>
     </section>
     <?php require SUBMITABORT; ?>
