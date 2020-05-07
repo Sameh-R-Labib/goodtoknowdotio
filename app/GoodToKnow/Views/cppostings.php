7ad49b6a79408a3053ecb6b6193dfc5290e55d63
Sameh-R-Labib <schoolbuscompany@gmail.com>
2020-05-07 18:06:50 -0400

    Edit A Topic Description wording update

--- app/GoodToKnow/Views/cppostings.php
+++ app/GoodToKnow/Views/cppostings.php
@@ -5,9 +5,9 @@
     <div id="maincontent">
         <h1>Postings</h1>
         <p><a href="/ax1/NewCommunity/page">Create Community</a></p>
-        <p><a href="/ax1/KommunityDescriptionEditor/page">Community Description Editor</a></p>
+        <p><a href="/ax1/KommunityDescriptionEditor/page">Edit A Community Description</a></p>
         <p><a href="/ax1/NewTopic/page">Create Topic</a></p>
-        <p><a href="/ax1/TopicDescriptionEditor/page">Topic Description Editor</a></p>
+        <p><a href="/ax1/TopicDescriptionEditor/page">Edit A Topic Description</a></p>
         <p><a href="/ax1/QuickPostDelete/page">Delete Any Post</a></p>
         <p><a href="/ax1/TransferPostOwnership/page">Transfer Post Ownership</a></p>
     </div><!-- End maincontent -->
