8440f1e0f4f875ffbaed369421718bd93bf44781
Sameh-R-Labib <schoolbuscompany@gmail.com>
2020-06-28 02:45:05 -0400

    Shrunk the logo

--- app/GoodToKnow/ViewsIncludes/topbardiv.php
+++ app/GoodToKnow/ViewsIncludes/topbardiv.php
@@ -1,6 +1,6 @@
 <!-- topbar -->
 <div id="topbar">
-    <a href="/ax1"><img src="/good1.jpg" alt="GoodToKnow.io" height="70" width="311" style="float: left"></a>
+    <a href="/ax1"><img src="/good1.jpg" alt="GoodToKnow.io" height="57" width="254" style="float: left"></a>
     <div id="sendmessage"><?php require SENDMESSAGELINK; ?></div>
     <div id="inboxlink"><p><a href="/ax1/Inbox/page">🎫 inbox</a></p></div>
     <div id="logindiv"><?php require LOGINDIVLINK; ?></div>
