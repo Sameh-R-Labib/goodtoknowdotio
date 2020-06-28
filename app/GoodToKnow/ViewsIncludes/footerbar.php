68991930ad2d0088110b68b6e6db378e9e588251
Sameh-R-Labib <schoolbuscompany@gmail.com>
2020-06-28 03:15:54 -0400

    #footerbar p margin: 14px;

--- app/GoodToKnow/ViewsIncludes/footerbar.php
+++ app/GoodToKnow/ViewsIncludes/footerbar.php
@@ -1,6 +1,6 @@
 <!-- footerbar -->
 <div id="footerbar">
-    <p align="center" style="font-size: .75em;">
+    <p align="center" style="font-size: .77em;">
         <img src="/Gnu-head-30-years-anniversary.svg" style="float:left;height: 19px;width: 19px;margin-top: -0px">
         © 2020 - Sameh Ramzy Labib
         <img src="/2000px-GPLv3_Logo.svg.png"
