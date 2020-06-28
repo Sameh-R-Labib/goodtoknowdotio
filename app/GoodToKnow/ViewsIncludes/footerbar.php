c3f0c98d8011fd31e7252b310e9c9d03a1692314
Sameh-R-Labib <schoolbuscompany@gmail.com>
2020-06-28 03:08:47 -0400

    Shrunk things in footer

--- app/GoodToKnow/ViewsIncludes/footerbar.php
+++ app/GoodToKnow/ViewsIncludes/footerbar.php
@@ -1,9 +1,9 @@
 <!-- footerbar -->
 <div id="footerbar">
-    <p align="center" style="font-size: 1em;">
-        <img src="/Gnu-head-30-years-anniversary.svg" style="float:left;height: 28px;width: 28px;margin-top: -0px">
+    <p align="center" style="font-size: .75em;">
+        <img src="/Gnu-head-30-years-anniversary.svg" style="float:left;height: 19px;width: 19px;margin-top: -0px">
         © 2020 - Sameh Ramzy Labib
         <img src="/2000px-GPLv3_Logo.svg.png"
-             height="28" width="66"
+             height="19" width="57"
              style="float:right;;margin-top: -0px"></p>
 </div>
