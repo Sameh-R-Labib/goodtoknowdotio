fbcbe3615bc4b593ebc8d8eb879a7e07b720781d
Sameh-R-Labib <schoolbuscompany@gmail.com>
2020-06-28 03:22:10 -0400

    Fine adjustments to footerbar

--- app/GoodToKnow/ViewsIncludes/footerbar.php
+++ app/GoodToKnow/ViewsIncludes/footerbar.php
@@ -1,9 +1,9 @@
 <!-- footerbar -->
 <div id="footerbar">
-    <p align="center" style="font-size: .77em;">
-        <img src="/Gnu-head-30-years-anniversary.svg" style="float:left;height: 19px;width: 19px;margin-top: -0px">
+    <p align="center" style="font-size: .78em;">
+        <img src="/Gnu-head-30-years-anniversary.svg" style="float:left;height: 20px;width: 20px;margin-top: -0px">
         © 2020 - Sameh Ramzy Labib
         <img src="/2000px-GPLv3_Logo.svg.png"
-             height="19" width="57"
+             height="20" width="59"
              style="float:right;;margin-top: -0px"></p>
 </div>
