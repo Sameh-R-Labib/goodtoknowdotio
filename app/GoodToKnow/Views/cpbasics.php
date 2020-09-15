a7fa1b5892acb8a2e00d1ae0bc26763a37fa74b5
Sameh-R-Labib <schoolbuscompany@gmail.com>
2020-09-15 14:25:54 -0400

    Moved tooltip

--- app/GoodToKnow/Views/cpbasics.php
+++ app/GoodToKnow/Views/cpbasics.php
@@ -3,13 +3,15 @@
 <?php require CBSOFREGULARPAGES; ?>
     <!-- maincontent -->
     <div id="maincontent">
-        <p><span class="tooltip">ℹ️<span class="tooltiptext tooltip-top">To have a new topic or community added ask admin to add it.</span></span>
+        <p>
             <a class="blackbtn" href="/ax1/AuthorDeletesOwnPost/page">Delete A Post</a>
             <a class="orangebtn" href="/ax1/ByUsernameMessage/page">Send A Message To Another User</a>
             <a class="clearbtn" href="/ax1/BlotOutSomeInboxMessages/page">Delete Some Messages</a>
             <a class="orangebtn" href="/ax1/DefaultCommunity/page">Switch To A Different Default Community</a>
             <a class="purplebtn" href="/ax1/DefaultTimeZone/page">Switch Timezone</a>
-            <a class="purplebtn" href="/ax1/ChangePassword/page">Change Passphrase</a></p>
+            <a class="purplebtn" href="/ax1/ChangePassword/page">Change Passphrase</a><br><br><br>
+            <span class="tooltip">ℹ️<span class="tooltiptext tooltip-top">To have a new topic (or community) added you
+                    need to ask admin.</span></span></p>
     </div><!-- End maincontent -->
 <?php require FOOTERBAR; ?>
 <?php require BOTTOMOFPAGES; ?>
