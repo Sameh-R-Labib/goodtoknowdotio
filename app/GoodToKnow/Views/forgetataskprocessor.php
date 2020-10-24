fb0b7803cf3c52259455701040854ad9bee5969d
Sameh-R-Labib <schoolbuscompany@gmail.com>
2020-10-24 15:28:24 -0400

    Replaced incorrect word

--- app/GoodToKnow/Views/forgetataskprocessor.php
+++ app/GoodToKnow/Views/forgetataskprocessor.php
@@ -1,19 +1,19 @@
 <?php require TOPFORFORMPAGES; ?>
-<form action="/ax1/ForgetATaskDelete/page" method="post">
-    <h1>Confirm</h1>
-    <?php require SESSIONMESSAGE; ?>
-    <p>&nbsp;</p>
-    <p><b>Account: </b><?php /** @noinspection PhpUndefinedVariableInspection */
-        echo $object->label; ?></p>
-    <p><b>Last 🕒: </b><?= $object->last ?></p>
-    <p><b>Next 🕒: </b><?= $object->next ?></p>
-    <p><b>Cycle Type: </b><?= $object->cycle_type ?></p>
-    <p><?= $object->comment ?></p>
-    <p>&nbsp;</p>
-    <p>Are you sure you want to delete this?</p>
-    <section>
-        <label for="yes" class="radio">
-            <input type="radio" id="yes" name="choice" value="yes">
+    <form action="/ax1/ForgetATaskDelete/page" method="post">
+        <h1>Confirm</h1>
+        <?php require SESSIONMESSAGE; ?>
+        <p>&nbsp;</p>
+        <p><b>Label: </b><?php /** @noinspection PhpUndefinedVariableInspection */
+            echo $object->label; ?></p>
+        <p><b>Last 🕒: </b><?= $object->last ?></p>
+        <p><b>Next 🕒: </b><?= $object->next ?></p>
+        <p><b>Cycle Type: </b><?= $object->cycle_type ?></p>
+        <p><?= $object->comment ?></p>
+        <p>&nbsp;</p>
+        <p>Are you sure you want to delete this?</p>
+        <section>
+            <label for="yes" class="radio">
+                <input type="radio" id="yes" name="choice" value="yes">
             Yes<br>
         </label>
         <label for="no" class="radio">
