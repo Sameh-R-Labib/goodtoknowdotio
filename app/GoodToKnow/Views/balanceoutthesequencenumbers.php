ae526baf0484002a195bd854646ec7f97ffb107d
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-04-16 17:04:49 -0400

    Improved info in view

--- app/GoodToKnow/Views/balanceoutthesequencenumbers.php
+++ app/GoodToKnow/Views/balanceoutthesequencenumbers.php
@@ -4,9 +4,9 @@
 <?php global $result; ?>
 <?php global $fields; ?>
     <form action="/ax1/BalanceOutTheSequenceNumbersFormProcessor/page" method="post">
-        <h2>Adjust sequence numbers</h2>
+        <h2>Adjust Sequence Numbers</h2>
         <p><b><?= $thing_type ?>:</b> <?= $thing_name ?></p>
-        <p>Sequence numbers have range 0 to 40,000,000.</p>
+        <p><b>Range:</b>b> 0 - 40000000</p>
         <?php require SESSIONMESSAGE; ?>
         <section>
             <?php echo $fields; ?>
