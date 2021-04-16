56dea5087ff5d911cac643d3d8d4eaad84adec15
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-04-15 23:51:24 -0400

    Informed about min and max sequence number

--- app/GoodToKnow/Views/balanceoutthesequencenumbers.php
+++ app/GoodToKnow/Views/balanceoutthesequencenumbers.php
@@ -1,7 +1,9 @@
 <?php require TOPFORFORMPAGES; ?>
+<?php global $thing_type; ?>
+<?php global $thing_name; ?>
     <form action="/ax1/BalanceOutTheSequenceNumbersFormProcessor/page" method="post">
-        <h2><?= $html_title ?></h2>
-        <p>The Type & Name of the Thing</p>
+        <h2>Adjust sequence numbers for <?= $thing_type ?> <?= $thing_name ?></h2>
+        <p>Sequence numbers have range 0 to 40,000,000.</p>
         <?php require SESSIONMESSAGE; ?>
         <section>
             <!-- Some foreach loop -->
