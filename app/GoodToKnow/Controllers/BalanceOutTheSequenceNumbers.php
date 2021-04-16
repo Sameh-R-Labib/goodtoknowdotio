ce8023e3563ac54e4c6c883dd9b330ee7dbbea15
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-04-16 17:21:19 -0400

    Changed label to arrow

--- app/GoodToKnow/Controllers/BalanceOutTheSequenceNumbers.php
+++ app/GoodToKnow/Controllers/BalanceOutTheSequenceNumbers.php
@@ -96,7 +96,7 @@ class BalanceOutTheSequenceNumbers
             // One html line for each record.
             foreach ($result as $object) {
                 // $object is current record
-                $fields .= "<p><label for=\"animal{$object->id}}\">Record: </label>\n";
+                $fields .= "<p><label for=\"animal{$object->id}}\"><b>⇰</b> </label>\n";
                 $fields .= "<input type=\"text\" value=\"{$object->sequence_number}\"";
                 $fields .= "name=\"animal[{$object->id}}]\" id=\"animal{$object->id}}\" required > ";
                 $fields .= $object->topic_description;
@@ -107,7 +107,7 @@ class BalanceOutTheSequenceNumbers
             // One html line for each record.
             foreach ($result as $object) {
                 // $object is current record
-                $fields .= "<p><label for=\"animal{$object->id}}\">Record: </label>\n";
+                $fields .= "<p><label for=\"animal{$object->id}}\"><b>⇰</b> </label>\n";
                 $fields .= "<input type=\"text\" value=\"{$object->sequence_number}\" ";
                 $fields .= "name=\"animal[{$object->id}}]\" id=\"animal{$object->id}}\" required > ";
                 $fields .= $object->title;
