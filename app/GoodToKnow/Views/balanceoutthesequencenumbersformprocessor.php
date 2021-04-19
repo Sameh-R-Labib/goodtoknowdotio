652da620a8812cdf58cbee153a27213d8139c60b
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-04-19 15:45:34 -0400

    5) Present all the contents of $result

--- app/GoodToKnow/Views/balanceoutthesequencenumbersformprocessor.php
+++ app/GoodToKnow/Views/balanceoutthesequencenumbersformprocessor.php
@@ -1,10 +1,12 @@
 <?php require TOPFORFORMPAGES; ?>
+<?php global $result; ?>
     <!-- globals declarations (if any) go here -->
     <!-- I will use form tags only for style css issues -->
     <form>
         <h2>Save or Cancel</h2>
         <?php require SESSIONMESSAGE; ?>
-        <!-- Display (All) the data which would be saved -->
+        <p>What you see is what will be saved.</p>
+        <pre><?php print_r($result); ?></pre>
         <!-- Present link buttons for Save and Cancel -->
     </form>
 <?php require BOTTOMOFPAGES; ?>
