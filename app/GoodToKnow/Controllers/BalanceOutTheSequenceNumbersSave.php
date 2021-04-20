57495ff178c14c5853d3f36628a4e68be9df5394
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-04-19 23:16:47 -0400

    Wrote mission statement for BalanceOutTheSequenceNumbersSave

--- app/GoodToKnow/Controllers/BalanceOutTheSequenceNumbersSave.php
+++ app/GoodToKnow/Controllers/BalanceOutTheSequenceNumbersSave.php
@@ -9,7 +9,16 @@ class BalanceOutTheSequenceNumbersSave
     function page()
     {
         /**
-         *
+         * $_SESSION['saved_arr01'] contains the records which have had their sequence numbers modified.
+         * The purpose of this route is to:
+         *      A) update the Topics table if we're in a Community
+         *      OR
+         *      B) update the Posts table if we're in a Topic.
+         * We are to update using the records found in the array $_SESSION['saved_arr01'].
+         * STEPS:
+         *  1) Update the database using the array we have.
+         *  2) Compose a session message based on success or failure.
+         *  3) Breakout to Home page.
          */
     }
 }
