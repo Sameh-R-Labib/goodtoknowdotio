448b1110f9e7ce743267a3707db1d85130d305b4
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-05-27 14:05:11 -0400

    Fixed is_password_asapair by making its error a global sessionMessage

--- app/GoodToKnow/ControllerHelpers/is_password_asapair.php
+++ app/GoodToKnow/ControllerHelpers/is_password_asapair.php
@@ -3,22 +3,23 @@
 namespace GoodToKnow\ControllerHelpers;
 
 /**
- * @param string $message
  * @param string $str01
  * @param string $str02
  * @return bool
  */
-function is_password_asapair(string &$message, string &$str01, string &$str02): bool
+function is_password_asapair(string &$str01, string &$str02): bool
 {
+    global $sessionMessage;
+
     /**
      * First make sure it has proper syntax.
      */
 
     require_once CONTROLLERHELPERS . DIRSEP . 'is_password_syntactically.php';
 
-    if (!is_password_syntactically($message, $str01)) {
+    if (!is_password_syntactically($sessionMessage, $str01)) {
 
-        $message .= " The password's syntax is invalid. ";
+        $sessionMessage .= " The password's syntax is invalid. ";
 
         return false;
     }
@@ -32,7 +33,7 @@ function is_password_asapair(string &$message, string &$str01, string &$str02): 
 
     if (!$are_equal) {
 
-        $message .= " Your two passwords don't match. ";
+        $sessionMessage .= " Your two passwords don't match. ";
 
         return false;
     }
--- app/GoodToKnow/ControllerHelpers/password_for_regandchange_prep.php
+++ app/GoodToKnow/ControllerHelpers/password_for_regandchange_prep.php
@@ -5,7 +5,6 @@ namespace GoodToKnow\ControllerHelpers;
 use mysqli;
 
 /**
- * @param mysqli $db
  * @return string
  */
 function password_for_regandchange_prep(): string
@@ -15,8 +14,6 @@ function password_for_regandchange_prep(): string
      * having his password changed.
      */
 
-    global $sessionMessage;
-
     require_once CONTROLLERHELPERS . DIRSEP . 'standard_form_field_prep.php';
 
     $submitted_first_try = standard_form_field_prep('first_try', 10, 264);
@@ -25,7 +22,7 @@ function password_for_regandchange_prep(): string
 
     require_once CONTROLLERHELPERS . DIRSEP . 'is_password_asapair.php';
 
-    if (!is_password_asapair($sessionMessage, $submitted_first_try, $submitted_password)) {
+    if (!is_password_asapair($submitted_first_try, $submitted_password)) {
 
         breakout(' There is something invalid about the password pair which you entered. ');
 
