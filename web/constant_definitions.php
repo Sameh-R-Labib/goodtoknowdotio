186a399d76b3b54bb88208c0277389766ec7a52c
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-06-10 17:47:16 -0400

    Moved constant definitions out of routehandler.php

---
+++ web/constant_definitions.php
@@ -0,0 +1,44 @@
+<?php
+
+const DIRSEP = DIRECTORY_SEPARATOR;
+const WEB_DIR = PROJ_ROOT . DIRSEP . 'web';
+const VENDOR_DIR = PROJ_ROOT . DIRSEP . 'vendor';
+
+/**
+ * Here I can define constants for the location
+ * of the directory for view includes and for
+ * the files for view includes.
+ */
+const VIEWS = PROJ_ROOT . DIRSEP . 'app' . DIRSEP . 'GoodToKnow' . DIRSEP . 'Views';
+const VIEWSINCLUDES = PROJ_ROOT . DIRSEP . 'app' . DIRSEP . 'GoodToKnow' . DIRSEP . 'ViewsIncludes';
+const CONTROLLERHELPERS = PROJ_ROOT . DIRSEP . 'app' . DIRSEP . 'GoodToKnow' . DIRSEP . 'ControllerHelpers';
+const CONTROLLERINCLUDES = PROJ_ROOT . DIRSEP . 'app' . DIRSEP . 'GoodToKnow' . DIRSEP . 'ControllerIncludes';
+
+const SESSIONMESSAGE = VIEWSINCLUDES . DIRSEP . 'sessionmessage.php';
+const URLOFMOSTRECENTUPLOAD = VIEWSINCLUDES . DIRSEP . 'urlofmostrecentupload.php';
+const COMMUNITIESFORTHISUSER = VIEWSINCLUDES . DIRSEP . 'communitiesforthisuser.php';
+const CURRENTTOPIC = VIEWSINCLUDES . DIRSEP . 'currenttopic.php';
+const LISTTOPICS = VIEWSINCLUDES . DIRSEP . 'listtopics.php';
+const LISTPOSTS = VIEWSINCLUDES . DIRSEP . 'listposts.php';
+const CURRENTPOST = VIEWSINCLUDES . DIRSEP . 'currentpost.php';
+const CONTROLPANELLINK = VIEWSINCLUDES . DIRSEP . 'controlpanellink.php';
+const SENDMESSAGELINK = VIEWSINCLUDES . DIRSEP . 'sendmessagelink.php';
+const LOGINDIVLINK = VIEWSINCLUDES . DIRSEP . 'logindivlink.php';
+const MESSAGETHEAUTHOR = VIEWSINCLUDES . DIRSEP . 'messagetheauthor.php';
+const BREADCRUMBS = VIEWSINCLUDES . DIRSEP . 'breadcrumbs.php';
+const CURRENTCOMMUNITY = VIEWSINCLUDES . DIRSEP . 'currentcommunity.php';
+const TOPOFREGULARPAGE = VIEWSINCLUDES . DIRSEP . 'topofregularpage.php';
+const BOTTOMOFPAGES = VIEWSINCLUDES . DIRSEP . 'bottomofpages.php';
+const COLLAGE = VIEWSINCLUDES . DIRSEP . 'collage.php';
+const TOPFORFORMPAGES = VIEWSINCLUDES . DIRSEP . 'topforformpages.php';
+const TOPBARDIV = VIEWSINCLUDES . DIRSEP . 'topbardiv.php';
+const CBSOFREGULARPAGES = VIEWSINCLUDES . DIRSEP . 'cbsofregularpages.php';
+const FOOTERBAR = VIEWSINCLUDES . DIRSEP . 'footerbar.php';
+const SUBMITABORT = VIEWSINCLUDES . DIRSEP . 'submitabort.php';
+const HEADINGONE = VIEWSINCLUDES . DIRSEP . 'headingone.php';
+const TIMEFORMFIELD = VIEWSINCLUDES . DIRSEP . 'timeformfield.php';
+const TIMEFORMFIELDPREFILLED = VIEWSINCLUDES . DIRSEP . 'timeformfieldprefilled.php';
+const TIMENEXTANDLASTFORMFIELDS = VIEWSINCLUDES . DIRSEP . 'timenextandlastformfields.php';
+const TIMENEXTANDLASTFORMFIELDSPREFILLED = VIEWSINCLUDES . DIRSEP . 'timenextandlastformfieldsprefilled.php';
+const TIMEBOUGHTSOLD = VIEWSINCLUDES . DIRSEP . 'timeboughtsold.php';
+const TIMEBOUGHTSOLDPREFILLED = VIEWSINCLUDES . DIRSEP . 'timeboughtsoldprefilled.php';
