da40a56e555791600408035ec959682588cba270
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-06-10 23:04:04 -0400

    Made posts list refresh every 7s

--- app/GoodToKnow/Controllers/Home.php
+++ app/GoodToKnow/Controllers/Home.php
@@ -165,7 +165,7 @@ class Home
 
         $time_since_refresh = time() - $g->last_refresh_posts;
 
-        if ($time_since_refresh > 20 && $g->type_of_resource_requested == 'topic') {
+        if ($time_since_refresh > 7 && $g->type_of_resource_requested == 'topic') {
             if ($g->db == 'not connected') {
 
                 $g->db = db_connect();
