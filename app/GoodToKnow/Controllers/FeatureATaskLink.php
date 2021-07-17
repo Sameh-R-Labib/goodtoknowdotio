d33d0f65b36b84d12cd325234850c74058acb959
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-07-16 20:33:34 -0400

    Completed FeatureATaskLink.php (untested)

--- app/GoodToKnow/Controllers/FeatureATaskLink.php
+++ app/GoodToKnow/Controllers/FeatureATaskLink.php
@@ -2,6 +2,9 @@
 
 namespace GoodToKnow\Controllers;
 
+use GoodToKnow\Models\Task;
+use function GoodToKnow\ControllerHelpers\get_date_h_m_s_from_a_timestamp;
+
 class FeatureATaskLink
 {
     function page(int $id)
@@ -12,8 +15,57 @@ class FeatureATaskLink
          *
          * This function is a modified version of FeatureATaskEdit. The difference is that
          * this function will get the id of the task from the request itself.
+         *
+         * A lot of comments were left out since the code was essentially copied from
+         * FeatureATaskEdit.
          */
 
-        echo "The id is $id";
+        global $g;
+
+
+        kick_out_loggedoutusers();
+
+
+        get_db();
+
+
+        $_SESSION['saved_int01'] = $id;
+
+        $g->object = Task::find_by_id($id);
+
+        if (!$g->object) {
+
+            breakout(' Unexpectedly, I could not find that task. ');
+
+        }
+
+        if ($g->object->user_id != $g->user_id) {
+
+            breakout(' Error 46985423. ');
+
+        }
+
+
+        /**
+         * Present a form which is populated with data from the task object.
+         */
+
+        /**
+         * This type of record has a field called `last` and a field called `next`. We are Not going to pre-populate
+         * form fields with them. Instead we derive the arrays called $g->last and $g->next from them and use
+         * the derived arrays to pre-populate the corresponding fields in the form which we present below.
+         */
+
+        require CONTROLLERHELPERS . DIRSEP . 'get_date_h_m_s_from_a_timestamp.php';
+
+        $g->last = get_date_h_m_s_from_a_timestamp($g->object->last);
+
+        $g->next = get_date_h_m_s_from_a_timestamp($g->object->next);
+
+
+        $g->html_title = 'Edit the task record';
+
+
+        require VIEWS . DIRSEP . 'featureataskedit.php';
     }
 }
