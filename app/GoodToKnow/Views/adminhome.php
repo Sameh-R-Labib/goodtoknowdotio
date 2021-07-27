2e8e277ef894cdae6679e7ba2fac8553baf39e86
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-07-26 22:35:06 -0400

    Added link in both CPs to our Proclamation

--- app/GoodToKnow/Views/adminhome.php
+++ app/GoodToKnow/Views/adminhome.php
@@ -11,8 +11,8 @@
             <a class="purplebtn" href="/ax1/CPRecurringPayments/page">Recurring Payment</a>
             <a class="clearbtn" href="/ax1/CPTaxDeductions/page">Tax Write-off</a>
             <a class="purplebtn" href="/ax1/CPTaxableIncome/page">Taxable Income</a>
-            <a class="blackbtn" href="/ax1/CPCapitalGains/page">Commodities Sold</a></p>
-        <hr>
+            <a class="blackbtn" href="/ax1/CPCapitalGains/page">Commodities Sold</a>
+            <a class="greenbtn" href="/ax1/Proclamation/page">Proclamation</a></p>
         <p><a class="greenbtn" href="/ax1/CPPurges/page">Cleanup</a>
             <a class="clearbtn" href="/ax1/CPAccounts/page">User</a>
             <a class="orangebtn" href="/ax1/CPPostings/page">Blog</a>
--- app/GoodToKnow/Views/controlpanel.php
+++ app/GoodToKnow/Views/controlpanel.php
@@ -11,7 +11,8 @@
             <a class="purplebtn" href="/ax1/CPRecurringPayments/page">Recurring Payment</a>
             <a class="clearbtn" href="/ax1/CPTaxDeductions/page">Tax Write-off</a>
             <a class="purplebtn" href="/ax1/CPTaxableIncome/page">Taxable Income</a>
-            <a class="blackbtn" href="/ax1/CPCapitalGains/page">Commodities Sold</a></p>
+            <a class="blackbtn" href="/ax1/CPCapitalGains/page">Commodities Sold</a>
+            <a class="greenbtn" href="/ax1/Proclamation/page">Proclamation</a></p>
         <?php require COLLAGE; ?>
     </div>
 <?php require FOOTERBAR; ?>
