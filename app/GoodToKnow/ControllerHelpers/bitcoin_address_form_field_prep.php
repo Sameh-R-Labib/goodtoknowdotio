047a57ed5552ef8a5b44665e1b2e576ee15c2f1a
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-07-28 14:08:21 -0400

    Improved bitcoin_address_form_field_prep.php

--- app/GoodToKnow/ControllerHelpers/bitcoin_address_form_field_prep.php
+++ app/GoodToKnow/ControllerHelpers/bitcoin_address_form_field_prep.php
@@ -46,7 +46,6 @@ function bitcoin_address_form_field_prep(string $field_name): string
 
     require_once CONTROLLERHELPERS . DIRSEP . 'standard_form_field_prep.php';
 
-    $string_for_return = standard_form_field_prep($field_name, 2, 264);
+    return standard_form_field_prep($field_name, 2, 264);
 
-    return $string_for_return;
 }
