3e5a518d0f71fda73c25b24b5a65e3daadf2d0d0
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-11-18 17:29:02 -0500

    Modified the view for creating a transaction

--- app/GoodToKnow/Views/buildabankingtransactionforbalances.php
+++ app/GoodToKnow/Views/buildabankingtransactionforbalances.php
@@ -9,7 +9,8 @@
         <section>
             <p>
                 <label for="label">Label (✅ emoji): </label>
-                <input id="label" name="label" type="text" value="" required minlength="3" maxlength="264"
+                <input id="label" name="label" type="text" value="<?= $g->saved_arr01['label'] ?>" required
+                       minlength="3" maxlength="264"
                        size="61" spellcheck="false" placeholder="Monthly Car Payment">
             </p>
             <hr>
@@ -20,7 +21,8 @@
                 <label for="amount">Amount <span class="tooltip">ℹ️<span class="tooltiptext
                 tooltip-top">If the amounts to be displayed should have 2 instead of  8 decimal places then ask the admin
                         to add your type of currency to the list of known fiat currencies.</span></span>: </label>
-                <input id="amount" name="amount" type="text" value="" required minlength="1" maxlength="24" size="24"
+                <input id="amount" name="amount" type="text" value="<?= $g->saved_arr01['amount'] ?>" required
+                       minlength="1" maxlength="24" size="24"
                        placeholder="-105.39">
             </p>
         </section>
--- app/GoodToKnow/ViewsIncludes/timeformfield.php
+++ app/GoodToKnow/ViewsIncludes/timeformfield.php
@@ -1,17 +1,21 @@
 <?php global $g; ?>
 <p>️
     <label for="date"></label>
-    <input id="date" name="date" type="text" required minlength="10" maxlength="14" size="14" placeholder="mm/dd/yyyy">
+    <input id="date" name="date" type="text" required minlength="10" maxlength="14" size="14" placeholder="mm/dd/yyyy"
+           value="<?= $g->saved_arr01['date'] ?>">
     <span class="tooltip">ℹ️<span class="tooltiptext tooltip-top">hr is military hour (0-23)</span></span>
     <label for="hour"></label>
-    <input id="hour" name="hour" type="text" required minlength="1" maxlength="2" size="3" placeholder="h">
+    <input id="hour" name="hour" type="text" required minlength="1" maxlength="2" size="3" placeholder="h"
+           value="<?= $g->saved_arr01['hour'] ?>">
     <label for="minute">: </label>
-    <input id="minute" name="minute" type="text" required minlength="1" maxlength="2" size="3" placeholder="m">
+    <input id="minute" name="minute" type="text" required minlength="1" maxlength="2" size="3" placeholder="m"
+           value="<?= $g->saved_arr01['minute'] ?>">
     <label for="second">: </label>
-    <input id="second" name="second" type="text" required minlength="1" maxlength="2" size="3" placeholder="s">
+    <input id="second" name="second" type="text" required minlength="1" maxlength="2" size="3" placeholder="s"
+           value="<?= $g->saved_arr01['second'] ?>">
     <label for="timezone"></label>
     <input id="timezone" name="timezone" type="text" placeholder="PHP Timezone" required
-           minlength="2" maxlength="60" size="18" value="<?= $g->timezone ?>">
+           minlength="2" maxlength="60" size="18" value="<?= $g->saved_arr01['timezone'] ?>">
     <span class="tooltip">ℹ️<span class="tooltiptext tooltip-top">See
                             <a href="https://www.php.net/manual/en/timezones.php"
                                target="_blank">www.php.net/manual/en/timezones.php</a></span></span>
