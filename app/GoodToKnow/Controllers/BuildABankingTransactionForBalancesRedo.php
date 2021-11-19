5b8288efc3b03423b367e20bd3543d4715c766f3
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-11-19 18:01:07 -0500

    Fixed BuildABankingTransactionForBalancesRedo.php

--- app/GoodToKnow/Controllers/BuildABankingTransactionForBalancesRedo.php
+++ app/GoodToKnow/Controllers/BuildABankingTransactionForBalancesRedo.php
@@ -2,6 +2,8 @@
 
 namespace GoodToKnow\Controllers;
 
+use function GoodToKnow\ControllerHelpers\get_html_select_box_containing_the_bank_accounts;
+
 class BuildABankingTransactionForBalancesRedo
 {
     function page()
@@ -12,11 +14,29 @@ class BuildABankingTransactionForBalancesRedo
 
         global $g;
 
+
         kick_out_loggedoutusers_or_if_there_is_error_msg();
+
 
         $g->message .= ' <b>We are giving you one chance to fix the time value which we think is wrong.</b> ';
 
+
         $g->html_title = 'One chance to redo';
+
+
+        /**
+         * Get the html for form's select box.
+         * It comes with one of the select options marked as selected.
+         * When the 2nd argument (banking_id) of get_html_select_box_containing_the_bank_accounts
+         * is 0 then none of the choices are marked as selected.
+         */
+
+        get_db(); // Is needed for get_html_select_box_containing_the_bank_accounts()
+
+        require CONTROLLERHELPERS . DIRSEP . 'get_html_select_box_containing_the_bank_accounts.php';
+
+        $g->account_type = get_html_select_box_containing_the_bank_accounts($g->user_id, $g->saved_arr01['bank_id']);
+
 
         require VIEWS . DIRSEP . 'buildabankingtransactionforbalances.php';
     }
