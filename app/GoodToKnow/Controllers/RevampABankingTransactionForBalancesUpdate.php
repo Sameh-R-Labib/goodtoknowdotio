899fc9ba922a8b77d910eb881742ea6ea58e2da8
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-11-20 20:59:36 -0500

    Modified RevampABankingTransactionForBalancesUpdate.php for redo

--- app/GoodToKnow/Controllers/RevampABankingTransactionForBalancesUpdate.php
+++ app/GoodToKnow/Controllers/RevampABankingTransactionForBalancesUpdate.php
@@ -39,9 +39,7 @@ class RevampABankingTransactionForBalancesUpdate
         require_once CONTROLLERHELPERS . DIRSEP . 'float_form_field_prep.php';
 
 
-        // amount
-
-        $edited_amount = float_form_field_prep('amount', -999999999999999.99, 999999999999999.99);
+        $label = standard_form_field_prep('label', 3, 264);
 
 
         // - - - Get $g->time (which is a timestamp) based on submitted `timezone` `date` `hour` `minute` `second`
@@ -51,14 +49,69 @@ class RevampABankingTransactionForBalancesUpdate
         // - - -
 
 
-        // bank_id
-
-        $edited_bank_id = integer_form_field_prep('bank_id', 1, PHP_INT_MAX);
+        $amount = float_form_field_prep('amount', -999999999999999.99, 999999999999999.99);
 
 
-        // label
+        $bank_id = integer_form_field_prep('bank_id', 1, PHP_INT_MAX);
 
-        $edited_label = standard_form_field_prep('label', 3, 264);
+
+        /**
+         * This is the section of code where we redirect to a redo route
+         * if the time which the user submitted is anomalous.
+         *
+         * Q: What condition would make $g->time anomalous?
+         * A: If $g->time is in the future then that would be anomalous.
+         *
+         * Also, as you see in the code there is a mechanism which causes what we are
+         * doing here to happen only once for the submitted data set. In other
+         * words the first time the user submits his data set we will check it
+         * and give him a chance to fix it. On the subsequent submit we will
+         * just let the submitted data be saved.
+         */
+
+        if ($g->is_first_attempt) {
+
+            if ($g->time > time()) {
+
+                /**
+                 * Reset 'is_first_attempt' in the session.
+                 *
+                 * We are setting 'is_first_attempt' to false so that once the user submits the form,
+                 * and it is being processed it will not be retested for anomalous time entries.
+                 */
+
+                $_SESSION['is_first_attempt'] = false;
+
+
+                // Put form data in an array to prepare it to be stored in $_SESSION['saved_arr01'].
+                $saved_arr01['label'] = $label;
+                $saved_arr01['amount'] = $amount;
+                $saved_arr01['date'] = $g->date;
+                $saved_arr01['hour'] = $g->hour;
+                $saved_arr01['minute'] = $g->minute;
+                $saved_arr01['second'] = $g->second;
+                $saved_arr01['timezone'] = $g->timezone;
+                $saved_arr01['bank_id'] = $bank_id; // determines which choice from drop down is selected.
+
+
+                // make form data survive the redirect
+                $_SESSION['saved_arr01'] = $saved_arr01;
+
+
+                redirect_to("/ax1/RevampABankingTransactionForBalancesRedo/page");
+            }
+
+        }
+
+
+        /**
+         * Reset 'is_first_attempt' in the session.
+         *
+         * We need to set it to true so the next time the user creates a task
+         * he will have the same opportunity to have his data checked.
+         */
+
+        $_SESSION['is_first_attempt'] = true;
 
 
         /**
@@ -80,9 +133,9 @@ class RevampABankingTransactionForBalancesUpdate
          * 3) Modify the retrieved record by updating it with the submitted data.
          */
 
-        $object->bank_id = $edited_bank_id;
-        $object->label = $edited_label;
-        $object->amount = $edited_amount;
+        $object->bank_id = $bank_id;
+        $object->label = $label;
+        $object->amount = $amount;
         $object->time = $g->time;
 
 
