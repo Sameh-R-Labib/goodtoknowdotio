ac688e87293bd12387124dc50e9d4f40da228380
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-11-21 21:04:13 -0500

    Modified view InitializeABitcoinRecord for redo

--- app/GoodToKnow/Controllers/InitializeABitcoinRecord.php
+++ app/GoodToKnow/Controllers/InitializeABitcoinRecord.php
@@ -7,15 +7,8 @@ class InitializeABitcoinRecord
     function page()
     {
         /**
-         * This feature enables any user to create a database record in the
-         * bitcoin table. The process will ask the user to ONLY supply a bitcoin
-         * address and the remaining field values will be supplied by an (not included)
-         * editor for the record.
-         */
-
-        /**
-         * This here script simply presents a form for the user to supply the bitcoin
-         * address for the "to be created" bitcoin record.
+         * This feature enables a user to create a database record in the
+         * bitcoin table.
          */
 
 
@@ -26,6 +19,36 @@ class InitializeABitcoinRecord
 
 
         $g->html_title = 'Create a New Bitcoin Record';
+
+
+        /**
+         * Because of the concept of redo we need to
+         * have a way of injecting values into the form.
+         * That is why you see the code below.
+         */
+
+        $g->saved_arr01['address'] = '';
+        $g->saved_arr01['initial_balance'] = '';
+        $g->saved_arr01['current_balance'] = '';
+        $g->saved_arr01['currency'] = '';
+        $g->saved_arr01['price_point'] = '';
+        $g->saved_arr01['comment'] = '';
+        $g->saved_arr01['date'] = '';
+        $g->saved_arr01['hour'] = '';
+        $g->saved_arr01['minute'] = '';
+        $g->saved_arr01['second'] = '';
+        $g->saved_arr01['timezone'] = $g->timezone; // user's default timezone
+
+        // Not Necessary:
+        //   Update the session variable
+        //   $_SESSION['saved_arr01'] = $g->saved_arr01;
+
+
+        /**
+         * This may be redundant, but we need to be sure (better than be sorry.)
+         */
+
+        $_SESSION['is_first_attempt'] = true;
 
 
         require VIEWS . DIRSEP . 'initializeabitcoinrecord.php';
