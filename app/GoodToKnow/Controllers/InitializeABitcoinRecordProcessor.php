eab69d916afafa63c413769d34d8386576ade17a
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-11-23 14:28:44 -0500

    Modified InitializeABitcoinRecordProcessor for redo

--- app/GoodToKnow/Controllers/InitializeABitcoinRecordProcessor.php
+++ app/GoodToKnow/Controllers/InitializeABitcoinRecordProcessor.php
@@ -48,6 +48,69 @@ class InitializeABitcoinRecordProcessor
 
 
         /**
+         * Redirect to give the user one chance to fix their time entry.
+         * A correct time entry for a Bitcoin record would be in the past.
+         *
+         * The currently submitted form data will be used to conveniently
+         * populate the redo form.
+         *
+         * As you see in the code there is a mechanism which causes what we are
+         * doing here to happen only once for the submitted data set. In other
+         * words the first time the user submits his data set we will check it
+         * and give him a chance to fix it. On the subsequent submit we will
+         * just let the submitted data be saved.
+         */
+
+        if ($g->is_first_attempt) {
+
+            if ($g->time > time()) {
+
+                /**
+                 * Reset 'is_first_attempt' in the session.
+                 *
+                 * We are setting 'is_first_attempt' to false so that once the user submits the form,
+                 * and it is being processed it will not be retested for anomalous time entries.
+                 */
+
+                $_SESSION['is_first_attempt'] = false;
+
+
+                // Put form data in an array to prepare it to be stored in $_SESSION['saved_arr01'].
+                $saved_arr01['address'] = $address;
+                $saved_arr01['initial_balance'] = $initial_balance;
+                $saved_arr01['current_balance'] = $current_balance;
+                $saved_arr01['currency'] = $g->currency;
+                $saved_arr01['price_point'] = $g->price_point;
+                $saved_arr01['comment'] = $comment;
+                $saved_arr01['date'] = $g->date;
+                $saved_arr01['hour'] = $g->hour;
+                $saved_arr01['minute'] = $g->minute;
+                $saved_arr01['second'] = $g->second;
+                $saved_arr01['timezone'] = $g->timezone; // this is the actual timezone the user had entered
+
+
+                // make form data survive the redirect
+                $_SESSION['saved_arr01'] = $saved_arr01;
+
+
+                redirect_to("/ax1/InitializeABitcoinRecordRedo/page");
+
+            }
+
+        }
+
+
+        /**
+         * Reset 'is_first_attempt' in the session.
+         *
+         * We need to set it to true so the next time the user creates a task
+         * he will have the same opportunity to have his data checked.
+         */
+
+        $_SESSION['is_first_attempt'] = true;
+
+
+        /**
          * Create a Bitcoin array for the record.
          */
 
