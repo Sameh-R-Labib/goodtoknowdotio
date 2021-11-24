1d71619efd3cc8d259c49fab0ced6d160459c3e0
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-11-23 21:47:52 -0500

    Modified EditABitcoinRecordSubmit for redo

--- app/GoodToKnow/Controllers/EditABitcoinRecordSubmit.php
+++ app/GoodToKnow/Controllers/EditABitcoinRecordSubmit.php
@@ -37,22 +37,22 @@ class EditABitcoinRecordSubmit
 
         // initial_balance
 
-        $edited_initial_balance = float_form_field_prep('initial_balance', 0.0, 21000000000.0);
+        $initial_balance = float_form_field_prep('initial_balance', 0.0, 21000000000.0);
 
 
         // current_balance
 
-        $edited_current_balance = float_form_field_prep('current_balance', 0.0, 21000000000.0);
+        $current_balance = float_form_field_prep('current_balance', 0.0, 21000000000.0);
 
 
         // currency
 
-        $edited_currency = standard_form_field_prep('currency', 1, 15);
+        $currency = standard_form_field_prep('currency', 1, 15);
 
 
         // price_point
 
-        $edited_price_point = float_form_field_prep('price_point', 0.0, 999999999999999.99);
+        $price_point = float_form_field_prep('price_point', 0.0, 999999999999999.99);
 
 
         // - - - Get $g->time (which is a timestamp) based on submitted `timezone` `date` `hour` `minute` `second`
@@ -64,7 +64,69 @@ class EditABitcoinRecordSubmit
 
         // comment
 
-        $edited_comment = standard_form_field_prep('comment', 0, 800);
+        $comment = standard_form_field_prep('comment', 0, 800);
+
+
+        /**
+         * Redirect to give the user one chance to fix their time entry.
+         * A correct time entry for a Bitcoin record would be in the past.
+         *
+         * The currently submitted form data will be used to conveniently
+         * populate the redo form.
+         *
+         * As you see in the code there is a mechanism which causes what we are
+         * doing here to happen only once for the submitted data set. In other
+         * words the first time the user submits his data set we will check it
+         * and give him a chance to fix it. On the subsequent submit we will
+         * just let the submitted data be saved.
+         */
+
+        if ($g->is_first_attempt) {
+
+            if ($g->time > time()) {
+
+                /**
+                 * Reset 'is_first_attempt' in the session.
+                 *
+                 * We are setting 'is_first_attempt' to false so that once the user submits the form,
+                 * and it is being processed it will not be retested for anomalous time entries.
+                 */
+
+                $_SESSION['is_first_attempt'] = false;
+
+
+                // Put form data in an array to prepare it to be stored in $_SESSION['saved_arr01'].
+                $saved_arr01['initial_balance'] = $initial_balance;
+                $saved_arr01['current_balance'] = $current_balance;
+                $saved_arr01['currency'] = $currency;
+                $saved_arr01['price_point'] = $price_point;
+                $saved_arr01['comment'] = $comment;
+                $saved_arr01['date'] = $g->date;
+                $saved_arr01['hour'] = $g->hour;
+                $saved_arr01['minute'] = $g->minute;
+                $saved_arr01['second'] = $g->second;
+                $saved_arr01['timezone'] = $g->timezone; // this is the actual timezone the user had entered
+
+
+                // make form data survive the redirect
+                $_SESSION['saved_arr01'] = $saved_arr01;
+
+
+                redirect_to("/ax1/EditABitcoinRecordRedo/page");
+
+            }
+
+        }
+
+
+        /**
+         * Reset 'is_first_attempt' in the session.
+         *
+         * We need to set it to true so the next time the user creates a task
+         * he will have the same opportunity to have his data checked.
+         */
+
+        $_SESSION['is_first_attempt'] = true;
 
 
         /**
@@ -86,12 +148,12 @@ class EditABitcoinRecordSubmit
          * 3) Modify the retrieved record by updating it with the submitted data.
          */
 
-        $g->bitcoin_object->initial_balance = $edited_initial_balance;
-        $g->bitcoin_object->current_balance = $edited_current_balance;
-        $g->bitcoin_object->currency = $edited_currency;
-        $g->bitcoin_object->price_point = $edited_price_point;
+        $g->bitcoin_object->initial_balance = $initial_balance;
+        $g->bitcoin_object->current_balance = $current_balance;
+        $g->bitcoin_object->currency = $currency;
+        $g->bitcoin_object->price_point = $price_point;
         $g->bitcoin_object->time = $g->time;
-        $g->bitcoin_object->comment = $edited_comment;
+        $g->bitcoin_object->comment = $comment;
 
 
         /**
