24dc77ad797f73460baa97b4978cb900815d2023
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-11-24 21:32:25 -0500

    Modified generateabankingaccountforbalances.php for redo

--- app/GoodToKnow/Views/generateabankingaccountforbalances.php
+++ app/GoodToKnow/Views/generateabankingaccountforbalances.php
@@ -1,12 +1,14 @@
+<?php global $g; ?>
 <?php require TOPFORFORMPAGES; ?>
     <form action="/ax1/GenerateABankingAccountForBalancesProcessor/page" method="post">
-        <h1>Create Bank Account</h1>
+        <h1>Create a Bank Account</h1>
         <?php require SESSIONMESSAGE; ?>
         <section>
             <p>
                 <label for="acct_name">Bank Account Name (✅ emoji): </label>
-                <input id="acct_name" name="acct_name" type="text" value="" required minlength="3" maxlength="30"
-                       size="34" spellcheck="false" placeholder="Personal Credit Card">
+                <input id="acct_name" name="acct_name" type="text" required minlength="3" maxlength="30"
+                       size="34" spellcheck="false" placeholder="Personal Credit Card"
+                       value="<?= $g->saved_arr01['acct_name'] ?>">
             </p>
             <hr>
             <p>Time at Beginning</p>
@@ -16,18 +18,19 @@
                 <label for="start_balance">Balance at Beginning <span class="tooltip">ℹ️<span class="tooltiptext
                 tooltip-top">If the amounts to be displayed should have 2 instead of  8 decimal places then ask the admin
                         to add your type of currency to the list of known fiat currencies.</span></span>: </label>
-                <input id="start_balance" name="start_balance" type="text" value="" required minlength="1"
-                       maxlength="24" size="24" placeholder="-85.14">
+                <input id="start_balance" name="start_balance" type="text" required minlength="1"
+                       maxlength="24" size="24" placeholder="-85.14"
+                       value="<?= $g->saved_arr01['start_balance'] ?>">
             </p>
             <p>
                 <label for="currency">Currency (✅ emoji): </label>
-                <input id="currency" name="currency" type="text" value="" required minlength="1" maxlength="15"
-                       size="15" placeholder="💵">
+                <input id="currency" name="currency" type="text" required minlength="1" maxlength="15"
+                       size="15" placeholder="💵" value="<?= $g->saved_arr01['currency'] ?>">
             </p>
             <p>
                 <label for="comment">Comment (🚫 markdown ✅ emoji ✅ line-break): </label>
                 <textarea id="comment" name="comment" rows="5" cols="77" wrap="soft" maxlength="800" spellcheck="false"
-                          placeholder="This banking account is my _ _ _ _ bank's _ _ _ _ account."></textarea>
+                          placeholder="This is my _ _ bank's _ _ account."><?= $g->saved_arr01['comment'] ?></textarea>
             </p>
         </section>
         <?php require SUBMITABORT; ?>
