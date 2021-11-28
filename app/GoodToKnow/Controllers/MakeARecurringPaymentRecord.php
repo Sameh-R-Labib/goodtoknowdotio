1b5b04cbefab87297979c298d7eaa75584308206
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-11-28 15:15:04 -0500

    Modified MakeARecurringPaymentRecord.php for redo

--- app/GoodToKnow/Controllers/MakeARecurringPaymentRecord.php
+++ app/GoodToKnow/Controllers/MakeARecurringPaymentRecord.php
@@ -26,6 +26,34 @@ class MakeARecurringPaymentRecord
 
         $g->html_title = 'Create a New Recurring Payment Record';
 
+        /**
+         * Because of the concept of redo we need to
+         * have a **generic** way of injecting values into the form.
+         * That is why you see the code below.
+         */
+
+        $g->saved_arr01['label'] = '';
+        $g->saved_arr01['currency'] = '';
+        $g->saved_arr01['amount_paid'] = '';
+        $g->saved_arr01['comment'] = '';
+        $g->saved_arr01['date'] = '';
+        $g->saved_arr01['hour'] = '';
+        $g->saved_arr01['minute'] = '';
+        $g->saved_arr01['second'] = '';
+        $g->saved_arr01['timezone'] = $g->timezone; // user's default timezone
+
+
+        // Not Necessary:
+        //   Update the session variable
+        //   $_SESSION['saved_arr01'] = $g->saved_arr01;
+
+
+        /**
+         * This may be redundant, but we need to be sure (better than be sorry.)
+         */
+
+        $_SESSION['is_first_attempt'] = true;
+
 
         require VIEWS . DIRSEP . 'makearecurringpaymentrecord.php';
     }
