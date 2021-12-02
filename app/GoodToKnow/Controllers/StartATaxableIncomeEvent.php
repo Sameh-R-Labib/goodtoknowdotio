42b4fb6bf64c4b5cf2146124b89acfc99021c18a
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-12-02 15:07:44 -0500

    Modified StartATaxableIncomeEvent.php for redo

--- app/GoodToKnow/Controllers/StartATaxableIncomeEvent.php
+++ app/GoodToKnow/Controllers/StartATaxableIncomeEvent.php
@@ -21,6 +21,35 @@ class StartATaxableIncomeEvent
         $g->html_title = 'Create a Taxable Income Event';
 
 
+        /**
+         * Because of the concept of redo we need to
+         * have a **generic** way of injecting values into the form.
+         * That is why you see the code below.
+         */
+
+        $g->saved_arr01['label'] = '';
+        $g->saved_arr01['year_received'] = '';
+        $g->saved_arr01['currency'] = '';
+        $g->saved_arr01['amount'] = '';
+        $g->saved_arr01['comment'] = '';
+        $g->saved_arr01['date'] = '';
+        $g->saved_arr01['hour'] = '';
+        $g->saved_arr01['minute'] = '';
+        $g->saved_arr01['second'] = '';
+        $g->saved_arr01['timezone'] = $g->timezone; // user's default timezone
+
+        // Not Necessary:
+        //   Update the session variable
+        //   $_SESSION['saved_arr01'] = $g->saved_arr01;
+
+
+        /**
+         * This may be redundant, but we need to be sure (better than be sorry.)
+         */
+
+        $_SESSION['is_first_attempt'] = true;
+
+
         require VIEWS . DIRSEP . 'startataxableincomeevent.php';
     }
 }
