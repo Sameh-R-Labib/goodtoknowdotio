973b495c0aa65e94b0188ec491b8de679ca6fad6
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-12-01 22:34:56 -0500

    Created PolishARecurringPaymentRecordRedo.php

--- app/GoodToKnow/Controllers/PolishARecurringPaymentRecordRedo.php
+++ app/GoodToKnow/Controllers/PolishARecurringPaymentRecordRedo.php
@@ -4,5 +4,24 @@ namespace GoodToKnow\Controllers;
 
 class PolishARecurringPaymentRecordRedo
 {
+    function page()
+    {
+        global $g;
 
+
+        kick_out_loggedoutusers_or_if_there_is_error_msg();
+
+
+        /**
+         * Tell the user he is seeing the form a 2nd time.
+         */
+
+        $g->message .= ' <b>We are giving you one chance to fix the time value which we think is wrong.</b> ';
+
+
+        $g->html_title = 'One chance to redo';
+
+
+        require VIEWS . DIRSEP . 'polisharecurringpaymentrecordprocessor.php';
+    }
 }
