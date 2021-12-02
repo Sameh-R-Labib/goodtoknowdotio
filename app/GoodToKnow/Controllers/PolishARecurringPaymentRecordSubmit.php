3175d1e82f3dd6fc2ef0e012dc85fb5add68f6ec
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-12-01 21:52:08 -0500

    Modified PolishARecurringPaymentRecordSubmit.php for redo

--- app/GoodToKnow/Controllers/PolishARecurringPaymentRecordSubmit.php
+++ app/GoodToKnow/Controllers/PolishARecurringPaymentRecordSubmit.php
@@ -35,11 +35,11 @@ class PolishARecurringPaymentRecordSubmit
         require_once CONTROLLERHELPERS . DIRSEP . 'standard_form_field_prep.php';
         require_once CONTROLLERHELPERS . DIRSEP . 'float_form_field_prep.php';
 
-        $edited_label = standard_form_field_prep('label', 3, 264);
+        $label = standard_form_field_prep('label', 3, 264);
 
-        $edited_currency = standard_form_field_prep('currency', 1, 15);
+        $currency = standard_form_field_prep('currency', 1, 15);
 
-        $edited_amount_paid = float_form_field_prep('amount_paid', 0.0, 999999999999999.99);
+        $amount_paid = float_form_field_prep('amount_paid', 0.0, 999999999999999.99);
 
 
         // - - - Get $g->time (which is a timestamp) based on submitted `timezone` `date` `hour` `minute` `second`
@@ -48,7 +48,68 @@ class PolishARecurringPaymentRecordSubmit
 
         // - - -
 
-        $edited_comment = standard_form_field_prep('comment', 0, 800);
+        $comment = standard_form_field_prep('comment', 0, 800);
+
+
+        /**
+         * Redirect to give the user one chance to fix their time entry.
+         * A correct time entry for a recurring payment record would be in the past.
+         *
+         * The currently submitted form data will be used to conveniently
+         * populate the redo form.
+         *
+         * As you see in the code there is a mechanism which causes what we are
+         * doing here to happen only once for the submitted data set. In other
+         * words the first time the user submits his data set we will check it
+         * and give him a chance to fix it. On the subsequent submit we will
+         * just let the submitted data be saved.
+         */
+
+        if ($g->is_first_attempt) {
+
+            if ($g->time > time()) {
+
+                /**
+                 * Reset 'is_first_attempt' in the session.
+                 *
+                 * We are setting 'is_first_attempt' to false so that once the user submits the form,
+                 * and it is being processed it will not be retested for anomalous time entries.
+                 */
+
+                $_SESSION['is_first_attempt'] = false;
+
+
+                // Put form data in an array to prepare it to be stored in $_SESSION['saved_arr01'].
+                $saved_arr01['label'] = $label;
+                $saved_arr01['currency'] = $currency;
+                $saved_arr01['amount_paid'] = $amount_paid;
+                $saved_arr01['comment'] = $comment;
+                $saved_arr01['date'] = $g->date;
+                $saved_arr01['hour'] = $g->hour;
+                $saved_arr01['minute'] = $g->minute;
+                $saved_arr01['second'] = $g->second;
+                $saved_arr01['timezone'] = $g->timezone; // this is the actual timezone the user had entered
+
+
+                // make form data survive the redirect
+                $_SESSION['saved_arr01'] = $saved_arr01;
+
+
+                redirect_to("/ax1/PolishARecurringPaymentRecordRedo/page");
+
+            }
+
+        }
+
+
+        /**
+         * Reset 'is_first_attempt' in the session.
+         *
+         * We need to set it to true so the next time the user creates a task
+         * he will have the same opportunity to have his data checked.
+         */
+
+        $_SESSION['is_first_attempt'] = true;
 
 
         /**
@@ -70,11 +131,11 @@ class PolishARecurringPaymentRecordSubmit
          * 3) Modify the retrieved record by updating it with the submitted data.
          */
 
-        $object->label = $edited_label;
-        $object->currency = $edited_currency;
-        $object->amount_paid = $edited_amount_paid;
+        $object->label = $label;
+        $object->currency = $currency;
+        $object->amount_paid = $amount_paid;
         $object->time = $g->time;
-        $object->comment = $edited_comment;
+        $object->comment = $comment;
 
 
         /**
