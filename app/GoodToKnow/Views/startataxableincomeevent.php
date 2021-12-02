fb8200d0aaa4c9b6c1e3b9df377162d41b7a42db
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-12-02 14:33:41 -0500

    Modified startataxableincomeevent.php for redo

--- app/GoodToKnow/Views/startataxableincomeevent.php
+++ app/GoodToKnow/Views/startataxableincomeevent.php
@@ -1,40 +1,44 @@
+<?php global $g; ?>
 <?php require TOPFORFORMPAGES; ?>
-<form action="/ax1/StartATaxableIncomeEventProcessor/page" method="post">
-    <h1>Create a Taxable 💸 Event 📽</h1>
-    <?php require SESSIONMESSAGE; ?>
-    <section>
-        <p>
-            <label for="label">Label (✅ emoji): </label>
-            <input id="label" name="label" type="text" required minlength="3" maxlength="264"
-                   size="61" spellcheck="false" placeholder="Technical support for a Gtk.io User">
-        </p>
-        <hr>
-        <p>Time The Event Occured</p>
-        <?php require TIMEFORMFIELD; ?>
-        <hr>
-        <p>
-            <label for="year_received">Year for when event occured: </label>
-            <input id="year_received" name="year_received" type="text" required minlength="4" maxlength="6"
-                   size="6" placeholder="2018">
-        </p>
-        <p>
-            <label for="currency">Currency (✅ emoji): </label>
-            <input id="currency" name="currency" type="text" required minlength="1" maxlength="15" size="15"
-                   placeholder="💵">
-        </p>
-        <p>
-            <label for="amount">Amount of currency received <span class="tooltip">ℹ️<span class="tooltiptext
+    <form action="/ax1/StartATaxableIncomeEventProcessor/page" method="post">
+        <h1>Create a Taxable Income Event 📽</h1>
+        <?php require SESSIONMESSAGE; ?>
+        <section>
+            <p>
+                <label for="label">Label (✅ emoji): </label>
+                <input id="label" name="label" type="text" value="<?= $g->saved_arr01['label'] ?>" required
+                       minlength="3" maxlength="264"
+                       size="61" spellcheck="false" placeholder="Technical support for a Gtk.io User">
+            </p>
+            <hr>
+            <p>Time The Event Occured</p>
+            <?php require TIMEFORMFIELD; ?>
+            <hr>
+            <p>
+                <label for="year_received">Year for when event occured: </label>
+                <input id="year_received" name="year_received" type="text"
+                       value="<?= $g->saved_arr01['year_received'] ?>" required minlength="4" maxlength="6"
+                       size="6" placeholder="2018">
+            </p>
+            <p>
+                <label for="currency">Currency (✅ emoji): </label>
+                <input id="currency" name="currency" type="text" value="<?= $g->saved_arr01['currency'] ?>" required
+                       minlength="1" maxlength="15" size="15"
+                       placeholder="💵">
+            </p>
+            <p>
+                <label for="amount">Amount of currency received <span class="tooltip">ℹ️<span class="tooltiptext
                 tooltip-top">If the amounts to be displayed should have 2 instead of  8 decimal places then ask the admin
                         to add your type of currency to the list of known fiat currencies.</span></span>: </label>
-            <input id="amount" name="amount" type="text" required minlength="1" maxlength="24" size="24"
-                   placeholder="150.24">
-        </p>
-        <p>
-            <label for="comment">Comment (🚫 markdown ✅ emoji ✅ line-break): </label>
-            <textarea id="comment" name="comment" rows="5" cols="77" wrap="soft" maxlength="800"
-                      placeholder="The frequency of this income is _ _ _ _."></textarea>
-        </p>
-    </section>
-    <?php require SUBMITABORT; ?>
-</form>
+                <input id="amount" name="amount" type="text" value="<?= $g->saved_arr01['amount'] ?>" required
+                       minlength="1" maxlength="24" size="24" placeholder="150.24">
+            </p>
+            <p>
+                <label for="comment">Comment (🚫 markdown ✅ emoji ✅ line-break): </label>
+                <textarea id="comment" name="comment" rows="5" cols="77" wrap="soft" maxlength="800"
+                          placeholder="The frequency with which I receive this income is _ _."><?= $g->saved_arr01['comment'] ?></textarea>
+            </p>
+        </section>
+        <?php require SUBMITABORT; ?>
+    </form>
 <?php require BOTTOMOFPAGES; ?>
