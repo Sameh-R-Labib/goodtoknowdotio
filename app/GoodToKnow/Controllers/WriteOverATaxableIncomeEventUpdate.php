7cefe913ae201f5df4424fd8a8d675ed509668f2
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-12-02 21:48:29 -0500

    Modified WriteOverATaxableIncomeEventUpdate for redo

--- app/GoodToKnow/Controllers/WriteOverATaxableIncomeEventUpdate.php
+++ app/GoodToKnow/Controllers/WriteOverATaxableIncomeEventUpdate.php
@@ -39,19 +39,19 @@ class WriteOverATaxableIncomeEventUpdate
 
         require_once CONTROLLERHELPERS . DIRSEP . 'standard_form_field_prep.php';
 
-        $edited_label = standard_form_field_prep('label', 3, 264);
+        $label = standard_form_field_prep('label', 3, 264);
 
 
         // year_received
 
         require_once CONTROLLERHELPERS . DIRSEP . 'integer_form_field_prep.php';
 
-        $edited_year_received = integer_form_field_prep('year_received', 1992, 65535);
+        $year_received = integer_form_field_prep('year_received', 1992, 65535);
 
 
         // comment
 
-        $edited_comment = standard_form_field_prep('comment', 0, 800);
+        $comment = standard_form_field_prep('comment', 0, 800);
 
 
         // - - - Get $g->time (which is a timestamp) based on submitted `timezone` `date` `hour` `minute` `second`
@@ -63,14 +63,76 @@ class WriteOverATaxableIncomeEventUpdate
 
         // currency
 
-        $edited_currency = standard_form_field_prep('currency', 1, 15);
+        $currency = standard_form_field_prep('currency', 1, 15);
 
 
         // amount
 
         require_once CONTROLLERHELPERS . DIRSEP . 'float_form_field_prep.php';
 
-        $edited_amount = float_form_field_prep('amount', 0.0, 999999999999999.99);
+        $amount = float_form_field_prep('amount', 0.0, 999999999999999.99);
+
+
+        /**
+         * Redirect to give the user one chance to fix their time entry.
+         * A correct time entry for a Taxable Income Event record would be in the past.
+         *
+         * The currently submitted form data will be used to conveniently
+         * populate the redo form.
+         *
+         * As you see in the code there is a mechanism which causes what we are
+         * doing here to happen only once for the submitted data set. In other
+         * words the first time the user submits his data set we will check it
+         * and give him a chance to fix it. On the subsequent submit we will
+         * just let the submitted data be saved.
+         */
+
+        if ($g->is_first_attempt) {
+
+            if ($g->time > time()) {
+
+                /**
+                 * Reset 'is_first_attempt' in the session.
+                 *
+                 * We are setting 'is_first_attempt' to false so that once the user submits the form,
+                 * and it is being processed it will not be retested for anomalous time entries.
+                 */
+
+                $_SESSION['is_first_attempt'] = false;
+
+
+                // Put form data in an array to prepare it to be stored in $_SESSION['saved_arr01'].
+                $saved_arr01['label'] = $label;
+                $saved_arr01['year_received'] = $year_received;
+                $saved_arr01['currency'] = $currency;
+                $saved_arr01['amount'] = $amount;
+                $saved_arr01['comment'] = $comment;
+                $saved_arr01['date'] = $g->date;
+                $saved_arr01['hour'] = $g->hour;
+                $saved_arr01['minute'] = $g->minute;
+                $saved_arr01['second'] = $g->second;
+                $saved_arr01['timezone'] = $g->timezone; // this is the actual timezone the user had entered
+
+
+                // make form data survive the redirect
+                $_SESSION['saved_arr01'] = $saved_arr01;
+
+
+                redirect_to("/ax1/WriteOverATaxableIncomeEventRedo/page");
+
+            }
+
+        }
+
+
+        /**
+         * Reset 'is_first_attempt' in the session.
+         *
+         * We need to set it to true so the next time the user creates a task
+         * he will have the same opportunity to have his data checked.
+         */
+
+        $_SESSION['is_first_attempt'] = true;
 
 
         /**
@@ -92,11 +154,11 @@ class WriteOverATaxableIncomeEventUpdate
          * 3) Modify the retrieved record by updating it with the submitted data.
          */
 
-        $object->label = $edited_label;
-        $object->year_received = $edited_year_received;
-        $object->comment = $edited_comment;
-        $object->amount = $edited_amount;
-        $object->currency = $edited_currency;
+        $object->label = $label;
+        $object->year_received = $year_received;
+        $object->comment = $comment;
+        $object->amount = $amount;
+        $object->currency = $currency;
         $object->time = $g->time;
 
 
