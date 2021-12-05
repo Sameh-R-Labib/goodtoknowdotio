e65d28534370872fd12e15e40070a6ffa0259cf7
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-12-05 18:45:14 -0500

    Created FineTuneACommoditySoldRedo.php

---
+++ app/GoodToKnow/Controllers/FineTuneACommoditySoldRedo.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace GoodToKnow\Controllers;
+
+class FineTuneACommoditySoldRedo
+{
+    function page()
+    {
+        global $g;
+
+
+        kick_out_loggedoutusers_or_if_there_is_error_msg();
+
+
+        /**
+         * Tell the user he is seeing the form a 2nd time.
+         */
+
+        $g->message .= ' <b>We are giving you one chance to fix the time values because we think they are wrong.</b> ';
+
+
+        $g->html_title = 'One chance to redo';
+
+
+        require VIEWS . DIRSEP . 'finetuneacommoditysoldedit.php';
+
+    }
+}
