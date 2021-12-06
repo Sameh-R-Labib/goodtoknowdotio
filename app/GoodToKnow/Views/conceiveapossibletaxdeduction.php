d40cff5f4ec37dbec1a6b7904ea0807507c2674a
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-12-06 16:46:47 -0500

    Modified conceiveapossibletaxdeduction.php for saved_arr01 values

--- app/GoodToKnow/Views/conceiveapossibletaxdeduction.php
+++ app/GoodToKnow/Views/conceiveapossibletaxdeduction.php
@@ -1,3 +1,4 @@
+<?php global $g; ?>
 <?php require TOPFORFORMPAGES; ?>
     <form action="/ax1/ConceiveAPossibleTaxDeductionProcessor/page" method="post">
         <h1>Create a Possible Tax Deduction</h1>
@@ -8,19 +9,21 @@
         <section>
             <p>
                 <label for="label">Label (✅ emoji): </label>
-                <input id="label" name="label" type="text" value="" required minlength="3" maxlength="264"
+                <input id="label" name="label" type="text" value="<?= $g->saved_arr01['label'] ?>" required
+                       minlength="3" maxlength="264"
                        size="61" spellcheck="false"
                        placeholder="Monthly Linode hosting Fees for Web server of goodtoknow.io">
             </p>
             <p>
                 <label for="year_paid">Year You Made the Expenditure: </label>
-                <input id="year_paid" name="year_paid" type="text" value="" required minlength="4" maxlength="6"
+                <input id="year_paid" name="year_paid" type="text" value="<?= $g->saved_arr01['year_paid'] ?>" required
+                       minlength="4" maxlength="6"
                        size="6" placeholder="2018">
             </p>
             <p>
                 <label for="comment">Comment (🚫 markdown ✅ emoji ✅ line-break): </label>
                 <textarea id="comment" name="comment" rows="5" cols="77" wrap="soft" maxlength="800"
-                          placeholder="List the actual payments here (Assuming there were multiple.)"></textarea>
+                          placeholder="List the actual payments here.">value="<?= $g->saved_arr01['comment'] ?>"</textarea>
             </p>
         </section>
         <?php require SUBMITABORT; ?>
