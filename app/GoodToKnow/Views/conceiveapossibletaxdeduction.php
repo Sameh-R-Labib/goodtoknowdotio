af43e52208750fbca9b8d5421d139fef5e946c79
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-12-06 23:01:34 -0500

    Bug fixes in view

--- app/GoodToKnow/Views/conceiveapossibletaxdeduction.php
+++ app/GoodToKnow/Views/conceiveapossibletaxdeduction.php
@@ -21,7 +21,7 @@
             <p>
                 <label for="comment">Comment (🚫 markdown ✅ emoji ✅ line-break): </label>
                 <textarea id="comment" name="comment" rows="5" cols="77" wrap="soft" maxlength="800"
-                          placeholder="List the actual payments here.">value="<?= $g->saved_arr01['comment'] ?>"</textarea>
+                          placeholder="List the actual payments here."><?= $g->saved_arr01['comment'] ?></textarea>
             </p>
         </section>
         <?php require SUBMITABORT; ?>
