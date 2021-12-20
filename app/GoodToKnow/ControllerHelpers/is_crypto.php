68847105094b2411ebd7f2ba1d6614c474e10363
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-12-20 18:16:12 -0500

    Created is_sixteen_decimal_places_currency.php

--- app/GoodToKnow/ControllerHelpers/is_crypto.php
+++ app/GoodToKnow/ControllerHelpers/is_crypto.php
@@ -8,6 +8,15 @@ namespace GoodToKnow\ControllerHelpers;
  */
 function is_crypto(string $currency): bool
 {
+    /**
+     * assumption:
+     *  - "is_crypto" means "is not fiat"
+     *  - "is not crypto" means "is fiat"
+     *  - it has to be one or the other (but never can be both.)
+     */
+
+    // List of all known fiat currencies
+
     $fiat_currencies = ['💵', '$', 'USD', 'dollar', 'US dollar', 'Dollar', 'US Dollar', 'ARS', 'AUD', 'BSD', 'BBD',
         'BYN', 'BZD', 'BMD', 'BOB', '$b', 'BAM', 'KM', 'BRL', 'R$', 'CAD', 'KYD', 'CLP', 'CNY', '¥', 'COP', 'CRC', '₡',
         'HRK', 'kn', 'CUP', '₱', 'CZK', 'Kč', 'DKK', 'kr', 'DOP', 'RD$', 'EGP', '£', 'EUR', '€', 'HNL', 'L', 'HKD',
@@ -16,11 +25,17 @@ function is_crypto(string $currency): bool
         'RSD', 'Дин.', 'SGD', 'ZAR', 'R', 'SEK', 'kr', 'CHF', 'SYP', 'TWD', 'NT$', 'TRY', 'UAH', '₴', 'GBP', 'VEF', 'Bs',
         'VND', '₫', 'YER', 'ZWD', 'Z$', '¢', '₣', '₲', 'ლ', 'лв.', '₺', '₥', '₹', '৳', '₮', 'zł', 'franc'];
 
+
     if (in_array($currency, $fiat_currencies)) {
+
+        // $currency is fiat
+        // thus $currency is not crypto.
+        // Therefore, return false.
 
         return false;
 
     }
+
 
     return true;
 }
---
+++ app/GoodToKnow/ControllerHelpers/is_sixteen_decimal_places_currency.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace GoodToKnow\ControllerHelpers;
+
+/**
+ * @param string $currency
+ * @return bool
+ */
+function is_sixteen_decimal_places_currency(string $currency): bool
+{
+
+    // List of currencies which get displayed with 16 decimal places
+
+    $sixteen_decimal_places_currencies = ['BAT'];
+
+
+    if (in_array($currency, $sixteen_decimal_places_currencies)) {
+
+        return true;
+
+    }
+
+
+    return false;
+    
+}
