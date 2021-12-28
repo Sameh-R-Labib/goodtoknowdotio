62faaa255bf85161b2e0ace7a3423951267acb5f
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-12-27 20:26:57 -0500

    Changed copyright year to 2022

--- app/GoodToKnow/ViewsIncludes/footerbar.php
+++ app/GoodToKnow/ViewsIncludes/footerbar.php
@@ -2,7 +2,7 @@
 <div id="footerbar">
     <p align="center" style="font-size: .78em;">
         <img src="/Gnu-head-30-years-anniversary.svg" style="float:left;height: 20px;width: 20px;margin-top: -0px">
-        © 2021 - Sameh Ramzy Labib
+        © 2022 - Sameh Ramzy Labib
         <img src="/2000px-GPLv3_Logo.svg.png"
              height="20" width="59"
              style="float:right;;margin-top: -0px"></p>
