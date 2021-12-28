e401ba25f883924372abc167f2d40ce436e7895a
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-12-28 17:47:52 -0500

    Made changes to Create a Commodity Record to reflect these aren't just bitcoin

--- app/GoodToKnow/Views/initializeabitcoinrecord.php
+++ app/GoodToKnow/Views/initializeabitcoinrecord.php
@@ -1,36 +1,36 @@
 <?php global $g; ?>
 <?php require TOPFORFORMPAGES; ?>
     <form action="/ax1/InitializeABitcoinRecordProcessor/page" method="post">
-        <h1>Create a ₿ 📽</h1>
+        <h1>Create a Commodity Record</h1>
         <?php require SESSIONMESSAGE; ?>
         <section>
             <p>
-                <label for="address">₿ address: </label>
+                <label for="address">Commodity (aka. C.) Address or Label: </label>
                 <input id="address" name="address" type="text" value="<?= $g->saved_arr01['address'] ?>" required
                        minlength="8" maxlength="264" size="60" spellcheck="false"
                        placeholder="bc1qar0srrr7xfkvy5l643lydnw9re59gtzzwf5mdq">
             </p>
             <p>
-                <label for="initial_balance">Initial BTC Balance: </label>
+                <label for="initial_balance">Initial C. Amount: </label>
                 <input id="initial_balance" name="initial_balance" type="text" placeholder="0.01500002"
                        value="<?= $g->saved_arr01['initial_balance'] ?>" required minlength="1" spellcheck="false"
                        size="33" maxlength="33">
             </p>
             <p>
-                <label for="current_balance">Current BTC Balance: </label>
+                <label for="current_balance">Current C. Amount: </label>
                 <input id="current_balance" name="current_balance" type="text" placeholder="0.01500002"
                        value="<?= $g->saved_arr01['current_balance'] ?>" required minlength="1" spellcheck="false"
                        size="33" maxlength="33">
             </p>
             <p>
-                <label for="currency">Currency (✅ emoji): </label>
+                <label for="currency">Currency Used For Purchase of C. (✅ emoji): </label>
                 <input id="currency" name="currency" type="text" placeholder="💵"
                        value="<?= $g->saved_arr01['currency'] ?>" required minlength="1" maxlength="15" size="15">
             </p>
             <p>
-                <label for="price_point">BTC Price at Time of Purchase <span class="tooltip">ℹ️<span class="tooltiptext
-                tooltip-top">If the amounts to be displayed should have 2 instead of  8 decimal places then ask the admin
-                        to add your type of currency to the list of known fiat currencies.</span></span>: </label>
+                <label for="price_point">Commodity's Price at Time of Purchase <span class="tooltip">ℹ️<span class="tooltiptext
+                tooltip-top">If the amounts for currency should have a different number of decimal places then ask the admin
+                        to fix this.</span></span>: </label>
                 <input id="price_point" name="price_point" type="text" placeholder="0.00"
                        value="<?= $g->saved_arr01['price_point'] ?>" minlength="1" spellcheck="false" size="33"
                        maxlength="33">
