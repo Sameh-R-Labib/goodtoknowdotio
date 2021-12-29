47dbef82c775781e9dd400e69c894cce4fd03480
Sameh-R-Labib <schoolbuscompany@gmail.com>
2021-12-28 20:59:23 -0500

    Updated initializeabitcoinrecord.php to include field commodity

--- app/GoodToKnow/Views/initializeabitcoinrecord.php
+++ app/GoodToKnow/Views/initializeabitcoinrecord.php
@@ -11,6 +11,11 @@
                        placeholder="bc1qar0srrr7xfkvy5l643lydnw9re59gtzzwf5mdq">
             </p>
             <p>
+                <label for="commodity">Commodity Purchased (✅ emoji): </label>
+                <input id="commodity" name="commodity" type="text" placeholder="₿"
+                       value="<?= $g->saved_arr01['commodity'] ?>" required minlength="1" maxlength="15" size="15">
+            </p>
+            <p>
                 <label for="initial_balance">Initial C. Amount: </label>
                 <input id="initial_balance" name="initial_balance" type="text" placeholder="0.01500002"
                        value="<?= $g->saved_arr01['initial_balance'] ?>" required minlength="1" spellcheck="false"
@@ -23,12 +28,12 @@
                        size="33" maxlength="33">
             </p>
             <p>
-                <label for="currency">Currency Used For Purchase of C. (✅ emoji): </label>
-                <input id="currency" name="currency" type="text" placeholder="💵"
+                <label for="currency">Currency Used to Purchase the C. (✅ emoji): </label>
+                <input id="currency" name="currency" type="text" placeholder="$"
                        value="<?= $g->saved_arr01['currency'] ?>" required minlength="1" maxlength="15" size="15">
             </p>
             <p>
-                <label for="price_point">Commodity's Price at Time of Purchase <span class="tooltip">ℹ️<span class="tooltiptext
+                <label for="price_point">C.'s Price at Time of Purchase <span class="tooltip">ℹ️<span class="tooltiptext
                 tooltip-top">If the amounts for currency should have a different number of decimal places then ask the admin
                         to fix this.</span></span>: </label>
                 <input id="price_point" name="price_point" type="text" placeholder="0.00"
