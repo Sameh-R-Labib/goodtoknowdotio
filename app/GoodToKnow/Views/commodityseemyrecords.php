cbda7a9e642c330ba04eaace7d21826dffdac09e
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-01-01 21:14:39 -0500

    Modified commodityseemyrecords.php for commodity type

--- app/GoodToKnow/Views/commodityseemyrecords.php
+++ app/GoodToKnow/Views/commodityseemyrecords.php
@@ -4,17 +4,18 @@
 <?php global $g; ?>
     <!-- maincontent -->
     <div id="maincontent">
-        <h1>Show ₿ 📽s</h1>
+        <h1>Commodity Records</h1>
         <hr>
         <?php if (!empty($g->array_of_commodity_objects)): ?>
             <?php $last = count($g->array_of_commodity_objects) - 1; ?>
             <?php foreach ($g->array_of_commodity_objects as $key => $commodity): ?>
                 <p><b>Time of purchase: </b><?= $commodity->time ?></p>
                 <p><b>Address: </b><?= $commodity->address ?></p>
-                <p><b>Price of 1₿ at 🕒 of purchase: </b><?= $commodity->currency ?>&nbsp;<?= $commodity->price_point ?>
+                <p><b>Price of 1<?= $commodity->commodity ?> at 🕒 of purchase: </b><?= $commodity->currency ?>
+                    &nbsp;<?= $commodity->price_point ?>
                 </p>
-                <p><b>Initial Balance: </b>₿&nbsp;<?= $commodity->initial_balance ?></p>
-                <p><b>Current Balance: </b>₿&nbsp;<?= $commodity->current_balance ?></p>
+                <p><b>Initial Balance: </b><?= $commodity->commodity ?>&nbsp;<?= $commodity->initial_balance ?></p>
+                <p><b>Current Balance: </b><?= $commodity->commodity ?>&nbsp;<?= $commodity->current_balance ?></p>
                 <p><?= $commodity->comment ?></p>
                 <?php if ($key != $last): ?>
                     <hr>
