b2fa4c594847827cca2cc676d39d430d909c4120
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-01-26 22:44:32 -0500

    More Words

--- app/GoodToKnow/Views/spycommoditiessoldyearfilter.php
+++ app/GoodToKnow/Views/spycommoditiessoldyearfilter.php
@@ -4,19 +4,19 @@
 <?php require CBSOFREGULARPAGES; ?>
     <!-- maincontent -->
     <div id="maincontent">
-        <h1><?= $g->tax_year ?>'s Commodity Sold Records</h1>
+        <h1><?= $g->tax_year ?>'s Commodity (a.k.a. C) Sold Records</h1>
         <hr>
         <?php if (!empty($g->array)): ?>
             <?php $last = count($g->array) - 1; ?>
             <?php foreach ($g->array as $key => $object): ?>
-                <h2 class="topofpage"><?= $object->time_sold ?></h2>
+                <h2 class="topofpage"><?= $object->currency_transacted ?>&nbsp;Bought At <?= $object->time_sold ?></h2>
                 <p><b>Time Sold: </b><?= $object->time_sold ?></p>
                 <p><b>Time Bought: </b><?= $object->time_bought ?></p>
-                <p><b>Price Per Unit Bought: </b><?= $object->currency_transacted ?>&nbsp;<?= $object->price_bought ?>
+                <p><b>Price Per C Unit Bought: </b><?= $object->currency_transacted ?>&nbsp;<?= $object->price_bought ?>
                 </p>
-                <p><b>Price Per Unit Sold: </b><?= $object->currency_transacted ?>&nbsp;<?= $object->price_sold ?></p>
-                <p><b>What Was Sold: </b><?= $object->commodity_type ?>&nbsp;<?= $object->commodity_amount ?></p>
-                <p><b>Label of Commodity Record From Which It Was Sold: </b><?= $object->commodity_label ?></p>
+                <p><b>Price Per C Unit Sold: </b><?= $object->currency_transacted ?>&nbsp;<?= $object->price_sold ?></p>
+                <p><b>Amount of C Sold: </b><?= $object->commodity_type ?>&nbsp;<?= $object->commodity_amount ?></p>
+                <p><b>Label of C Record From Which It Was Sold: </b><?= $object->commodity_label ?></p>
                 <p><b>Tax Year: </b><?= $object->tax_year ?></p>
                 <p><b>Net Profit: </b><?= $object->currency_transacted ?>&nbsp;<?= $object->profit ?></p>
                 <?php if ($key != $last): ?>
--- app/GoodToKnow/ViewsDuplicatesIncludes/commodity_record_form.php
+++ app/GoodToKnow/ViewsDuplicatesIncludes/commodity_record_form.php
@@ -2,7 +2,7 @@
 <?php global $g; ?>
     <section>
         <p>
-            <label for="address">Commodity (aka. C.) Address or Label: </label>
+            <label for="address">Commodity (aka. C) Address or Label: </label>
             <input id="address" name="address" type="text" value="<?= $g->saved_arr01['address'] ?>" required
                    minlength="8" maxlength="264" size="60" spellcheck="false"
                    placeholder="bc1qar0srrr7xfkvy5l643lydnw9re59gtzzwf5mdq">
@@ -15,26 +15,26 @@
                    value="<?= $g->saved_arr01['commodity'] ?>" required minlength="1" maxlength="15" size="15">
         </p>
         <p>
-            <label for="initial_balance">Initial C. Amount: </label>
+            <label for="initial_balance">Initial C Amount: </label>
             <input id="initial_balance" name="initial_balance" type="text" placeholder="0.01500002"
                    value="<?= $g->saved_arr01['initial_balance'] ?>" required spellcheck="false"
                    size="33" minlength="1" maxlength="33">
         </p>
         <p>
-            <label for="current_balance">Current C. Amount: </label>
+            <label for="current_balance">Current C Amount: </label>
             <input id="current_balance" name="current_balance" type="text" placeholder="0.01500002"
                    value="<?= $g->saved_arr01['current_balance'] ?>" required spellcheck="false"
                    size="33" minlength="1" maxlength="33">
         </p>
         <p>
-            <label for="currency">Currency Used to Purchase the C. (✅ emoji) <span class="tooltip">ℹ️<span class="tooltiptext
+            <label for="currency">Currency Used to Purchase the C (✅ emoji) <span class="tooltip">ℹ️<span class="tooltiptext
                 tooltip-top">Do not change the currency type after you create this record unless you are sure the new
                         type uses same number of decimal places.</span></span>: </label>
             <input id="currency" name="currency" type="text" placeholder="$"
                    value="<?= $g->saved_arr01['currency'] ?>" required size="15" minlength="1" maxlength="15">
         </p>
         <p>
-            <label for="price_point">C.'s Price at Time of Purchase <span class="tooltip">ℹ️<span class="tooltiptext
+            <label for="price_point">C's Price at Time of Purchase <span class="tooltip">ℹ️<span class="tooltiptext
                 tooltip-top">If the amounts for currency should have a different number of decimal places then ask the admin
                         to fix this.</span></span>: </label>
             <input id="price_point" name="price_point" type="text" placeholder="0.00" spellcheck="false"
