beb180dba32504f50d92f6f39e33ab483cce3d74
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-01-26 22:35:24 -0500

    Improved wording

--- app/GoodToKnow/Views/spycommoditiessoldyearfilter.php
+++ app/GoodToKnow/Views/spycommoditiessoldyearfilter.php
@@ -12,10 +12,11 @@
                 <h2 class="topofpage"><?= $object->time_sold ?></h2>
                 <p><b>Time Sold: </b><?= $object->time_sold ?></p>
                 <p><b>Time Bought: </b><?= $object->time_bought ?></p>
-                <p><b>Price Bought: </b><?= $object->currency_transacted ?>&nbsp;<?= $object->price_bought ?></p>
-                <p><b>Price Sold: </b><?= $object->currency_transacted ?>&nbsp;<?= $object->price_sold ?></p>
+                <p><b>Price Per Unit Bought: </b><?= $object->currency_transacted ?>&nbsp;<?= $object->price_bought ?>
+                </p>
+                <p><b>Price Per Unit Sold: </b><?= $object->currency_transacted ?>&nbsp;<?= $object->price_sold ?></p>
                 <p><b>What Was Sold: </b><?= $object->commodity_type ?>&nbsp;<?= $object->commodity_amount ?></p>
-                <p><b>Label of What Was Sold: </b><?= $object->commodity_label ?></p>
+                <p><b>Label of Commodity Record From Which It Was Sold: </b><?= $object->commodity_label ?></p>
                 <p><b>Tax Year: </b><?= $object->tax_year ?></p>
                 <p><b>Net Profit: </b><?= $object->currency_transacted ?>&nbsp;<?= $object->profit ?></p>
                 <?php if ($key != $last): ?>
