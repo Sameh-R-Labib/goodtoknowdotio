b30eebfb474246af0b19ebd7371a8c1ce39df801
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-01-31 00:44:07 -0500

    …

--- app/GoodToKnow/ViewsIncludes/breadcrumbs.php
+++ app/GoodToKnow/ViewsIncludes/breadcrumbs.php
@@ -43,7 +43,7 @@ switch ($g->page) {
         echo "<a href=\"/ax1/SpyCommoditiesSold/page\">One Year's Commodity Sold Records</a>";
         break;
     case 'CPBasics':
-        echo "<a href=\"/ax1/CPBasics/page\">More &DotDot;</a>";
+        echo "<a href=\"/ax1/CPBasics/page\">More …</a>";
         break;
     case 'CPTransactions':
         echo "<a href=\"/ax1/CPTransactions/page\">Banking Transaction</a>";
