362dc5f881e361b1a74ef893fd262556378d58f8
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-01-31 17:38:50 -0500

    Created the Class for transaction

--- app/GoodToKnow/Controllers/TransferAnAmount.php
+++ app/GoodToKnow/Controllers/TransferAnAmount.php
@@ -6,6 +6,24 @@ class TransferAnAmount
 {
     function page()
     {
+        /**
+         * Transfer An Amount
+         * ==================
+         *
+         * Transfer An Amount makes it convenient to record the two transactions
+         * involved when a single amount is transferred from one bank account to
+         * another.
+         */
 
+        global $g;
+
+
+        kick_out_loggedoutusers_or_if_there_is_error_msg();
+
+
+        $g->html_title = 'Enter Transfer Data';
+
+
+        require VIEWS . DIRSEP . 'transferanamount.php';
     }
 }
