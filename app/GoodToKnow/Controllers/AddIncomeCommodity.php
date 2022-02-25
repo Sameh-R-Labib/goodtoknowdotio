bc511917ffc6818805dc3f970ff732bf7d30ec94
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-02-24 21:08:16 -0500

    I started coding the first route of AddIncomeCommodity

--- app/GoodToKnow/Controllers/AddIncomeCommodity.php
+++ app/GoodToKnow/Controllers/AddIncomeCommodity.php
@@ -6,6 +6,29 @@ class AddIncomeCommodity
 {
     function page()
     {
+        /**
+         * Add Income Commodity
+         * ====================
+         *
+         * Add Income Commodity makes it convenient to both create a TaxableIncomeEvent
+         * and a Community record. The user only needs to fill out one form.
+         */
 
+        global $g;
+
+
+        kick_out_loggedoutusers_or_if_there_is_error_msg();
+
+
+        $g->html_title = 'Enter Transfer Data';
+
+
+        /**
+         * We need to assign default values for the form field
+         * variables. The reason we need these particular variable names
+         * is that the form is also used by the redo.
+         *
+         * All the form's variables are elements of $g->saved_arr01.
+         */
     }
 }
