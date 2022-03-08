7c5076357f1b524ed0a1aa50d5412b223385408b
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-03-08 17:21:21 -0500

    Shield emoji

--- app/GoodToKnow/Views/adminhome.php
+++ app/GoodToKnow/Views/adminhome.php
@@ -13,7 +13,7 @@
             <a class="orangebtn" href="/ax1/CPCommodities/page">Commodity</a>
             <a class="blackbtn" href="/ax1/CPCapitalGains/page">Commodity Sold</a>
             <a class="greenbtn" href="/ax1/Proclamation/page">Proclamation</a></p>
-        <p><a class="greenbtn" href="/ax1/CPPurges/page">Cleanup</a>
+        <p>🛡️ <a class="greenbtn" href="/ax1/CPPurges/page">Cleanup</a>
             <a class="clearbtn" href="/ax1/CPAccounts/page">User</a>
             <a class="orangebtn" href="/ax1/CPPostings/page">Blog</a>
         </p>
