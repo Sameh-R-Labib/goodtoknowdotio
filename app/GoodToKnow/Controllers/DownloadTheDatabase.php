5f1c22912e7de2d37522de6d3fe34b1312446bc9
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-03-08 21:22:37 -0500

    Save what I've gleaned

--- app/GoodToKnow/Controllers/DownloadTheDatabase.php
+++ app/GoodToKnow/Controllers/DownloadTheDatabase.php
@@ -16,6 +16,36 @@ class DownloadTheDatabase
 
 
         kick_out_nonadmins_or_if_there_is_error_msg();
-        
+
+
+        /**
+         * Make sure this syntax is correct for my version of mysqldump command.
+         *
+         * Other possible syntax:
+         *  * mysqldump --user=... --password=... --host=... DB_NAME > /path/to/output/file.sql
+         *  * Usage: mysqldump [OPTIONS] database [tables]
+         */
+
+
+        /**
+         * Will send strait to the browser.
+         */
+
+        $DBUSER = "user";
+        $DBPASSWD = "password";
+        $DATABASE = "user_db";
+
+        $filename = "backup-" . date("d-m-Y") . ".sql.gz";
+        $mime = "application/x-gzip";
+
+        header("Content-Type: " . $mime);
+        header('Content-Disposition: attachment; filename="' . $filename . '"');
+
+        $cmd = "mysqldump -u $DBUSER --password=$DBPASSWD $DATABASE | gzip --best";
+
+        passthru($cmd);
+
+        exit(0);
+
     }
 }
