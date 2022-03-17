09729f863a130910bca84fe512dc9a0cf0a71dad
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-03-17 15:23:59 -0400

    Completed EditPostTitleProcessor.php

--- app/GoodToKnow/Controllers/EditPostTitleProcessor.php
+++ app/GoodToKnow/Controllers/EditPostTitleProcessor.php
@@ -4,5 +4,16 @@ namespace GoodToKnow\Controllers;
 
 class EditPostTitleProcessor
 {
+    function page()
+    {
+        kick_out_loggedoutusers_or_if_there_is_error_msg();
+
+
+        require CONTROLLERINCLUDES . DIRSEP . 'get_and_save_the_topic_id.php';
+
+
+        redirect_to("/ax1/EditPostTitleChoosePost/page");
+
+    }
 
 }
