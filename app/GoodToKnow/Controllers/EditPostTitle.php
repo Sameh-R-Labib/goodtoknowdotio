167401b43962886c2b73c9cb1bf09d0f463658d4
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-03-17 15:11:04 -0400

    Completed editposttitle.php

--- app/GoodToKnow/Controllers/EditPostTitle.php
+++ app/GoodToKnow/Controllers/EditPostTitle.php
@@ -6,6 +6,25 @@ class EditPostTitle
 {
     function page()
     {
-        
+        /**
+         * This is the first in a series of routes aimed at editing the title
+         * and extended title of preexisting post belonging to the user.
+         *
+         * This route will present a form which asks which topic does the post exist in.
+         */
+
+
+        kick_out_loggedoutusers_or_if_there_is_error_msg();
+
+
+        get_db();
+
+
+        require CONTROLLERINCLUDES . DIRSEP . 'get_topics_for_a_comm_inside_part.php';
+
+
+        require VIEWS . DIRSEP . 'editposttitle.php';
+
     }
+
 }
