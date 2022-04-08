391499e9f72d069f7b3076ce63a1dff24b07ba23
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-04-08 17:55:28 -0400

    Move which post

--- app/GoodToKnow/Views/movepost.php
+++ app/GoodToKnow/Views/movepost.php
@@ -3,7 +3,7 @@
     <form action="/ax1/move_post_get_post/page" method="post">
         <h1>Move Post From Current Topic to Different Topic</h1>
         <?php require SESSIONMESSAGE; ?>
-        <p>Which post to moved?</p>
+        <p>Move which post?</p>
         <section>
             <?php foreach ($g->array_of_post_objects as $key => $post_object): ?>
                 <label for="choice-<?= $key ?>" class="radio">
