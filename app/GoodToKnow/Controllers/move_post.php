d9049d81ac1bd2c9e671edc4b92825987732034d
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-04-07 22:35:47 -0400

    Completed the form which presents choices of posts

--- app/GoodToKnow/Controllers/move_post.php
+++ app/GoodToKnow/Controllers/move_post.php
@@ -2,7 +2,58 @@
 
 namespace GoodToKnow\Controllers;
 
+use GoodToKnow\Models\topic_to_post;
+
 class move_post
 {
+    function page()
+    {
+        /**
+         * This route starts the "Move a Post" feature.
+         *
+         * Q: What is the "Move a Post" feature?
+         * A: - The "Move a Post" feature is an Admin tool.
+         *    - It enables Admin to select a post from his current topic.
+         *    - Q: What is 'his current topic'?
+         *      A: 'his current topic' is the topic specified in his session variable topic_id.
+         *         When he last navigated to a topic or a post then topic_id got set .
+         *    - Then, Admin assigns a new topic to the post. In essence this moves the post from
+         *      his current topic to the topic he selected.
+         *    - If he wants to move a post which is not in his current topic then he needs to navigate
+         *      to that other topic before running this feature.
+         *
+         * Q: More specifically, what should this route do?
+         * A: This route will:
+         *    - use $g->topic_id
+         *    - get an array of all the posts within the topic
+         *    - present a form for choosing a post
+         */
 
+
+        global $g;
+
+
+        kick_out_nonadmins_or_if_there_is_error_msg();
+
+
+        // $g->topic_id is the id of the topic in which the post exists
+
+
+        get_db();
+
+
+        $g->array_of_post_objects = topic_to_post::get_posts_array_for_a_topic($g->topic_id);
+
+        if (!$g->array_of_post_objects) {
+
+            breakout(' There are no posts in your current topic. ');
+
+        }
+
+
+        $g->html_title = 'Which post is to be moved?';
+
+
+        require VIEWS . DIRSEP . 'movepost.php';
+    }
 }
---
+++ app/GoodToKnow/Views/movepost.php
@@ -0,0 +1,17 @@
+<?php global $g; ?>
+<?php require TOPFORFORMPAGES; ?>
+    <form action="/ax1/move_post_get_post/page" method="post">
+        <h1>Move Post From Current Topic to Different Topic</h1>
+        <?php require SESSIONMESSAGE; ?>
+        <p>Select the post to be moved.</p>
+        <section>
+            <?php foreach ($g->array_of_post_objects as $key => $post_object): ?>
+                <label for="choice-<?= $key ?>" class="radio">
+                    <input type="radio" id="choice-<?= $key ?>" name="choice" value="<?= $post_object->id ?>">
+                    <?= $post_object->title . " | " . $post_object->extensionfortitle ?>
+                </label>
+            <?php endforeach; ?>
+        </section>
+        <?php require SUBMITABORT; ?>
+    </form>
+<?php require BOTTOMOFPAGES; ?>
