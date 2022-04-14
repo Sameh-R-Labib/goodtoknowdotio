d6126c86415a694e2b0a7ff0a3a956541b197bd9
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-04-13 22:56:06 -0400

    Save and look

--- app/GoodToKnow/ViewsIncludes/footerbar.php
+++ app/GoodToKnow/ViewsIncludes/footerbar.php
@@ -1,6 +1,6 @@
 <!-- footerbar -->
 <div id="footerbar">
-    <p align="center" style="font-size: .78em;">
+    <p align="center" style="font-size: .58em;">
         <img src="/Gnu-head-30-years-anniversary.svg" style="float:left;height: 20px;width: 20px;margin-top: -0px">
         © 2022 - Sameh Ramzy Labib
         <img src="/2000px-GPLv3_Logo.svg.png"
