043c45b38766d34b6a789db815cc6dabed961b19
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-04-15 15:32:25 -0400

    Wrote main comment for find_too_close_sequence_numbers.php

--- app/GoodToKnow/Controllers/find_too_close_sequence_numbers.php
+++ app/GoodToKnow/Controllers/find_too_close_sequence_numbers.php
@@ -6,6 +6,16 @@ class find_too_close_sequence_numbers
 {
     function page()
     {
-        
+        /**
+         * What This Feature Does
+         * ======================
+         *   - It tells Admin which communities have topics whose sequence numbers are too close to each other.
+         *   - It tells Admin which Topics have posts whose sequence numbers are too close to each other.
+         */
+
+        global $g;
+
+
+        kick_out_nonadmins_or_if_there_is_error_msg();
     }
 }
