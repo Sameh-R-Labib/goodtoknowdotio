9765be8735987b1ef937f58944876d0f4e93f9a4
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-04-14 22:43:16 -0400

    Edited what is page

--- app/GoodToKnow/Views/whatisthissite.php
+++ app/GoodToKnow/Views/whatisthissite.php
@@ -4,18 +4,19 @@
     <!-- maincontent -->
     <div id="maincontent">
         <h1>Software Description</h1>
+        <p>Also, see the <a class="no-frills-link" href="/ax1/proclamation/page" target="_blank">Terms Of Service</p>
         <p>This site is an instance of the GoodToKnow.io web app software. The code is GPLv3 Affero licensed FLOSS
             software. The primary use for this software is to facilitate the parsing and sharing of practical useful
             information via blog posts to be shared amongst family and a closed circle of acquaintances. The
             users are divided into communities in order to control who has access to which blog posts.
             Additionally, (on a user-by-user basis) it has features for tracking things which one would normally
             track in one's personal life such as recurring payments, banking transactions and recurring to-do list
-            items. In other words you get to create and share blog posts and you get to keep track of personal
+            items. In other words you get to create and share blog posts, and you get to keep track of personal
             records.</p>
-        <blockquote>GoodToKnow.io instances are run by (at least) one admin who`s in charge of (among other things)
+        <blockquote>GoodToKnow.io instances are run by (at least) one admin who's in charge of (among other things)
             adding communities, adding topics, adding users, and approving content.
         </blockquote>
-        <p>If you`d like to become a member of this instance then contact its admin.
+        <p>If you'd like to become a member of this instance then contact its admin.
             If you stumble across a different instance you can ask its admin to join.
             Otherwise, if you'd like to create your own instance you can either use the source code yourself found at <a
                     href="https://github.com/Sameh-R-Labib/goodtoknowdotio" target="_blank">https://github.com/Sameh-R-Labib/goodtoknowdotio</a>
