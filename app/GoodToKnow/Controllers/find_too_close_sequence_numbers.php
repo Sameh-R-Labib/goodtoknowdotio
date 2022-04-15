688f285352ab48b04296df3639ba8d9aa2fd574c
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-04-15 15:03:21 -0400

    Created link to find_too_close_sequence_numbers.php

---
+++ app/GoodToKnow/Controllers/find_too_close_sequence_numbers.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace GoodToKnow\Controllers;
+
+class find_too_close_sequence_numbers
+{
+    function page()
+    {
+        
+    }
+}
--- app/GoodToKnow/Views/cppostings.php
+++ app/GoodToKnow/Views/cppostings.php
@@ -3,6 +3,7 @@
 <?php require CBSOFREGULARPAGES; ?>
     <!-- maincontent -->
     <div id="maincontent">
+        <p><a href="/ax1/find_too_close_sequence_numbers/page">Find Too Close Sequence Numbers</a></p>
         <p><a href="/ax1/balance_out_the_sequence_numbers/page">Balance Out The Sequence Numbers</a></p>
         <p><a href="/ax1/new_community/page">Create Community</a></p>
         <p><a href="/ax1/kommunity_description_editor/page">Edit A Community</a></p>
