abe557645b105431d31c5968df1bc935d95646a5
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-04-15 22:36:46 -0400

    Got all communities in the system

--- app/GoodToKnow/Controllers/find_too_close_sequence_numbers.php
+++ app/GoodToKnow/Controllers/find_too_close_sequence_numbers.php
@@ -2,6 +2,8 @@
 
 namespace GoodToKnow\Controllers;
 
+use GoodToKnow\Models\community;
+
 class find_too_close_sequence_numbers
 {
     function page()
@@ -17,5 +19,21 @@ class find_too_close_sequence_numbers
 
 
         kick_out_nonadmins_or_if_there_is_error_msg();
+
+
+        $g->line_item_for_report = [];
+
+
+        /**
+         * Get all the communities in the system.
+         */
+
+        $coms_in_this_system = community::find_all();
+
+        if ($coms_in_this_system === false) {
+
+            breakout(' Unable to retrieve communities. ');
+
+        }
     }
 }
