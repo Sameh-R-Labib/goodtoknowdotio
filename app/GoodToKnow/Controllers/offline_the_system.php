421573e937c14daaebc21131de9059ea77c07ce7
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-05-06 14:25:27 -0400

    Described the feature

--- app/GoodToKnow/Controllers/offline_the_system.php
+++ app/GoodToKnow/Controllers/offline_the_system.php
@@ -7,7 +7,17 @@ class offline_the_system
     function page()
     {
         /**
+         * FEATURE OVERVIEW
+         * ================
          *
+         * Feature Name: "Offline The System"
+         *
+         * This feature enables Admin to toggle a running Gtk.io system
+         * between "normal" and "offline" state.
+         *
+         * The offline state kicks out all non-Admin users to the login page.
+         * This gives Admin the opportunity to do maintenance on the system
+         * without bothering with user activity which may cause anomalies.
          */
     }
 }
