9146929db25c650a6594dfb47d7eace93bb13d5a
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-05-15 20:43:01 -0400

    Laid out form skeleton html

--- app/GoodToKnow/Views/setsystemwidealert.php
+++ app/GoodToKnow/Views/setsystemwidealert.php
@@ -1,1 +1,16 @@
-<?php
+<?php global $g; ?>
+<?php require TOPFORFORMPAGES; ?>
+    <form action="/ax1/set_system_wide_alert_proc/page" method="post">
+        <h1><?= $g->html_title ?></h1>
+        <?php require SESSIONMESSAGE; ?>
+        <p class="tooltip">ℹ️
+            <span class="tooltiptext tooltip-top">The system-wide alert system enables Admin to make announcements.</span>
+        </p>
+        <p>The current alert name is: <b><?= $g->current_status ?></b>.<br>
+            Here, you can modify the alert record which is stored in the database.</p>
+        <section>
+
+        </section>
+        <?php require SUBMITABORT; ?>
+    </form>
+<?php require BOTTOMOFPAGES; ?>
