a01d31188074384a370b393357b095ad73d6cf43
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-05-17 15:14:56 -0400

    Blinking

--- app/GoodToKnow/Controllers/home.php
+++ app/GoodToKnow/Controllers/home.php
@@ -105,7 +105,7 @@ class home
 
             if ($status_object->name == 'system_alert') {
 
-                $g->message .= "\n<br><span style=\"font-size:25px\">🚷 <b>$status_object->message</b> 🚷</span> ";
+                $g->message .= "\n<br><span class=\"blinking\">🚷 <b>$status_object->message</b> </span> ";
 
             }
 
