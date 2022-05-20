9aed29914b4627bf7f743dba438b79a80081ab86
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-05-20 13:08:07 -0400

    greenbtn

--- app/GoodToKnow/Views/cpbasics.php
+++ app/GoodToKnow/Views/cpbasics.php
@@ -8,7 +8,7 @@
             <a class="purplebtn" href="/ax1/edit_post_title/page">Edit Post Title</a>
             <a class="purplebtn" href="/ax1/edit_my_post/page">Edit Post Content</a>
             <a class="blackbtn" href="/ax1/author_deletes_own_post/page">Delete A Post</a>
-            <a class="blackbtn" href="/ax1/discover_other_communities/page">Discover Other Communities</a>
+            <a class="greenbtn" href="/ax1/discover_other_communities/page">Discover Other Communities</a>
             <a class="orangebtn" href="/ax1/by_username_message/page">Send A Message To Another User</a>
             <a class="clearbtn" href="/ax1/blot_out_some_inbox_messages/page">Delete Some Messages</a>
             <a class="orangebtn" href="/ax1/default_community/page">Switch To A Different Default Community</a>
