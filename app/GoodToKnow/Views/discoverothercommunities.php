aa75b77dc3848b6d633163b10ad9b3a5fc2df21d
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-05-20 18:44:31 -0400

    Mad into ul

--- app/GoodToKnow/Views/discoverothercommunities.php
+++ app/GoodToKnow/Views/discoverothercommunities.php
@@ -5,9 +5,11 @@
         <h2>Discover Other Communities</h2>
         <?php require SESSIONMESSAGE; ?>
         <p>These are communities on this Gtk.io system which you can ask to join:</p>
-        <?php foreach ($g->coms_user_does_not_belong_to as $community): ?>
-            <p><b><?= $community->community_name ?></b> ― <?= $community->community_description ?></p>
-        <?php endforeach; ?>
+        <ul>
+            <?php foreach ($g->coms_user_does_not_belong_to as $community): ?>
+                <li><b><?= $community->community_name ?></b> ― <?= $community->community_description ?></li>
+            <?php endforeach; ?>
+        </ul>
         <!-- Button to dismiss this view -->
         <p><a class="modify" href="/ax1/clear_session_vars/page">Home</a></p>
     </form>
