c8874e786c94128f87ac3bda2c53794874dcb34f
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-05-20 13:09:56 -0400

    orangebtn

--- app/GoodToKnow/Views/cpbasics.php
+++ app/GoodToKnow/Views/cpbasics.php
@@ -6,7 +6,7 @@
         <p>
             <a class="greenbtn" href="/ax1/create_new_post/page">Create A Post</a>
             <a class="purplebtn" href="/ax1/edit_post_title/page">Edit Post Title</a>
-            <a class="purplebtn" href="/ax1/edit_my_post/page">Edit Post Content</a>
+            <a class="orangebtn" href="/ax1/edit_my_post/page">Edit Post Content</a>
             <a class="blackbtn" href="/ax1/author_deletes_own_post/page">Delete A Post</a>
             <a class="greenbtn" href="/ax1/discover_other_communities/page">Discover Other Communities</a>
             <a class="orangebtn" href="/ax1/by_username_message/page">Send A Message To Another User</a>
