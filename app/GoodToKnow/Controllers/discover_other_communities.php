d18136dbce921ce9265712624e90e0f08e899cbd
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-05-20 13:16:40 -0400

    Comment regardin the purpose of the feature

--- app/GoodToKnow/Controllers/discover_other_communities.php
+++ app/GoodToKnow/Controllers/discover_other_communities.php
@@ -4,5 +4,13 @@ namespace GoodToKnow\Controllers;
 
 class discover_other_communities
 {
-
+    function page()
+    {
+        /**
+         * This feature (whose first route this is) lists
+         * the communities (on the Gtk.io server) which the
+         * user is not currently a member of. The purpose is
+         * to enable the user to discover other communities.
+         */
+    }
 }
