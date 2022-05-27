bc91a169019c78f987b688bfc52c4c844e8bcc5f
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-05-26 20:29:36 -0400

    Redid authordeletesownpost

--- app/GoodToKnow/ControllerIncludes/get_and_save_the_topic_id.php
+++ app/GoodToKnow/ControllerIncludes/get_and_save_the_topic_id.php
@@ -5,17 +5,16 @@ use function GoodToKnow\ControllerHelpers\integer_form_field_prep;
 
 global $g;
 
+/**
+ * $g->id is topic id which user chose.
+ */
 
-require_once CONTROLLERHELPERS . DIRSEP . 'integer_form_field_prep.php';
 
-$g->chosen_topic_id = integer_form_field_prep('choice', 1, PHP_INT_MAX);
+if (!array_key_exists((int)$g->id, $g->special_topic_array)) {
 
-
-if (!array_key_exists($g->chosen_topic_id, $g->special_topic_array)) {
-
-    breakout(' Unexpected error: topic id not found in topic array. ');
+    breakout(' Unexpected error 18391: topic id not found in topic array. ');
 
 }
 
 
-$_SESSION['saved_int01'] = $g->chosen_topic_id;
+$_SESSION['saved_int01'] = $g->id;
