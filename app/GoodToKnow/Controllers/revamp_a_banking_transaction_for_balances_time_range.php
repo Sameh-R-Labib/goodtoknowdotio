588369e154e2b1953fab0b55e36a11a92c6ffd3f
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-05-28 20:27:42 -0400

    Redid revampabankingtransactionforbalances

--- app/GoodToKnow/Controllers/revamp_a_banking_transaction_for_balances_time_range.php
+++ app/GoodToKnow/Controllers/revamp_a_banking_transaction_for_balances_time_range.php
@@ -4,7 +4,7 @@ namespace GoodToKnow\Controllers;
 
 class revamp_a_banking_transaction_for_balances_time_range
 {
-    function page()
+    function page(string $s = 'none')
     {
         /**
          * 1) Validate the submitted choice of time range (A,B,C,D,E.)
@@ -14,7 +14,13 @@ class revamp_a_banking_transaction_for_balances_time_range
          */
 
 
+        global $g;
+
+
         kick_out_loggedoutusers_or_if_there_is_error_msg();
+
+
+        $g->s = $s;
 
 
         require CONTROLLERINCLUDES . DIRSEP . 'find_min_max_time_range_based_on_choice.php';
--- app/GoodToKnow/Views/revampabankingtransactionforbalances.php
+++ app/GoodToKnow/Views/revampabankingtransactionforbalances.php
@@ -1,30 +1,17 @@
 <?php require TOPFORFORMPAGES; ?>
-    <form action="/ax1/revamp_a_banking_transaction_for_balances_time_range/page" method="post">
+    <form>
         <h1>Edit a Bank Transaction for Ledger</h1>
         <?php require SESSIONMESSAGE; ?>
         <p>Which time range does the transaction fall under?</p>
         <section>
-            <label for="A" class="radio">
-                <input type="radio" id="A" name="choice" value="A">
-                Around now<br>
-            </label>
-            <label for="B" class="radio">
-                <input type="radio" id="B" name="choice" value="B">
-                30 - 60 day range<br>
-            </label>
-            <label for="C" class="radio">
-                <input type="radio" id="C" name="choice" value="C">
-                60 - 90 day range<br>
-            </label>
-            <label for="D" class="radio">
-                <input type="radio" id="D" name="choice" value="D">
-                Beyond 90 days<br>
-            </label>
-            <label for="E" class="radio">
-                <input type="radio" id="E" name="choice" value="E">
-                All<br>
-            </label>
+            <a href="/ax1/revamp_a_banking_transaction_for_balances_time_range/page/A" class="choose">Around now</a>
+            <a href="/ax1/revamp_a_banking_transaction_for_balances_time_range/page/B" class="choose">30 - 60 day
+                range</a>
+            <a href="/ax1/revamp_a_banking_transaction_for_balances_time_range/page/C" class="choose">60 - 90 day
+                range</a>
+            <a href="/ax1/revamp_a_banking_transaction_for_balances_time_range/page/D" class="choose">Beyond 90 days</a>
+            <a href="/ax1/revamp_a_banking_transaction_for_balances_time_range/page/E" class="choose">All</a>
         </section>
-        <?php require SUBMITABORT; ?>
-</form>
+        <?php require ABORT; ?>
+    </form>
 <?php require BOTTOMOFPAGES; ?>
