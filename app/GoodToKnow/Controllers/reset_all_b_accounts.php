04b063bf4926ef6082679333b9d429f349083516
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-06-01 16:32:10 -0400

    Created button for Reset All

---
+++ app/GoodToKnow/Controllers/reset_all_b_accounts.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace GoodToKnow\Controllers;
+
+class reset_all_b_accounts
+{
+    function page()
+    {
+        /**
+         * When the user runs this feature he ends up with bank account
+         * records whose start dates are not more than 38 days old.
+         */
+    }
+}
--- app/GoodToKnow/Views/cpbankingaccounts.php
+++ app/GoodToKnow/Views/cpbankingaccounts.php
@@ -7,7 +7,9 @@
             <a class="purplebtn" href="/ax1/generate_a_banking_account_for_balances/page">Create</a>
             <a class="blackbtn" href="/ax1/populate_a_banking_account_for_balances/page">Edit</a>
             <a class="orangebtn" href="/ax1/view_all_banking_accounts_for_balances/page">Show</a>
-            <a class="clearbtn" href="/ax1/annul_a_banking_acct_for_balances/page">Delete</a></p>
+            <a class="clearbtn" href="/ax1/annul_a_banking_acct_for_balances/page">Delete</a>
+            <a class="blackbtn" href="/ax1/reset_all_b_accounts/page">Reset All</a>
+        </p>
     </div><!-- End maincontent -->
 <?php require FOOTERBAR; ?>
 <?php require BOTTOMOFPAGES; ?>
