f8dd7dbc7fffd893badf4d30b5e3c29da0ed366e
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-06-02 14:09:16 -0400

    Let me take a look at the array_of_objects

--- app/GoodToKnow/Controllers/reset_all_b_accounts.php
+++ app/GoodToKnow/Controllers/reset_all_b_accounts.php
@@ -20,5 +20,29 @@ class reset_all_b_accounts
         /**
          * We need the bank accounts.
          */
+
+
+        global $g;
+
+
+        kick_out_loggedoutusers();
+
+
+        get_db();
+
+
+        require CONTROLLERINCLUDES . DIRSEP . 'get_bankingaccountsforbalances.php';
+
+
+        // $g->array_of_objects is what I got.
+
+        /**
+         * Debug Code
+         */
+        echo "\n<p>Begin debug</p>\n";
+        echo "<p>Var_dump \$g->array_of_objects: </p>\n<pre>";
+        var_dump($g->array_of_objects);
+        echo "</pre>\n";
+        die("<p>End debug</p>\n");
     }
 }
