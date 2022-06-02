c3d86fb183a0cabb95d0c26cc1e3c9ae8831afe3
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-06-02 13:40:45 -0400

    Real good comment

--- app/GoodToKnow/Controllers/reset_all_b_accounts.php
+++ app/GoodToKnow/Controllers/reset_all_b_accounts.php
@@ -10,5 +10,15 @@ class reset_all_b_accounts
          * When the user runs this feature he ends up with bank account
          * records whose start dates are not more than 38 days old.
          */
+
+        /**
+         * In this route I will iterate over the user's bank accounts.
+         * For each iteration I will call a function which resets the
+         * bank account I'm iterating over.
+         */
+
+        /**
+         * We need the bank accounts.
+         */
     }
 }
