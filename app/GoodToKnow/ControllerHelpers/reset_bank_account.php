ca90a2497e68adb4d93d79d87496023d1f82fd55
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-06-02 15:15:25 -0400

    Fixed wording of comment

--- app/GoodToKnow/ControllerHelpers/reset_bank_account.php
+++ app/GoodToKnow/ControllerHelpers/reset_bank_account.php
@@ -10,9 +10,10 @@ function reset_bank_account(object $account)
 {
     /**
      * Reset the start_time and start_balance of object $account.
-     * Make them reflect a point in the account's history which
-     * is closer to time(). This reset account will update the
-     * corresponding existing account in the database.
+     * Make start_time and start_balance reflect a point in the
+     * account's history where start_time is closer to time().
+     * This $reset version of $account will is used to update the
+     * database record of $account.
      */
 
 }
