a9df51dbd0271a5256490cc4447a0e0aba2c7af8
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-06-09 11:44:01 -0400

    Added words to whatisthissite.php

--- app/GoodToKnow/Views/whatisthissite.php
+++ app/GoodToKnow/Views/whatisthissite.php
@@ -17,7 +17,7 @@
             users are divided into communities in order to limit which blog posts each user has access to.
             Additionally, (on a user-by-user basis) it has features for tracking things which one would normally
             track in one's personal life such as recurring payments, banking transactions and recurring to-do list
-            items. In other words you get to , and you get to:
+            items. In other words you get to:
         </p>
         <ul>
             <li>create and share blog posts</li>
@@ -27,11 +27,13 @@
             GoodToKnow.io instances are run by (at least) one Admin who's in charge of (among other things)
             adding communities, adding topics, adding users, and approving content.
         </blockquote>
-        <p>If you'd like to become a member of this instance then contact its Admin.
-            If you stumble across a different instance you can ask its Admin to join.
+        <p>If you'd like to become a member of this GoodToKnow.io instance then contact its Admin.
+            If you stumble across a different GoodToKnow.io instance (having a different URL) you can ask its Admin to
+            join it.
             Otherwise, if you'd like to create your own instance you can either use the source code yourself found at
             <a href="https://github.com/Sameh-R-Labib/goodtoknowdotio" target="_blank">https://github.com/Sameh-R-Labib/goodtoknowdotio</a>
-            or you could contact Admin and try to persuade him/her to help you set it up. You'll find the email
+            and / or you could contact an Admin and try to persuade him / her to help you set it up. You'll find the
+            email
             address of each instance's Admin on the login page/form.
         </p>
         <p>
