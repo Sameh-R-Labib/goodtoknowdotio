08aadbd0590fb7a19f08e20d93bcf8c94a897688
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-06-10 15:09:16 -0400

    script

--- app/GoodToKnow/Views/cpserver.php
+++ app/GoodToKnow/Views/cpserver.php
@@ -5,6 +5,7 @@
     <div id="maincontent">
         <p><a target="_blank" href="/rockadily.php">PhpInfo</a></p>
         <p><a target="_blank" href="/gartirssj6">Php-fpm Status Page</a></p>
+        <p><a target="_blank" href="/jssritrag3">Php-fpm Status Script</a></p>
     </div><!-- End maincontent -->
 <?php require FOOTERBAR; ?>
 <?php require BOTTOMOFPAGES; ?>
