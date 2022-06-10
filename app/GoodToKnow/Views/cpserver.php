8f7eaaa49832979e6f0c3db8c62fa08ce638b272
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-06-10 15:05:22 -0400

    fpm health

--- app/GoodToKnow/Views/cpserver.php
+++ app/GoodToKnow/Views/cpserver.php
@@ -4,6 +4,7 @@
     <!-- maincontent -->
     <div id="maincontent">
         <p><a target="_blank" href="/rockadily.php">PhpInfo</a></p>
+        <p><a target="_blank" href="/gartirssj6">Php-fpm Status Page</a></p>
     </div><!-- End maincontent -->
 <?php require FOOTERBAR; ?>
 <?php require BOTTOMOFPAGES; ?>
