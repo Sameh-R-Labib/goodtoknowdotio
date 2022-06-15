fdc49b2751a5fed650008ab017f582fd4489541d
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-06-14 20:02:20 -0400

    No need for Edit

--- app/GoodToKnow/Views/cpbankingobservers.php
+++ app/GoodToKnow/Views/cpbankingobservers.php
@@ -5,9 +5,8 @@
     <div id="maincontent">
         <p>
             <a class="purplebtn" href="/ax1/produce_a_bank_acct_observer/page">Create</a>
-            <a class="blackbtn" href="/ax1/adapt_a_bank_acct_observer/page">Edit</a>
-            <a class="orangebtn" href="/ax1/show_all_bank_acct_observers/page">Show</a>
             <a class="clearbtn" href="/ax1/destroy_a_bank_acct_observer/page">Delete</a>
+            <a class="orangebtn" href="/ax1/show_all_bank_acct_observers/page">Show</a>
         </p>
     </div><!-- End maincontent -->
 <?php require FOOTERBAR; ?>
