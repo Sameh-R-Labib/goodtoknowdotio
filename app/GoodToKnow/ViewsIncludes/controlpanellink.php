f6417dcd7e583c39de7c950d55e1519fc5f45cab
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-06-27 12:41:20 -0400

    118

--- app/GoodToKnow/ViewsIncludes/controlpanellink.php
+++ app/GoodToKnow/ViewsIncludes/controlpanellink.php
@@ -1,8 +1,8 @@
 <?php global $g; ?>
 <?php if ($g->is_admin): ?>
-    <a href="/ax1/admin_home/page"><img src="/more-button-md.png" alt="Admin Panel" height="86" width="108"></a>
+    <a href="/ax1/admin_home/page"><img src="/more-button-md.png" alt="Admin Panel" height="118" width="118"></a>
 <?php elseif ($g->is_guest): ?>
     <img src="/cpicon.png" alt="Admin Panel" height="86" width="108">
 <?php else: ?>
-    <a href="/ax1/control_panel/page"><img src="/more-button-md.png" alt="Admin Panel" height="86" width="108"></a>
+    <a href="/ax1/control_panel/page"><img src="/more-button-md.png" alt="Admin Panel" height="118" width="118"></a>
 <?php endif; ?>
