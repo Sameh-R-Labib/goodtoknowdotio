a037332b2b88a4cef8f2de442eac73a2ba1fdbe9
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-06-27 12:36:31 -0400

    More Button for CP

--- app/GoodToKnow/ViewsIncludes/controlpanellink.php
+++ app/GoodToKnow/ViewsIncludes/controlpanellink.php
@@ -1,8 +1,8 @@
 <?php global $g; ?>
 <?php if ($g->is_admin): ?>
-    <a href="/ax1/admin_home/page"><img src="/cpicon.png" alt="Admin Panel" height="86" width="108"></a>
+    <a href="/ax1/admin_home/page"><img src="/more-button-md.png" alt="Admin Panel" height="86" width="108"></a>
 <?php elseif ($g->is_guest): ?>
     <img src="/cpicon.png" alt="Admin Panel" height="86" width="108">
 <?php else: ?>
-    <a href="/ax1/control_panel/page"><img src="/cpicon.png" alt="Admin Panel" height="86" width="108"></a>
+    <a href="/ax1/control_panel/page"><img src="/more-button-md.png" alt="Admin Panel" height="86" width="108"></a>
 <?php endif; ?>
