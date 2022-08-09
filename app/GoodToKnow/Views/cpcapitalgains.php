f604615ac07eed54367092fca8700fdada2efaaa
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-08-08 21:52:47 -0400

    Created the button for process a commodity sale

--- app/GoodToKnow/Views/cpcapitalgains.php
+++ app/GoodToKnow/Views/cpcapitalgains.php
@@ -7,7 +7,8 @@
             <a class="greenbtn" href="/ax1/father_a_commodity_sold/page">Create</a>
             <a class="purplebtn" href="/ax1/fine_tune_a_commodity_sold/page">Edit</a>
             <a class="blackbtn" href="/ax1/spy_commodities_sold/page">See 1 Year's</a>
-            <a class="orangebtn" href="/ax1/drop_a_commodity_sold/page">Delete</a></p>
+            <a class="orangebtn" href="/ax1/drop_a_commodity_sold/page">Delete</a>
+            <a class="clearbtn" href="/ax1/process_a_commodity_sale/page">Process A Commodity Sale</a></p>
     </div><!-- End maincontent -->
 <?php require FOOTERBAR; ?>
 <?php require BOTTOMOFPAGES; ?>
