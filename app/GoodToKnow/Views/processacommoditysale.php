f97c0b77a2ae454d3bafb48d160ea97dd1a7f3ee
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-08-10 16:48:26 -0400

    Started the view

--- app/GoodToKnow/Views/processacommoditysale.php
+++ app/GoodToKnow/Views/processacommoditysale.php
@@ -1,1 +1,13 @@
-<?php
+<?php global $g; ?>
+<?php require TOPFORFORMPAGES; ?>
+    <form action="/ax1/process_a_commodity_sale_form_processor/page" method="post">
+        <h1>Process A Commodity Sale</h1>
+        <?php require SESSIONMESSAGE; ?>
+        <p>Process A Commodity Sale makes it convenient to both modify Commodity records and create Commodity Sold
+            records when you make a sale of a commodity.</p>
+        <section>
+            <p></p>
+        </section>
+        <?php require SUBMITABORT; ?>
+    </form>
+<?php require BOTTOMOFPAGES; ?>
