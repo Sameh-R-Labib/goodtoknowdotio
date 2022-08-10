cfdc58ed36d2ed421446be618fdb3f766ef16e3d
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-08-09 22:07:28 -0400

    Created process_a_commodity_sale.php

---
+++ app/GoodToKnow/Controllers/process_a_commodity_sale.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace GoodToKnow\Controllers;
+
+class process_a_commodity_sale
+{
+    function page()
+    {
+        /**
+         * Process A Commodity Sale
+         * ========================
+         *
+         * Process A Commodity Sale makes it convenient to both modify Commodity
+         * records and create Commodity Sold records when a user makes a sale
+         * of a commodity at a point in time.
+         *
+         * If you understand how this is done manually you should be able to figure
+         * out what this feature does. Just think of what kind of records you would
+         * need to do capital gains tax reporting.
+         */
+
+        global $g;
+
+
+        kick_out_loggedoutusers_or_if_there_is_error_msg();
+
+
+        $g->html_title = 'Process A Commodity Sale';
+
+
+        require VIEWS . DIRSEP . 'processacommoditysale.php';
+    }
+}
