da3e7ec2546afbb33a1d6636fecb7cc8e8e70ff3
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-08-10 17:37:45 -0400

    Amount field added

--- app/GoodToKnow/Views/processacommoditysale.php
+++ app/GoodToKnow/Views/processacommoditysale.php
@@ -6,7 +6,18 @@
         <p>Process A Commodity Sale makes it convenient to both modify Commodity records and create Commodity Sold
             records when you make a sale of a commodity.</p>
         <section>
-            <p></p>
+            <p>
+                <label for="commodity">Type of Commodity Sold: </label>
+                <input id="commodity" name="commodity" type="text" placeholder="BAT, BTC"
+                       value="" required minlength="1" maxlength="15" size="15">
+            </p>
+            <p>
+                <label for="amount">Amount of Commodity Sold <span class="tooltip">ℹ️<span class="tooltiptext
+                tooltip-top">If the amounts to be displayed should have 2 instead of 8 decimal places then ask Admin
+                        to add your type of currency to the list of known fiat currencies.</span></span>: </label>
+                <input id="amount" name="amount" type="text" value="" required minlength="1" maxlength="33" size="33"
+                       placeholder=".0100600300440002">
+            </p>
         </section>
         <?php require SUBMITABORT; ?>
     </form>
