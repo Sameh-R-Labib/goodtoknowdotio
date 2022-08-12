35b3589612d184bd93fea5a4292af70b7346dd09
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-08-12 16:20:15 -0400

    Extraneos character

--- app/GoodToKnow/Views/processacommoditysale.php
+++ app/GoodToKnow/Views/processacommoditysale.php
@@ -40,7 +40,7 @@
                 <label for="price_sold">Price Per Unit — Sold <span class="tooltip">ℹ️<span class="tooltiptext
                 tooltip-top">If the amounts should have 2 decimal places ask Admin to add your currency to list of
                             fiat.</span></span>: </label>
-                <input id="price_sold" name="price_sold" type="text" value=">" required minlength="1" maxlength="33"
+                <input id="price_sold" name="price_sold" type="text" value="" required minlength="1" maxlength="33"
                        size="33" placeholder="150.33">
             </p>
             <p>
