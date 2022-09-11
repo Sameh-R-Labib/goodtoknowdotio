fafa7675facba007a8a656a13207b3f644b2a5ae
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-09-10 21:41:15 -0400

    Removed var_dump and added comment

--- app/GoodToKnow/Controllers/process_a_commodity_sale_generate_changes.php
+++ app/GoodToKnow/Controllers/process_a_commodity_sale_generate_changes.php
@@ -9,13 +9,21 @@ class process_a_commodity_sale_generate_changes
         /**
          * 2. Generate the modified commodity objects and the new commodity_sold objects.
          *    At this stage these objects will NOT be updating the database.
+         *
+         *
+         * What the submitted / saved form data could look like:
+         *
+         * Var_dump $g->saved_arr01:
+         *   array(7)
+         *     ["commodity"]=> string(3) "BTC"
+         *     ["amount"]=> float(0.0198)
+         *     ["time"]=> int(1662048061)
+         *     ["tax_year"]=> int(2022)
+         *     ["currency"]=> string(1) "$"
+         *     ["price_sold"]=> float(23925)
+         *     ["reason"]=> string(17) "for doing a test."
          */
 
         global $g;
-
-        // Debug.
-        echo "<p>Var_dump \$g->saved_arr01: </p>\n<pre>";
-        var_dump($g->saved_arr01);
-        echo "</pre>\n";
     }
 }
