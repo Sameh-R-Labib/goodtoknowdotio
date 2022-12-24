c7b435c17d689cdca83947b41459cfae817dc660
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-12-24 18:33:34 -0500

    Changed the verbiage about my ownership of Gtk.io

--- app/GoodToKnow/ViewsIncludes/footerbar.php
+++ app/GoodToKnow/ViewsIncludes/footerbar.php
@@ -2,7 +2,7 @@
 <div id="footerbar">
     <p align="center" style="font-size: .58em;">
         <img src="/Gnu-head-30-years-anniversary.svg" style="float:left;height: 20px;width: 20px;margin-top: -0px">
-        © 2022 - Sameh Ramzy Labib
+        Sameh Ramzy Labib owns this software
         <img src="/2000px-GPLv3_Logo.svg.png"
              height="20" width="59"
              style="float:right;;margin-top: -0px"></p>
