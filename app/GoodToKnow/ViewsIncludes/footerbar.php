0aa4f7aa7422dba3aaa528b03cd3b15006319e80
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-12-24 18:38:40 -0500

    Reworded ownership

--- app/GoodToKnow/ViewsIncludes/footerbar.php
+++ app/GoodToKnow/ViewsIncludes/footerbar.php
@@ -2,7 +2,7 @@
 <div id="footerbar">
     <p align="center" style="font-size: .58em;">
         <img src="/Gnu-head-30-years-anniversary.svg" style="float:left;height: 20px;width: 20px;margin-top: -0px">
-        Sameh Ramzy Labib owns this software
+        Sameh Ramzy Labib owns the software stored at https://github.com/Sameh-R-Labib/goodtoknowdotio
         <img src="/2000px-GPLv3_Logo.svg.png"
              height="20" width="59"
              style="float:right;;margin-top: -0px"></p>
