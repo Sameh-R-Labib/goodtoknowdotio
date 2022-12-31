d47b4556c69bb6de2b265324bcd51723f8ef5e9b
Sameh-R-Labib <schoolbuscompany@gmail.com>
2022-12-30 22:08:40 -0500

    Some fixes and enhancements

--- app/GoodToKnow/Views/processacommoditysalegeneratechanges.php
+++ app/GoodToKnow/Views/processacommoditysalegeneratechanges.php
@@ -5,8 +5,8 @@
         <h1>Save or Cancel</h1>
         <?php require SESSIONMESSAGE; ?>
         <p><b>What you see is what will be saved:</b></p>
-        <h2>$g->sold_remaining</h2>
-        <p><b>Sold Remaining: </b><?= $g->sold_remaining ?></p>
+        <h2>That Which Will Not Be Expensed</h2>
+        <p><b>Sold Remaining: </b><?= $g->saved_arr01["commodity"] ?><?= $g->sold_remaining ?></p>
 
         <!-- Present link buttons for Save and Abort -->
         <p><a class="save" href="/ax1/process_a_commodity_sale_generate_changes_save/page">Save</a>
