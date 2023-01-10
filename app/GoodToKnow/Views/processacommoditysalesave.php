a511b653f867a687b0e6e9be1d3fa43992546a89
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-01-10 14:03:36 -0500

    Skeleton added of regular page

--- app/GoodToKnow/Views/processacommoditysalesave.php
+++ app/GoodToKnow/Views/processacommoditysalesave.php
@@ -1,1 +1,11 @@
-<?php
+<?php global $g; ?>
+<?php require TOPOFREGULARPAGE; ?>
+<?php require TOPBARDIV; ?>
+<?php require CBSOFREGULARPAGES; ?>
+    <!-- maincontent -->
+    <div id="maincontent">
+        <h2>Evidence It Was Saved</h2>
+
+    </div><!-- End maincontent -->
+<?php require FOOTERBAR; ?>
+<?php require BOTTOMOFPAGES; ?>
