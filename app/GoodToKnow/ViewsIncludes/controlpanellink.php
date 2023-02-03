d7449338a535b6256427b7a62bb0380601ab880b
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-02-03 10:22:34 -0500

    Shrunk the more button a lot

--- app/GoodToKnow/ViewsIncludes/controlpanellink.php
+++ app/GoodToKnow/ViewsIncludes/controlpanellink.php
@@ -1,8 +1,8 @@
 <?php global $g; ?>
 <?php if ($g->is_admin): ?>
-    <a href="/ax1/admin_home/page"><img src="/more-button-md.png" alt="Admin Panel" height="98" width="98"></a>
+    <a href="/ax1/admin_home/page"><img src="/more-button-md.png" alt="Admin Panel" height="90" width="90"></a>
 <?php elseif ($g->is_guest): ?>
-    <img src="/more-button-md.png" alt="Admin Panel" height="98" width="98">
+    <img src="/more-button-md.png" alt="Admin Panel" height="90" width="90">
 <?php else: ?>
-    <a href="/ax1/control_panel/page"><img src="/more-button-md.png" alt="Admin Panel" height="98" width="98"></a>
+    <a href="/ax1/control_panel/page"><img src="/more-button-md.png" alt="Admin Panel" height="90" width="90"></a>
 <?php endif; ?>
