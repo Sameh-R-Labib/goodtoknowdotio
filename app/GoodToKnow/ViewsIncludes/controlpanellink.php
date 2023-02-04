6d198f3a80f328710a19f75fc948c5202ef1fbc5
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-02-03 20:43:35 -0500

    Made more button tiny

--- app/GoodToKnow/ViewsIncludes/controlpanellink.php
+++ app/GoodToKnow/ViewsIncludes/controlpanellink.php
@@ -1,8 +1,8 @@
 <?php global $g; ?>
 <?php if ($g->is_admin): ?>
-    <a href="/ax1/admin_home/page"><img src="/more-button-md.png" alt="Admin Panel" height="90" width="90"></a>
+    <a href="/ax1/admin_home/page"><img src="/more-button-md.png" alt="Admin Panel" height="82" width="82"></a>
 <?php elseif ($g->is_guest): ?>
-    <img src="/more-button-md.png" alt="Admin Panel" height="90" width="90">
+    <img src="/more-button-md.png" alt="Admin Panel" height="82" width="82">
 <?php else: ?>
-    <a href="/ax1/control_panel/page"><img src="/more-button-md.png" alt="Admin Panel" height="90" width="90"></a>
+    <a href="/ax1/control_panel/page"><img src="/more-button-md.png" alt="Admin Panel" height="82" width="82"></a>
 <?php endif; ?>
