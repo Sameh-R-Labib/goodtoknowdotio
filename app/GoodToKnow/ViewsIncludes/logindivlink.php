02818d65e3d162b3a61e7c025936ba8475107583
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-02-12 23:05:41 -0500

    logoutbuttonblue.png added

--- app/GoodToKnow/ViewsIncludes/logindivlink.php
+++ app/GoodToKnow/ViewsIncludes/logindivlink.php
@@ -2,5 +2,5 @@
 <?php if ($g->is_guest): ?>
     <p><a href="/ax1/login_form/page">🔑 log in</a></p>
 <?php else: ?>
-    <p><a href="/ax1/logout/page">log out</a></p>
+    <p><a href="/ax1/logout/page"><img src="/logoutbuttonblue.png" alt="Admin Panel" height="48" width="100"></a></p>
 <?php endif; ?>
