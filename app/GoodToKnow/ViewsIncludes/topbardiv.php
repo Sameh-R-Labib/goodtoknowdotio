73b7346fe3b5e4cd290c75e70d1f9e883320f772
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-02-12 23:40:25 -0500

    Inbox gif

--- app/GoodToKnow/ViewsIncludes/topbardiv.php
+++ app/GoodToKnow/ViewsIncludes/topbardiv.php
@@ -2,6 +2,7 @@
 <div id="topbar">
     <a href="/ax1"><img src="/good1.jpg" alt="GoodToKnow.io" height="57" width="254" style="float: left"></a>
     <div id="sendmessage"><?php require SENDMESSAGELINK; ?></div>
-    <div id="inboxlink"><p><a href="/ax1/inbox/page">🎫 inbox</a></p></div>
+    <div id="inboxlink"><p><a href="/ax1/inbox/page"><img src="/inbox_btn_gr.gif" alt="Inbox" height="25"
+                                                          width="25"></a></a></p></div>
     <div id="logindiv"><?php require LOGINDIVLINK; ?></div>
 </div>
