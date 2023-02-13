5ac2540614074c6d474f0181bb86bdd1bb1a9669
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-02-12 23:30:18 -0500

    25 x 25

--- app/GoodToKnow/ViewsIncludes/logindivlink.php
+++ app/GoodToKnow/ViewsIncludes/logindivlink.php
@@ -1,6 +1,6 @@
 <?php global $g; ?>
 <?php if ($g->is_guest): ?>
-    <p><a href="/ax1/login_form/page"><img src="/loginbwicon.png" alt="Login" height="53" width="53"></a></p>
+    <p><a href="/ax1/login_form/page"><img src="/loginbwicon.png" alt="Login" height="25" width="25"></a></p>
 <?php else: ?>
     <p><a href="/ax1/logout/page"><img src="/logoutbuttonblue.png" alt="Logout" height="31" width="63"></a></p>
 <?php endif; ?>
