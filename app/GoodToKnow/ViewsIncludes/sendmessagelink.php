20629cded79f3fa7a773a311311b6a1de30134b2
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-02-12 23:49:19 -0500

    Message all users icon

--- app/GoodToKnow/ViewsIncludes/sendmessagelink.php
+++ app/GoodToKnow/ViewsIncludes/sendmessagelink.php
@@ -1,6 +1,7 @@
 <?php global $g; ?>
 <?php if ($g->is_admin): ?>
-    <p><a href="/ax1/broadcast_msg/page">message all users</a></p>
+    <p><a href="/ax1/broadcast_msg/page"><img src="/message_users.ico" alt="message all users" height="25"
+                                              width="25">all users</a></p>
 <?php elseif ($g->is_guest): ?>
     <p>✊🤬😭&#x2192;☭Ⓐ</p>
 <?php else: ?>
--- app/GoodToKnow/ViewsIncludes/topbardiv.php
+++ app/GoodToKnow/ViewsIncludes/topbardiv.php
@@ -3,6 +3,6 @@
     <a href="/ax1"><img src="/good1.jpg" alt="GoodToKnow.io" height="57" width="254" style="float: left"></a>
     <div id="sendmessage"><?php require SENDMESSAGELINK; ?></div>
     <div id="inboxlink"><p><a href="/ax1/inbox/page"><img src="/inbox_btn_gr.gif" alt="Inbox" height="25"
-                                                          width="25"></a></a></p></div>
+                                                          width="25"></a></p></div>
     <div id="logindiv"><?php require LOGINDIVLINK; ?></div>
 </div>
