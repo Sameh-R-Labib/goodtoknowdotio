086d8e03e59674c76799ca6422c07c36042488a8
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-02-13 00:33:22 -0500

    Smaller admin icon

--- app/GoodToKnow/ViewsIncludes/sendmessagelink.php
+++ app/GoodToKnow/ViewsIncludes/sendmessagelink.php
@@ -5,6 +5,6 @@
 <?php elseif ($g->is_guest): ?>
     <p>✊🤬😭&#x2192;☭Ⓐ</p>
 <?php else: ?>
-    <p><a href="/ax1/write_to_admin/page">💬 <img src="/anon_admin.png" alt="message admin" height="28" width="28"></a>
+    <p><a href="/ax1/write_to_admin/page"><img src="/anon_admin.png" alt="message admin" height="24" width="24"></a>
     </p>
 <?php endif; ?>
