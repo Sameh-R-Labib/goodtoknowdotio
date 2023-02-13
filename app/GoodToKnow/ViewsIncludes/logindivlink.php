88f4c69856b67c97a77b9c9deb6f778f93f4a7a8
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-02-13 00:03:45 -0500

    Logout 23 57

--- app/GoodToKnow/ViewsIncludes/logindivlink.php
+++ app/GoodToKnow/ViewsIncludes/logindivlink.php
@@ -2,5 +2,5 @@
 <?php if ($g->is_guest): ?>
     <p><a href="/ax1/login_form/page"><img src="/loginbwicon.png" alt="Login" height="25" width="25"></a></p>
 <?php else: ?>
-    <p><a href="/ax1/logout/page"><img src="/logoutbuttonblue.png" alt="Logout" height="31" width="63"></a></p>
+    <p><a href="/ax1/logout/page"><img src="/logoutbuttonblue.png" alt="Logout" height="23" width="57"></a></p>
 <?php endif; ?>
