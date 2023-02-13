1747c1c390043a1f9f371841a5f3f34d6991daef
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-02-13 01:06:42 -0500

    70 for More

--- app/GoodToKnow/ViewsIncludes/controlpanellink.php
+++ app/GoodToKnow/ViewsIncludes/controlpanellink.php
@@ -1,8 +1,8 @@
 <?php global $g; ?>
 <?php if ($g->is_admin): ?>
-    <a href="/ax1/admin_home/page"><img src="/more-button-md.png" alt="Admin Panel" height="82" width="82"></a>
+    <a href="/ax1/admin_home/page"><img src="/more-button-md.png" alt="Admin Panel" height="70" width="70"></a>
 <?php elseif ($g->is_guest): ?>
-    <img src="/more-button-md.png" alt="Admin Panel" height="82" width="82">
+    <img src="/more-button-md.png" alt="Admin Panel" height="70" width="70">
 <?php else: ?>
-    <a href="/ax1/control_panel/page"><img src="/more-button-md.png" alt="Admin Panel" height="82" width="82"></a>
+    <a href="/ax1/control_panel/page"><img src="/more-button-md.png" alt="Admin Panel" height="70" width="70"></a>
 <?php endif; ?>
