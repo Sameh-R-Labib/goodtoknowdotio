9110f1a249a5b083c5457f645e25ca7cdbe09bf7
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-03-02 13:50:33 -0500

    Reset All My Bank Accounts

--- app/GoodToKnow/Views/cptransactions.php
+++ app/GoodToKnow/Views/cptransactions.php
@@ -8,6 +8,7 @@
             <a class="purplebtn" href="/ax1/check_my_banking_account_tx_balances/page">See Transactions</a>
             <a class="blackbtn" href="/ax1/omit_a_banking_transaction_for_balances/page">Delete Transaction</a>
             <a class="orangebtn" href="/ax1/transfer_an_amount/page">Transfer An Amount</a>
+            <a class="purplebtn" href="/ax1/reset_all_b_accounts_instruct/page">Reset All My Bank Accounts</a>
         </p>
     </div><!-- End maincontent -->
 <?php require FOOTERBAR; ?>
