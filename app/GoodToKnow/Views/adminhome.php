fba89f2051371513ecf17717668826f4740276b1
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-03-04 17:33:17 -0500

    Moved control panel content up higher

--- app/GoodToKnow/Views/adminhome.php
+++ app/GoodToKnow/Views/adminhome.php
@@ -4,11 +4,10 @@
     <!-- maincontent -->
     <div id="maincontent">
         <?php require USERCPBUTTONS; ?>
-        <p>🛡️ <a class="orangebtn" href="/ax1/c_p_postings/page">Blog Management</a>
-            <a class="clearbtn" href="/ax1/c_p_accounts/page">User Management</a>
-            <a class="greenbtn" href="/ax1/c_p_purges/page">System Maintenance</a>
-            <a class="blackbtn" href="/ax1/c_p_server/page">Server</a>
-        </p>
+        <br><br>🛡️ <a class="orangebtn" href="/ax1/c_p_postings/page">Blog Management</a>
+        <a class="clearbtn" href="/ax1/c_p_accounts/page">User Management</a>
+        <a class="greenbtn" href="/ax1/c_p_purges/page">System Maintenance</a>
+        <a class="blackbtn" href="/ax1/c_p_server/page">Server</a>
     </div><!-- End maincontent -->
 <?php require FOOTERBAR; ?>
 <?php require BOTTOMOFPAGES; ?>
