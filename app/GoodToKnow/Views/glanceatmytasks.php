d82f1d3ffb91879e41f2d8085ccf4af953ac8d60
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-03-12 17:19:28 -0400

    Modified display of tasks

--- app/GoodToKnow/Views/glanceatmytasks.php
+++ app/GoodToKnow/Views/glanceatmytasks.php
@@ -6,10 +6,12 @@
     <div id="maincontent">
         <h1>All My Tasks</h1>
         <?php if (!empty($g->array)): ?>
-            <?php foreach ($g->array as $object): ?>
-                <p><a class="cross" href="/ax1/feature_a_task_link/page/<?= $object->id ?>">✎</a>
-                    <?= $object->label ?> ◜ <em><?= $object->next ?></em> ⇁ <?= $object->cycle_type ?></p>
-            <?php endforeach; ?>
+            <p>
+                <?php foreach ($g->array as $object): ?>
+                    <a class="cross" href="/ax1/feature_a_task_link/page/<?= $object->id ?>">✎</a>
+                    <?= $object->label ?> ◜ <em><?= $object->next ?></em> ⇁ <?= $object->cycle_type ?><br>
+                <?php endforeach; ?>
+            </p>
         <?php else: ?>
             <p>There are no to-do tasks.</p>
         <?php endif; ?>
