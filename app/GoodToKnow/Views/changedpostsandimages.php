f913ba09845adf550d6e0e38677b07680e5518ac
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-03-22 00:19:30 -0400

    Added content to changedpostsandimages.php

--- app/GoodToKnow/Views/changedpostsandimages.php
+++ app/GoodToKnow/Views/changedpostsandimages.php
@@ -5,6 +5,43 @@
         <?php require SESSIONMESSAGE; ?>
         <p><b>Disclaimer:</b> this information is not enough to protect you from lawsuits. You should consult with a
             lawyer.</p>
+        <p>There is a law named <b>47 U.S.C. § 230</b> or <b>Section 230</b>. Congress recognized that for user speech
+            to thrive on the Internet, it had to protect the services that power users’ speech. That law states:
+            "No provider or user of an interactive computer service shall be treated as the publisher or speaker of any
+            information provided by another information content provider." (47 U.S.C. § 230(c)(1)).</p>
+        <p>Section 230 embodies that principle that we should all be responsible for our own actions and statements
+            online, but generally not those of others. The law prevents most civil suits against users or services that
+            are based on what others say. When harmful speech takes place, it’s the speaker that should be held
+            responsible, not the service that hosts the speech.</p>
+        <p>Section 230’s protections are not absolute. It does not protect companies that violate federal criminal law.
+            It does not protect companies that create illegal or harmful content. Nor does Section 230 protect companies
+            from intellectual property claims.</p>
+        <p>Section 230 allows for web operators, large and small, to moderate user speech and content as they see fit.
+            This reinforces the First Amendment’s protections for publishers to decide what content they will
+            distribute. Different approaches to moderating users’ speech allows users to find the places online that
+            they like, and avoid places they don’t.</p>
+        <h2>Platform Responsibilities</h2>
+        <p>In spite of congress making it easier to host users on a platform. There are things which are not allowed.
+            Here are some of them:</p>
+        <ul>
+            <li>No copyright infringement</li>
+            <li>No child porn or obscenity.</li>
+            <li>No prostitution or sex trafficking.</li>
+            <li>No illegal content.</li>
+            <li>Harassing or stalking content: Content that is intended to harass, stalk, or threaten a person is not
+                protected under Section 230.
+            </li>
+            <li>Content related to terrorism: Online platforms can be held liable for content that promotes or
+                facilitates terrorism, including content that provides instructions on how to engage in terrorist
+                activities.
+            </li>
+            <li>Content that violates federal civil rights law, such as the Fair Housing Act or the Americans with
+                Disabilities Act.
+            </li>
+        </ul>
+        <p>It's important to note that while Section 230 provides immunity to online platforms for user-generated
+            content, platforms can still be held liable for their own content or for any content that they have a direct
+            hand in creating or publishing.</p>
         <!-- Present link buttons for Save and Abort -->
         <p><a class="modify" href="/ax1/changed_posts_and_images_list/page">Continue</a>
             <a class="abort" href="/ax1/clear_session_vars/page">Abort</a></p>
