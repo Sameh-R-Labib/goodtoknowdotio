332ab4ec9cc799bae00e7a8050c9c5f5e58bb965
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-03-22 00:39:30 -0400

    Initialized changed_posts_and_images_list.php

---
+++ app/GoodToKnow/Controllers/changed_posts_and_images_list.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace GoodToKnow\Controllers;
+
+class changed_posts_and_images_list
+{
+    function page()
+    {
+        /**
+         *
+         */
+
+
+        kick_out_nonadmins_or_if_there_is_error_msg();
+    }
+}
