ed80c0da519e09072c2f751239d6d34beed9aac2
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-03-25 12:22:59 -0400

    Another simplification

--- app/GoodToKnow/ControllerIncludes/get_special_topic_array.php
+++ app/GoodToKnow/ControllerIncludes/get_special_topic_array.php
@@ -9,7 +9,7 @@ global $g;
 $g->special_topic_array = community_to_topic::get_topics_array_for_a_community($g->community_id);
 
 
-if ($g->special_topic_array == false) $g->special_topic_array = [];
+if (!$g->special_topic_array) $g->special_topic_array = [];
 
 
 $_SESSION['special_topic_array'] = $g->special_topic_array;
