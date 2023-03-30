3a7b7fdb8b9f591da002f8b374dcb2e787dac037
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-03-29 21:20:13 -0400

    Shortened info

--- app/GoodToKnow/Views/checkmybankingaccounttxbalancesshowbalances.php
+++ app/GoodToKnow/Views/checkmybankingaccounttxbalancesshowbalances.php
@@ -8,8 +8,8 @@
         <p><b>Starting time: </b><?= $g->account->start_time ?><br>
             <b>Starting balance: </b><?= $g->account->currency ?>&nbsp;<?= $g->account->start_balance ?>
             <span class="tooltip">ⅈ
-                        <span class="tooltiptext tooltip-top">Balance will be incorrect if Admin has purged transactions
-                            older than 90 days and the start_time for this account is older than 90 days.</span>
+                        <span class="tooltiptext tooltip-top">Incorrect if Admin purged transactions
+                            older than 90 days and start_time older than 90.</span>
                     </span><br>
             <b>Comment: </b><?= $g->account->comment ?></p>
         <table>
