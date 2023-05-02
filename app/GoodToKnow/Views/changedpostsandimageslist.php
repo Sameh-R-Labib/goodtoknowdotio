2e91f049fc1cdd8292bec8e3d80b45efabf84959
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-05-02 00:33:50 -0400

    Completed the view

--- app/GoodToKnow/Views/changedpostsandimageslist.php
+++ app/GoodToKnow/Views/changedpostsandimageslist.php
@@ -3,7 +3,16 @@
 <?php require CBSOFREGULARPAGES; ?>
     <!-- maincontent -->
     <div id="maincontent">
-        <p>Future place where you will see a list of changed content.</p>
+        <h1>Changed Content</h1>
+        <?php if (!empty($g->cc_objects)): ?>
+            <p>
+                <?php foreach ($g->cc_objects as $cc_object): ?>
+                    🕒 <?= $cc_object->time ?> ⏳ <?= $cc_object->expires ?> 🔍 <?= $cc_object->name ?><br>
+                <?php endforeach; ?>
+            </p>
+        <?php else: ?>
+            <p>There is no changed content.</p>
+        <?php endif; ?>
     </div><!-- End maincontent -->
 <?php require FOOTERBAR; ?>
 <?php require BOTTOMOFPAGES; ?>
