b0885e84a65fe186a041719201af6fd513980a16
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-05-02 17:00:42 -0400

    Wrote main comment for cull_the_herd.php

--- app/GoodToKnow/Controllers/cull_the_herd.php
+++ app/GoodToKnow/Controllers/cull_the_herd.php
@@ -4,5 +4,12 @@ namespace GoodToKnow\Controllers;
 
 class cull_the_herd
 {
-
+    function page()
+    {
+        /**
+         * This route will delete expired changed_content objects from the changed_content
+         * database table and this route will delete changed_content objects from the
+         * changed_content table which are older copies of an existing changed_content object.
+         */
+    }
 }
