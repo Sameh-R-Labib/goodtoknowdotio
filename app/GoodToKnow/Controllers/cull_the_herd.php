96b78d7eabe6db88df4020920dceb015461b1860
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-05-13 19:25:48 -0400

    Retrieved the objects

--- app/GoodToKnow/Controllers/cull_the_herd.php
+++ app/GoodToKnow/Controllers/cull_the_herd.php
@@ -2,14 +2,41 @@
 
 namespace GoodToKnow\Controllers;
 
+use GoodToKnow\Models\changed_content;
+
 class cull_the_herd
 {
     function page()
     {
         /**
-         * This route will delete expired changed_content objects from the changed_content
-         * database table and this route will delete changed_content objects from the
-         * changed_content table which are older copies of an existing changed_content object.
+         * This route will delete expired and  older duplicates changed_content objects from the changed_content
+         * database table.
          */
+
+
+        kick_out_nonadmins_or_if_there_is_error_msg();
+
+
+        get_db();
+
+
+        /**
+         * Get all the changed_content objects from the database table named changed_content.
+         */
+
+        $array_of_objects = changed_content::find_all();
+
+        if ($array_of_objects === false) {
+
+            breakout(' Unable to retrieve changed_content. ');
+
+        }
+
+        // Handling the case where NO old messages exist
+        if (empty($array_of_objects)) {
+
+            breakout(' There are no changed_content objects in the system. ');
+
+        }
     }
 }
