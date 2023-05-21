766a313cffecbce4b795cafe8ddd358e77e604f9
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-05-21 17:21:54 -0400

    Modified script.js

--- web/js/script.js
+++ web/js/script.js
@@ -1,10 +1,10 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+const coll = document.getElementsByClassName("collapsible");
+let i;
 
 for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function () {
         this.classList.toggle("active");
-        var content = this.nextElementSibling;
+        let content = this.nextElementSibling;
         if (content.style.maxHeight) {
             content.style.maxHeight = null;
         } else {
@@ -12,3 +12,14 @@ for (i = 0; i < coll.length; i++) {
         }
     });
 }
+
+
+window.addEventListener('load', function () {
+    // Set the desired width and height for the window
+    let desiredWidth = 800;
+    let desiredHeight = 600;
+
+    // Resize the window using the resizeTo() method
+    window.resizeTo(desiredWidth, desiredHeight);
+});
+
