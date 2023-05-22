42cfaf3fff2fe46a50c11c384e8ba5bd5e5edc01
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-05-21 21:15:56 -0400

    Window symbol instead of text

--- app/GoodToKnow/ViewsIncludes/sessionmessage.php
+++ app/GoodToKnow/ViewsIncludes/sessionmessage.php
@@ -1,6 +1,8 @@
 <?php global $g; ?>
 <?php if (!empty($g->message)): ?>
     <p>
-        <button class="open-window-button">Open New Window</button>
-        👨🏽‍🦱:&nbsp;&nbsp;<?= $g->message ?></p>
+        <button class="open-window-button">🪟</button>
+        ≬
+        👨🏽‍🦱:&nbsp;&nbsp;<?= $g->message ?>
+    </p>
 <?php endif; ?>
