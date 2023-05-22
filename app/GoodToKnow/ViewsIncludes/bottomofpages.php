fa34e078fbcd58b23ba6dc8d2e00882b763aa459
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-05-22 08:54:39 -0400

    Made the new JS part of HTML

--- app/GoodToKnow/ViewsIncludes/bottomofpages.php
+++ app/GoodToKnow/ViewsIncludes/bottomofpages.php
@@ -1,3 +1,12 @@
 <script src="/js/script.js"></script>
+<script>
+    let openWindowButton = document.querySelector('.open-window-button');
+    openWindowButton.addEventListener('click', function () {
+        let desiredWidth = 1024;
+        let desiredHeight = 1700;
+        let windowFeatures = 'width=' + desiredWidth + ',height=' + desiredHeight;
+        window.open('<?= SERVER_URL ?>', '_blank', windowFeatures);
+    });
+</script>
 </body>
 </html>
