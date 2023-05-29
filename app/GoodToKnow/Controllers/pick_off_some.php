894d4c32bcb1bb38018913e55ace4f3eee816e51
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-05-29 16:20:58 -0400

    Started pick_off_some.php

---
+++ app/GoodToKnow/Controllers/pick_off_some.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace GoodToKnow\Controllers;
+
+class pick_off_some
+{
+    function page()
+    {
+        /**
+         * I.
+         */
+    }
+}
--- app/GoodToKnow/Views/cpchangedcontent.php
+++ app/GoodToKnow/Views/cpchangedcontent.php
@@ -6,6 +6,7 @@
         <div class="cp-btn-section">
             <a class="purplebtn" href="/ax1/changed_posts_and_images/page">Changed Posts and Uploaded Images</a>
             <a class="orangebtn" href="/ax1/cull_the_herd/page">Cull The Herd</a>
+            <a class="blackbtn" href="/ax1/pick_off_some/page">Pick Off Some</a>
         </div>
     </div><!-- End maincontent -->
 <?php require FOOTERBAR; ?>
