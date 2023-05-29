8971a701268248733407969d88331a4aa1f60c25
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-05-29 17:04:54 -0400

    Wrote comment for purpose of feature

--- app/GoodToKnow/Controllers/pick_off_some.php
+++ app/GoodToKnow/Controllers/pick_off_some.php
@@ -7,7 +7,16 @@ class pick_off_some
     function page()
     {
         /**
-         * I.
+         * As with "Cull The Herd" feature, the goal is to reduce the amount of
+         * changed_content table rows in the database. In the "Pick Off Some" feature
+         * we show the administrator some checkboxes next to the names associated with rows
+         * of changed_content table. Thus enabling the deletion of one or more rows.
          */
+
+
+        kick_out_nonadmins_or_if_there_is_error_msg();
+
+
+        get_db();
     }
 }
