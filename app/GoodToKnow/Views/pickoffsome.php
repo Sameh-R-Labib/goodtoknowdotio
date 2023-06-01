7f80317c7de0c9a35bb27ae66a9d3884494ccad6
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-06-01 13:37:41 -0400

    Created rudiments of the view

--- app/GoodToKnow/Views/pickoffsome.php
+++ app/GoodToKnow/Views/pickoffsome.php
@@ -1,1 +1,18 @@
-<?php
+<?php global $g; ?>
+<?php require TOPFORFORMPAGES; ?>
+<form action="/ax1/pick_off_some_processor/page" method="post">
+    <h1><?= $g->html_title ?></h1>
+    <p class="tooltip">ⅈ
+        <span class="tooltiptext tooltip-top">Feature "Pick Off Some" is a compliment to "Cull The Herd".</span>
+    </p>
+    <?php require SESSIONMESSAGE; ?>
+    <section>
+        <?php foreach ($g->array_of_objects as $key => $value): ?>
+            <label class="checkbox">
+                <input type="checkbox" name="choice-<?= $key + 1 ?>" value="<?= $value->id ?>">
+                <?= $value->name ?>
+            </label>
+        <?php endforeach; ?>
+    </section>
+    <?php require SUBMITABORT; ?>
+    <?php require BOTTOMOFPAGES; ?>
