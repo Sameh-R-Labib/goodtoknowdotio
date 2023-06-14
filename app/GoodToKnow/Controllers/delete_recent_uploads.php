88647a211ea169f027024a882ef5ab7d3e3be0b9
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-06-13 23:29:08 -0400

    Main comment

--- app/GoodToKnow/Controllers/delete_recent_uploads.php
+++ app/GoodToKnow/Controllers/delete_recent_uploads.php
@@ -7,7 +7,13 @@ class delete_recent_uploads
     function page()
     {
         /**
+         * This feature presents checkboxes for choosing which image files
+         * to delete from the server's file system.
          *
+         * The choice is from amongst the uploads listed in the changed_content
+         * database table.
          */
+
+
     }
 }
