ecc672dd358a3f4b023051eb578dc139a2a64cbb
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-06-15 20:54:07 -0400

    Completed reset_a_passphrase_submitted_passphrase.php

--- app/GoodToKnow/Controllers/reset_a_passphrase_submitted_passphrase.php
+++ app/GoodToKnow/Controllers/reset_a_passphrase_submitted_passphrase.php
@@ -2,6 +2,9 @@
 
 namespace GoodToKnow\Controllers;
 
+use GoodToKnow\Models\user;
+use function GoodToKnow\ControllerHelpers\password_for_regandchange_prep;
+
 class reset_a_passphrase_submitted_passphrase
 {
     function page()
@@ -24,5 +27,46 @@ class reset_a_passphrase_submitted_passphrase
 
 
         get_db();
+
+
+        /**
+         * Get the passphrase admin submitted.
+         * If there is something wrong with it then a breakout occurs.
+         */
+
+        require_once CONTROLLERHELPERS . DIRSEP . 'password_for_regandchange_prep.php';
+
+        $password = password_for_regandchange_prep();
+
+
+        /**
+         * Get the user object for the user whose passphrase we are resetting.
+         */
+
+        $user_object = user::find_by_username($g->saved_str01);
+
+
+        /**
+         * Put the hash of the new password in the user object.
+         */
+
+        $user_object->password = password_hash($password, PASSWORD_DEFAULT);
+
+        // Save the user object
+
+        $is_saved = $user_object->save();
+
+        if (!$is_saved) {
+
+            breakout(" Failed to update $user_object->username's record with passphrase $password. ");
+
+        }
+
+
+        /**
+         * Report success and redirect.
+         */
+
+        breakout(" The passphrase for $user_object->username has been reset to $password. ");
     }
 }
