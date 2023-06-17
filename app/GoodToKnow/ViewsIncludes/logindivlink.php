723224a359e2235527daf9801fe426a8034ee6e2
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-06-17 13:00:41 -0400

    greenbtn

--- app/GoodToKnow/ViewsIncludes/logindivlink.php
+++ app/GoodToKnow/ViewsIncludes/logindivlink.php
@@ -1,6 +1,6 @@
 <?php global $g; ?>
 <?php if ($g->is_guest): ?>
-    <p><a class="purplebtn" href="/ax1/login_form/page">Login</a></p>
+    <p><a class="greenbtn" href="/ax1/login_form/page">Login</a></p>
 <?php else: ?>
     <p><a class="orangebtn" href="/ax1/logout/page">Logout</a></p>
 <?php endif; ?>
