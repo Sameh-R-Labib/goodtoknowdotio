b35c5512349cf1970d8887e7a4dab5a2706263ce
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-06-17 12:26:27 -0400

    Changed the logout button

--- app/GoodToKnow/ViewsIncludes/logindivlink.php
+++ app/GoodToKnow/ViewsIncludes/logindivlink.php
@@ -2,5 +2,5 @@
 <?php if ($g->is_guest): ?>
     <p><a href="/ax1/login_form/page"><img src="/img/loginbwicon.png" alt="Login" height="18" width="18"></a></p>
 <?php else: ?>
-    <p><a href="/ax1/logout/page"><img src="/img/logoutbuttonblue.png" alt="Logout" height="20" width="44"></a></p>
+    <p><a class="orangebtn" href="/ax1/logout/page">Logout</a></p>
 <?php endif; ?>
