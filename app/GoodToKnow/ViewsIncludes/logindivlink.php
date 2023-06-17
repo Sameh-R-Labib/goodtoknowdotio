63495fd90cf6fa1389a235f0ace5c5a24653de08
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-06-17 12:30:26 -0400

    Changed the login button

--- app/GoodToKnow/ViewsIncludes/logindivlink.php
+++ app/GoodToKnow/ViewsIncludes/logindivlink.php
@@ -1,6 +1,6 @@
 <?php global $g; ?>
 <?php if ($g->is_guest): ?>
-    <p><a href="/ax1/login_form/page"><img src="/img/loginbwicon.png" alt="Login" height="18" width="18"></a></p>
+    <p><a class="purplebtn" href="/ax1/login_form/page">Login/a></p>
 <?php else: ?>
     <p><a class="orangebtn" href="/ax1/logout/page">Logout</a></p>
 <?php endif; ?>
