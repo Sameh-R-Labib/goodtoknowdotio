1cbb207cc50f3ee0bea40ecfb244c18d3e1415dc
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-06-20 16:12:46 -0400

    Main comment added for cover_page.php

--- app/GoodToKnow/Controllers/cover_page.php
+++ app/GoodToKnow/Controllers/cover_page.php
@@ -7,7 +7,14 @@ class cover_page
     function page()
     {
         /**
+         * The cover_page is almost the same as the home page.
+         * The main difference is that the cover_page does not
+         * present blog community, topic or post.
          *
+         * Just like the home page the cover_page will be
+         * frequently the target of redirection and will
+         * present messages and alerts. Also, it will enforce
+         * user suspensions and system offline conditions.
          */
     }
 }
