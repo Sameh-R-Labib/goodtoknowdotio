0ec171be5e3d84e01d714fe4e98a50e82e78d839
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-06-22 09:17:25 -0400

    Modified clear_session_vars.php

--- app/GoodToKnow/Controllers/clear_session_vars.php
+++ app/GoodToKnow/Controllers/clear_session_vars.php
@@ -6,8 +6,24 @@ namespace GoodToKnow\Controllers;
 
 class clear_session_vars
 {
-    function page()
+    function page(string $last_controller = '')
     {
+        global $g;
+
+
+        /**
+         * We are doing this so that breakout will
+         * redirect to the most convenient route
+         * for the user after he clicks on Abort.
+         *
+         * Under normal circumstances $g->controller_name
+         * would already be the last controller.
+         */
+        if (!empty($last_controller)) {
+            $g->controller_name = $last_controller;
+        }
+
+
         breakout(" You're back on Blog. ");
     }
 }
