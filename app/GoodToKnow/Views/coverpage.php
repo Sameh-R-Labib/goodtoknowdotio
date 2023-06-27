21c11e8d1e8b94ffeb1281e8401a1732541a515a
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-06-27 00:40:18 -0400

    Image css

--- app/GoodToKnow/Views/coverpage.php
+++ app/GoodToKnow/Views/coverpage.php
@@ -4,7 +4,7 @@
 <?php require CBSOFREGULARPAGES; ?>
 <!-- maincontent -->
 <div id="maincontent">
-    <p>Grunge Spirit</p>
+    <p>Have Grunge Spirit ✌🏻</p>
     <figure>
         <img class="photo" alt="Green Fender Mustang" src="/img/fender_mustang_green.png">
     </figure>
