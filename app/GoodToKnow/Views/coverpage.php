ee8fde61ded573ad4d63917420f0489f14ae3d88
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-06-27 00:24:07 -0400

    Kurt DS-1

--- app/GoodToKnow/Views/coverpage.php
+++ app/GoodToKnow/Views/coverpage.php
@@ -4,9 +4,12 @@
 <?php require CBSOFREGULARPAGES; ?>
 <!-- maincontent -->
 <div id="maincontent">
-    <p>Home or More?</p>
+    <p>Grunge Spirit</p>
     <figure>
         <img class="photo" alt="Green Fender Mustang" src="/img/fender_mustang_green.png">
+    </figure>
+    <figure>
+        <img class="photo" alt="Kurt's DS-1 Pedal" src="/img/kurt_ds1_pedal.png">
     </figure>
     <figure>
         <img class="photo" alt="Kurt Cobain Grunge" src="/img/kurt_cobain_grunge.png">
