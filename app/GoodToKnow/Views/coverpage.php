23c3d5ab4888a9a5e9c4b7c9475d3896329c992c
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-06-27 00:03:37 -0400

    Added photos to cover page

--- app/GoodToKnow/Views/coverpage.php
+++ app/GoodToKnow/Views/coverpage.php
@@ -4,7 +4,19 @@
 <?php require CBSOFREGULARPAGES; ?>
 <!-- maincontent -->
 <div id="maincontent">
-    <p><i>Do something!</i></p>
+    <p>Home or More?</p>
+    <figure>
+        <img class="photo" alt="Green Fender Mustang" src="/img/fender_mustang_green.png">
+    </figure>
+    <figure>
+        <img class="photo" alt="Kurt Cobain Grunge" src="/img/kurt_cobain_grunge.png">
+    </figure>
+    <figure>
+        <img class="photo" alt="Dave Grohl Nirvana" src="/img/dave_grohl_nirvana.png">
+    </figure>
+    <figure>
+        <img class="photo" alt="Kurt Cobain Back" src="/img/kurt_cobain_back.png">
+    </figure>
 </div><!-- End maincontent -->
 <?php require FOOTERBAR; ?>
 <?php require BOTTOMOFPAGES; ?>
