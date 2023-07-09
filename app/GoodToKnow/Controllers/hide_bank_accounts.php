399735760ca363e89b3197bddf8341ac86456194
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-07-09 16:09:05 -0400

    Save

--- app/GoodToKnow/Controllers/hide_bank_accounts.php
+++ app/GoodToKnow/Controllers/hide_bank_accounts.php
@@ -4,5 +4,23 @@ namespace GoodToKnow\Controllers;
 
 class hide_bank_accounts
 {
+    function page()
+    {
+        /**
+         * Show form with checkboxes. Each checkbox represents a bank account which
+         * is not hidden. This makes it possible for the user to select which bank
+         * accounts he wants to make hidden.
+         */
 
+
+        global $g;
+
+
+        kick_out_loggedoutusers_or_if_there_is_error_msg();
+
+
+        /**
+         * Get all the banking_acct_for_balances records which are not hidden.
+         */
+    }
 }
