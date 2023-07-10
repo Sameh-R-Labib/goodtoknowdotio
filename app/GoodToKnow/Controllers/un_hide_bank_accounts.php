c8154def2710e650b6d85d477aaac4d32c3b147c
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-07-09 20:40:53 -0400

    Created button to un_hide_bank_accounts.php

---
+++ app/GoodToKnow/Controllers/un_hide_bank_accounts.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace GoodToKnow\Controllers;
+
+class un_hide_bank_accounts
+{
+
+}
--- app/GoodToKnow/Views/cpbankingaccounts.php
+++ app/GoodToKnow/Views/cpbankingaccounts.php
@@ -10,6 +10,7 @@
             <a class="clearbtn" href="/ax1/annul_a_banking_acct_for_balances/page">Delete</a>
             <a class="purplebtn" href="/ax1/reset_all_b_accounts_instruct/page">Reset All</a>
             <a class="greenbtn" href="/ax1/hide_bank_accounts/page">Hide Bank Accounts</a>
+            <a class="purplebtn" href="/ax1/un_hide_bank_accounts/page">Un-hide Bank Account</a>
         </div>
     </div><!-- End maincontent -->
 <?php require FOOTERBAR; ?>
