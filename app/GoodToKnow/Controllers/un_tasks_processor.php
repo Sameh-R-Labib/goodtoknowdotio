68fb6cdd94525f88f52da1051f0ec323c3a950ec
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-07-10 21:18:16 -0400

    Completed unhidetasks.php

---
+++ app/GoodToKnow/Controllers/un_tasks_processor.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace GoodToKnow\Controllers;
+
+class un_tasks_processor
+{
+
+}
--- app/GoodToKnow/Views/unhidetasks.php
+++ app/GoodToKnow/Views/unhidetasks.php
@@ -1,1 +1,19 @@
-<?php
+<?php global $g; ?>
+<?php require TOPFORFORMPAGES; ?>
+    <form action="/ax1/un_tasks_processor/page" method="post">
+        <h1>Un-hide Tasks</h1>
+        <p class="tooltip">ⅈ
+            <span class="tooltiptext tooltip-top">Un-hide the tasks you want to be presented with.</span>
+        </p>
+        <?php require SESSIONMESSAGE; ?>
+        <section>
+            <?php foreach ($g->array_of_objects as $key => $value): ?>
+                <label class="checkbox">
+                    <input type="checkbox" name="choice-<?= $key + 1 ?>" value="<?= $value->id ?>">
+                    <?= $value->label ?>
+                </label>
+            <?php endforeach; ?>
+        </section>
+        <?php require SUBMITABORT; ?>
+    </form>
+<?php require BOTTOMOFPAGES; ?>
