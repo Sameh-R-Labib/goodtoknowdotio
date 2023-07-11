a48f061a00ed915ce420f6dfe17e673b9c5cd423
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-07-10 21:39:48 -0400

    purplebtn

--- app/GoodToKnow/Views/adminhome.php
+++ app/GoodToKnow/Views/adminhome.php
@@ -5,7 +5,7 @@
     <div id="maincontent">
         <div class="cp-btn-section">
             <?php require USERCPBUTTONS; ?>
-            <br><br>🛡️ <a class="blackbtn" href="/ax1/c_p_changed_content/page">Changed Content</a>
+            <br><br>🛡️ <a class="purplebtn" href="/ax1/c_p_changed_content/page">Changed Content</a>
             <a class="orangebtn" href="/ax1/c_p_postings/page">Blog Management</a>
             <a class="clearbtn" href="/ax1/c_p_accounts/page">User Management</a>
             <a class="greenbtn" href="/ax1/c_p_purges/page">System Maintenance</a>
