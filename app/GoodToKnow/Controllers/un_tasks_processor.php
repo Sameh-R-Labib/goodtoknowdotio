9de3eacc5326e931a21ec3329c2c9e0188caee8f
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-07-10 21:22:53 -0400

    Save this

--- app/GoodToKnow/Controllers/un_tasks_processor.php
+++ app/GoodToKnow/Controllers/un_tasks_processor.php
@@ -2,7 +2,36 @@
 
 namespace GoodToKnow\Controllers;
 
+use function GoodToKnow\ControllerHelpers\checkbox_section_form_field_prep;
+
 class un_tasks_processor
 {
+    function page()
+    {
+        /**
+         * Get the submitted checkboxes. And, use that information to un-hide
+         * their associated task records.
+         */
 
+
+        global $g;
+
+
+        kick_out_loggedoutusers_or_if_there_is_error_msg();
+
+
+        // This route is similar to the well documented include get_the_submitted_community_ids.php
+
+
+        require_once CONTROLLERHELPERS . DIRSEP . 'checkbox_section_form_field_prep.php';
+
+        $g->submitted_task_ids = checkbox_section_form_field_prep('choice-');
+
+        if (empty($g->submitted_task_ids)) {
+
+            breakout(' You did not submit any tasks to un-hide. ');
+
+        }
+
+    }
 }
