1a6e55f2b3425530e502d525cd2ca7c68558d18c
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-08-20 10:28:57 -0400

    Moved Up One

--- app/GoodToKnow/Views/coverpage.php
+++ app/GoodToKnow/Views/coverpage.php
@@ -14,10 +14,10 @@
         <img class="photo" alt="Kurt in a sweater" src="/img/kurt_in_sweater.jpg">
     </figure>
     <figure>
-        <img class="photo" alt="1993 Concert In Florida" src="/img/kurt_cobain_1993.png">
+        <img class="photo" alt="Mosrite Guitar Live" src="/img/kurt_mosrite_live.jpeg">
     </figure>
     <figure>
-        <img class="photo" alt="Mosrite Guitar Live" src="/img/kurt_mosrite_live.jpeg">
+        <img class="photo" alt="1993 Concert In Florida" src="/img/kurt_cobain_1993.png">
     </figure>
     <figure>
         <img class="photo" alt="Kurt's Pedals On Stage" src="/img/kurt_cobain_pedals.png">
