62cb0bb6e02221f5ce6f82d7975928f239d4c2ab
Sameh-R-Labib <schoolbuscompany@gmail.com>
2023-10-11 22:37:12 -0400

    Many more cover photos

--- app/GoodToKnow/Views/coverpage.php
+++ app/GoodToKnow/Views/coverpage.php
@@ -8,6 +8,9 @@
         <img class="photo" alt="Kurt liked this photo" src="/img/kurt_liked_this_photo.jpg">
     </figure>
     <figure>
+        <img class="photo" alt="Mo Ali Talking a Man Out Of Jumping" src="/img/mo_ali_helping_a_man.png">
+    </figure>
+    <figure>
         <img class="photo" alt="Kurt with cat and Francis" src="/img/kurt_cat_francis.jpg">
     </figure>
     <figure>
@@ -29,6 +32,9 @@
         <img class="photo" alt="Kurt's DS-1 Pedal" src="/img/kurt_ds1_pedal.png">
     </figure>
     <figure>
+        <img class="photo" alt="Kurt's DS-1 Pedal" src="/img/nirvana_set_list.jpg">
+    </figure>
+    <figure>
         <img class="photo" alt="Find A Journey Worthy of Your Heart and Soul" src="/img/find_a_journey_worthy.png">
     </figure>
     <figure>
@@ -44,7 +50,40 @@
         <img class="photo" alt="Kurt Cobain Back" src="/img/kurt_cobain_back.png">
     </figure>
     <figure>
-        <img class="photo" alt="Kurt Cobain Back" src="/img/kurt_art_shirt.png">
+        <img class="photo" alt="Kurt Art Shirt" src="/img/kurt_art_shirt.png">
+    </figure>
+    <figure>
+        <img class="photo" alt="Kurt Grunge DS1" src="/img/kurt_grunging_the_ds1.jpg">
+    </figure>
+    <figure>
+        <img class="photo" alt="Kurt Punk Guitar" src="/img/kurt_punk_guitar.jpg">
+    </figure>
+    <figure>
+        <img class="photo" alt="Kurt Smashing Guitar 1" src="/img/kurt_smashing_1.jpg">
+    </figure>
+    <figure>
+        <img class="photo" alt="Kurt smashing Guitar 2" src="/img/kurt_smashing_2.jpg">
+    </figure>
+    <figure>
+        <img class="photo" alt="Baby Chasing Money" src="/img/chasing_dollars.jpg">
+    </figure>
+    <figure>
+        <img class="photo" alt="Kurt Under Water" src="/img/kurt_swimming.jpg">
+    </figure>
+    <figure>
+        <img class="photo" alt="Kurt Showing Vulnerability" src="/img/kurt_vulnerable_but_not_alone.jpg">
+    </figure>
+    <figure>
+        <img class="photo" alt="Krist Cared About Kurt" src="/img/krist_concerned.jpg">
+    </figure>
+    <figure>
+        <img class="photo" alt="Krist Typical Pose" src="/img/krist_typical_pose.jpg">
+    </figure>
+    <figure>
+        <img class="photo" alt="Kurt Moshing" src="/img/kurt_moshing.jpg">
+    </figure>
+    <figure>
+        <img class="photo" alt="Grunge Fans Showing Concern" src="/img/fans_showing_concern.jpg">
     </figure>
 </div><!-- End maincontent -->
 <?php require FOOTERBAR; ?>
